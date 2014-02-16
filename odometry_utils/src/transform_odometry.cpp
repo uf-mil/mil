uf-mil/mil

commit db92497ccc9ff59b2f800815a8c99ebcf6b73100

cleaned up

odometry_utils/src/transform_odometry.cpp
@@ -1,7 +1,6 @@
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
 #include <tf/LinearMath/Quaternion.h>
 #include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
@@ -10,66 +9,14 @@
 
 #include <odom_estimator/util.h>
 #include <odom_estimator/unscented_transform.h>
+#include <odom_estimator/odometry.h>
 
 namespace odometry_utils {
 
 using namespace odom_estimator;
 
 using namespace Eigen;
-typedef Matrix<double, 6, 6> Matrix6d;
-typedef Matrix<double, 12, 12> Matrix12d;
 
-struct Odom {
-  Vector3d pos;
-  Quaterniond orient;
-  Vector3d vel;
-  Vector3d ang_vel;
-  static const int RowsAtCompileTime = 3*4;
-  unsigned int rows() const {
-    return RowsAtCompileTime;
-  }
-  
-  Odom(Vector3d pos, Quaterniond orient,
-      Vector3d vel, Vector3d ang_vel) :
-    pos(pos), orient(orient.normalized()),
-    vel(vel), ang_vel(ang_vel) { }
-  Odom(const geometry_msgs::Pose &pose,
-       const geometry_msgs::Twist &twist) {
-    tf::pointMsgToEigen(pose.position, pos);
-    tf::quaternionMsgToEigen(pose.orientation, orient);
-    tf::vectorMsgToEigen(twist.linear, vel);
-    tf::vectorMsgToEigen(twist.angular, ang_vel);
-  }
-  
-  Vec<RowsAtCompileTime> operator-(const Odom &other) const {
-    return (Vec<RowsAtCompileTime>() <<
-      pos - other.pos,
-      rotvec_from_quat(orient * other.orient.conjugate()),
-      vel - other.vel,
-      ang_vel - other.ang_vel).finished();
-  }
-  Odom operator+(const Vec<RowsAtCompileTime> &other) const {
-    return Odom(
-      pos + other.segment<3>(0),
-      quat_from_rotvec(other.segment<3>(3)) * orient,
-      vel + other.segment<3>(6),
-      ang_vel + other.segment<3>(9));
-  }
-  
-  Odom transform(const tf::Transform &left,
-                 const tf::Transform &right) const {
-    Vector3d left_p; tf::vectorTFToEigen(left.getOrigin(), left_p);
-    Quaterniond left_q; tf::quaternionTFToEigen(left.getRotation(), left_q);
-    Vector3d right_p; tf::vectorTFToEigen(right.getOrigin(), right_p);
-    Quaterniond right_q; tf::quaternionTFToEigen(right.getRotation(), right_q);
-    
-    return Odom(
-      left_p + left_q._transformVector(pos + orient._transformVector(right_p)),
-      left_q * orient * right_q,
-      right_q.inverse()._transformVector(vel - right_p.cross(ang_vel)),
-      right_q.inverse()._transformVector(ang_vel));
-  }
-};
 
 class transform_odometry : public nodelet::Nodelet {
 private:
@@ -81,52 +28,45 @@ private:
   ros::Subscriber sub;
   ros::Publisher pub;
   
-  void handle(const nav_msgs::Odometry::ConstPtr& msg) {
-      tf::StampedTransform lefttransform;
-      try {
-          listener.lookupTransform(frame_id, msg->header.frame_id,
-            ros::Time(0), lefttransform);
-      } catch (tf::TransformException ex) {
-          ROS_ERROR("%s", ex.what());
-          return;
-      }
-      
-      tf::StampedTransform righttransform;
-      try {
-          listener.lookupTransform(msg->child_frame_id, child_frame_id,
-            ros::Time(0), righttransform);
-      } catch (tf::TransformException ex) {
-          ROS_ERROR("%s", ex.what());
-          return;
-      }
-      
-      Matrix12d cov = Matrix12d::Zero();
-      cov.block<6, 6>(0, 0) = Map<const Matrix6d>(msg->pose.covariance.data());
-      cov.block<6, 6>(6, 6) = Map<const Matrix6d>(msg->twist.covariance.data());
-      GaussianDistribution<Odom> res =
-        EasyDistributionFunction<Odom, Odom, Vec<0> >(
-          [&lefttransform, &righttransform](Odom const &odom, Vec<0> const &extra) {
-            return odom.transform(lefttransform, righttransform);
-          },
-          GaussianDistribution<Vec<0> >(Vec<0>(), SqMat<0>()))
-        (GaussianDistribution<Odom>(
-          Odom(msg->pose.pose, msg->twist.twist),
-          cov));
-      
-      nav_msgs::Odometry result;
-      result.header.frame_id = frame_id;
-      result.header.stamp = msg->header.stamp;
-      result.child_frame_id = child_frame_id;
-      
-      tf::pointEigenToMsg(res.mean.pos, result.pose.pose.position);
-      tf::quaternionEigenToMsg(res.mean.orient, result.pose.pose.orientation);
-      Map<Matrix6d>(result.pose.covariance.data()) = res.cov.block<6, 6>(0, 0);
-      
-      tf::vectorEigenToMsg(res.mean.vel, result.twist.twist.linear);
-      tf::vectorEigenToMsg(res.mean.ang_vel, result.twist.twist.angular);
-      Map<Matrix6d>(result.twist.covariance.data()) = res.cov.block<6, 6>(6, 6);
-      
-      pub.publish(result);
+  void handle(const nav_msgs::Odometry::ConstPtr& msgp) {
+    tf::StampedTransform lefttransform;
+    try {
+      listener.lookupTransform(frame_id, msgp->header.frame_id,
+        ros::Time(0), lefttransform);
+    } catch (tf::TransformException ex) {
+      ROS_ERROR("%s", ex.what());
+      return;
+    }
+    
+    tf::StampedTransform righttransform;
+    try {
+      listener.lookupTransform(msgp->child_frame_id, child_frame_id,
+        ros::Time(0), righttransform);
+    } catch (tf::TransformException ex) {
+      ROS_ERROR("%s", ex.what());
+      return;
+    }
+    
+    Vector3d left_p; tf::vectorTFToEigen(lefttransform.getOrigin(), left_p);
+    Quaterniond left_q; tf::quaternionTFToEigen(lefttransform.getRotation(), left_q);
+    Vector3d right_p; tf::vectorTFToEigen(righttransform.getOrigin(), right_p);
+    Quaterniond right_q; tf::quaternionTFToEigen(righttransform.getRotation(), right_q);
+    
+    EasyDistributionFunction<Odom, Odom, Vec<0> > transformer(
+      [&left_p, &left_q, &right_p, &right_q](Odom const &odom,
+                                             Vec<0> const &extra) {
+        return Odom(
+          odom.stamp,
+          odom.frame_id,
+          odom.child_frame_id,
+          left_p + left_q._transformVector(odom.pos + odom.orient._transformVector(right_p)),
+          left_q * odom.orient * right_q,
+          right_q.inverse()._transformVector(odom.vel - right_p.cross(odom.ang_vel)),
+          right_q.inverse()._transformVector(odom.ang_vel));
+      },
+      GaussianDistribution<Vec<0> >(Vec<0>(), SqMat<0>()));
+    
+    pub.publish(msg_from_odom(transformer(odom_from_msg(*msgp))));
   }
 
 public:
@@ -146,6 +86,6 @@ public:
   }
 };
 PLUGINLIB_DECLARE_CLASS(odometry_utils, transform_odometry,
-    odometry_utils::transform_odometry, nodelet::Nodelet);
+  odometry_utils::transform_odometry, nodelet::Nodelet);
 
 }

odom_estimator/include/odom_estimator/kalman.h
@@ -7,29 +7,35 @@
 namespace odom_estimator {
 
 
-
-template<typename DistributionFunction, typename InType>
-GaussianDistribution<InType> kalman_update(DistributionFunction const &df,
-                                          GaussianDistribution<InType> const &input) {
-  auto res = df(input);
+// does a kalman update given error function `df`, which should return the
+// distribution of the difference between predicted values and measured values
+template<typename InType, typename ErrorType>
+GaussianDistribution<InType>
+kalman_update(IDistributionFunction<InType, ErrorType> const &df,
+              GaussianDistribution<InType> const &input) {
+  GaussianDistributionWithCrossCov<ErrorType, InType> res = df(input);
   
   Mat<InType::RowsAtCompileTime, res.mean.RowsAtCompileTime> P_xz =
     res.cross_cov.transpose();
   Mat<res.mean.RowsAtCompileTime, res.mean.RowsAtCompileTime> P_zz = res.cov;
   
-  //Mat<InType::RowsAtCompileTime, res.mean.RowsAtCompileTime> K = P_xz * P_zz.inverse();
+  //Mat<InType::RowsAtCompileTime, res.mean.RowsAtCompileTime> K =
+  //  P_xz * P_zz.inverse();
   // instead, using matrix solver:
   // K = P_xz P_zz^-1
   // K P_zz = P_xz
   // P_zz' K' = P_xz'
   // K' = solve(P_zz', P_xz')
   // K = solve(P_zz', P_xz')'
-  if(P_zz.rows() == 0) return input; // Matrix<>.ldlt() crashes for size-zero matrices
+  if(P_zz.rows() == 0) {
+    return input; // Eigen::MatrixBase::ldlt() crashes for zero-sized matrices
+  }
   Mat<InType::RowsAtCompileTime, res.mean.RowsAtCompileTime> K =
     P_zz.transpose().ldlt().solve(P_xz.transpose()).transpose();
   
   InType new_mean = input.mean + K*-res.mean;
-  SqMat<InType::RowsAtCompileTime> new_cov = input.cov - K*res.cov*K.transpose();
+  SqMat<InType::RowsAtCompileTime> new_cov =
+    input.cov - K*res.cov*K.transpose();
   
   return GaussianDistribution<InType>(new_mean, new_cov);
 }

odom_estimator/include/odom_estimator/odometry.h
@@ -1,0 +1,104 @@
+#ifndef GUARD_XKTOCTGPUKGKWLWG
+#define GUARD_XKTOCTGPUKGKWLWG
+
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+
+#include <odom_estimator/unscented_transform.h>
+
+namespace odom_estimator {
+
+
+struct Odom {
+  ros::Time stamp;
+  std::string frame_id;
+  std::string child_frame_id;
+  
+  Vec<3> pos;
+  Quaternion orient;
+  Vec<3> vel;
+  Vec<3> ang_vel;
+  static const int RowsAtCompileTime = 3*4;
+  unsigned int rows() const {
+    return RowsAtCompileTime;
+  }
+  
+  Odom(ros::Time stamp,
+       std::string frame_id, std::string child_frame_id,
+       Vec<3> pos, Quaternion orient,
+       Vec<3> vel, Vec<3> ang_vel) :
+    stamp(stamp),
+    frame_id(frame_id), child_frame_id(child_frame_id),
+    pos(pos), orient(orient.normalized()),
+    vel(vel), ang_vel(ang_vel) {
+  }
+  
+  Vec<RowsAtCompileTime> operator-(const Odom &other) const {
+    assert(other.stamp == stamp);
+    assert(other.frame_id == frame_id);
+    assert(other.child_frame_id == child_frame_id);
+    return (Vec<RowsAtCompileTime>() <<
+      pos - other.pos,
+      rotvec_from_quat(orient * other.orient.conjugate()),
+      vel - other.vel,
+      ang_vel - other.ang_vel).finished();
+  }
+  Odom operator+(const Vec<RowsAtCompileTime> &other) const {
+    return Odom(
+      stamp,
+      frame_id,
+      child_frame_id,
+      pos + other.segment<3>(0),
+      quat_from_rotvec(other.segment<3>(3)) * orient,
+      vel + other.segment<3>(6),
+      ang_vel + other.segment<3>(9));
+  }
+};
+
+
+template<typename ArgType, typename ResType>
+ResType functional(void func(ArgType const &, ResType &),
+                   ArgType const &arg) {
+  ResType res;
+  func(arg, res);
+  return res;
+}
+
+GaussianDistribution<Odom> odom_from_msg(nav_msgs::Odometry const &msg) {
+  return GaussianDistribution<Odom>(
+    Odom(
+      msg.header.stamp,
+      msg.header.frame_id,
+      msg.child_frame_id,
+      functional(tf::pointMsgToEigen, msg.pose.pose.position),
+      functional(tf::quaternionMsgToEigen, msg.pose.pose.orientation),
+      functional(tf::vectorMsgToEigen, msg.twist.twist.linear),
+      functional(tf::vectorMsgToEigen, msg.twist.twist.angular)),
+    (SqMat<12>() << 
+      Eigen::Map<const SqMat<6> >(msg.pose.covariance.data()),
+        SqMat<6>::Zero(),
+      SqMat<6>::Zero(),
+        Eigen::Map<const SqMat<6> >(msg.twist.covariance.data())).finished());
+}
+
+nav_msgs::Odometry msg_from_odom(GaussianDistribution<Odom> const &res) {
+  nav_msgs::Odometry result;
+  result.header.stamp = res.mean.stamp;
+  result.header.frame_id = res.mean.frame_id;
+  result.child_frame_id = res.mean.child_frame_id;
+  
+  tf::pointEigenToMsg(res.mean.pos, result.pose.pose.position);
+  tf::quaternionEigenToMsg(res.mean.orient, result.pose.pose.orientation);
+  Eigen::Map<SqMat<6> >(result.pose.covariance.data()) = res.cov.block<6, 6>(0, 0);
+  
+  tf::vectorEigenToMsg(res.mean.vel, result.twist.twist.linear);
+  tf::vectorEigenToMsg(res.mean.ang_vel, result.twist.twist.angular);
+  Eigen::Map<SqMat<6> >(result.twist.covariance.data()) = res.cov.block<6, 6>(6, 6);
+  
+  return result;
+}
+
+
+}
+
+#endif

odom_estimator/include/odom_estimator/unscented_transform.h
@@ -38,7 +38,7 @@ public:
   GaussianDistributionWithCrossCov(GaussianDistribution<PointType> const &gd,
                                    CrossCovType const &cross_cov) :
     GaussianDistribution<PointType>(gd), cross_cov(cross_cov) {
-    assert(cross_cov.rows() == mean.rows());
+    assert(cross_cov.rows() == this->mean.rows());
   }
 };
 

commit b4f8234d0829524ec706514e93eb35d4d80d3ee6

odometry_utils: fixed bug introduced in db92497c

odometry_utils/src/transform_odometry.cpp
@@ -53,12 +53,12 @@ private:
     Quaterniond right_q; tf::quaternionTFToEigen(righttransform.getRotation(), right_q);
     
     EasyDistributionFunction<Odom, Odom, Vec<0> > transformer(
-      [&left_p, &left_q, &right_p, &right_q](Odom const &odom,
-                                             Vec<0> const &extra) {
+      [this, &left_p, &left_q, &right_p, &right_q](Odom const &odom,
+                                                    Vec<0> const &extra) {
         return Odom(
           odom.stamp,
-          odom.frame_id,
-          odom.child_frame_id,
+          frame_id,
+          child_frame_id,
           left_p + left_q._transformVector(odom.pos + odom.orient._transformVector(right_p)),
           left_q * odom.orient * right_q,
           right_q.inverse()._transformVector(odom.vel - right_p.cross(odom.ang_vel)),