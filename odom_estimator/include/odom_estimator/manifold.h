2d48c23e30680b73d76488d3b487b733dc4861be
Forrest Voight <forrest@forre.st>
2014-02-21 17:03:29 -0500

    owrk

--- odom_estimator/include/odom_estimator/manifold.h
+++ odom_estimator/include/odom_estimator/manifold.h
@@ -3,9 +3,131 @@
 
 #include <odom_estimator/util.h>
 
+#include <boost/preprocessor/seq/fold_right.hpp>
+#include <boost/fusion/adapted/struct/define_struct.hpp>
+
 namespace odom_estimator {
 
+static inline constexpr int addRowsAtCompileTime(int a, int b) {
+  return a == Dynamic ? Dynamic :
+         b == Dynamic ? Dynamic :
+         a + b;
+}
 
+#define BOOST_FUSION_DEFINE_STRUCT_ATTR_I(R, ATTRIBUTE_TUPEL_SIZE, ATTRIBUTE)   \
+                                                                                \
+    BOOST_PP_TUPLE_ELEM(ATTRIBUTE_TUPEL_SIZE,0,ATTRIBUTE)                       \
+        BOOST_PP_TUPLE_ELEM(ATTRIBUTE_TUPEL_SIZE,1,ATTRIBUTE);
+
+#define BOOST_FUSION_DEFINE_STRUCT_SEQ_DEFAULT_CTOR_FILLER_I(                   \
+    R, ATTRIBUTE_TUPEL_SIZE, I, ATTRIBUTE)                                      \
+                                                                                \
+    BOOST_PP_COMMA_IF(I)                                                        \
+    BOOST_PP_TUPLE_ELEM(ATTRIBUTE_TUPEL_SIZE,1,ATTRIBUTE)()
+
+#define BOOST_FUSION_DEFINE_STRUCT_COPY_CTOR_FILLER_I(                          \
+    R, ATTRIBUTE_TUPEL_SIZE, I, ATTRIBUTE)                                      \
+                                                                                \
+    BOOST_PP_COMMA_IF(I)                                                        \
+    BOOST_PP_TUPLE_ELEM(ATTRIBUTE_TUPEL_SIZE,1,ATTRIBUTE)(                      \
+        other_self.BOOST_PP_TUPLE_ELEM(ATTRIBUTE_TUPEL_SIZE,1,ATTRIBUTE))
+
+#define myop(s, state, x) addRowsAtCompileTime(state, BOOST_PP_TUPLE_ELEM(2,0,x)::RowsAtCompileTime)
+#define myop2(s, state, x) state + BOOST_PP_TUPLE_ELEM(2,1,x).rows()
+
+#define BOOST_FUSION_DEFINE_STRUCT_IMPL_IMPL(                                   \
+    NAME, ATTRIBUTES_SEQ, ATTRIBUTE_TUPEL_SIZE)                                 \
+                                                                                \
+    BOOST_PP_SEQ_FOR_EACH_R(                                                    \
+        1,                                                                      \
+        BOOST_FUSION_DEFINE_STRUCT_ATTR_I,                                      \
+        ATTRIBUTE_TUPEL_SIZE,                                                   \
+        ATTRIBUTES_SEQ)                                                         \
+                                                                                \
+    static int const RowsAtCompileTime =                                        \
+        BOOST_PP_SEQ_FOLD_RIGHT(myop, 0, ATTRIBUTES_SEQ);                       \
+                                                                                \
+    unsigned int rows() const {                                                 \
+        return BOOST_PP_SEQ_FOLD_RIGHT(myop2, 0, ATTRIBUTES_SEQ);               \
+    }                                                                           \
+                                                                                \
+    DeltaType operator-(const ManifoldPair<First, Second> &other) const {       \
+      return (DeltaType() <<                                                    \
+        first - other.first,                                                    \
+        second - other.second).finished();                                      \
+    }                                                                           \
+    ManifoldPair<First, Second> operator+(const DeltaType &other) const {       \
+      return ManifoldPair<First, Second>(                                       \
+        first + other.head(first.rows()),                                       \
+        second + other.tail(second.rows()));                                    \
+    }                                                                           \
+                                                                                \
+    NAME(self_type const& other_self)                                           \
+      : BOOST_PP_SEQ_FOR_EACH_I_R(                                              \
+            1,                                                                  \
+            BOOST_FUSION_DEFINE_STRUCT_COPY_CTOR_FILLER_I,                      \
+            ATTRIBUTE_TUPEL_SIZE,                                               \
+            ATTRIBUTES_SEQ)                                                     \
+    {}
+
+#define BOOST_FUSION_DEFINE_NONEMPTY_STRUCT_IMPL(                               \
+    NAME, ATTRIBUTES_SEQ, ATTRIBUTE_TUPEL_SIZE)                                 \
+                                                                                \
+        BOOST_FUSION_DEFINE_STRUCT_IMPL_IMPL(                                   \
+            NAME, BOOST_PP_SEQ_TAIL(ATTRIBUTES_SEQ), ATTRIBUTE_TUPEL_SIZE)      \
+                                                                                \
+        BOOST_FUSION_DEFINE_STRUCT_CTOR(                                        \
+            NAME, BOOST_PP_SEQ_TAIL(ATTRIBUTES_SEQ), ATTRIBUTE_TUPEL_SIZE)
+
+#define BOOST_FUSION_DEFINE_EMPTY_STRUCT_IMPL(                                  \
+    NAME, ATTRIBUTES_SEQ, ATTRIBUTE_TUPEL_SIZE)                                 \
+                                                                                \
+        NAME()                                                                  \
+        {}
+
+#define BOOST_FUSION_DEFINE_STRUCT_IMPL(                                        \
+    NAMESPACE_SEQ, NAME, ATTRIBUTES_SEQ, ATTRIBUTE_TUPEL_SIZE)                  \
+                                                                                \
+    BOOST_FUSION_ADAPT_STRUCT_NAMESPACE_DEFINITION_BEGIN(NAMESPACE_SEQ)         \
+                                                                                \
+    struct NAME                                                                 \
+    {                                                                           \
+        typedef NAME self_type;                                                 \
+                                                                                \
+        BOOST_PP_IF(BOOST_PP_DEC(BOOST_PP_SEQ_SIZE(ATTRIBUTES_SEQ)),            \
+            BOOST_FUSION_DEFINE_NONEMPTY_STRUCT_IMPL,                           \
+            BOOST_FUSION_DEFINE_EMPTY_STRUCT_IMPL)(                             \
+                NAME, ATTRIBUTES_SEQ, ATTRIBUTE_TUPEL_SIZE)                     \
+    };                                                                          \
+                                                                                \
+    BOOST_FUSION_ADAPT_STRUCT_NAMESPACE_DEFINITION_END(NAMESPACE_SEQ)
+
+
+#define BOOST_FUSION_DEFINE_STRUCT(NAMESPACE_SEQ, NAME, ATTRIBUTES)             \
+    BOOST_FUSION_DEFINE_STRUCT_IMPL(                                            \
+        (0)NAMESPACE_SEQ,                                                       \
+        NAME,                                                                   \
+        BOOST_PP_CAT(BOOST_FUSION_ADAPT_STRUCT_FILLER_0(0,0)ATTRIBUTES,_END),   \
+        2)
+
+BOOST_FUSION_DEFINE_STRUCT(
+  (odom_estimator),
+  NewManifoldPair,
+  (Vec<3>, first)
+  (Vec<3>, second)
+  )
+
+BOOST_FUSION_DEFINE_STRUCT(
+  (odom_estimator),
+  NewManifoldPair,
+  
+  )
+
+a
+BOOST_FUSION_ADAPT_STRUCT_FILLER_0(0,0)
+b
+
+namespace odom_estimator {
 
 template<typename First, typename Second>
 class ManifoldPair {
