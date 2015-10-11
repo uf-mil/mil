24c546306607762bfc8bd70c1f517066c88de3ba
Jacob Panikulam <jpanikulam@ufl.edu>
2015-10-10 22:39:38 -0400

    SIM;PHYSICS: Add set pose/twist support for sub
    - Can now set twist in addition to pose

--- simulation/sub8_simulation/sub8_sim_tools/physics/vehicle.py
+++ simulation/sub8_simulation/sub8_sim_tools/physics/vehicle.py
@@ -12,25 +12,29 @@ from std_msgs.msg import Header
 
 
 class Sub8(Box):
-    _cmd_timeout = np.inf
-    # _cmd_timeout = 10.
+    _cmd_timeout = 2.
     def __init__(self, world, space, position):
         '''Yes, right now we're approximating the sub as a box
         Not taking any parameters because this is the literal Sub8
         TODO:
             - Make the thruster list a parameter
+            - Stop the sub from being able to fly (Only thrust if thruster_z < 0)
 
         See Annie for how the thrusters work
         '''
-        lx, ly, lz = 10 * 0.5588, 10 * 0.5588, 10 * 0.381  # Meters
-        volume = lx * ly * lz
-        weight = 31.75  # kg
+        lx, ly, lz = 0.5588, 0.5588, 0.381  # Meters
+        self.radius = max((lx, ly, lz)) * 0.5  # For spherical approximation
+        volume = lx * ly * lz * 0.25
+        # weight = 32.75  # kg
+        weight = 25.0  # kg
         density = weight / volume
         super(self.__class__, self).__init__(world, space, position, density, lx, ly, lz)
 
         self.truth_odom_pub = rospy.Publisher('truth/odom', Odometry, queue_size=1)
         self.thruster_sub = rospy.Subscriber('thrusters/thrust', Thrust, self.thrust_cb, queue_size=2)
         self.thrust_dict = {}
+
+        self.last_force = (0.0, 0.0, 0.0)
 
         # Make this a parameter
         # name, relative direction, relative position (COM)
@@ -63,8 +67,14 @@ class Sub8(Box):
         rotation_q = rosmsg_to_numpy(srv.pose.orientation)
         rotation_norm = np.linalg.norm(rotation_q)
 
+        velocity = rosmsg_to_numpy(srv.twist.linear)
+        angular = rosmsg_to_numpy(srv.twist.angular)
+
+        self.body.setLinearVel(velocity)
+        self.body.setAngularVel(angular)
+
         # If the commanded rotation is valid
-        if np.isclose(rotation_norm, 1.):
+        if np.isclose(rotation_norm, 1., atol=1e-3):
             self.body.setQuaternion(rotation_q)
         else:
             rospy.logwarn("Commanded quaternion was not a unit quaternion, NOT setting rotation")
@@ -73,7 +83,8 @@ class Sub8(Box):
 
     def publish_pose(self):
         pose_matrix = np.transpose(self.pose)
-        linear_vel, angular_vel = self.velocity
+        linear_vel = self.velocity
+        angular_vel = self.angular_vel
         quaternion = tf.transformations.quaternion_from_matrix(pose_matrix)
         translation = tf.transformations.translation_from_matrix(pose_matrix)
 
@@ -112,16 +123,25 @@ class Sub8(Box):
             self.thrust_dict[thrust_cmd.name] = thrust_cmd.thrust
 
     def step(self, dt):
-        '''Ignore dt'''
+        '''Ignore dt
+            TODO: Check if thruster is underwater
+        '''
         # If timeout, reset thrust commands
         if (time() - self.last_cmd_time) > self._cmd_timeout:
             self.thrust_dict = {}
 
-        for i, (name, rel_dir, rel_pos) in enumerate(self.thruster_list):
-            thruster_force = self.thrust_dict.get(name, 0.0)
-            body_force = rel_dir * thruster_force
-            self.body.addRelForceAtRelPos(body_force, rel_pos)
+        if self.pos[2] < 0:
+            # Apply thruster force only if underwater
 
+            for i, (name, rel_dir, rel_pos) in enumerate(self.thruster_list):
+                thruster_force = self.thrust_dict.get(name, 0.0)
+                body_force = rel_dir * thruster_force
+                self.body.addRelForceAtRelPos(body_force, rel_pos)
+                total_force += body_force
+
+        self.last_force = self.body.getForce()
+
+        self.apply_buoyancy_force()
         self.apply_damping_force()
         # self.apply_damping_torque()
         self.publish_pose()
