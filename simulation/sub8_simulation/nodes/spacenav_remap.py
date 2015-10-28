cd66368df490ca224b11374f6bba2cb0155cfc91
Jacob Panikulam <jpanikulam@ufl.edu>
2015-10-28 02:26:47 -0400

    SIM: Add 3D Mouse support
    - 3D Connexxion mouse is now supported for issuing wrenches to the
      vehicle. It is not required that this be done through simulation, but
    I would not suggest you try to jockey the real sub in a shallow pool.

---
+++ simulation/sub8_simulation/nodes/spacenav_remap.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+'''This node is for taking input from a Spacenav (3D Mouse)
+    [1] http://www.3dconnexion.com/products/spacemouse.html
+'''
+import rospy
+import tf
+import numpy as np
+from geometry_msgs.msg import Twist, Wrench, WrenchStamped
+import nav_msgs.msg as nav_msgs
+import sub8_ros_tools as sub8_utils
+
+
+class Spacenav(object):
+    def __init__(self):
+        rospy.init_node('spacenav_remap')
+
+        self.linear_gain = 500
+        self.angular_gain = 500
+
+        self.behavior = rospy.get_param('~behavior', 'wrench')
+        behaviors = ['wrench', 'velocity', 'position']
+        assert self.behavior in behaviors, "Parameter 'behavior' invalid, must be in {}".format(
+            behaviors)
+
+        if self.behavior == 'wrench':
+            self.wrench_pub = rospy.Publisher('/wrench', WrenchStamped, queue_size=1)
+        else:
+            raise(NotImplementedError("Velocity and position control by mouse are not yet supported"))
+
+        self.transformer = tf.TransformerROS()
+        self.world_to_body = np.eye(3)
+        self.odom_sub = rospy.Subscriber('/truth/odom', nav_msgs.Odometry, self.odom_cb, queue_size=1)
+        self.twist_sub = rospy.Subscriber('/spacenav/twist', Twist, self.twist_cb, queue_size=1)
+
+    def odom_cb(self, msg):
+        '''HACK: Intermediate hack until we have tf set up'''
+        pose, twist, _, _ = sub8_utils.odometry_to_numpy(msg)
+        position, orientation = pose
+        self.world_to_body = self.transformer.fromTranslationRotation(position, orientation)[:3, :3]
+
+    def twist_cb(self, msg):
+        linear = sub8_utils.rosmsg_to_numpy(msg.linear)
+        angular = sub8_utils.rosmsg_to_numpy(msg.angular)
+
+        if self.behavior == 'wrench':
+            # Directly transcribe linear and angular 'velocities' to torque
+            # TODO: Setup actual TF!
+
+            self.wrench_pub.publish(
+                sub8_utils.make_wrench_stamped(
+                    force=self.linear_gain * self.world_to_body.dot(linear),
+                    torque=self.angular_gain * self.world_to_body.dot(angular)
+                )
+            )
+        else:
+            raise(NotImplementedError("Velocity and position control by mouse are not yet supported"))
+
+
+if __name__ == '__main__':
+    sn = Spacenav()
+    rospy.spin()
--- simulation/sub8_simulation/readme.md
+++ simulation/sub8_simulation/readme.md
@@ -14,17 +14,30 @@ You need to install Vispy from source, do this..
     git checkout b48e4d3cf410b853a74b666c475c603e46725e55
     sudo python setup.py develop
 
-You'll also need pyode
+You'll also need pyode.
 
 # Usage
 
 run
-    
+
     roslaunch sub8_simulation sim_full.launch
 
+To use with the 3D mouse, sending wrenches, do
+
+    roslaunch sub8_simulation sim_full.launch 3dmouse:=True
+
+To set the speed higher, then in the roslaunch you'll have to set the `time_acceleration` parameter like this
+
+    <param name="time_acceleration" value="60" type="double"/>
+    <param name="draw" value=False />
+
+The `time_acceleration` is the number of simulated seconds that should pass for every real-world second. In this case, that's one simulated minute per real second. In *general*, though it is not guaranteed, if the sim runs slower than the acceleration you've asked for, time will slow down accordingly and everything will move smoothly.
+
+Note: For time-acceleration greater than 1.5, you must disable rendering, by setting the `draw` parameter to false
 
 # Truth
 
 Truth odometry is published on /truth/odom
 
 You can set the truth pose by calling the service /sim/vehicle/set_pose using the sub8_simulation.srv.SimSetPose service type
+
