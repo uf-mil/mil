4329b67f80c063f0db29107ba7589ccab63ac629
tesshbianchi <tesshbianchi@gmail.com>
2015-11-10 23:27:00 -0500

    SIMULATION: Added multiple lights

--- simulation/sub8_simulation/sub8_sim_tools/shaders/lighting.glsl
+++ simulation/sub8_simulation/sub8_sim_tools/shaders/lighting.glsl
@@ -66,6 +66,7 @@ attribute vec3 a_normal;
 varying vec3 v_normal;
 varying vec3 v_position;
 
+
 void main(){
     gl_Position = u_projection * u_view * u_model * vec4(a_position, 1.0);
     vec4 vertPos4 = u_view * u_model * vec4(a_position, 1.0);
@@ -73,45 +74,50 @@ void main(){
     v_normal = vec3(u_normal * vec4(a_normal, 0.0));
 }
 
+
 >phong:fragment
 // Shamelessly lifted from wikipedia
-uniform vec3 u_light_position;
-uniform vec3 u_light_intensity;
+uniform vec3 u_lights[16];
 uniform vec4 u_color;
-uniform vec3 u_specular_color;// = vec3(1.0, 1.0, 1.0);
-uniform float u_shininess;// = 16.0;
+uniform vec3 u_specular_color;
+uniform float u_shininess;
+uniform int u_numLights;
 
 varying vec3 v_normal;
 varying vec3 v_position;
 
-// uniform vec3 ambient_color = vec3(0.1, 0.0, 0.0);
-// const float u_shininess = 16.0;
 const float screen_gamma = 2.2; // Assume the monitor is calibrated to the sRGB color space
 
 void main() {
-    vec3 ambient_color = u_color.rgb;
     vec3 normal = normalize(v_normal);
-    vec3 light_direction = normalize(u_light_position - v_position);
+    vec3 ambient_color = u_color.rgb;
+    vec3 color_linear = ambient_color; 
+     
+    int i;
+    for(i = 0; i!= u_numLights * 2; i += 2){
+        vec3 lightPos = u_lights[i];
+        vec3 intensity = u_lights[i+1];
+        vec3 speccolor = u_specular_color;
+        vec3 light_direction = normalize(lightPos - v_position);
+        float lambertian = max(dot(light_direction,normal), 0.0);
+        float specular = 0.0;
 
-    float lambertian = max(dot(light_direction,normal), 0.0);
-    float specular = 0.0;
+        if(lambertian > 0.0) {
 
-    if(lambertian > 0.0) {
+             vec3 view_direction = normalize(-v_position);
 
-        vec3 view_direction = normalize(-v_position);
+                // this is blinn phong
+            vec3 half_direction = normalize(light_direction + view_direction);
+            float specular_angle = max(dot(half_direction, normal), 0.0);
+            specular = pow(specular_angle, u_shininess);
 
-        // this is blinn phong
-        vec3 half_direction = normalize(light_direction + view_direction);
-        float specular_angle = max(dot(half_direction, normal), 0.0);
-        specular = pow(specular_angle, u_shininess);
-           
+        }
+        color_linear += (intensity * lambertian) +
+                            (specular * speccolor);
+       
     }
-  vec3 color_linear = ambient_color +
-                     lambertian * u_light_intensity +
-                     specular * u_specular_color;
-  // apply gamma correction (assume ambient_color, u_light_intensity and u_specular_color
-  // have been linearized, i.e. have no gamma correction in them)
+
   vec3 color_gamma_corrected = pow(color_linear, vec3(1.0 / screen_gamma));
   // use the gamma corrected color in the fragment
   gl_FragColor = vec4(color_gamma_corrected, u_color.a);
-}
+  }
