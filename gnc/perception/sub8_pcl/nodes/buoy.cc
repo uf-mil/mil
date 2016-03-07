32cedbe3148d1ba117a68955f327dd5b16f463a8
Jacob Panikulam <jpanikulam@ufl.edu>
2016-03-07 02:12:11 -0500

    PERCEPTION: Make buoy detector service-based

--- gnc/perception/sub8_pcl/nodes/buoy.cc
+++ gnc/perception/sub8_pcl/nodes/buoy.cc
@@ -21,12 +21,16 @@
 #include <sensor_msgs/image_encodings.h>
 
 #include <eigen_conversions/eigen_msg.h>
+#include <Eigen/StdVector>
 #include <sub8_pcl/pcl_tools.hpp>
 #include <sub8_pcl/cv_tools.hpp>
 
 #include "ros/ros.h"
 
+#include "sub8_msgs/VisionRequest.h"
+
 // For stack-tracing on seg-fault
+#define BACKWARD_HAS_BFD 1
 #include <sub8_build_tools/backward.hpp>
 
 // #define VISUALIZE
@@ -36,6 +40,10 @@
 // [1]
 // http://docs.ros.org/hydro/api/image_geometry/html/c++/classimage__geometry_1_1PinholeCameraModel.html
 // [2] http://docs.pointclouds.org/trunk/classpcl_1_1visualization_1_1_p_c_l_visualizer.html
+/* TODO
+  - Smarter buoy tracking
+  - Better cacheing (instead of immediate assignment)
+*/
 
 bool visualize = false;
 std::string meshpath;
@@ -47,6 +55,13 @@ class Sub8BuoyDetector {
   void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr &);
   void image_callback(const sensor_msgs::ImageConstPtr &msg,
                       const sensor_msgs::CameraInfoConstPtr &info_msg);
+  void determine_buoy_position(const image_geometry::PinholeCameraModel &cam_model,
+                               const cv::Mat &image_raw,
+                               const sub::PointCloudT::Ptr &point_cloud_raw,
+                               Eigen::Vector3f &center);
+
+  bool request_buoy_position(sub8_msgs::VisionRequest::Request &req,
+                             sub8_msgs::VisionRequest::Response &resp);
   // Visualize
   int vp1;
   int vp2;
@@ -61,14 +76,19 @@ class Sub8BuoyDetector {
   image_transport::ImageTransport image_transport;
   image_geometry::PinholeCameraModel cam_model;
 
+  ros::Time image_time;
+  ros::Time last_cloud_time;
   sub::PointCloudT::Ptr current_cloud;
-  bool got_cloud, line_added, computing;
+  cv::Mat current_image;
+  bool got_cloud, line_added, computing, need_new_cloud;
 };
 
 Sub8BuoyDetector::Sub8BuoyDetector()
     : vp1(0),
       vp2(1),
+#ifdef VISUALIZE
       viewer(new pcl::visualization::PCLVisualizer("Incoming Cloud")),
+#endif
       image_transport(nh) {
   pcl::console::print_highlight("Initializing PCL SLAM\n");
   // Perform match computations
@@ -76,121 +96,162 @@ Sub8BuoyDetector::Sub8BuoyDetector()
   image_sub = image_transport.subscribeCamera("stereo/left/image_rect_color", 1,
                                               &Sub8BuoyDetector::image_callback, this);
 
+#ifdef VISUALIZE
   viewer->addCoordinateSystem(1.0);
   viewer->createViewPort(0.5, 0.0, 1.0, 1.0, vp1);
   viewer->createViewPort(0.0, 0.0, 0.5, 1.0, vp2);
+#endif
 
   got_cloud = false;
   line_added = false;
   computing = false;
+  need_new_cloud = false;
 
   pcl::console::print_highlight("--PCL SLAM Initialized\n");
   data_sub = nh.subscribe("/stereo/points2", 1, &Sub8BuoyDetector::cloud_callback, this);
+  service =
+      nh.advertiseService("/vision/buoys/yellow", &Sub8BuoyDetector::request_buoy_position, this);
 }
 
-Sub8BuoyDetector::~Sub8BuoyDetector() { viewer->close(); }
+Sub8BuoyDetector::~Sub8BuoyDetector() {
+#ifdef VISUALIZE
+  viewer->close();
+#endif
+}
 
-void Sub8BuoyDetector::compute_loop(const ros::TimerEvent &timer_event) { viewer->spinOnce(); }
+// Compute the buoy position in the camera model frame
+// TODO: Check if we have an image
+// TODO: Synchronize cloud and image better
+bool Sub8BuoyDetector::request_buoy_position(sub8_msgs::VisionRequest::Request &req,
+                                             sub8_msgs::VisionRequest::Response &resp) {
+  std::string tf_frame;
+  Eigen::Vector3f position;
 
-void Sub8BuoyDetector::image_callback(const sensor_msgs::ImageConstPtr &image_msg,
-                                      const sensor_msgs::CameraInfoConstPtr &info_msg) {
+  if (!got_cloud) {
+    // Failure, yo!
+    return false;
+  }
   computing = true;
-  pcl::console::print_highlight("Getting image\n");
-  cv::Mat image_raw;
+  tf_frame = cam_model.tfFrame();
+  determine_buoy_position(cam_model, current_image, current_cloud, position);
+
+  tf::pointEigenToMsg(position.cast<double>(), resp.pose.pose.position);
+  resp.pose.header.frame_id = tf_frame;
+
+#ifdef VISUALIZE
+  cv::Point2d cv_pt_uv =
+      cam_model.project3dToPixel(cv::Point3f(position.x(), position.y(), position.z()));
+  cv::circle(current_image, cv_pt_uv, 7, cv::Scalar(0, 20, 240), 2);
+  cv::imshow("input", current_image);
+  cv::waitKey(50);
+#endif
+  computing = false;
+
+  return true;
+}
+
+void Sub8BuoyDetector::compute_loop(const ros::TimerEvent &timer_event) {
+#ifdef VISUALIZE
+  viewer->spinOnce();
+#endif
+}
+
+void Sub8BuoyDetector::determine_buoy_position(
+    const image_geometry::PinholeCameraModel &camera_model, const cv::Mat &image_raw,
+    const sub::PointCloudT::Ptr &point_cloud_raw, Eigen::Vector3f &center) {
   cv::Mat image_hsv;
   cv::Mat image_thresh;
 
-  cv_bridge::CvImagePtr input_bridge;
-  try {
-    input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
-    image_raw = input_bridge->image;
-  } catch (cv_bridge::Exception &ex) {
-    ROS_ERROR("[draw_frames] Failed to convert image");
-    return;
-  }
-  cam_model.fromCameraInfo(info_msg);
   // believe it or not, this is on purpose (So blue replaces red in HSV)
   cv::cvtColor(image_raw, image_hsv, CV_RGB2HSV);
 
-  if (!got_cloud) {
-    return;
-  }
   cv::Point2d pt_cv_2d(250, 250);
-  sub::PointXYZT pcl_pt;
-  pcl_pt = sub::project_uv_to_cloud(*current_cloud, pt_cv_2d, cam_model);
+  sub::PointXYZT pcl_pt_3d;
+  pcl_pt_3d = sub::project_uv_to_cloud(*point_cloud_raw, pt_cv_2d, camera_model);
 
-#ifdef VISUALIZE
-  viewer->removeAllShapes(0);
-  pcl::console::print_highlight("Drawing line\n");
-#endif
+  // Reprojection (Useful for visualization)
+  cv::Point2d cv_pt_uv =
+      cam_model.project3dToPixel(cv::Point3f(pcl_pt_3d.x, pcl_pt_3d.y, pcl_pt_3d.z));
 
-  cv::Point2d cv_uv = cam_model.project3dToPixel(cv::Point3f(pcl_pt.x, pcl_pt.y, pcl_pt.z));
+  // Threshold -- > This is what must be replaced with better 2d vision
   cv::inRange(image_hsv, cv::Scalar(105, 135, 135), cv::Scalar(120, 255, 255), image_thresh);
   std::vector<sub::Contour> contours;
   std::vector<cv::Vec4i> hierarchy;
   cv::findContours(image_thresh.clone(), contours, hierarchy, CV_RETR_EXTERNAL,
                    CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+  // TODO: ^^^ Make into function ^^^
 
-  std::vector<int> seed_points;
-  sub::PointCloudT::Ptr append_buffer(new sub::PointCloudT());
-  sub::PointCloudT::Ptr segmented_cloud(new sub::PointCloudT());
+  // Not returning segmented point cloud for now
+  // sub::PointCloudT::Ptr segmented_cloud(new sub::PointCloudT());
+  // Vector of Eigen-vectors
+  std::vector<Eigen::Vector3f, Eigen::aligned_allocator<Eigen::Vector3f> > buoy_centers;
 
+  // Loop through the contours we found and find the positions
   for (size_t i = 0; i < contours.size(); i++) {
-    cv::Point center = sub::contour_centroid(contours[i]);
+    cv::Point contour_centroid = sub::contour_centroid(contours[i]);
     std::vector<cv::Point> approx;
+
+    // Magic num: 5
     cv::approxPolyDP(contours[i], approx, 5, true);
     double area = cv::contourArea(approx);
+
+    // Ignore small regions
     if (area < 100) {
       continue;
     }
-
-    sub::PointXYZT centroid_projected = sub::project_uv_to_cloud(*current_cloud, center, cam_model);
-    size_t centroid_projected_index;
-    centroid_projected_index = sub::project_uv_to_cloud_index(*current_cloud, center, cam_model);
-    seed_points.push_back(centroid_projected_index);
-
-    cv::Point3d pt_cv;
-    pt_cv = cam_model.projectPixelTo3dRay(center);
-    sub::PointXYZT pt1;
-    pt1.x = pt_cv.x * 3;
-    pt1.y = pt_cv.y * 3;
-    pt1.z = pt_cv.z * 3;
-
-    sub::PointXYZT pt2(0.0, 0.0, 0.0);
-
-#ifdef VISUALIZE
-    cv::drawContours(image_raw, contours, i, cv::Scalar(255, 255, 0), -1, 8, hierarchy, 0,
-                     cv::Point());
-    cv::circle(image_raw, center, 6.0, cv::Scalar(0, 255, 0), -1);
-
-    std::string title = "line" + boost::to_string(i);
-    // Line to actual projection
-    viewer->addLine(sub::PointXYZT(0.0, 0.0, 0.0), centroid_projected, 0.0, 255.0, 255.0, title, 0);
-
-    // Line of direction
-    viewer->addLine(pt1, pt2, 255.0, 255.0, 0.0, title + "doop", 0);
-
-    viewer->addCube(sub::point_to_eigen(centroid_projected), Eigen::Quaternionf(0.0, 0.0, 0.0, 1.0),
-                    0.3, 0.3, 0.3, "cube" + boost::to_string(i), 0);
-#endif
-    sub::segment_box(current_cloud, sub::point_to_eigen(centroid_projected), 0.3, *append_buffer);
-
-    (*segmented_cloud) += *append_buffer;
+    sub::PointXYZT centroid_projected =
+        sub::project_uv_to_cloud(*point_cloud_raw, contour_centroid, camera_model);
+    buoy_centers.push_back(sub::point_to_eigen(centroid_projected));
   }
+  // ^^^^ The above could be eliminated with better approach
 
-#ifdef VISUALIZE
-  viewer->removeAllPointClouds(vp2);
-  viewer->addPointCloud(segmented_cloud, "viz_cloud", vp2);
-  cv::imshow("input", image_raw);
-  cv::waitKey(50);
-#endif
-  computing = false;
+  // TODO: initialize to inf
+  double closest_distance = 1000;
+  size_t closest_buoy_index = 0;
+
+  for (size_t buoy_index = 0; buoy_index < buoy_centers.size(); buoy_index++) {
+    const double distance = buoy_centers[buoy_index].norm();
+    if (buoy_centers[buoy_index].norm() < closest_distance) {
+      closest_distance = distance;
+      closest_buoy_index = buoy_index;
+    }
+  }
+  center = buoy_centers[closest_buoy_index];
+}
+
+void Sub8BuoyDetector::image_callback(const sensor_msgs::ImageConstPtr &image_msg,
+                                      const sensor_msgs::CameraInfoConstPtr &info_msg) {
+  need_new_cloud = true;
+  pcl::console::print_highlight("Getting image\n");
+  // cv::Mat current_image;
+
+  cv_bridge::CvImagePtr input_bridge;
+  try {
+    input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+    current_image = input_bridge->image;
+  } catch (cv_bridge::Exception &ex) {
+    ROS_ERROR("[draw_frames] Failed to convert image");
+    return;
+  }
+  cam_model.fromCameraInfo(info_msg);
+  image_time = image_msg->header.stamp;
 }
 
 void Sub8BuoyDetector::cloud_callback(const sensor_msgs::PointCloud2::ConstPtr &input_cloud) {
   if (computing) {
+    pcl::console::print_highlight("Ignoring new point cloud because computing\n");
     return;
   }
+  if (((input_cloud->header.stamp - image_time) < ros::Duration(0.3)) and (need_new_cloud)) {
+    last_cloud_time = input_cloud->header.stamp;
+    need_new_cloud = false;
+  } else {
+    // std::cout << input_cloud->header.stamp << std::endl;
+    // std::cout << image_time << std::endl;
+    pcl::console::print_highlight("Ignoring because time\n");
+    return;
+  }
+  pcl::console::print_highlight("Getting Point Cloud\n");
   sub::PointCloudT::Ptr scene(new sub::PointCloudT());
   sub::PointCloudT::Ptr scene_buffer(new sub::PointCloudT());
 
