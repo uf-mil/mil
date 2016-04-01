017c92dc69247b9194ea2d6ef829e2a547f1ed4d
Jacob Panikulam <jpanikulam@ufl.edu>
2016-04-01 02:51:57 -0400

    GAZEBO: Add full thruster plugin

--- simulation/sub8_gazebo/include/sub8_gazebo/sub8_thrusters.hpp
+++ simulation/sub8_gazebo/include/sub8_gazebo/sub8_thrusters.hpp
@@ -1,34 +1,71 @@
 #pragma once
 
+#include <algorithm>
+#include <vector>
 #include <map>
+#include "gazebo/common/Assert.hh"
+#include "gazebo/common/Events.hh"
 #include "gazebo/common/Event.hh"
 #include "gazebo/common/Plugin.hh"
 #include "gazebo/math/Vector3.hh"
 #include "gazebo/physics/physics.hh"
+#include "sub8_msgs/Thrust.h"
+#include "sub8_msgs/ThrusterCmd.h"
+#include <ros/ros.h>
 
-namespace gazebo
-{
-  class ThrusterPlugin : public ModelPlugin
-  {
-    public: ThrusterPlugin();
+namespace gazebo {
 
-    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+struct Thruster {
+  Thruster() {
+    min = 0;
+    max = 0;
+  }
+  Thruster(std::string param_name) {
+    std::vector<double> v_direction, v_position, v_bounds;
+    ros::param::get(param_name + "/direction", v_direction);
+    ros::param::get(param_name + "/position", v_position);
+    ros::param::get(param_name + "/bounds", v_bounds);
+    position = math::Vector3(v_position[0], v_position[1], v_position[2]);
+    direction = math::Vector3(v_direction[0], v_direction[1], v_direction[2]);
+    min = v_bounds[0];
+    max = v_bounds[1];
+  }
+  math::Vector3 position;
+  math::Vector3 direction;
+  double min;
+  double max;
+};
 
-    public: virtual void Init();
+class ThrusterPlugin : public ModelPlugin {
+ public:
+  ThrusterPlugin();
+  virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+  virtual void Init();
+  void ThrustCallback(const sub8_msgs::Thrust::ConstPtr &thrust);
 
-    protected: virtual void OnUpdate();
+ protected:
+  // virtual void OnUpdate();
+  ros::NodeHandle nh;
+  ros::Subscriber thrustSub;
+  virtual void OnUpdate();
 
-    protected: event::ConnectionPtr updateConnection;
+ protected:
+  event::ConnectionPtr updateConnection;
+  physics::ModelPtr model;
+  physics::PhysicsEnginePtr physicsEngine;
+  sdf::ElementPtr sdf;
+  std::string layoutParam;
+  std::map<std::string, Thruster> thrusterMap;
+  physics::LinkPtr targetLink;
+  ros::Time lastTime;
+  std::vector<sub8_msgs::ThrusterCmd> cmdQueue;
 
-    protected: physics::ModelPtr model;
+  // Store
+  // math::Vector3 netForce;
+  // math::Vector3 netTorque;
 
-    protected: physics::PhysicsEnginePtr physicsEngine;
-
-    protected: sdf::ElementPtr sdf;
-
-    // protected: double fluidDensity;
-    // protected: double dragCoeff;
-    // protected: std::map<int, VolumeProperties> volPropsMap;
-  };
+  // protected: double fluidDensity;
+  // protected: double dragCoeff;
+  // protected: std::map<int, VolumeProperties> volPropsMap;
+};
 }
-
--- simulation/sub8_gazebo/src/sub8_thrusters.cc
+++ simulation/sub8_gazebo/src/sub8_thrusters.cc
@@ -1,26 +1,100 @@
-#include "sub8_gazebo/sub8_buoyancy.hpp"
+#include "sub8_gazebo/sub8_thrusters.hpp"
 #include <gazebo/common/Plugin.hh>
-#include <ros/ros.h>
 
 namespace gazebo {
-class ThrusterPlugin : public ModelPlugin {
- public:
-  ThrusterPlugin() : ModelPlugin() {}
 
-  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
-    GZ_ASSERT(_model != NULL, "Received NULL model pointer");
-    this->model = _model;
-    physics::WorldPtr world = _model->GetWorld();
-    GZ_ASSERT(world != NULL, "Model is in a NULL world");
-    this->physicsEngine = world->GetPhysicsEngine();
-    GZ_ASSERT(this->physicsEngine != NULL, "Physics engine was NULL");
+GZ_REGISTER_MODEL_PLUGIN(ThrusterPlugin)
 
-    GZ_ASSERT(_sdf != NULL, "Received NULL SDF pointer");
-    this->sdf = _sdf;
+ThrusterPlugin::ThrusterPlugin() {}
 
-    // Make sure the ROS node for Gazebo has already been initialized
-    GZ_ASSERT(!ros::isInitialized(), "ROS not initialized");
+void ThrusterPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+  GZ_ASSERT(_model != NULL, "Received NULL model pointer");
+
+  this->model = _model;
+  physics::WorldPtr world = _model->GetWorld();
+  GZ_ASSERT(world != NULL, "Model is in a NULL world");
+
+  this->physicsEngine = world->GetPhysicsEngine();
+  GZ_ASSERT(this->physicsEngine != NULL, "Physics engine was NULL");
+
+  GZ_ASSERT(_sdf != NULL, "Received NULL SDF pointer");
+  this->sdf = _sdf;
+
+  // Make sure the ROS node for Gazebo has already been initialized
+  GZ_ASSERT(ros::isInitialized(), "ROS not initialized");
+  thrustSub = nh.subscribe("thrusters/thrust", 1, &ThrusterPlugin::ThrustCallback, this);
+
+  // Parse SDF
+  if (this->sdf->HasElement("layout_param")) {
+    this->layoutParam = this->sdf->Get<std::string>("layout_param");
+  } else {
+    GZ_ASSERT(false, "Requires layout_param to be set!");
   }
-};
-GZ_REGISTER_WORLD_PLUGIN(ThrusterPlugin)
+
+  // Apply to multiple links
+  GZ_ASSERT(this->sdf->HasElement("link"), "Must have a link specified!");
+
+  std::string link_name = this->sdf->Get<std::string>("link");
+  this->targetLink = this->model->GetLink(link_name);
+  GZ_ASSERT(this->targetLink, "Could not find specified link");
+
+  // for (sdf::ElementPtr linkElem = this->sdf->GetElement("link"); linkElem;
+  //      linkElem = this->sdf->GetNextElement("link")) {
+  //   int id = -1;
+  //   std::string name = "";
+  //   if (linkElem->HasAttribute("name")) {
+  //     name = linkElem->Get<std::string>("name");
+  //     this->targetLink = this->model->GetLink(name);
+  //     GZ_ASSERT(this->targetLink, "Could not find specified link");
+  //     id = this->targetLink->GetId();
+  //   }
+  // }
+
+  std::vector<std::string> thrusterNames;
+  ros::param::get(this->layoutParam + "/thruster_names", thrusterNames);
+
+  // Prevent silly warnings (WHO IS STILL USING VARIADIC MACROS!?)
+  ROS_INFO("Found %d thrusters", static_cast<int>(thrusterNames.size()));
+  for (std::string thrusterName : thrusterNames) {
+    Thruster thruster(layoutParam + "/" + thrusterName);
+    thrusterMap[thrusterName] = thruster;
+  }
+  this->lastTime = ros::Time::now();
+}
+
+void ThrusterPlugin::Init() {
+  this->updateConnection =
+      event::Events::ConnectWorldUpdateBegin(std::bind(&ThrusterPlugin::OnUpdate, this));
+}
+
+void ThrusterPlugin::ThrustCallback(const sub8_msgs::Thrust::ConstPtr &thrust) {
+  math::Vector3 netForce = math::Vector3::Zero;
+  math::Vector3 netTorque = math::Vector3::Zero;
+
+  this->cmdQueue.clear();
+  this->lastTime = ros::Time::now();
+  this->cmdQueue = thrust->thruster_commands;
+  // for (auto thrustCmd : thrust->thruster_commands) {
+  // this->cmdQueue.push_back(thrustCmd);
+  // }
+}
+void ThrusterPlugin::OnUpdate() {
+  if ((ros::Time::now() - this->lastTime) > ros::Duration(2.0)) {
+    ROS_ERROR("CLEARING COMMAND QUEUE");
+    this->cmdQueue.clear();
+  }
+  for (auto thrustCmd : this->cmdQueue) {
+    const std::string name = thrustCmd.name;
+    const double thrust = thrustCmd.thrust;
+    const Thruster thruster = this->thrusterMap[name];
+
+    // clamp idea from: https://www.c-plusplus.net/forum/323030-full
+    // email me (jake) if you ever find this line
+    // math::Vector3 force = thruster.direction * std::max(thruster.min, std::min(thruster.max,
+    // thrust));
+    math::Vector3 force = thruster.direction * thrust;
+
+    this->targetLink->AddForceAtRelativePosition(force, thruster.position);
+  }
+}
 }
