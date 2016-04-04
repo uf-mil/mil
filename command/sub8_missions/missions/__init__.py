8dbfb4028af2bf2fd952b874bd176ff9168c800f
Jacob Panikulam <jpanikulam@ufl.edu>
2016-04-04 02:10:57 -0400

    MISSIONS: Add channel-marker alignment

--- command/sub8_missions/missions/__init__.py
+++ command/sub8_missions/missions/__init__.py
@@ -1,5 +1,8 @@
 # flake8: noqa
-import surface
-import tx_test
-import level_off
-import down_5m
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+for name in __all__:
+    if name.startswith('__'):
+        __all__.remove(name)
