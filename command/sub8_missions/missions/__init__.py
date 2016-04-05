bd0d41250dec71885e37a50d4d51880683c6cc06
Jacob Panikulam <jpanikulam@ufl.edu>
2016-04-05 17:56:21 -0400

    MISSIONS: Fix __init__ not recognizing missions

--- command/sub8_missions/missions/__init__.py
+++ command/sub8_missions/missions/__init__.py
@@ -1,8 +1,9 @@
 # flake8: noqa
-from os.path import dirname, basename, isfile
-import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
-for name in __all__:
-    if name.startswith('__'):
-        __all__.remove(name)
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+    print module
+del module
+del os
