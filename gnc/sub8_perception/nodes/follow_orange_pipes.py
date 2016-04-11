471216189eeb571bca51543794a9ba81839fd41b
Jacob Panikulam <jpanikulam@ufl.edu>
2016-04-11 02:09:57 -0400

    ORANGE MARKERS: Find with normalized cross-corr
    - For channel markers
    - NCC, or normalized cross-correlation
    - Some legends suggest better performance might come from patchwise SSE
        - But our hero is doubtful!

--- gnc/sub8_perception/nodes/follow_orange_pipes.py
+++ gnc/sub8_perception/nodes/follow_orange_pipes.py
@@ -1,50 +1,58 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
-from tf import transformations
 import sys
 import rospy
-from sensor_msgs.msg import Image
-from sub8_msgs.srv import VisionRequestResponse, VisionRequest
 from sub8_msgs.srv import VisionRequest2DResponse, VisionRequest2D
 import sub8_ros_tools
-from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion, Pose2D
-from cv_bridge import CvBridge, CvBridgeError
-
-# define threshold for orange color detection
-# ORANGE_MIN = np.array([1, 90, 180], np.float32)
-# ORANGE_MAX = np.array([23, 255, 255], np.float32)
-range_default = np.array([
-    [0., 24.8631],
-    [134.589, 201.8835],
-    [74.3835, 226.6695]
-])
+from geometry_msgs.msg import Pose2D
 
 
 class PipeFinder:
     def __init__(self):
         self.last_image = None
         self.last_draw_image = None
-        self.pose_pub = rospy.Publisher("orange_pipe_vision", Pose, queue_size=1)
         self.pose_service = rospy.Service("vision/channel_marker/2D", VisionRequest2D, self.request_pipe)
         self.image_sub = sub8_ros_tools.Image_Subscriber('/down/left/image_raw', self.image_cb)
         self.image_pub = sub8_ros_tools.Image_Publisher("down/left/target_info")
+
+        # Occasional status publisher
         self.timer = rospy.Timer(rospy.Duration(1), self.publish_target_info)
 
         self.range = sub8_ros_tools.get_parameter_range('/color/channel_guide')
-        print self.range
-        print range_default
-
 
     def publish_target_info(self, *args):
         if self.last_image is None:
             return
+
         self.find_pipe(np.copy(self.last_image))
         if self.last_draw_image is not None:
             self.image_pub.publish(self.last_draw_image)
 
     def image_cb(self, image):
+        '''Hang on to last image'''
         self.last_image = image
+
+    def ncc(self, image, mean_thresh, scale=15):
+        '''Compute normalized cross correlation w.r.t a shadowed pillbox fcn
+
+        TODO:
+            Compute the kernel only once
+                (Too cheap to need)
+        '''
+        kernel = np.ones((scale, scale)) * -1
+        midpoint = (scale // 2, scale // 2)
+        cv2.circle(kernel, midpoint, midpoint[0], 1, -1)
+
+        mean, std_dev = cv2.meanStdDev(image)
+
+        # Check if the scene is brighter than our a priori target
+        if mean > mean_thresh:
+            kernel = -kernel
+
+        normalized_cross_correlation = cv2.filter2D((image - mean) / std_dev, -1, kernel)
+        renormalized = normalized_cross_correlation
+        return renormalized
 
     def find_pipe(self, img):
         rows, cols = img.shape[:2]
@@ -53,14 +61,20 @@ class PipeFinder:
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # mask = cv2.inRange(hsv, ORANGE_MIN, ORANGE_MAX)
-        mask = cv2.inRange(hsv, self.range[:, 0], self.range[:, 1])
+        channel = 2
+        norc = self.ncc(hsv[::2, ::2, channel], self.range[channel, 0])
+
+        color_mask = cv2.inRange(hsv, self.range[:, 0], self.range[:, 1])
+        ncc_mask = cv2.resize(np.uint8(norc > 0.9), None, fx=2, fy=2)
+
+        mask = color_mask & ncc_mask
+        # todo: use svm
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         blank_img = np.zeros((rows, cols), np.uint8)
         draw_image = np.copy(img)
 
-        if contours:
+        if len(contours) > 0:
 
             # sort contours by area (greatest --> least)
             contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
@@ -71,7 +85,6 @@ class PipeFinder:
                 box = cv2.cv.BoxPoints(rect)  # get corner coordinates of that rectangle
                 box = np.int0(box)            # convert coordinates to ints
 
-                # draw minAreaRect around pipe
                 cv2.drawContours(blank_img, [cnt], 0, (255, 255, 255), -1)
                 cv2.drawContours(draw_image, [cnt], 0, (255, 255, 255), -1)
 
@@ -105,12 +118,11 @@ class PipeFinder:
                 denom = np.linalg.norm(max_eigv) * np.linalg.norm(vert_vect)
                 angle_rad = np.arcsin(num / denom)
 
-                if angle_rad >= np.pi / 2:
-                    angle_rad -= np.pi / 2
-
                 cv2.line(draw_image, tuple(np.int0(center)), tuple(np.int0(center + (2 * max_eigv))), (0, 255, 30), 2)
                 cv2.line(draw_image, tuple(np.int0(center)), tuple(np.int0(center + (2 * min_eigv))), (0, 30, 255), 2)
 
+                norc_res = cv2.resize(norc, None, fx=2, fy=2)
+                draw_image[:, :, 0] = norc_res
                 self.last_draw_image = np.copy(draw_image)
                 return center, angle_rad
 
