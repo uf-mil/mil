fd0405833d5983cfb598d4af99de129c7d37f14d
Jacob Panikulam <jpanikulam@ufl.edu>
2016-04-24 19:57:53 -0400

    MISSIONS: Make down_5m down_2m

--- command/sub8_missions/missions/down_2m.py
+++ command/sub8_missions/missions/down_2m.py
@@ -3,6 +3,6 @@ from txros import util
 
 @util.cancellableInlineCallbacks
 def run(sub_singleton):
-    print "Dropping 5m"
-    yield sub_singleton.move.down(5.0).go()
+    print "Dropping 2m"
+    yield sub_singleton.move.down(2.0).go()
     print "Done!"
