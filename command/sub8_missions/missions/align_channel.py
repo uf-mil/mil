87dbece9347d74232b9bad9bf935b38e78bb12bb
Matthew Langford <matthew.langford95@gmail.com>
2016-04-27 00:11:49 -0400

    MARKER: Finish setup of mission provided marker is in FOV initially.

--- command/sub8_missions/missions/align_channel.py
+++ command/sub8_missions/missions/align_channel.py
@@ -1,4 +1,5 @@
 from txros import util
+from twisted.internet import defer
 import rospy
 import tf
 from sub8_ros_tools import normalize, clip_norm
@@ -10,13 +11,9 @@ import numpy as np
 def run(sub_singleton):
     # Right now we assume the marker is in our field of view. (Will add searching later)
     # Go to max height to see as much as we can.
+    rospy.sleep(1)
 
-    rospy.init_node('align_channel_mission')
-    tf_listener = tf.TransformListener()
-
-    yield rospy.sleep(2)  # allow TF to build a buffer.
-
-    iterations = 1
+    iterations = 2
     # yield sub_singleton.move.depth(.75).zero_roll_and_pitch().go()
 
     for i in range(iterations):
@@ -27,12 +24,12 @@ def run(sub_singleton):
             # Handle issues
             rospy.logerr("No response")
 
-        rel_position = yield transform_px_to_m(response, tf_listener)
+        rel_position = yield transform_px_to_m(response, sub_singleton._tf_listener)
         print rel_position
+        yield sub_singleton.move.relative([rel_position[0], rel_position[1], 0]).yaw_left(response.pose.theta).go()
 
-        yield sub_singleton.move.relative([rel_position[0], rel_position[1], 0]).go()
 
-
+@util.cancellableInlineCallbacks
 def transform_px_to_m(response, tf_listener):
     '''
     Finds the position of the marker in meters relative to the current position.
@@ -44,22 +41,23 @@ def transform_px_to_m(response, tf_listener):
     cam.fromCameraInfo(response.camera_info)
 
     # Calculate position of marker accounting for camera rotation in the camera frame.
-    tf_listener.waitForTransform("/ground", "/downward", timestamp, rospy.Duration(5.0))
-    trans, _ = tf_listener.lookupTransform("/ground", "/downward", timestamp)
-    height = trans[2]
+    transform = yield tf_listener.get_transform("/ground", "/downward", timestamp)
+    height = transform._p[2]
 
     dir_vector = unit_vector(np.array([cam.cx(), cam.cy()]) - [response.pose.x, response.pose.y])
-    trans, rot = tf_listener.lookupTransform("/map", "/downward", timestamp)
-    cam_rotation = tf.transformations.euler_from_quaternion(rot)[2] + np.pi / 2
+
+    transform = yield tf_listener.get_transform("/map", "/downward", timestamp)
+    cam_rotation = tf.transformations.euler_from_quaternion(transform._q)[2] + np.pi / 2
     dir_vector = np.dot(dir_vector, make_2D_rotation(cam_rotation))
 
     # Calculate distance from middle of frame to marker in meters.
-    mid_ray = unit_vector(cam.projectPixelTo3dRay((cam.cx(), cam.cy())))
+    mid_ray = np.array([0, 0, 1])
     marker_ray = unit_vector(cam.projectPixelTo3dRay([response.pose.x, response.pose.y]))
     theta = np.arccos(np.dot(mid_ray, marker_ray))
     magnitude = np.tan(theta) * height
     rel_position = dir_vector[::-1] * magnitude
-    return rel_position
+
+    defer.returnValue(rel_position)
 
 
 def unit_vector(vect):
