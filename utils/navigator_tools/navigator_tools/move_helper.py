7b2c8c94717f07e3d106cf4f895731a2706a30ff
Jason Nezvadovitz <nez.jason7@gmail.com>
2016-04-30 10:34:51 -0400

    MERGE: Lake Day 4/23/16 (#17)
    * ROS: cleanup
    
    * ROS: clean up, waypoint is a pose not a point
    
    * LAKE DAY: new gains, and now has option to use external trajectory generator
    
    * NEW NODE: launch c3 trajectory generator from sub
    
    * INTRODUCE: node for running neural net controller as alternative to MRAC
    
    * MRAC TGEN: modified kinematic limits to match lake tuning
    
    * DEFAULTS: make external c3 tgen the default
    
    * BUG FIX: reference frame issue when using external tgen, and weird list comprehension
    
    * BUG FIX: simulator would start with quaternion [0, 0, 0, 0]

--- utils/navigator_tools/navigator_tools/move_helper.py
+++ utils/navigator_tools/navigator_tools/move_helper.py
@@ -5,18 +5,17 @@ import roslib
 import numpy,math,tf,threading
 from tf import transformations as trns
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseStamped, Point
 from navigator_tools import geometry_helper as gh
 
 rospy.init_node('move_helper')
 
 class move_helper(object):
-    # Base class for whatever you are writing
+
     def __init__(self):
 
         self.odom = Odometry()
-        self.pose_pub = rospy.Publisher("/set_desired_pose", Point, queue_size = 1)
-
+        self.pose_pub = rospy.Publisher("/waypoint", PoseStamped, queue_size = 0)
         rospy.Subscriber("/odom", Odometry, self.odom_cb)
         rospy.Subscriber("/move_helper", Point, self.move_cb)
         rospy.spin()
@@ -26,7 +25,8 @@ class move_helper(object):
 
     def move_cb(self, msg):
 
-        to_send = Point()
+        to_send = PoseStamped()
+        to_send.header.frame_id = "/enu"
 
         R = trns.quaternion_matrix([self.odom.pose.pose.orientation.x, self.odom.pose.pose.orientation.y, self.odom.pose.pose.orientation.z, self.odom.pose.pose.orientation.w])[:2, :2]
         theta = gh.quat_to_euler(self.odom.pose.pose.orientation)
@@ -34,15 +34,16 @@ class move_helper(object):
         current = numpy.array([self.odom.pose.pose.position.x, self.odom.pose.pose.position.y, numpy.rad2deg(theta[2])])
         shift = numpy.concatenate((R.dot([msg.x, msg.y]), [msg.z]))
         desired = current + shift
+        desired_quaternion = trns.quaternion_from_euler(0, 0, numpy.deg2rad(desired[2]))
 
-        to_send.x = desired[0]
-        to_send.y = desired[1]
-        to_send.z = desired[2]
+        to_send.pose.position.x = desired[0]
+        to_send.pose.position.y = desired[1]
+        to_send.pose.orientation.x, to_send.pose.orientation.y, to_send.pose.orientation.z, to_send.pose.orientation.w  = desired_quaternion
 
         self.pose_pub.publish(to_send)
 
 
 
 if __name__ == "__main__":
-    #
+
     helper = move_helper()
