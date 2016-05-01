c217e905e1f89b3e12b0d24dfd9b2369ccfbddb7
Jacob Panikulam <jpanikulam@ufl.edu>
2016-05-01 04:22:13 -0400

    VISION: Add particle filter for multi-view posing
    - Given a sequence of single-projection observations, the filter will [quickly] converge to the true center of the object
    - The observation must be of the same 3d point every time, or something that lies along the ray between the camera and the 3d point
    
    - For example, we can filter the buoy center despite not being able to directly observe the buoy's 3d center, which is occluded by the exterior of the buoy

---
+++ gnc/sub8_perception/sub8_vision_tools/estimation.py
@@ -0,0 +1,260 @@
+from __future__ import division
+import image_geometry
+import numpy as np
+from scipy import stats
+from mayavi import mlab
+
+
+class ProjectionParticleFilter(object):
+    def __init__(self, camera_model, num_particles=10000, max_Z=25, aggressive=True):
+        """
+        :param camera_model: ros image_geometry PinholeCameraModel, must be initialized
+        :param num_particles: How many particles to use, we can operate comfortably with 100,000
+        :param max_Z: The maximum depth to search in
+        :param aggressive: Use a gaussian error distributions instead of error**2, much more aggressive
+            may sometimes throw probability did not sum to one errors (because it is too aggressive!)
+
+        Functions:
+        :function column_vectorize: Make a row vector into a column vector, and do nothing to a column vector
+
+        """
+        assert isinstance(camera_model, image_geometry.PinholeCameraModel), "Must be pinhole camera"
+        self.aggressive = aggressive
+        self.min_Z = 0.0
+        self.max_Z = max_Z
+
+        self.camera_model = camera_model
+        self.K = np.array(camera_model.fullIntrinsicMatrix(), dtype=np.float32)
+        self.K_inv = np.linalg.inv(self.K)
+
+        self.num_particles = num_particles
+        self.particles = np.random.uniform((-25, -25, self.min_Z), (25, 25, self.max_Z), size=(self.num_particles, 3)).transpose()
+        self.weights = np.ones(self.num_particles)
+        self.weights = self.weights / np.sum(self.weights)
+        self.t = np.zeros(3)
+        self.R = np.diag([1.0, 1.0, 1.0])
+        self.first_observation = True
+
+    def get_best(self):
+        ind = np.argmax(self.weights)
+        return self.particles[:, ind]
+
+    def column_vectorize(self, v):
+        return np.reshape(v, (len(v), 1))
+
+    def in_fov(self, pts):
+        projected_h = self.K.dot(pts)
+        not_infinite = projected_h[2, :] > 1e-5
+        projected = projected_h[:2, :] / projected_h[2, :]
+
+        # I acknowledge that the image edges are NOT guaranteed to be at (2cx, 2cy)
+        in_image = np.logical_and(
+            np.all(projected > self.column_vectorize((0.0, 0.0)), axis=0),
+            np.all(projected < self.column_vectorize((self.camera_model.cx() * 2, self.camera_model.cy() * 2)), axis=0)
+        )
+        in_front = pts[2, :] > 0
+        in_fov = np.logical_and(np.logical_and(
+            in_image, in_front
+        ), not_infinite)
+        print np.sum(not_infinite)
+        return in_fov
+
+    def set_pose(self, t, R):
+        assert R.shape == (3, 3), "Rotation must be 3x3 rotation matrix"
+        assert len(t) == 3, "Translation vector must be of length 3"
+        self.t = self.column_vectorize(t)
+        self.R = R
+
+    def _transform_pts(self, t, R):
+        """Supply a transform to the camera frame"""
+        particles = np.copy(np.dot(R.transpose(), self.particles) - R.transpose().dot(t))
+        return particles
+
+    def get_ray(self, observation):
+        """Returns a ray in camera frame pointing towards the observation
+
+            self.R.dot(observation) will give the ray in world frame
+            self.t would be the base of the ray
+        """
+        obs_column = self.column_vectorize(observation)
+        ray = self.K_inv.dot(np.vstack([obs_column, 1.0]))
+        unit_ray = ray / np.linalg.norm(ray)
+        return unit_ray
+
+    def _reset_to_ray(self, observation, weights=None):
+        unit_ray = self.R.dot(self.get_ray(observation))
+
+        if weights is None:
+            self.particles = self.t + (np.random.uniform(self.min_Z, self.max_Z, size=self.num_particles) * unit_ray)
+            self.particles += np.random.normal(scale=(0.1, 0.1, 0.1), size=(self.num_particles, 3)).transpose()
+
+        else:
+            average_p = 0.2 * np.average(weights)
+            self.particles[:, weights < average_p] = np.reshape(-self.t, (3, 1)) + np.random.uniform(
+                self.min_Z,
+                self.max_Z,
+                size=self.particles[:, weights < average_p]
+            ) * unit_ray
+
+        self.first_observation = False
+
+    def observe(self, observation):
+        """
+        observation should be a column vector
+
+        TODO
+            - Must shuffle instead of relying on state transition distribution
+        """
+        if self.first_observation:
+            self._reset_to_ray(observation)
+            self.first_observation = False
+
+        # compute p(z | x)
+        particles = self._transform_pts(self.t, self.R)
+        expected_obs_h = self.K.dot(particles)
+        infront = np.sum(self.in_fov(particles))
+        print 'In front:', infront
+        if infront < 100:
+            print "Not enough in front"
+            return
+        expected_obs = expected_obs_h[:2, :] / expected_obs_h[2, :]
+
+        error = np.linalg.norm(expected_obs - observation, axis=0)
+
+        if self.aggressive:
+            prob = stats.norm.pdf(error, scale=5.0) * stats.uniform.pdf(particles[2, :], loc=0.0, scale=self.max_Z)
+        else:
+            prob = np.power(error ** 2, -1)
+
+        normalized_prob = prob / np.sum(prob)
+
+        to_sample = int(self.num_particles)
+        choice_indices = np.random.choice(
+            a=np.arange(particles.shape[1]),
+            p=normalized_prob,
+            size=to_sample,
+            replace=True,
+        )
+
+        choices = self.particles[:, choice_indices]
+
+        self.particles = choices + np.random.normal(scale=(0.01, 0.01, 0.01), size=(self.num_particles, 3)).transpose()
+
+        new_weights = normalized_prob[choice_indices]
+        weights = new_weights
+        self.weights = weights / np.sum(weights)
+
+
+def draw_particles(ppf, color_hsv=(1.0, 0.2, 1.0), scale=0.05):
+    mlab.points3d(
+        ppf.particles[0, :][::20],
+        ppf.particles[1, :][::20],
+        ppf.particles[2, :][::20],
+        scale_factor=scale,
+        color=color_hsv,
+        colormap='hsv'
+    )
+    mlab.axes()
+
+
+def draw_cameras(observations, cameras):
+    for observation, camera in zip(observations, cameras):
+        t, R = camera
+        draw_camera(t, R)
+        draw_line(t, t + (R.dot(observation.flatten()) * 15), color=(0.0, 1.0, 0.0))
+
+
+def draw_camera(t, R):
+    colors = (
+        (1.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0),
+        (0.0, 0.0, 1.0),
+    )
+    for line, color in zip(R, colors):
+        draw_line(t, t + line, color=color)
+
+    mlab.points3d(
+        [t[0]],
+        [t[1]],
+        [t[2]],
+        color=(0.0, 1.0, 0.0),
+        scale_factor=0.3
+    )
+
+
+def draw_line(pt_1, pt_2, color=(1.0, 0.0, 0.0)):
+    mlab.plot3d(
+        np.hstack([pt_1[0], pt_2[0]]),
+        np.hstack([pt_1[1], pt_2[1]]),
+        np.hstack([pt_1[2], pt_2[2]]),
+        color=color
+    )
+
+
+def main():
+    import sub8_ros_tools
+    import time
+    import rospy
+    rospy.init_node('test_estimation')
+    q = sub8_ros_tools.Image_Subscriber('/stereo/left/image_rect_color')
+    while(q.camera_info is None):
+        time.sleep(0.1)
+
+    camera_model = image_geometry.PinholeCameraModel()
+    camera_model.fromCameraInfo(q.camera_info)
+
+    ppf = ProjectionParticleFilter(camera_model, 100000)
+    real = np.array([1.0, 3.0, 7.0])
+    projected_h = ppf.K.dot(real)
+    projected = projected_h[:2] / projected_h[2]
+
+    print 'starting'
+    R = np.diag([1.0, 1.0, 1.0])
+    camera_t = np.array([0.0, 0.0, 0.0])
+    cameras = []
+    observations = []
+
+    max_k = 3
+    for k in range(max_k):
+
+        if k < 1:
+            print 'doop'
+            camera_t = np.array([0.0, -8.0, 7.0])
+            R = np.array([
+                [1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0],
+                [0.0, -1.0, 0.0],
+            ])
+
+        else:
+            camera_t = np.hstack([(np.random.random(2) - 0.5) * 5, 0.0])
+
+        projected_h = ppf.K.dot(np.dot(R.transpose(), real) - R.transpose().dot(camera_t))
+        print projected_h
+        projected = projected_h[:2] / projected_h[2]
+
+        obs_final = projected + np.random.normal(scale=3.0, size=2)
+
+        cameras.append((camera_t, R))
+        observations.append(ppf.get_ray(obs_final))
+        draw_cameras(observations, cameras)
+        mlab.show()
+        draw_particles(ppf, color_hsv=((k + 1) / (max_k + 1), 0.7, 0.8), scale=0.1 * ((k + 1) / max_k))
+
+        ppf.set_pose(camera_t, R)
+        ppf.observe(np.reshape(obs_final, (2, 1)))
+
+    print 'cov:', np.diag(np.cov(ppf.particles))
+    best = ppf.get_best()
+
+    print 'best'
+    print best, best / np.linalg.norm(best)
+    print real, real / np.linalg.norm(real)
+    draw_cameras(observations, cameras)
+    draw_particles(ppf, color_hsv=((k + 1) / (max_k + 1), 0.7, 0.8), scale=0.1 * ((k + 1) / max_k))
+
+    mlab.show()
+
+
+if __name__ == '__main__':
+    main()
