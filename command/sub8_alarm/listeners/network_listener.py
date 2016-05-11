52c8913e6d479790657aa6ebce18f43e5ac823fd
Jacob Panikulam <jpanikul@gmail.com>
2016-05-11 19:54:22 -0400

    ALARM-LISTNERS: Move network-timeout to alarms

--- command/sub8_alarm/listeners/network_listener.py
+++ command/sub8_alarm/listeners/network_listener.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from kill_handling.broadcaster import KillBroadcaster
+from sub8_alarm import single_alarm
 
 
 class KeepAlive(object):
@@ -9,6 +9,14 @@ class KeepAlive(object):
         self.timeout = rospy.Duration(timeout)
         self.last_time = rospy.Time.now()
         self.sub = rospy.Subscriber('/keep_alive', String, self.got_keepalive, queue_size=1)
+        self.alarm_broadcaster, self.alarm = single_alarm('network-timeout', severity=1)
+        rospy.Timer(rospy.Duration(0.01), self.check)
+
+    def check(self, *args):
+        if self.need_kill():
+            self.alarm.raise_alarm()
+        else:
+            self.alarm.clear_alarm()
 
     def got_keepalive(self, msg):
         self.last_time = rospy.Time.now()
@@ -22,13 +30,5 @@ class KeepAlive(object):
 
 if __name__ == '__main__':
     rospy.init_node('network_kill')
-    # Interact with legacy kill stuff
-    kb = KillBroadcaster(id='network', description='Network timeout')
-    ka = KeepAlive(timeout=0.3)
-
-    while(not rospy.is_shutdown()):
-        rospy.sleep(2.0)
-        if ka.need_kill():
-            kb.send(active=True)
-        else:
-            kb.send(active=False)
+    ka = KeepAlive(timeout=0.1)
+    rospy.spin()
