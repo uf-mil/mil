afd60484501d73c8ed35d5e43e272f5545d1d124
Jacob Panikulam <jpanikul@gmail.com>
2016-05-11 19:55:28 -0400

    ALARMS: Fix existing alarm scenarios

--- command/sub8_alarm/sub8_alarm/alarms/kill.py
+++ command/sub8_alarm/sub8_alarm/alarms/kill.py
@@ -3,13 +3,13 @@ import rospy
 from kill_handling.broadcaster import KillBroadcaster
 
 
-class LegacyKill(object):
-    alarm_name = 'legacy_kill'
+class Handler(object):
+    alarm_name = 'kill'
 
     def __init__(self):
         # Keep some knowledge of which thrusters we have working
         self.dropped_thrusters = []
-        self.kb = KillBroadcaster(id='network', description='Network timeout')
+        self.kb = KillBroadcaster(id='alarm-kill', description='Kill by alarm')
 
     def handle(self, time_sent, parameters):
         self.kb.send(active=True)
