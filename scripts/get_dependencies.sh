1d1e5bb0742651bfe67522722cf97c749c35a629
Anthony Olive <anthony@iris-systems.net>
2016-05-18 19:17:48 -0400

    INSTALL: Fixed mayavi VTK dependency issue

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -31,6 +31,21 @@ instlog "Installing dependencies from the Ubuntu repositories"
 sudo apt-get update -qq
 sudo apt-get install -qq python-scipy python-pygame python-numpy python-serial
 sudo apt-get install -qq cmake binutils-dev python-pip
+sudo apt-get install -qq libvtk5-dev python-vtk
+sudo apt-get install -qq libboost-all-dev python-dev python-qt4-dev python-qt4-gl python-opengl freeglut3-dev libassimp-dev
+sudo apt-get install -qq libpcl-1.7-all libpcl-1.7-all-dev ros-indigo-pcl-conversions
+sudo apt-get install -qq libompl-dev
+sudo apt-get install -qq libusb-1.0-0-dev
+
+instlog "Installing ROS dependencies"
+sudo apt-get install -qq ros-indigo-gazebo7-msgs ros-indigo-gazebo7-ros ros-indigo-gazebo7-plugins ros-indigo-gazebo7-ros-control
+sudo apt-get install -qq ros-indigo-control-toolbox ros-indigo-controller-manager ros-indigo-transmission-interface ros-indigo-joint-limits-interface ros-indigo-hardware-interface
+sudo apt-get install -qq ros-indigo-sophus
+sudo apt-get install -qq ros-indigo-driver-base
+sudo apt-get install -qq ros-indigo-camera-info-manager
+sudo apt-get install -qq ros-indigo-spacenav-node
+sudo apt-get install -qq ros-indigo-camera1394
+sudo apt-get install -qq ros-indigo-stereo-image-proc
 
 instlog "Installing dependencies from Python PIP"
 sudo pip install -q -U setuptools
@@ -43,21 +58,5 @@ instlog "Getting ROS packages we need to install from source"
 ros_git_get https://github.com/txros/txros.git
 ros_git_get https://github.com/uf-mil/rawgps-tools.git
 ros_git_get https://github.com/ros-simulation/gazebo_ros_pkgs.git
-
-instlog "Installing ROS dependencies"
-sudo apt-get install -qq ros-indigo-gazebo7-msgs ros-indigo-gazebo7-ros ros-indigo-gazebo7-plugins ros-indigo-gazebo7-ros-control
-sudo apt-get install -qq ros-indigo-control-toolbox ros-indigo-controller-manager ros-indigo-transmission-interface ros-indigo-joint-limits-interface ros-indigo-hardware-interface
-sudo apt-get install -qq ros-indigo-sophus
-sudo apt-get install -qq ros-indigo-driver-base
-sudo apt-get install -qq ros-indigo-camera-info-manager
-sudo apt-get install -qq ros-indigo-spacenav-node
-sudo apt-get install -qq ros-indigo-camera1394
-sudo apt-get install -qq ros-indigo-stereo-image-proc
-
-instlog "Installing miscellaneous dependencies"
-sudo apt-get install -qq libboost-all-dev python-dev python-qt4-dev python-qt4-gl python-opengl freeglut3-dev libassimp-dev
-sudo apt-get install -qq libpcl-1.7-all libpcl-1.7-all-dev ros-indigo-pcl-conversions
-sudo apt-get install -qq libompl-dev
-sudo apt-get install -qq libusb-1.0-0-dev
 
 # catkin_make -C $INSTALL_FOLDER/..
