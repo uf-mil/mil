28b1df9738a4e3b2f1975aef5fb19794bb8109eb
Jacob Panikulam <jpanikul@gmail.com>
2016-05-22 01:29:22 -0400

    VISION: Add 2d multi-view pose estimator
    - Make buoys estimate 3d pose using 2d multiview

--- gnc/sub8_perception/sub8_vision_tools/__init__.py
+++ gnc/sub8_perception/sub8_vision_tools/__init__.py
@@ -2,4 +2,5 @@
 import visual_threshold_tools
 import threshold_tools
 from estimation import ProjectionParticleFilter
+from multi_observation import MultiObservation
 from marker_occ_grid import MarkerOccGrid
