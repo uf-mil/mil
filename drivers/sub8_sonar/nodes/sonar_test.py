0d91619026eb62f6e890537dfe681c1e76111f20
DSsoto <d.soto@ufl.edu>
2016-07-07 09:06:02 -0400

    SONAR: work on processing signals from Jake's board

--- drivers/sub8_sonar/nodes/sonar_test.py
+++ drivers/sub8_sonar/nodes/sonar_test.py
@@ -45,35 +45,38 @@ if __name__ == '__main__':
     # print "\t\x1b[33m".ljust(22) + error(res, pulse.position()) + "\x1b[0m"
 
     # pulses will be generated with inside a cube with side-length $(pulse_range) (mm)
-    for h in range(3,8):
-        # smallest cube will be a meter wide, largest will be 10 km wide
-        pulse_range = 10**h  # in mm
-        rand_args = [-pulse_range, pulse_range + 1]
-        num_pulses = 10
-        print "\n\x1b[1mGenerating " + str(num_pulses) + " pulses within a " \
-            + str(2*pulse_range/1000) + " meters wide cube\x1b[0m\n"
+    try:
+        for h in range(3,8):
+            # smallest cube will be a meter wide, largest will be 10 km wide
+            pulse_range = 10**h  # in mm
+            rand_args = [-pulse_range, pulse_range + 1]
+            num_pulses = 10
+            print "\n\x1b[1mGenerating " + str(num_pulses) + " pulses within a " \
+                + str(2*pulse_range/1000) + " meters wide cube\x1b[0m\n"
 
-        for i in range(num_pulses):
-            pulse = Pulse(random.randrange(*rand_args),
-                          random.randrange(*rand_args),
-                          random.randrange(*rand_args), 0)
-            tstamps = hydrophone_array.listen(pulse)
-            tstamps = tstamps - tstamps[0]
-            print_green(str(i).ljust(2) + str(pulse))
-            print "Perfect timestamps: (microseconds)\n\t", tstamps
-            res_msg = sonar.getPulseLocation(np.array(tstamps))
-            delete_last_lines(4)  # more concise output
-            res = np.array([res_msg.x, res_msg.y, res_msg.z])
-            print "\t\x1b[33m".ljust(22) + error(res, pulse.position()) + "\x1b[0m"
-            print "Progressively adding noise to timestamps..."
-
-            for j in range(-5, 2):
-                sigma = 10**j
-                noisy_tstamps = [x + np.random.normal(0, sigma) for x in tstamps]
-                noisy_tstamps[0] = 0
-                print "Noisy timestamps:\n\t", noisy_tstamps
-                res_msg = sonar.getPulseLocation(np.array(noisy_tstamps))
-                res = np.array([res_msg.x, res_msg.y, res_msg.z])
+            for i in range(num_pulses):
+                pulse = Pulse(random.randrange(*rand_args),
+                              random.randrange(*rand_args),
+                              random.randrange(*rand_args), 0)
+                tstamps = hydrophone_array.listen(pulse)
+                tstamps = tstamps - tstamps[0]
+                print_green(str(i).ljust(2) + str(pulse))
+                print "Perfect timestamps: (microseconds)\n\t", tstamps
+                res_msg = sonar.getPulseLocation(np.array(tstamps))
                 delete_last_lines(4)  # more concise output
-                print "\t\x1b[33m" + ("sigma: " +  str(sigma)).ljust(16) \
-                    + error(res, pulse.position()) + "\x1b[0m"
+                res = np.array([res_msg.x, res_msg.y, res_msg.z])
+                print "\t\x1b[33m".ljust(22) + error(res, pulse.position()) + "\x1b[0m"
+                print "Progressively adding noise to timestamps..."
+
+                for j in range(-5, 2):
+                    sigma = 10**j
+                    noisy_tstamps = [x + np.random.normal(0, sigma) for x in tstamps]
+                    noisy_tstamps[0] = 0
+                    print "Noisy timestamps:\n\t", noisy_tstamps
+                    res_msg = sonar.getPulseLocation(np.array(noisy_tstamps))
+                    res = np.array([res_msg.x, res_msg.y, res_msg.z])
+                    delete_last_lines(4)  # more concise output
+                    print "\t\x1b[33m" + ("sigma: " +  str(sigma)).ljust(16) \
+                        + error(res, pulse.position()) + "\x1b[0m"
+    except KeyboardInterrupt:
+        print "\nAborting mutilateration tests prematurely"
