638588423a5a0593c734d711f0657e2189f852c9
mattlangford <matthew.langford95@gmail.com>
2016-07-23 15:40:33 -0500

    Marker mission and TF changes (#171)
    * UTILS: Update __init__.
    
    * SIM: Add to services to common Job class, fix logging issues in job_runner.
    
    * MISSION: Add visualization for marker mission. Attempt to fix defer errors. Make mission work.
    
    * TF: Rotate down camera 180 degrees.
    
    * UTILS: Add stamp in make_header.
    
    * COMMAND: Add global speed limit for pose editor
    
    * MISSION: Switch buoy_2d to boost. Manually threshold follow_orange_pipes.
    
    * SIM: Fix job_runner bug. Add rviz sub model to duck.launch.
    
    * UTIL: Make image_dumper take single bag or folder of bags.
    
    * BUOYS: Get ready for pool day.
    
    * MARKER: Fixes for pool day.
    
    * MISSONS: Add toggles in missions for thresholding/boosting. Add classifier path.

--- utils/sub8_ros_tools/sub8_ros_tools/__init__.py
+++ utils/sub8_ros_tools/sub8_ros_tools/__init__.py
@@ -13,7 +13,7 @@ from msg_helpers import (
     make_header, odom_sub,
     rosmsg_to_numpy, pose_to_numpy, twist_to_numpy, odometry_to_numpy, posetwist_to_numpy,
     make_wrench_stamped, make_pose_stamped,
-    numpy_to_quaternion, numpy_pair_to_pose, numpy_to_point, numpy_quat_pair_to_pose
+    numpy_to_quaternion, numpy_pair_to_pose, numpy_to_point, numpy_quat_pair_to_pose, numpy_to_twist
 )
 from threading_helpers import thread_lock
 from geometry_helpers import (make_rotation, normalize, skew_symmetric_cross, deskew, compose_transformation,
