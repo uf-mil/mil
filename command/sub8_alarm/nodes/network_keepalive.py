f33c9e7d0b438a737e7c7fe8a8f9500359a2bf22
David Santiago Soto <d.soto@ufl.edu>
2016-07-29 10:10:42 -0700

    LEGACY SONAR (#179)
    * LAUNCH: configure perception nodes w/ yaml files
    
    * COMMAND: make autonomous mode easy to switch on and off
    
    * COMMAND: address PR175 comments
    
    * LEGACY SONAR: fix configuration parameters to match current sub

--- command/sub8_alarm/nodes/network_keepalive.py
+++ command/sub8_alarm/nodes/network_keepalive.py
@@ -4,12 +4,13 @@ from std_msgs.msg import String
 
 
 class KeepAlive(object):
-    def __init__(self):
+    def __init__(self, auto):
         self.pub = rospy.Publisher('/keep_alive', String, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(0.1), self.keepalive_pub)
 
     def keepalive_pub(self, *args):
-        if rospy.get_param('/autonomous'):
+        self.auto_mode = KeepAlive(rospy.get_param('/autonomous'))
+        if self.auto_mode:
             self.pub.publish(String('auto'))
         else:
             self.pub.publish(String('keep_alive'))
