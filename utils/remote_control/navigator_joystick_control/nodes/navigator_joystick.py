c338de58d089f2a6629fa889cea3ac4a02145be9
mattlangford <matthew.langford95@gmail.com>
2016-08-28 02:53:54 -0400

    RC: Add start button to prevent miscontrol. Remove docking mode.

--- utils/remote_control/navigator_joystick_control/nodes/navigator_joystick.py
+++ utils/remote_control/navigator_joystick_control/nodes/navigator_joystick.py
@@ -17,12 +17,42 @@ from sub8_alarm import AlarmBroadcaster
 rospy.init_node("joystick")
 
 class Joystick(object):
-    # Base class for whatever you are writing
     def __init__(self):
 
         self.force_scale = rospy.get_param("~force_scale")
         self.torque_scale = rospy.get_param("~torque_scale")
 
+        self.wrench_choices = itertools.cycle(['rc', 'autonomous'])
+        self.current_pose = Odometry()
+
+        self.alarm_broadcaster = AlarmBroadcaster()
+        self.kill_alarm = self.alarm_broadcaster.add_alarm(
+            name='kill',
+            action_required=True,
+            severity=0
+        )
+
+        # self.docking_alarm = self.alarm_broadcaster.add_alarm(
+        #     name='docking',
+        #     action_required=True,
+        #     severity=0
+        # )
+
+        self.wrench_pub = rospy.Publisher("/wrench/rc", WrenchStamped, queue_size=1)
+        self.kb = KillBroadcaster(id='station_hold', description='Resets Pose')
+        # rospy.wait_for_service('/change_wrench')
+        self.wrench_changer = rospy.ServiceProxy('/change_wrench', WrenchSelect)
+
+        rospy.Subscriber("joy", Joy, self.joy_cb)
+        self.reset()
+
+    def reset(self):
+        '''
+        Used to reset the state of the controller.
+        Sometimes when it disconnects then comes back online, the settings are all
+            out of wack.
+        '''
+        self.last_start = False
         self.last_change_mode = False
         self.last_station_hold_state = False
         self.last_kill = False
@@ -30,53 +60,39 @@ class Joystick(object):
         self.last_auto = False
 
         self.killed = False
-        self.docking = False
+        # self.docking = False
 
-        self.wrench_choices = itertools.cycle(['rc', 'autonomous'])
-
-        self.current_pose = Odometry()
-
-        self.alarm_broadcaster = AlarmBroadcaster()
-
-        self.kill_alarm = self.alarm_broadcaster.add_alarm(
-            name='kill',
-            action_required=True,
-            severity=0
-        )
-
-        self.docking_alarm = self.alarm_broadcaster.add_alarm(
-            name='docking',
-            action_required=True,
-            severity=0
-        )
-
-        self.wrench_pub = rospy.Publisher("/wrench/rc", WrenchStamped, queue_size=1)
-        self.kb = KillBroadcaster(id='station_hold', description='Reset Pose')
-        #rospy.wait_for_service('/change_wrench')
-        self.wrench_changer = rospy.ServiceProxy('/change_wrench', WrenchSelect)
-
-        rospy.Subscriber("joy", Joy, self.joy_cb)
-        rospy.Subscriber("odom", Odometry, self.odom_cb)
-
-    def odom_cb(self, msg):
-        self.current_pose = msg
+        wrench = WrenchStamped()
+        wrench.header.frame_id = "/base_link"
+        wrench.wrench.force.x = 0
+        wrench.wrench.force.y = 0
+        wrench.wrench.torque.z = 0
+        self.wrench_pub.publish(wrench)
 
     def joy_cb(self, joy):
 
         # Handle Button presses
+        start = bool(joy.buttons[7])
         change_mode = bool(joy.buttons[3])  # Y
         kill = bool(joy.buttons[2])  # X
         station_hold = bool(joy.buttons[0])  # A
-        docking = bool(joy.buttons[1])  # B
+        # docking = bool(joy.buttons[1])  # B
         rc_control = bool(joy.buttons[11])  # d-pad left
         auto_control = bool(joy.buttons[12])  # d-pad right
+
+        # Reset controller state
+        if start == 1 and start != self.last_start:
+            rospy.loginfo("Resetting controller state.")
+            self.reset()
+            self.kill_alarm.clear_alarm()
+            self.wrench_changer("rc")
 
         # Change vehicle mode
         if change_mode == 1 and change_mode != self.last_change_mode:
             mode = next(self.wrench_choices)
             rospy.loginfo("Changing Control Mode: {}".format(mode))
             self.wrench_changer(mode)
-        
+
         if rc_control == 1 and rc_control != self.last_rc:
             rospy.loginfo("Changing Control to RC")
             self.wrench_changer("rc")
@@ -105,24 +121,25 @@ class Joystick(object):
 
             self.killed = not self.killed
 
-        # Turn on docking mode
-        if docking == 1 and docking != self.last_docking_state:
-            rospy.loginfo("Toggling Docking")
+        # # Turn on docking mode
+        # if docking == 1 and docking != self.last_docking_state:
+        #     rospy.loginfo("Toggling Docking")
 
-            if self.docking:
-                self.docking_alarm.clear_alarm()
-            else:
-                self.docking_alarm.raise_alarm(
-                    problem_description='Docking kill from location: joystick'
-                )
+        #     if self.docking:
+        #         self.docking_alarm.clear_alarm()
+        #     else:
+        #         self.docking_alarm.raise_alarm(
+        #             problem_description='Docking kill from location: joystick'
+        #         )
 
-            self.docking = not self.docking
-            
+        #     self.docking = not self.docking
 
+
+        self.last_start = start
         self.last_change_mode = change_mode
         self.last_kill = kill
         self.last_station_hold_state = station_hold
-        self.last_docking_state = docking
+        # self.last_docking_state = docking
         self.last_auto_control = auto_control
         self.last_rc = rc_control
         self.last_auto = auto_control
