3bc8a401db044abdf7ce1fd2bb7e6ac559d035f8
mike <kma1660@gmail.com>
2016-09-03 19:48:15 -0400

    Basic alignment mission for finind nearby target

--- mission_control/navigator_missions/nav_missions/symbol_mission.py
+++ mission_control/navigator_missions/nav_missions/symbol_mission.py
@@ -7,33 +7,80 @@ import numpy
 from geometry_msgs.msg import Point
 import sensor_msgs.msg
 import nav_msgs.msg
-
+from navigator_tools import rosmsg_to_numpy
 import cv2
 
 from twisted.internet import defer
+error_threshold = 0.1
+width_proportion = 0.30;
+width_error_threshold = 0.2
 
+def boundingRect(points):
+    maxX = 0
+    minX = 2000
+    maxY = 0
+    minY = 2000
+    for i in range(len(points)):
+        if(points[i].x > maxX):
+            maxX = points[i].x;
+        if(points[i].y > maxY):
+            maxY = points[i].y;
+        if(points[i].x < minX):
+            minX = points[i].x;
+        if(points[i].y < minY):
+            minY = points[i].y;
+    return numpy.array([maxX, maxY, minX, minY])
+def area_of_rect(rect):
+    return (rect[1] - rect[2]) * (rect[1] - rect[3])
+  
 @txros.util.cancellableInlineCallbacks
 def main(navigator):
+  #shooterLoad = rospy.ServiceProxy("/shooter/load", std_srvs.srv.Trigger)
   #shooterFire = rospy.ServiceProxy("/shooter/fire", std_srvs.srv.Trigger)
   resp = yield navigator.vision_request("get_shape")
-  while not resp.found:
+  while not (resp.found and resp.symbol.img_width != 0):
     yield navigator.move.yaw_left(0.15).go();
     resp = yield navigator.vision_request("get_shape")
 
-  resp = yield navigator.vision_request("get_shape")
+  print "Found"
+  print "Loading"
   error = float(resp.symbol.CenterX)/resp.symbol.img_width - 0.5
   while abs(error) > error_threshold:
-      try:
-          resp = yield navigator.vision_request("get_shape")
-          error = float(resp.symbol.CenterX)/resp.symbol.img_width - 0.5
-          print error
-          if error < 0:
-              yield navigator.move.yaw_left(0.1).go()
-              print "Turning Left"
-          elif error > 0:
-              yield navigator.move.yaw_right(0.1).go()
-              print "Turning Right"
-      except:
-          print "Erorr excepted"
+    resp = yield navigator.vision_request("get_shape")
+    error = float(resp.symbol.CenterX)/resp.symbol.img_width - 0.5
+    print error
+    if error < 0:
+        print "Turning Left"
+        yield navigator.move.yaw_left(0.1).go()
+    elif error > 0:
+        print "Turning Right"
+        yield navigator.move.yaw_right(0.1).go()
+  print "Centerted"
+
+  #shoooterLoad()
+  
+  resp = yield navigator.vision_request("get_shape")
+  rect = boundingRect(resp.symbol.points)
+  width = (rect[0] - rect[2]) / resp.symbol.img_width
+  width_error = width - width_proportion
+  print "Width", width
+  while abs(width_error) > width_error_threshold:
+    resp = yield navigator.vision_request("get_shape")
+    if width_error < 0:
+      print "Moving Towards"
+      #yield navigator.move.right(1).go()
+      yield navigator.move.forward(1).go()
+    elif width_error > 0:
+      print "Moving Away"
+      #yield navigator.move.left(1).go()
+      yield navigator.move.backward(1).go()
+    resp = yield navigator.vision_request("get_shape")
+    rect = boundingRect(resp.symbol.points)
+    width = (rect[0] - rect[2]) / resp.symbol.img_width
+    width_error = width - width_proportion
+    
+  print "Correct Distance"
   yield navigator.move.go()
+  
+  print "Shooting"
   #shooterFire()
