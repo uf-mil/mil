tessbianchi/Navigator

SCAN THE CODE: optimizes the algorithm so that it doesn't have to check for every combination of four points

gnc/navigator_perception/helper/model.cc
@@ -1,9 +1,65 @@
 #include <navigator_vision_lib/model.h>
 
-Model::Model(float width, float height): width(width), height(height)
+Model::Model(float width, float height, int min_points): width(width), height(height), min_points(min_points)
 {
+    unused_distances.push_back(width);
+    unused_distances.push_back(height);
+    unused_distances.push_back(sqrt(pow(width, 2) + pow(height, 2)));
 }
 
 Model::~Model()
 {
+}
+
+bool Model::check_point(Eigen::Vector3d point)
+{
+    current_points.push_back(point);
+    if(current_points.size()  == 1)
+        {
+            return true;
+        }
+
+    Eigen::Vector3d starting_point = current_points[0];
+    Eigen::Vector3d diff = starting_point - point;
+    float dist_from_starting = sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2));
+    float err = ((width + height) / 2) * .05;
+    for(float dist : unused_distances)
+        {
+            float upper = dist + err;
+            float lower = dist - err;
+            if(dist_from_starting > lower && dist_from_starting < upper)
+                {
+                    unused_distances.erase(std::remove(unused_distances.begin(), unused_distances.end(), dist), unused_distances.end());
+                    point_to_distance.insert(std::pair<Eigen::Vector3d*,float>(&point, dist));
+                    if(current_points.size()  == min_points)
+                        {
+                            // This is supposed to copy current_poitns into a new element of potential points.
+                            potential_models.push_back(current_points);
+                        }
+                    return true;
+                }
+        }
+    return false;
+}
+
+void Model::remove_point(Eigen::Vector3d point)
+{
+    current_points.erase(std::remove(current_points.begin(), current_points.end(), point), current_points.end());
+    float dist = point_to_distance[&point];
+    unused_distances.push_back(dist);
+    point_to_distance.erase(&point);
+}
+
+bool Model::get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<Eigen::Vector3d>& model2d)
+{
+
+}
+
+bool Model::complete()
+{
+    if(current_points.size()  == 4)
+        {
+            return true;
+        }
+    return true;
 }

gnc/navigator_perception/include/navigator_vision_lib/model.h
@@ -1,16 +1,26 @@
 #ifndef MODEL_H
 #define MODEL_H
-#include <string>
 #include <vector>
+#include <math.h>
+#include <map>
+#include <Eigen/Core>
 
 class Model
 {
 public:
-    Model(float width, float height);
+    Model(float width, float height, int min_points);
     ~Model();
-     float width, height;
+    float width, height;
+    int min_points;
+    bool check_point(Eigen::Vector3d point);
+    void remove_point(Eigen::Vector3d point);
+    bool get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<Eigen::Vector3d>& model2d);
+    bool complete();
 
 private:
-
+    std::vector<std::vector<Eigen::Vector3d>> potential_models;
+    std::vector<Eigen::Vector3d> current_points;
+    std::vector<float> unused_distances;
+    std::map<Eigen::Vector3d *, float> point_to_distance;
 };
 #endif // MODEL_H

gnc/navigator_perception/include/navigator_vision_lib/scan_the_code_detector.hpp
@@ -45,77 +45,75 @@
 
 /*
   Warning:
-  Because of its multithreadedness, this class cannot be copy constructed. 
+  Because of its multithreadedness, this class cannot be copy constructed.
   For examlple, the following will not compile:
     ScanTheCodeDetector tb_detector = ScanTheCodeDetector();
   Do this instead:
     ScanTheCodeDetector tb_detector();
 */
 
-class ScanTheCodeDetector {
+class ScanTheCodeDetector
+{
 
 public:
-  ScanTheCodeDetector();
-  ~ScanTheCodeDetector();
+    ScanTheCodeDetector();
+    ~ScanTheCodeDetector();
 
-  // Public Variables
-  double image_proc_scale, feature_min_distance;
-  int diffusion_time, max_features, feature_block_size;
+    // Public Variables
+    double image_proc_scale, feature_min_distance;
+    int diffusion_time, max_features, feature_block_size;
 
 
 private:
+    StereoModelFitter* model_fitter;
 
-  double model_height = .381;
-  double model_width = .1905;
-  StereoModelFitter* model_fitter;
+    //ATTRIBUTES:
 
-  //ATTRIBUTES:
+    // ROS
+    ros::NodeHandle nh;
+    ros::ServiceServer detection_switch;
+    ros::ServiceClient pose_client;
+    image_transport::CameraSubscriber left_image_sub, right_image_sub;
+    image_transport::ImageTransport image_transport;
+    image_transport::Publisher debug_image_pub;
+    image_geometry::PinholeCameraModel left_cam_model, right_cam_model;
 
-  // ROS
-  ros::NodeHandle nh;
-  ros::ServiceServer detection_switch;
-  ros::ServiceClient pose_client;
-  image_transport::CameraSubscriber left_image_sub, right_image_sub;
-  image_transport::ImageTransport image_transport;
-  image_transport::Publisher debug_image_pub;
-  image_geometry::PinholeCameraModel left_cam_model, right_cam_model;
+    // Scan The Code Board detection will be attempted when true
+    bool active;
 
-  // Scan The Code Board detection will be attempted when true
-  bool active;
+    // Goes into sequential id for pos_est srv request
+    long long int run_id;
 
-  // Goes into sequential id for pos_est srv request
-  long long int run_id;
+    // To prevent invalid img pointers from being passed to toCvCopy (segfault)
+    boost::mutex left_mtx, right_mtx;
 
-  // To prevent invalid img pointers from being passed to toCvCopy (segfault)
-  boost::mutex left_mtx, right_mtx;
+    // RVIZ
+    sub::RvizVisualizer rviz;
 
-  // RVIZ
-  sub::RvizVisualizer rviz;
-
-  // DBG images will be generated and published when true
-  bool generate_dbg_img;
-  cv::Mat debug_image;
-  cv::Rect upper_left, upper_right, lower_left, lower_right;
+    // DBG images will be generated and published when true
+    bool generate_dbg_img;
+    cv::Mat debug_image;
+    cv::Rect upper_left, upper_right, lower_left, lower_right;
 
 
-  //METHODS:
+    //METHODS:
 
-  // Callbacks
-  bool detection_activation_switch(
-      sub8_msgs::TBDetectionSwitch::Request &req,
-      sub8_msgs::TBDetectionSwitch::Response &resp);
+    // Callbacks
+    bool detection_activation_switch(
+        sub8_msgs::TBDetectionSwitch::Request &req,
+        sub8_msgs::TBDetectionSwitch::Response &resp);
 
-  void left_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
-                           const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
-  void
-  right_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
-                       const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
+    void left_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
+                             const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
+    void
+    right_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
+                         const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
 
-  // Detection / Processing
-  void run();
-  void process_current_image();
-  void init_ros(std::stringstream& log_msg);
-  void validate_pictures(cv::Mat& current_image_left, cv::Mat& current_image_right, cv::Mat& processing_size_image_left, cv::Mat& processing_size_image_right);
+    // Detection / Processing
+    void run();
+    void process_current_image();
+    void init_ros(std::stringstream& log_msg);
+    void validate_pictures(cv::Mat& current_image_left, cv::Mat& current_image_right, cv::Mat& processing_size_image_left, cv::Mat& processing_size_image_right);
 
 
 

uf-mil/mil

gnc/navigator_perception/helper/model.cc
@@ -1,11 +1,66 @@
 #include <navigator_vision_lib/model.h>
 
-PerceptionModel::PerceptionModel(float width, float height)
-    : width(width),
-      height(height)
+
+PerceptionModel::PerceptionModel(float width, float height, int min_points): width(width), height(height), min_points(min_points)
 {
+    unused_distances.push_back(width);
+    unused_distances.push_back(height);
+    unused_distances.push_back(sqrt(pow(width, 2) + pow(height, 2)));
 }
 
 PerceptionModel::~PerceptionModel()
 {
+}
+
+bool PerceptionModel::check_point(Eigen::Vector3d point)
+{
+    current_points.push_back(point);
+    if(current_points.size()  == 1)
+        {
+            return true;
+        }
+
+    Eigen::Vector3d starting_point = current_points[0];
+    Eigen::Vector3d diff = starting_point - point;
+    float dist_from_starting = sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2));
+    float err = ((width + height) / 2) * .05;
+    for(float dist : unused_distances)
+        {
+            float upper = dist + err;
+            float lower = dist - err;
+            if(dist_from_starting > lower && dist_from_starting < upper)
+                {
+                    unused_distances.erase(std::remove(unused_distances.begin(), unused_distances.end(), dist), unused_distances.end());
+                    point_to_distance.insert(std::pair<Eigen::Vector3d*,float>(&point, dist));
+                    if(current_points.size()  == min_points)
+                        {
+                            // This is supposed to copy current_poitns into a new element of potential points.
+                            potential_models.push_back(current_points);
+                        }
+                    return true;
+                }
+        }
+    return false;
+}
+
+void PerceptionModel::remove_point(Eigen::Vector3d point)
+{
+    current_points.erase(std::remove(current_points.begin(), current_points.end(), point), current_points.end());
+    float dist = point_to_distance[&point];
+    unused_distances.push_back(dist);
+    point_to_distance.erase(&point);
+}
+
+bool PerceptionModel::get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<Eigen::Vector3d>& model2d)
+{
+
+}
+
+bool PerceptionModel::complete()
+{
+    if(current_points.size()  == 4)
+        {
+            return true;
+        }
+    return true;
 }

gnc/navigator_perception/include/navigator_vision_lib/model.h
@@ -1,14 +1,26 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <math.h>
+#include <map>
+#include <Eigen/Core>
 
 class PerceptionModel
 {
 public:
-    PerceptionModel(float width, float height);
+
+    PerceptionModel(float width, float height, int min_points);
     ~PerceptionModel();
-     float width, height;
+    float width, height;
+    int min_points;
+    bool check_point(Eigen::Vector3d point);
+    void remove_point(Eigen::Vector3d point);
+    bool get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<Eigen::Vector3d>& model2d);
+    bool complete();
 
 private:
-
+    std::vector<std::vector<Eigen::Vector3d>> potential_models;
+    std::vector<Eigen::Vector3d> current_points;
+    std::vector<float> unused_distances;
+    std::map<Eigen::Vector3d *, float> point_to_distance;
 };

gnc/navigator_perception/include/navigator_vision_lib/scan_the_code_detector.hpp
@@ -45,77 +45,77 @@
 
 /*
   Warning:
-  Because of its multithreadedness, this class cannot be copy constructed. 
+  Because of its multithreadedness, this class cannot be copy constructed.
   For examlple, the following will not compile:
     ScanTheCodeDetector tb_detector = ScanTheCodeDetector();
   Do this instead:
     ScanTheCodeDetector tb_detector();
 */
 
-class ScanTheCodeDetector {
+class ScanTheCodeDetector
+{
 
 public:
-  ScanTheCodeDetector();
-  ~ScanTheCodeDetector();
+    ScanTheCodeDetector();
+    ~ScanTheCodeDetector();
 
-  // Public Variables
-  double image_proc_scale, feature_min_distance;
-  int diffusion_time, max_features, feature_block_size;
+    // Public Variables
+    double image_proc_scale, feature_min_distance;
+    int diffusion_time, max_features, feature_block_size;
 
 
 private:
+    StereoModelFitter* model_fitter;
 
-  double model_height = .381;
-  double model_width = .1905;
-  StereoModelFitter* model_fitter;
+    //ATTRIBUTES:
 
-  //ATTRIBUTES:
+    // ROS
+    ros::NodeHandle nh;
+    ros::ServiceServer detection_switch;
+    ros::ServiceClient pose_client;
+    image_transport::CameraSubscriber left_image_sub, right_image_sub;
+    image_transport::ImageTransport image_transport;
+    image_transport::Publisher debug_image_pub;
+    image_geometry::PinholeCameraModel left_cam_model, right_cam_model;
 
-  // ROS
-  ros::NodeHandle nh;
-  ros::ServiceServer detection_switch;
-  ros::ServiceClient pose_client;
-  image_transport::CameraSubscriber left_image_sub, right_image_sub;
-  image_transport::ImageTransport image_transport;
-  image_transport::Publisher debug_image_pub;
-  image_geometry::PinholeCameraModel left_cam_model, right_cam_model;
+    // Scan The Code Board detection will be attempted when true
+    bool active;
 
-  // Scan The Code Board detection will be attempted when true
-  bool active;
+    // Goes into sequential id for pos_est srv request
+    long long int run_id;
 
-  // Goes into sequential id for pos_est srv request
-  long long int run_id;
+    // To prevent invalid img pointers from being passed to toCvCopy (segfault)
+    boost::mutex left_mtx, right_mtx;
 
-  // To prevent invalid img pointers from being passed to toCvCopy (segfault)
-  boost::mutex left_mtx, right_mtx;
+    // RVIZ
+    sub::RvizVisualizer rviz;
 
   // RVIZ
   nav::RvizVisualizer rviz;
+    // DBG images will be generated and published when true
+    bool generate_dbg_img;
+    cv::Mat debug_image;
+    cv::Rect upper_left, upper_right, lower_left, lower_right;
 
-  // DBG images will be generated and published when true
-  bool generate_dbg_img;
-  cv::Mat debug_image;
-  cv::Rect upper_left, upper_right, lower_left, lower_right;
 
+    //METHODS:
 
-  //METHODS:
+    // Callbacks
+    bool detection_activation_switch(
+        sub8_msgs::TBDetectionSwitch::Request &req,
+        sub8_msgs::TBDetectionSwitch::Response &resp);
 
-  // Callbacks
-  bool detection_activation_switch(
-      sub8_msgs::TBDetectionSwitch::Request &req,
-      sub8_msgs::TBDetectionSwitch::Response &resp);
+    void left_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
+                             const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
+    void
+    right_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
+                         const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
 
-  void left_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
-                           const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
-  void
-  right_image_callback(const sensor_msgs::ImageConstPtr &image_msg_ptr,
-                       const sensor_msgs::CameraInfoConstPtr &info_msg_ptr);
-
-  // Detection / Processing
-  void run();
-  void process_current_image();
-  void init_ros(std::stringstream& log_msg);
-  void validate_frame(cv::Mat& current_image_left, cv::Mat& current_image_right, cv::Mat& processing_size_image_left, cv::Mat& processing_size_image_right);
+    // Detection / Processing
+    void run();
+    void process_current_image();
+    void init_ros(std::stringstream& log_msg);
+    void validate_frame(cv::Mat& current_image_left, cv::Mat& current_image_right, cv::Mat& processing_size_image_left, cv::Mat& processing_size_image_right);
 
 
 