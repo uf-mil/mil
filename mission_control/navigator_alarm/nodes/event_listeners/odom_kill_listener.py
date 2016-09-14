06a7bfbcb29744eaccbc95645e72d1e28150ecd2
DSsoto <d.soto@ufl.edu>
2016-09-14 18:36:03 -0400

    ALARMS: make fixes to odom kill

--- mission_control/navigator_alarm/nodes/event_listeners/odom_kill_listener.py
+++ mission_control/navigator_alarm/nodes/event_listeners/odom_kill_listener.py
@@ -4,6 +4,7 @@ from std_msgs.msg import String
 from navigator_alarm import single_alarm
 from twisted.internet import reactor
 import time
+import nav_msgs.msg import Odometry
 
 
 class OdomKillListener(object):
@@ -14,9 +15,10 @@ class OdomKillListener(object):
     def __init__(self, timeout=0.5):
         self.timeout = rospy.Duration(timeout)
         self.last_time = rospy.Time.now()
-        self.last_msg = None
+        self.last_position = None
+        self.max_jump = 1.0
+        self.odom_discontinuity = False
         self.sub = rospy.Subscriber('/odom', String, self.got_network_msg, queue_size=1)
-
         self.alarm_broadcaster, self.alarm = single_alarm('kill', severity=3,
                                                           problem_description="Fatal state estimation error")
         rospy.Timer(rospy.Duration(0.1), self.check)
@@ -29,13 +31,21 @@ class OdomKillListener(object):
             self.alarm.clear_alarm()
 
     def got_odom_msg(self, msg):
-        self.last_msg = msg.data
+        self.odom_discontinuity = not check_continuity(msg)
+        self.last_position = msg.pose.pose.position
         self.last_time = rospy.Time.now()
 
     def need_kill(self):
-        return ((rospy.Time.now() - self.last_time) > self.timeout)
+        odom_loss =  ((rospy.Time.now() - self.last_time) > self.timeout)
+        return odom_loss or self.odom_discontinuity
 
-
+    def check_continuity(new_odom_msg):
+        this_p = new_odom_msg.pose.pose.position
+        last_p = self.last_position
+        d_sq = (this_p.x - last_p.x) ** 2 + (this_p.y - last_p.y) ** 2
+        return d_sq ** 0.5 > self.max_jump
+       
+        
 if __name__ == '__main__':
     rospy.init_node('odom_kill')
     mattfucious = OdomKillListener()
