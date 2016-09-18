5a9ebdea7153a14c0062dbe6fba37d5516668e10
Kevin Allen <kma1660@gmail.com>
2016-09-18 13:37:29 -0400

    many syntax error fixes

--- mission_control/navigator_missions/nav_missions/detect_deliver.py
+++ mission_control/navigator_missions/nav_missions/detect_deliver.py
@@ -19,9 +19,9 @@ class DetectDeliverMission:
   width_proportion = 0.15 #Desired proportion of frame width symbol is present in
   width_error_threshold = 0.03
   resp = None
-  def __init__(self,navigator)
-    self.center_error = 1;
-    self.width_error = 1;
+  def __init__(self,navigator):
+    self.center_error = 1
+    self.width_error = 1
     self.navigator = navigator
   def bounding_rect(points):
       maxX = 0
@@ -39,62 +39,63 @@ class DetectDeliverMission:
               minY = points[i].y
       return np.array([maxX, maxY, minX, minY])
   @txros.util.cancellableInlineCallbacks
-  def isFound():
+  def isFound(self):
     self.resp = yield self.navigator.vision_request("get_shape")
-    return self.resp.found and self.resp.symbol.img_width != 0
+    yield self.resp.found and self.resp.symbol.img_width != 0
   @txros.util.cancellableInlineCallbacks
-  def isCentered():
-    if (!self.isFound()) return False
+  def isCentered(self):
+    if not (yield self.isFound()):
+      yield False
     center = float(self.resp.symbol.CenterX) / self.resp.symbol.img_width
     self.center_error = center - 0.5
     print "Center ", center
-    return abs(self.center_error) < self.error_threshold:
+    yield abs(self.center_error) < self.error_threshold
   @txros.util.cancellableInlineCallbacks
-  def isCorrectDistance():
-    if (!self.isFound()) return False
+  def isCorrectDistance(self):
+    if not (yield self.isFound()):
+      yield False
     rect = self.bounding_rect(self.resp.symbol.points)
     width = (rect[0] - rect[2]) / self.resp.symbol.img_width
     self.width_error = width - self.width_proportion
     print "Width ", width
-    return abs(self.width_error) < self.width_error_threshold:
+    yield abs(self.width_error) < self.width_error_threshold
   @txros.util.cancellableInlineCallbacks
-  def shootAllBalls():
+  def shootAllBalls(self):
     for i in range(0,3):
       time.sleep(3)
       self.shooterLoad()
       time.sleep(5)
       self.shooterFire()
   @txros.util.cancellableInlineCallbacks
-  def findAndShoot():
-    if not yield self.isFound():
+  def findAndShoot(self):
+    if not (yield self.isFound()):
       self.navigator.move.yaw_left(360, "deg").go()
-      while not yield self.isFound():
-        print ".",
+      while not (yield self.isFound()):
+        print "...Searching for target"
     print "Found"
     yield self.navigator.move.go() #Stop moving once shape seen
-    if not yield self.isCentered()
-       if self.center_error < 0:
-          print "Turning Left"
-          self.navigator.move.yaw_left(180,"deg").go()
+    if not (yield self.isCentered()):
+      if self.center_error < 0:
+        print "Turning Left"
+        self.navigator.move.yaw_left(180,"deg").go()
       elif self.center_error > 0:
-          print "Turning Right"
-          self.navigator.move.yaw_right(180,"deg").go()
-      while not yield self.isCentered():
-        print ".",
+        print "Turning Right"
+        self.navigator.move.yaw_right(180,"deg").go()
+      while not (yield self.isCentered()):
+        print "...Centering on target"
     print "Centered"
     yield self.navigator.move.go() #Stop moving once shape seen
-    if not yield self.isCorrectDistance()
+    if not (yield self.isCorrectDistance()):
       if self.width_error < 0:
         print "Moving Towards"
         self.navigator.move.right(50).go()
       elif self.width_error > 0:
         print "Moving Away"
         self.navigator.move.left(50).go()
-      while not yield self.isCorrectDistance():
-        print ".",
+      while not (yield self.isCorrectDistance()):
+        print "...Moving to correct distance"
     yield self.navigator.move.go() #Stop moving once centered
     print "Correct Distance"
-    yield self.navigator.move.go()
     
     print "Shooting"
     self.shootAllBalls()
