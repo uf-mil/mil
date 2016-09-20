tessbianchi/Navigator

SCAN THE CODE: code clean up, and final param tweaks

gnc/navigator_perception/helper/model.cc
@@ -18,52 +18,22 @@ void Model::clear(){
 
 bool Model::check_point(Eigen::Vector3d point, cv:: Mat img, cv::Matx34d left_cam_mat, bool check)
 {
-//    std::cout<<"check point"<<std::endl;
     current_points.push_back(point);
-//    for(Eigen::Vector3d point : current_points){
-//        std::cout << "[ " << point(0) << ", "  << point(1) << ", " << point(2) << "]" << std::endl;
-//    }
-
-//    std::cout<<"size"<<current_points.size()<<std::endl;
-//    for(float dist : unused_distances){
-//        std::cout << dist << ", ";
-//    }
-//    std::cout<<std::endl;
-
     if(current_points.size()  == 1)
     {
         return true;
     }
-
-
-
     Eigen::Vector3d starting_point = current_points[0];
     Eigen::Vector3d diff = starting_point - point;
     float dist_from_starting = sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2));
-//    std::cout<<"dist_from_starting"<<std::endl;
-//    std::cout<<dist_from_starting<<std::endl;
-
-//    for(Eigen::Vector3d point : current_points){
-//      std::cout<<point(0)<<point(1)<<point(2)<<std::endl;
-//    }
-
-    if(check){
-      //visualize_points(current_points, img, left_cam_mat, "poo");
-    }
     bool ans = false;
     double mindist = 100;
     double minel = -1;
-//    std::cout<<"-------"<<std::endl;
     for(float dist : unused_distances)
     {
         float err = dist * .4;
         float upper = dist + err;
         float lower = dist - err;
-
-
-//        std::cout<<"dist"<< dist<<std::endl;
-//        std::cout<<"Upper"<< upper<<std::endl;
-//        std::cout<<"lower"<< lower<<std::endl;
 
         if(dist_from_starting > lower && dist_from_starting < upper)
         {
@@ -79,17 +49,11 @@ bool Model::check_point(Eigen::Vector3d point, cv:: Mat img, cv::Matx34d left_ca
             point_to_distance.insert(std::pair<std::string,float>(ss.str(), dist));
             if(current_points.size()  == min_points)
             {
-                // This is supposed to copy current_points into a new element of potential points.
                 potential_models.push_back(current_points);
-//                std::cout<<"ADD MODEL"<<std::endl;
             }
             ans = true;
         }
     }
-//    std::cout<<"-------"<<std::endl;
-//    std::cout<<" "<<std::endl;
-//    std::cout<<" "<<std::endl;
-//    std::cout<<" "<<std::endl;
     if(ans){
        unused_distances.erase(std::remove(unused_distances.begin(), unused_distances.end(), minel), unused_distances.end());
     }
@@ -132,17 +96,12 @@ void Model::visualize_points(std::vector<Eigen::Vector3d>  feature_pts_3d, cv:: 
     cv::waitKey(33);
 }
 
-bool Model::get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<cv::Point>& model2d, cv::Mat picture, cv::Matx34d left_cam_mat)
+bool Model::get_model(std::vector<Eigen::Vector3d>& model3d, cv::Mat picture, cv::Matx34d left_cam_mat)
 {
-//    std::cout<<"POTENTIAL MODELS"<<std::endl;
-//  std::cout<< potential_models.size()<<std::endl;
-       double min_cost = 100000;
+    double min_cost = 100000;
     std::vector<Eigen::Vector3d> min_model;
 
-
-
     for(std::vector<Eigen::Vector3d> mymodel: potential_models){
-        //visualize_points(mymodel, picture, left_cam_mat, "models");
         double cost = 0;
         for(int i = 0; i != 4; ++i){
           Eigen::Vector3d a = mymodel[i];
@@ -174,27 +133,21 @@ bool Model::get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<cv::Poi
           min_cost = cost;
           min_model = mymodel;
         }
-        //std::cout<<cost<<std::endl;
     }
     model3d = min_model;
-
-//    std::cout<<"DA BEST MODEL"<<std::endl;
-//    std::cout<< min_cost<<std::endl;
     if(potential_models.size() != 0 && min_cost < 4){
-      //visualize_points(min_model, picture, left_cam_mat, "WINNER");
+      visualize_points(min_model, picture, left_cam_mat, "WINNER");
     }
-
-
     return true;
 }
 
 bool Model::complete()
 {
     if(current_points.size()  == 4)
-        {
-            return true;
-        }
-    return true;
+    {
+        return true;
+    }
+    return false;
 }
 
 int Model::get_furthest_point(std::vector<Eigen::Vector3d> mymodel, int point){
@@ -212,32 +165,3 @@ int Model::get_furthest_point(std::vector<Eigen::Vector3d> mymodel, int point){
   return max;
 }
 
-int Model::which_point( Eigen::Vector3d a,  Eigen::Vector3d b){
-  Eigen::Vector3d diff = a - b;
-  float dist_from_starting = sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2));
-  float min_err = 1000;
-  float min = -1;
-  for(float dist : unused_distances)
-  {
-      float err = dist * .2;
-      float upper = dist + err;
-      float lower = dist - err;
-
-      float min_ans = -1;
-      if(dist_from_starting > lower && dist_from_starting < upper)
-      {
-        if(fabs(dist_from_starting-dist) < min_err){
-          min_err = fabs(dist_from_starting-dist);
-          min = dist;
-        }
-      }
-  }
-  if(min == width){
-    return 1;
-  }
-  if(min == height){
-    return 2;
-  }else{
-    return 3;
-  }
-}

gnc/navigator_perception/scan_the_code_detector.cc
@@ -51,8 +51,8 @@ void ScanTheCodeDetector::validate_pictures(Mat& current_image_left, Mat& curren
     // Prevent segfault if service is called before we get valid img_msg_ptr's
     if (left_most_recent.image_msg_ptr == NULL || right_most_recent.image_msg_ptr == NULL)
         {
-            ROS_WARN("Torpedo Board Detector: Image Pointers are NULL.");
-            throw "ROS ERROR";
+            throw "Torpedo Board Detector: Image Pointers are NULL.";
+
         }
     double sync_thresh = 0.5;
     // Get the most recent frames and camera info for both cameras
@@ -62,10 +62,6 @@ void ScanTheCodeDetector::validate_pictures(Mat& current_image_left, Mat& curren
 
             left_mtx.lock();
             right_mtx.lock();
-
-            Matx34d left_cam_mat = left_cam_model.fullProjectionMatrix();
-
-            //cout << "A = "<< endl << " "  << left_cam_mat << endl << endl;
 
             // Left Camera
             input_bridge = cv_bridge::toCvCopy(left_most_recent.image_msg_ptr,
@@ -77,8 +73,7 @@ void ScanTheCodeDetector::validate_pictures(Mat& current_image_left, Mat& curren
                    image_proc_scale, image_proc_scale);
             if (current_image_left.channels() != 3)
                 {
-                    ROS_ERROR("The left image topic does not contain a color image.");
-                    throw "ROS ERROR";
+                    throw "The left image topic does not contain a color image.";
                 }
 
             // Right Camera
@@ -90,8 +85,7 @@ void ScanTheCodeDetector::validate_pictures(Mat& current_image_left, Mat& curren
                    image_proc_scale, image_proc_scale);
             if (current_image_right.channels() != 3)
                 {
-                    ROS_ERROR("The right image topic does not contain a color image.");
-                    throw "ROS ERROR";
+                    throw "The right image topic does not contain a color image.";
                 }
 
             left_mtx.unlock();
@@ -132,8 +126,8 @@ void ScanTheCodeDetector::init_ros(stringstream& log_msg)
     string dbg_topic_default = "/scan_the_code/dbg_imgs";
     string pose_est_srv_default = "/scan_the_code/pose_est_srv";
     float image_proc_scale_default = 0.5;
-    int diffusion_time_default = 20;
-    int max_features_default = 20;
+    int diffusion_time_default = 5;
+    int max_features_default = 35;
     int feature_block_size_default = 11;
     float feature_min_distance_default = 15.0;
     bool generate_dbg_img_default = true;
@@ -228,48 +222,41 @@ void ScanTheCodeDetector::run()
                 }
             loop_rate.sleep();
         }
+
     return;
 }
 
 void ScanTheCodeDetector::process_current_image()
 {
-    Eigen::Vector3d position;
     Mat current_image_left, current_image_right, processing_size_image_left,
         processing_size_image_right;
-
-
     try
-        {
-            validate_pictures(current_image_left, current_image_right,
-                              processing_size_image_left, processing_size_image_right);
-        }
+    {
+        validate_pictures(current_image_left,
+                          current_image_right,
+                          processing_size_image_left,
+                          processing_size_image_right);
+    }
+
     catch(const char* msg)
-        {
-            return;
-        }
+    {
+        ROS_ERROR(msg);
+        return;
+    }
 
     Matx34d left_cam_mat = left_cam_model.fullProjectionMatrix();
     Matx34d  right_cam_mat = right_cam_model.fullProjectionMatrix();
-
-    ++count;
-    clock_t begin = clock();
-
-    bool ret = model_fitter->determine_model_position(position,
-               max_features, feature_block_size,
-               feature_min_distance,
-               image_proc_scale, diffusion_time,
-               current_image_left, current_image_right,
-               left_cam_mat, right_cam_mat);
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    avg = (avg * (count-1) + elapsed_secs)/count;
-
-    if(count == 300){
-      std::cout<<avg<<std::endl;
-      exit(0);
-    }
-
-
+    vector<Eigen::Vector3d> position;
+    model_fitter->determine_model_position(position,
+                                           max_features,
+                                           feature_block_size,
+                                           feature_min_distance,
+                                           image_proc_scale,
+                                           diffusion_time,
+                                           current_image_left,
+                                           current_image_right,
+                                           left_cam_mat,
+                                           right_cam_mat);
 }
 
 bool ScanTheCodeDetector::detection_activation_switch(

uf-mil/mil

gnc/navigator_perception/helper/model.cc
@@ -20,52 +20,22 @@ void PerceptionModel::clear(){
 bool PerceptionModel::check_point(Eigen::Vector3d point, cv:: Mat img, cv::Matx34d left_cam_mat, bool check)
 
 {
-//    std::cout<<"check point"<<std::endl;
     current_points.push_back(point);
-//    for(Eigen::Vector3d point : current_points){
-//        std::cout << "[ " << point(0) << ", "  << point(1) << ", " << point(2) << "]" << std::endl;
-//    }
-
-//    std::cout<<"size"<<current_points.size()<<std::endl;
-//    for(float dist : unused_distances){
-//        std::cout << dist << ", ";
-//    }
-//    std::cout<<std::endl;
-
     if(current_points.size()  == 1)
     {
         return true;
     }
-
-
-
     Eigen::Vector3d starting_point = current_points[0];
     Eigen::Vector3d diff = starting_point - point;
     float dist_from_starting = sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2));
-//    std::cout<<"dist_from_starting"<<std::endl;
-//    std::cout<<dist_from_starting<<std::endl;
-
-//    for(Eigen::Vector3d point : current_points){
-//      std::cout<<point(0)<<point(1)<<point(2)<<std::endl;
-//    }
-
-    if(check){
-      //visualize_points(current_points, img, left_cam_mat, "poo");
-    }
     bool ans = false;
     double mindist = 100;
     double minel = -1;
-//    std::cout<<"-------"<<std::endl;
     for(float dist : unused_distances)
     {
         float err = dist * .4;
         float upper = dist + err;
         float lower = dist - err;
-
-
-//        std::cout<<"dist"<< dist<<std::endl;
-//        std::cout<<"Upper"<< upper<<std::endl;
-//        std::cout<<"lower"<< lower<<std::endl;
 
         if(dist_from_starting > lower && dist_from_starting < upper)
         {
@@ -81,17 +51,11 @@ bool PerceptionModel::check_point(Eigen::Vector3d point, cv:: Mat img, cv::Matx3
             point_to_distance.insert(std::pair<std::string,float>(ss.str(), dist));
             if(current_points.size()  == min_points)
             {
-                // This is supposed to copy current_points into a new element of potential points.
                 potential_models.push_back(current_points);
-//                std::cout<<"ADD MODEL"<<std::endl;
             }
             ans = true;
         }
     }
-//    std::cout<<"-------"<<std::endl;
-//    std::cout<<" "<<std::endl;
-//    std::cout<<" "<<std::endl;
-//    std::cout<<" "<<std::endl;
     if(ans){
        unused_distances.erase(std::remove(unused_distances.begin(), unused_distances.end(), minel), unused_distances.end());
     }
@@ -134,17 +98,13 @@ void PerceptionModel::visualize_points(std::vector<Eigen::Vector3d>  feature_pts
     cv::waitKey(33);
 }
 
-bool PerceptionModel::get_model(std::vector<Eigen::Vector3d>& model3d, std::vector<cv::Point>& model2d, cv::Mat picture, cv::Matx34d left_cam_mat)
+
+bool PerceptionModel::get_model(std::vector<Eigen::Vector3d>& model3d, cv::Mat picture, cv::Matx34d left_cam_mat)
 {
-//    std::cout<<"POTENTIAL MODELS"<<std::endl;
-//  std::cout<< potential_models.size()<<std::endl;
-       double min_cost = 100000;
+    double min_cost = 100000;
     std::vector<Eigen::Vector3d> min_model;
 
-
-
     for(std::vector<Eigen::Vector3d> mymodel: potential_models){
-        //visualize_points(mymodel, picture, left_cam_mat, "models");
         double cost = 0;
         for(int i = 0; i != 4; ++i){
           Eigen::Vector3d a = mymodel[i];
@@ -176,27 +136,21 @@ bool PerceptionModel::get_model(std::vector<Eigen::Vector3d>& model3d, std::vect
           min_cost = cost;
           min_model = mymodel;
         }
-        //std::cout<<cost<<std::endl;
     }
     model3d = min_model;
-
-//    std::cout<<"DA BEST MODEL"<<std::endl;
-//    std::cout<< min_cost<<std::endl;
     if(potential_models.size() != 0 && min_cost < 4){
-      //visualize_points(min_model, picture, left_cam_mat, "WINNER");
+      visualize_points(min_model, picture, left_cam_mat, "WINNER");
     }
-
-
     return true;
 }
 
 bool PerceptionModel::complete()
 {
     if(current_points.size()  == 4)
-        {
-            return true;
-        }
-    return true;
+    {
+        return true;
+    }
+    return false;
 }
 
 int PerceptionModel::get_furthest_point(std::vector<Eigen::Vector3d> mymodel, int point){
@@ -212,34 +166,4 @@ int PerceptionModel::get_furthest_point(std::vector<Eigen::Vector3d> mymodel, in
     }
   }
   return max;
-}
-
-int PerceptionModel::which_point( Eigen::Vector3d a,  Eigen::Vector3d b){
-  Eigen::Vector3d diff = a - b;
-  float dist_from_starting = sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2));
-  float min_err = 1000;
-  float min = -1;
-  for(float dist : unused_distances)
-  {
-      float err = dist * .2;
-      float upper = dist + err;
-      float lower = dist - err;
-
-      float min_ans = -1;
-      if(dist_from_starting > lower && dist_from_starting < upper)
-      {
-        if(fabs(dist_from_starting-dist) < min_err){
-          min_err = fabs(dist_from_starting-dist);
-          min = dist;
-        }
-      }
-  }
-  if(min == width){
-    return 1;
-  }
-  if(min == height){
-    return 2;
-  }else{
-    return 3;
-  }
 }

gnc/navigator_perception/scan_the_code_detector.cc
@@ -50,8 +50,8 @@ void ScanTheCodeDetector::validate_frame(Mat& current_image_left, Mat& current_i
     // Prevent segfault if service is called before we get valid img_msg_ptr's
     if (left_most_recent.image_msg_ptr == NULL || right_most_recent.image_msg_ptr == NULL)
         {
-            ROS_WARN("Torpedo Board Detector: Image Pointers are NULL.");
-            throw "ROS ERROR";
+            throw "Torpedo Board Detector: Image Pointers are NULL.";
+
         }
     double sync_thresh = 0.5;
     // Get the most recent frames and camera info for both cameras
@@ -61,10 +61,6 @@ void ScanTheCodeDetector::validate_frame(Mat& current_image_left, Mat& current_i
 
             left_mtx.lock();
             right_mtx.lock();
-
-            Matx34d left_cam_mat = left_cam_model.fullProjectionMatrix();
-
-            //cout << "A = "<< endl << " "  << left_cam_mat << endl << endl;
 
             // Left Camera
             input_bridge = cv_bridge::toCvCopy(left_most_recent.image_msg_ptr,
@@ -76,8 +72,7 @@ void ScanTheCodeDetector::validate_frame(Mat& current_image_left, Mat& current_i
                    image_proc_scale, image_proc_scale);
             if (current_image_left.channels() != 3)
                 {
-                    ROS_ERROR("The left image topic does not contain a color image.");
-                    throw "ROS ERROR";
+                    throw "The left image topic does not contain a color image.";
                 }
 
             // Right Camera
@@ -89,8 +84,7 @@ void ScanTheCodeDetector::validate_frame(Mat& current_image_left, Mat& current_i
                    image_proc_scale, image_proc_scale);
             if (current_image_right.channels() != 3)
                 {
-                    ROS_ERROR("The right image topic does not contain a color image.");
-                    throw "ROS ERROR";
+                    throw "The right image topic does not contain a color image.";
                 }
 
             left_mtx.unlock();
@@ -227,48 +221,40 @@ void ScanTheCodeDetector::run()
                 }
             loop_rate.sleep();
         }
+
     return;
 }
 
 void ScanTheCodeDetector::process_current_image()
 {
-    Eigen::Vector3d position;
     Mat current_image_left, current_image_right, processing_size_image_left,
         processing_size_image_right;
-
-
     try
-        {
-            validate_frame(current_image_left, current_image_right,
-                              processing_size_image_left, processing_size_image_right);
-        }
+    {
+        validate_frame(current_image_left,
+                          current_image_right,
+                          processing_size_image_left,
+                          processing_size_image_right);
+    }
     catch(const char* msg)
-        {
-            return;
-        }
+    {
+        ROS_ERROR(msg);
+        return;
+    }
 
     Matx34d left_cam_mat = left_cam_model.fullProjectionMatrix();
     Matx34d  right_cam_mat = right_cam_model.fullProjectionMatrix();
-
-    ++count;
-    clock_t begin = clock();
-
-    bool ret = model_fitter->determine_model_position(position,
-               max_features, feature_block_size,
-               feature_min_distance,
-               image_proc_scale, diffusion_time,
-               current_image_left, current_image_right,
-               left_cam_mat, right_cam_mat);
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    avg = (avg * (count-1) + elapsed_secs)/count;
-
-    if(count == 300){
-      std::cout<<avg<<std::endl;
-      exit(0);
-    }
-
-
+    vector<Eigen::Vector3d> position;
+    model_fitter->determine_model_position(position,
+                                           max_features,
+                                           feature_block_size,
+                                           feature_min_distance,
+                                           image_proc_scale,
+                                           diffusion_time,
+                                           current_image_left,
+                                           current_image_right,
+                                           left_cam_mat,
+                                           right_cam_mat);
 }
 
 bool ScanTheCodeDetector::detection_activation_switch(