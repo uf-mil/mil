295d5eed096d69f0b568395c75912650115dd921
Tess <tesshbianchi@gmail.com>
2016-09-21 22:23:10 -0400

    SCAN THE CODE: fixes for PR: nullptr check, timing bug, removes using namespace

--- simulation/navigator_gazebo/src/animated_box.cc
+++ simulation/navigator_gazebo/src/animated_box.cc
@@ -1,59 +1,53 @@
 //#include <QDebug>
 
 #include "animated_box.h"
-using namespace gazebo;
 
 // Register this plugin with the simulator
 GZ_REGISTER_VISUAL_PLUGIN(ModelPush)
 
-void ModelPush::Load(rendering::VisualPtr _parent, sdf::ElementPtr _sdf)
-{
-    std::cout << "loading plugin, animated_box..." << std::endl;
-    // Store the pointer to the model
-    GZ_ASSERT(_parent != NULL, "Received NULL model pointer");
+void ModelPush::Load(gazebo::rendering::VisualPtr _parent,
+                     sdf::ElementPtr _sdf) {
+  // Store the pointer to the model
+  GZ_ASSERT(_parent != nullptr, "Received NULL model pointer");
 
-    this->model = _parent;
-    // Listen to the update event. This event is broadcast pre-render update???
-    this->updateConnection = event::Events::ConnectPreRender(boost::bind(&ModelPush::OnUpdate, this));
-    begin_time = clock();
+  this->model = _parent;
+  // Listen to the update event. This event is broadcast pre-render update???
+  this->updateConnection = gazebo::event::Events::ConnectPreRender(
+      boost::bind(&ModelPush::OnUpdate, this));
+  begin_time = clock();
 }
 
 // Called by the world update start event
-void ModelPush::OnUpdate()
-{
-    float secs = float( clock () - begin_time ) /  CLOCKS_PER_SEC;
-    common::Color c = prev_color;
+void ModelPush::OnUpdate() {
+  if (this->model == nullptr) {
+    return;
+  }
+  float secs = float(clock() - begin_time) / CLOCKS_PER_SEC;
+  gazebo::common::Color c = prev_color;
 
-    if(count != 0 && secs < 1.1 && secs > .9){
-      if(count == 1){
-        c = common::Color(0.0,1.0,0.0);
-
-      }
-      if(count == 2){
-        c = common::Color(0.0,0.0,1.0);
-        
-      }
-      if(count == 3){
-        c = common::Color(0.0,0.0,0.0);
-      }
-      begin_time = clock();
-      count = (count + 1) % 4;
-
+  if (count != 0 && secs > .9) {
+    if (count == 1) {
+      c = gazebo::common::Color(0.0, 1.0, 0.0);
     }
-
-    else if(secs < 2.1 && secs > 1.9){
-      c = common::Color(1.0,0.0,0.0);
-      begin_time = clock(); 
-      count = (count + 1) % 4;
+    if (count == 2) {
+      c = gazebo::common::Color(0.0, 0.0, 1.0);
     }
+    if (count == 3) {
+      c = gazebo::common::Color(0.0, 0.0, 0.0);
+    }
+    begin_time = clock();
+    count = (count + 1) % 4;
 
-    prev_color = c;
+  }
 
-    this->model->SetAmbient(c);
-    this->model->SetDiffuse(c);
-    //
+  else if (secs > 1.9) {
+    c = gazebo::common::Color(1.0, 0.0, 0.0);
+    begin_time = clock();
+    count = (count + 1) % 4;
+  }
+
+  prev_color = c;
+
+  this->model->SetAmbient(c);
+  this->model->SetDiffuse(c);
 }
-
-
-
-
--- simulation/navigator_gazebo/src/animated_box.h
+++ simulation/navigator_gazebo/src/animated_box.h
@@ -7,24 +7,20 @@
 #include "gazebo/common/common.hh"
 #include <ctime>
 
-namespace gazebo
-{
-class ModelPush : public VisualPlugin
-{
+class ModelPush : public gazebo::VisualPlugin {
 public:
-    void Load(rendering::VisualPtr _parent, sdf::ElementPtr /*_sdf*/);
-    // Called by the world update start event
-    void OnUpdate();
+  void Load(gazebo::rendering::VisualPtr _parent, sdf::ElementPtr /*_sdf*/);
+  // Called by the world update start event
+  void OnUpdate();
 
 private:
-    // Pointer to the model
-    rendering::VisualPtr model = NULL;
-    // Pointer to the update event connection
-    event::ConnectionPtr updateConnection = NULL;
-    clock_t begin_time;
-    int count = 0;
-    common::Color prev_color;
+  // Pointer to the model
+  gazebo::rendering::VisualPtr model = nullptr;
+  // Pointer to the update event connection
+  gazebo::event::ConnectionPtr updateConnection = nullptr;
+  clock_t begin_time;
+  int count = 0;
+  gazebo::common::Color prev_color;
 };
-}
 
 #endif // ANIMATED_BOX_H
