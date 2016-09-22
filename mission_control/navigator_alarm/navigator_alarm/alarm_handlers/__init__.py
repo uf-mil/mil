c88fe69465872217c293a49c6660f9ba840a5c6e
mattlangford <matthew.langford95@gmail.com>
2016-09-21 23:59:42 -0400

    Revert "PR57 REDO"

--- mission_control/navigator_alarm/navigator_alarm/alarm_handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-import os
-for module in os.listdir(os.path.dirname(__file__)):
-    if module[0] == '_' or module[-3:] != '.py':
-        continue
-    __import__(module[:-3], locals(), globals())
-del module
-del os
