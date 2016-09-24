ddf74f377c70c5c124639babb433a0eab19e19ac
Tess <tesshbianchi@gmail.com>
2016-09-23 22:15:51 -0400

    SCAN THE CODE: finished scan the code

--- mission_control/navigator_missions/nav_missions/scan_the_code.py
+++ mission_control/navigator_missions/nav_missions/scan_the_code.py
@@ -12,39 +12,26 @@ import navigator_msgs.srv as navigator_srvs
 @txros.util.cancellableInlineCallbacks
 def main(navigator):
     navigator.change_wrench("autonomous")
+
+    # Go to first point
+    radius = 5
+    stc_position = [-5,10,0]
+    print navigator.move.position
+    diff = np.subtract(stc_position, navigator.move.position)
+    diff = np.array([diff[0], diff[1], 0])
+    print diff
+    length = np.linalg.norm(diff)
+    print length
+    print diff * (length-radius)/(length)
+    if(length - radius >= 0):
+        yield navigator.move.rel_position(diff * (length-radius)/(length)).look_at(stc_position).go()
+    print "Got there"
     yield navigator.vision_request("scan_the_code_activate")
+    pattern = navigator.move.circle_point(stc_position, radius=radius)
+    searcher = navigator.search(vision_proxy='scan_the_code_status', search_pattern=pattern)
 
-    # activate stereo model fitter
-    granularity = 6
-    distance = 3
-    angle = 360/granularity * 3.14/180
-    n = [0,0]
-    # this is the absolute position, give it the relative position option
-    #base = [-5,5,0]
-    base = navigator.move.latlon() + navigator.move.position
+    yield searcher.start_search(spotings_req=1, speed=.9)
 
-    for i in range(0,granularity+1):
-        n[0] = distance * math.cos(i*angle) ; n[1] = distance * math.sin(i*angle)
-        new = [n[0]+base[0], n[1]+base[1],0]
-        yield navigator.move.set_position(new).look_at(base).go()
-        m = yield navigator.vision_request("scan_the_code_status")
-        if(m.tracking_model):
-            if(m.mission_complete):
-                print "Mission complete ", m.colors
-                # SET COLORS ON NAVIGATOR
-                return
-            else:
-                #rospy.sleep(15.)
-                m = yield navigator.vision_request("scan_the_code_status")
-                if(m.mission_complete):
-                    print "Mission complete ", m.colors
-                    # SET COLORS ON NAVIGATOR
-                    return
-
-
-
-    print "Mission failed"
-    # SET COLORS ON NAVIGATOR TO FAKE VALS
 
 
 
