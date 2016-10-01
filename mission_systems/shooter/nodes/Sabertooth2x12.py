44f295252e61634c806ea83078bf0c75c2a037a8
mike <kma1660@gmail.com>
2016-09-30 20:13:26 -0400

    Fix motor controller driver

--- mission_systems/shooter/nodes/Sabertooth2x12.py
+++ mission_systems/shooter/nodes/Sabertooth2x12.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
 import serial
 import math
+import time
+import struct
 class Sabertooth2x12:
   def _map(self,x, in_min, in_max, out_min, out_max):
     return int(math.ceil( (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min ) )
@@ -11,8 +14,9 @@ class Sabertooth2x12:
       return
     self.motor_1_speed = contrained_num 
     send_char = self._map(contrained_num,-1.0,1.0,1,127)
-    # ~print "Motor #1 speed=",send_char
-    #self.ser.write(send_char)
+    byts = struct.pack("B",send_char)
+    self.ser.write(byts)
+    self.ser.flush()
   def getMotor1(self):
     return self.motor_1_speed
   def setMotor2(self,speed):
@@ -21,21 +25,23 @@ class Sabertooth2x12:
       return
     self.motor_2_speed = contrained_num 
     send_char = self._map(contrained_num,-1.0,1.0,128,255)
-    #self.ser.write(send_char)
+    byts = struct.pack("B",send_char)
+    self.ser.write(byts)
+    self.ser.flush()
   def getMotor2(self):
     return self.motor_2_speed
-  #def test(self):
-    #print self._contrain(-22,-1.0,1.0)
-    #print self._map(0,-1.0,1.0,1,127)
-    #print self._map(0,-1.0,1.0,128,255)
-    #print self._map(1,-1.0,1.0,1,127)
-    #print self._map(1,-1.0,1.0,128,255)
-    #print self._map(-1,-1.0,1.0,1,127)
-    #print self._map(-1,-1.0,1.0,128,255)
-  def __init__(self,filename,):
-    #self.test()
-    self.motor_1_speed = 0.0;
-    self.motor_2_speed = 0.0;
+  def __init__(self,filename):
+    self.motor_1_speed = 0.0
+    self.motor_2_speed = 0.0
     self.filename = filename
-    self.ser = serial.Serial()
-    #self.ser = serial.Serial(filename)
+    self.ser = serial.Serial(filename,baudrate=19200)
+
+if __name__ == '__main__':
+  st = Sabertooth2x12("/dev/ttyUSB0")
+  while True:
+    st.setMotor1(1)
+    print "on"
+    time.sleep(5)
+    st.setMotor1(0)
+    print "off"
+    time.sleep(5)
