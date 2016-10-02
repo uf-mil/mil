89578106025ffda4f3507b5816601c0e50506c3b
mike <kma1660@gmail.com>
2016-10-02 11:18:20 -0400

    Fix detectdeliver mission lint

--- mission_control/navigator_missions/nav_missions/detect_deliver.py
+++ mission_control/navigator_missions/nav_missions/detect_deliver.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import txros
-import rospy
 import std_srvs.srv
 import numpy as np
 from navigator_msgs.msg import ShooterDoAction, ShooterDoActionGoal
@@ -9,115 +8,115 @@ import time
 from twisted.internet import defer
 
 class DetectDeliverMission:
-  #Note, this will be changed when the shooter switches to actionlib
-  shooterLoad = nh.get_service_client("/shooter/load", std_srvs.srv.Trigger)
-  shooterFire = nh.get_service_client("/shooter/fire", std_srvs.srv.Trigger)
-  center_error_threshold = 0.1
-  width_proportion = 0.15 #Desired proportion of frame width symbol is present in
-  width_error_threshold = 0.03
-  resp = None
+    #Note, this will be changed when the shooter switches to actionlib
+    shooterLoad = nh.get_service_client("/shooter/load", std_srvs.srv.Trigger)
+    shooterFire = nh.get_service_client("/shooter/fire", std_srvs.srv.Trigger)
+    center_error_threshold = 0.1
+    width_proportion = 0.15 #Desired proportion of frame width symbol is present in
+    width_error_threshold = 0.03
+    resp = None
 
 
-  def __init__(self,navigator):
-    self.center_error = 1
-    self.width_error = 1
-    self.navigator = navigator
-    # ~self.shooterLoad = txros.action.ActionClient(self.navigator.nh, '/shooter/load', ShooterDoAction)
-    # ~self.shooterFire = txros.action.ActionClient(self.navigator.nh, '/shooter/fire', ShooterDoAction)
+    def __init__(self, navigator):
+        self.center_error = 1
+        self.width_error = 1
+        self.navigator = navigator
+        # ~self.shooterLoad = txros.action.ActionClient(self.navigator.nh, '/shooter/load', ShooterDoAction)
+        # ~self.shooterFire = txros.action.ActionClient(self.navigator.nh, '/shooter/fire', ShooterDoAction)
 
 
-  def bounding_rect(points):
-      maxX = 0
-      minX = 2000
-      maxY = 0
-      minY = 2000
-      for i in range(len(points)):
-          if(points[i].x > maxX):
-              maxX = points[i].x
-          if(points[i].y > maxY):
-              maxY = points[i].y
-          if(points[i].x < minX):
-              minX = points[i].x
-          if(points[i].y < minY):
-              minY = points[i].y
-      return np.array([maxX, maxY, minX, minY])
+    def bounding_rect(self,points):
+        maxX = 0
+        minX = 2000
+        maxY = 0
+        minY = 2000
+        for i in range(len(points)):
+            if points[i].x > maxX:
+                maxX = points[i].x
+            if points[i].y > maxY:
+                maxY = points[i].y
+            if points[i].x < minX:
+                minX = points[i].x
+            if points[i].y < minY:
+                minY = points[i].y
+        return np.array([maxX, maxY, minX, minY])
 
 
-  @txros.util.cancellableInlineCallbacks
-  def isFound(self):
-    self.resp = yield self.navigator.vision_request("get_shape")
-    yield self.resp.found and self.resp.symbol.img_width != 0
+    @txros.util.cancellableInlineCallbacks
+    def isFound(self):
+        self.resp = yield self.navigator.vision_request("get_shape")
+        yield self.resp.found and self.resp.symbol.img_width != 0
 
 
-  @txros.util.cancellableInlineCallbacks
-  def isCentered(self):
-    if not (yield self.isFound()):
-      yield False
-    center = self.resp.symbol.CenterX / self.resp.symbol.img_width
-    self.center_error = center - 0.5
-    print "Center ", center
-    yield abs(self.center_error) < self.error_threshold
+    @txros.util.cancellableInlineCallbacks
+    def isCentered(self):
+        if not (yield self.isFound()):
+            yield False
+        center = self.resp.symbol.CenterX / self.resp.symbol.img_width
+        self.center_error = center - 0.5
+        print "Center ", center
+        yield abs(self.center_error) < self.center_error_threshold
 
 
-  @txros.util.cancellableInlineCallbacks
-  def isCorrectDistance(self):
-    if not (yield self.isFound()):
-      yield False
-    rect = self.bounding_rect(self.resp.symbol.points)
-    width = (rect[0] - rect[2]) / self.resp.symbol.img_width
-    self.width_error = width - self.width_proportion
-    print "Width ", width
-    yield abs(self.width_error) < self.width_error_threshold
+    @txros.util.cancellableInlineCallbacks
+    def isCorrectDistance(self):
+        if not (yield self.isFound()):
+            yield False
+        rect = self.bounding_rect(self.resp.symbol.points)
+        width = (rect[0] - rect[2]) / self.resp.symbol.img_width
+        self.width_error = width - self.width_proportion
+        print "Width ", width
+        yield abs(self.width_error) < self.width_error_threshold
 
 
-  @txros.util.cancellableInlineCallbacks
-  def shootAllBalls(self):
-    for i in range(3):
-      time.sleep(3)
-      self.shooterLoad(std_srvs.srv.TriggerRequest())
-      time.sleep(5)
-      self.shooterFire(std_srvs.srv.TriggerRequest())
-      # ~self.shooterLoad.send_goal(ShooterDoAction())
-      # ~res = yield goal.get_result()
-      # ~self.shooterFire.send_goal(ShooterDoAction())
-      # ~res = yield goal.get_result()
+    @txros.util.cancellableInlineCallbacks
+    def shootAllBalls(self):
+        for i in range(3):
+            time.sleep(3)
+            self.shooterLoad(std_srvs.srv.TriggerRequest())
+            time.sleep(5)
+            self.shooterFire(std_srvs.srv.TriggerRequest())
+            # ~self.shooterLoad.send_goal(ShooterDoAction())
+            # ~res = yield goal.get_result()
+            # ~self.shooterFire.send_goal(ShooterDoAction())
+            # ~res = yield goal.get_result()
 
-  @txros.util.cancellableInlineCallbacks
-  def findAndShoot(self):
-    #Remove, should have already found when mission run
-    #if not (yield self.isFound()):
-      #self.navigator.move.yaw_left(360, "deg").go()
-      #while not (yield self.isFound()):
-        #print "...Searching for target"
-    #print "Found"
-    yield self.navigator.move.go() #Stop moving once shape seen
-    if not (yield self.isCentered()):
-      if self.center_error < 0:
-        print "Turning Left"
-        self.navigator.move.yaw_left(180,"deg").go()
-      elif self.center_error > 0:
-        print "Turning Right"
-        self.navigator.move.yaw_right(180,"deg").go()
-      while not (yield self.isCentered()):
-        print "...Centering on target"
-    print "Centered"
-    yield self.navigator.move.go() #Stop moving once shape seen
-    if not (yield self.isCorrectDistance()):
-      if self.width_error < 0:
-        print "Moving Towards"
-        self.navigator.move.right(50).go()
-      elif self.width_error > 0:
-        print "Moving Away"
-        self.navigator.move.left(50).go()
-      while not (yield self.isCorrectDistance()):
-        print "...Moving to correct distance"
-    yield self.navigator.move.go() #Stop moving once centered
-    print "Correct Distance"
-    
-    print "Shooting"
-    self.shootAllBalls()
+    @txros.util.cancellableInlineCallbacks
+    def findAndShoot(self):
+        #Remove, should have already found when mission run
+        #if not (yield self.isFound()):
+          #self.navigator.move.yaw_left(360, "deg").go()
+          #while not (yield self.isFound()):
+            #print "...Searching for target"
+        #print "Found"
+        yield self.navigator.move.go() #Stop moving once shape seen
+        if not (yield self.isCentered()):
+            if self.center_error < 0:
+                print "Turning Left"
+                self.navigator.move.yaw_left(180, "deg").go()
+            elif self.center_error > 0:
+                print "Turning Right"
+                self.navigator.move.yaw_right(180, "deg").go()
+            while not (yield self.isCentered()):
+                print "...Centering on target"
+        print "Centered"
+        yield self.navigator.move.go() #Stop moving once shape seen
+        if not (yield self.isCorrectDistance()):
+            if self.width_error < 0:
+                print "Moving Towards"
+                self.navigator.move.right(50).go()
+            elif self.width_error > 0:
+                print "Moving Away"
+                self.navigator.move.left(50).go()
+            while not (yield self.isCorrectDistance()):
+                print "...Moving to correct distance"
+        yield self.navigator.move.go() #Stop moving once centered
+        print "Correct Distance"
+
+        print "Shooting"
+        self.shootAllBalls()
 
 @txros.util.cancellableInlineCallbacks
 def main(navigator):
-  mission = DetectDeliverMission(navigator)
-  yield mission.findAndShoot()
+    mission = DetectDeliverMission(navigator)
+    yield mission.findAndShoot()
