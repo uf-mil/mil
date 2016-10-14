0683209167864ce8286337f6262f9ba12f82fd0f
mattlangford <matthew.langford95@gmail.com>
2016-10-14 02:00:18 -0400

    TOOLS: Add ability to switch from displaying quaternion and euler angles

--- utils/navigator_tools/nodes/tf_fudger.py
+++ utils/navigator_tools/nodes/tf_fudger.py
@@ -12,7 +12,8 @@ rospy.init_node("tf_fudger", anonymous=True)
 br = tf.TransformBroadcaster()
 
 usage_msg = "Useful to test transforms between two frames."
-desc_msg = "Pass the name of the parent frame and the child name as well as a an optional multiplier."
+desc_msg = "Pass the name of the parent frame and the child name as well as a an optional multiplier. \n\
+At anypoint you can press q to switch between displaying a quaternion and an euler angle."
 
 parser = argparse.ArgumentParser(usage=usage_msg, description=desc_msg)
 parser.add_argument(dest='tf_parent',
@@ -52,18 +53,20 @@ toTfLin = lambda x: x * x_res - 0.5 * x_range
 toTfAng = lambda x: x * ang_res - 0.5 * ang_range
 
 p = q = None
+q_mode = False
 
-try:
-    while not rospy.is_shutdown():
-        x, y, z = toTfLin(cv2.getTrackbarPos("x", "tf")), toTfLin(cv2.getTrackbarPos("y", "tf")), toTfLin(cv2.getTrackbarPos("z", "tf"))
-        p = (x, y, z)
-        rpy = (toTfAng(cv2.getTrackbarPos("roll", "tf")), toTfAng(cv2.getTrackbarPos("pitch", "tf")), toTfAng(cv2.getTrackbarPos("yaw", "tf")))
-        q = tf.transformations.quaternion_from_euler(*rpy)
+while not rospy.is_shutdown():
+    x, y, z = toTfLin(cv2.getTrackbarPos("x", "tf")), toTfLin(cv2.getTrackbarPos("y", "tf")), toTfLin(cv2.getTrackbarPos("z", "tf"))
+    p = (x, y, z)
+    rpy = (toTfAng(cv2.getTrackbarPos("roll", "tf")), toTfAng(cv2.getTrackbarPos("pitch", "tf")), toTfAng(cv2.getTrackbarPos("yaw", "tf")))
+    q = tf.transformations.quaternion_from_euler(*rpy)
 
-        print "xyz:", [round(x, 5) for x in p], "  rpy:", [round(np.degrees(x), 5) for x in rpy]
-        br.sendTransform(p, q, rospy.Time.now(), args.tf_child, args.tf_parent)
+    rpy_feedback = "xyz: {}, euler: {}".format([round(x, 5) for x in p], [round(np.degrees(x), 5) for x in rpy])
+    q_feedback = "xyz: {},     q: {}".format([round(x, 5) for x in p], [round(x, 5) for x in q])
+    print q_feedback if q_mode else rpy_feedback
 
-        cv2.waitKey(10)
-except KeyboardInterrupt:
-    print  "Final TF:", p, q
-    cv2.destroyAllWindows()
+    k = cv2.waitKey(10) & 0xFF
+    if k == ord('q'):
+        q_mode = not q_mode
+
+print  "\nFinal TF:", p, q
