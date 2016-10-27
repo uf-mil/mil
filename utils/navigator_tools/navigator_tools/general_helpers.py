5dcf14024446c254b0c31c8e4e1e6915a116ef33
mattlangford <matthew.langford95@gmail.com>
2016-10-27 00:32:23 -0400

    TOOLS: Add 'title' param to print_t

--- utils/navigator_tools/navigator_tools/general_helpers.py
+++ utils/navigator_tools/navigator_tools/general_helpers.py
@@ -1,12 +1,32 @@
 import rospy
 
-def print_t(_str, time=None):
-    # Needs a rospy init for ros time
+def print_t(_str, time=None, title=None):
+    time_header = None
+    title_header = False
+
+    if title is not None:
+        title_header = "\033[94m\033[1m{}\033[0m".format(title)
+
     if time is None:
         try:
             time = rospy.Time.now().to_sec()
-            print "\033[1m{}:\033[0m {}".format(time, _str)
+            time_header = "\033[1m{}\033[0m".format(time)
         except rospy.exceptions.ROSInitException:
-            print _str
+            pass
     else:
-        print "\033[1m{}:\033[0m {}".format(time, _str)
+        time_header = "\033[1m{}\033[0m".format(time)
+
+    if title_header and time_header:
+        # (TIME) HEADER: message
+        to_print = "{time} {title}: {msg}"
+    elif time_header:
+        # (TIME): message
+        to_print = "{time}: {msg}"
+    elif title_header:
+        # HEADER: message
+        to_print = "{title}: {msg}"
+    else:
+        # message
+        to_print = "{msg}"
+
+    print to_print.format(time=time_header, title=title_header, msg=_str)
