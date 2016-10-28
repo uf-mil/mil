1aa50b9cbef1a4aacebebb6a20576902d8831fd7
Ira J <ijh3@ufl.edu>
2016-10-28 05:33:11 -0400

    LIDAR: Small typos

--- sensor_control/navigator_lidar_oa/src/lidar.cpp
+++ sensor_control/navigator_lidar_oa/src/lidar.cpp
@@ -42,13 +42,13 @@ using namespace std;
 const double MAP_SIZE_METERS = 1500;
 const double ROI_SIZE_METERS = 201;
 const double VOXEL_SIZE_METERS = 0.30;
-const int MIN_HITS_FOR_OCCUPANCY = 10; //20
+const int MIN_HITS_FOR_OCCUPANCY = 25; //20
 const int MAX_HITS_IN_CELL = 125; //500
 const double MAXIMUM_Z_BELOW_LIDAR = 2; //2
 const double MAXIMUM_Z_ABOVE_LIDAR = 2.5;
 const double MAX_ROLL_PITCH_ANGLE_DEG = 5.3;
 const double LIDAR_VIEW_ANGLE_DEG = 160;
-const double LIDAR_VIEW_DISTANCE_METERS = 75;
+const double LIDAR_VIEW_DISTANCE_METERS = 45;
 const double LIDAR_MIN_VIEW_DISTANCE_METERS = 5.5;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
