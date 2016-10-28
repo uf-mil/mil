ff924dbb4dd43aa5af0af59760b6d4e37dcbcf69
David <d.soto@ufl.edu>
2016-10-27 22:15:24 -0400

    PERCEPTION: restructure pcd_colorizer and image acquisition code, add pcd IO

--- perception/navigator_vision/nodes/camera_stream_demo.cpp
+++ perception/navigator_vision/nodes/camera_stream_demo.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <ros/ros.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <ros_camera_stream.hpp>  // dont forget this include for camera stream functionality
+#include <image_acquisition/ros_camera_stream.hpp>  // dont forget this include for camera stream functionality
 
 using namespace std;
 
