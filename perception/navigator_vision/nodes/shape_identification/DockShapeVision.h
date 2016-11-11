343ed0eb827bb65a058eb4feeff9f66b02a8c72d
mike <kma1660@gmail.com>
2016-11-10 23:10:31 -0500

    DETECT DELIVER: Clearer text in debug image

--- perception/navigator_vision/nodes/shape_identification/DockShapeVision.h
+++ perception/navigator_vision/nodes/shape_identification/DockShapeVision.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <navigator_msgs/DockShapes.h>
+#include <navigator_msgs/DockShape.h>
 #include <ros/ros.h>
 #include "opencv2/opencv.hpp"
 class DockShapeVision {
@@ -9,5 +10,8 @@ class DockShapeVision {
 
  public:
   virtual void GetShapes(cv::Mat& frame, navigator_msgs::DockShapes& symbols) = 0;
+  static void DrawShapes(cv::Mat& frame, navigator_msgs::DockShapes& symbols);
+  static int fontFace;
+  static double fontScale;
   virtual void init() = 0;
 };
