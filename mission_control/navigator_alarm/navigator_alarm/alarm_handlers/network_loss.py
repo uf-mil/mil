5e934a607a6bec9ee2b872675d2784140d72af09
Matthew Langford <matthew.langford95@gmail.com>
2016-11-13 08:43:38 -0500

    ALARMS: Change Network kill

--- mission_control/navigator_alarm/navigator_alarm/alarm_handlers/network_loss.py
+++ mission_control/navigator_alarm/navigator_alarm/alarm_handlers/network_loss.py
@@ -8,10 +8,15 @@ class NetworkLossHandler(HandlerBase):
     alarm_name = 'network_loss'
 
     def __init__(self):
-        self.station_hold_alarm = self.alarm_broadcaster.add_alarm('station_hold', node_name="NetworkLossHandler")
+        # To kill or to station hold. That is the question
+        # TODO: Check if we can station hold? Listen to /move_to/feedback and check 'tracking'.
+        self.kill = self.alarm_broadcaster.add_alarm('kill', node_name="NetworkLossHandler")
+        #self.station_hold_alarm = self.alarm_broadcaster.add_alarm('station_hold', node_name="NetworkLossHandler")
 
     def handle(self, alarm, time_sent, parameters):
-        self.station_hold_alarm.raise_alarm()
+        #self.station_hold_alarm.raise_alarm()
+        self.kill.raise_alarm()
+
 
     def cancel(self, alarm, time_sent, parameters):
         pass
