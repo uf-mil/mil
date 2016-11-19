717fcf42c5ae39bffb7e98b7ee8f557334c47aec
Ira J <ijh3@ufl.edu>
2016-11-18 20:35:37 -0500

    LIDAR: removed const for easier param changing

--- perception/navigator_lidar_oa/src/lidarParams.cpp
+++ perception/navigator_lidar_oa/src/lidarParams.cpp
@@ -3,24 +3,24 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Critical global constants
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const double MAP_SIZE_METERS = 1500;
-const double ROI_SIZE_METERS = 201;
-const double VOXEL_SIZE_METERS = 0.30;
-const int MIN_HITS_FOR_OCCUPANCY = 20; 
-const int MAX_HITS_IN_CELL = 125; 
-const double MAXIMUM_Z_BELOW_LIDAR = 2; 
-const double MAXIMUM_Z_ABOVE_LIDAR = 2.5;
-const double MAX_ROLL_PITCH_ANGLE_DEG = 2.5;
-const double LIDAR_VIEW_ANGLE_DEG = 160;
-const double LIDAR_VIEW_DISTANCE_METERS = 60;
-const double LIDAR_CONFIDENCE_DISTANCE_METERS = 40;
-const double LIDAR_MIN_VIEW_DISTANCE_METERS = 5.5;
-const int MIN_LIDAR_POINTS_FOR_OCCUPANCY = 10;
-const double MIN_OBJECT_HEIGHT_METERS = 0.025;
-const double MIN_OBJECT_SEPERATION_DISTANCE = 3.0;
-const std::vector<std::string> ROIS = {"BuoyField","CoralSurvey","FindBreak","AcousticPinger","Shooter","Scan_The_Code","Gate_1","Gate_2","Gate_3","Dock"};
-const double MIN_GATE_SEPERATION = 22;
-const double MAX_GATE_SEPERATION = 40;
-const double MAX_GATE_ERROR_METRIC = 15;
-const int MIN_HITS_FOR_VOLUME = 31;
-const int OBJECT_INFLATION_PARAMETER = 2;
+double MAP_SIZE_METERS = 1500;
+double ROI_SIZE_METERS = 201;
+double VOXEL_SIZE_METERS = 0.30;
+int MIN_HITS_FOR_OCCUPANCY = 20; 
+int MAX_HITS_IN_CELL = 125; 
+double MAXIMUM_Z_BELOW_LIDAR = 2; 
+double MAXIMUM_Z_ABOVE_LIDAR = 2.5;
+double MAX_ROLL_PITCH_ANGLE_DEG = 2.5;
+double LIDAR_VIEW_ANGLE_DEG = 160;
+double LIDAR_VIEW_DISTANCE_METERS = 60;
+double LIDAR_CONFIDENCE_DISTANCE_METERS = 40;
+double LIDAR_MIN_VIEW_DISTANCE_METERS = 5.5;
+int MIN_LIDAR_POINTS_FOR_OCCUPANCY = 10;
+double MIN_OBJECT_HEIGHT_METERS = 0.025;
+double MIN_OBJECT_SEPERATION_DISTANCE = 3.0;
+std::vector<std::string> ROIS = {"BuoyField","CoralSurvey","FindBreak","AcousticPinger","Shooter","Scan_The_Code","Gate_1","Gate_2","Gate_3","Dock"};
+double MIN_GATE_SEPERATION = 22;
+double MAX_GATE_SEPERATION = 40;
+double MAX_GATE_ERROR_METRIC = 15;
+int MIN_HITS_FOR_VOLUME = 31;
+int OBJECT_INFLATION_PARAMETER = 2;
--- perception/navigator_lidar_oa/src/lidarParams.h
+++ perception/navigator_lidar_oa/src/lidarParams.h
@@ -7,26 +7,26 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Critical global constants
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-extern const double MAP_SIZE_METERS;
-extern const double ROI_SIZE_METERS;
-extern const double VOXEL_SIZE_METERS;
-extern const int MIN_HITS_FOR_OCCUPANCY;
-extern const int MAX_HITS_IN_CELL;
-extern const double MAXIMUM_Z_BELOW_LIDAR;
-extern const double MAXIMUM_Z_ABOVE_LIDAR;
-extern const double MAX_ROLL_PITCH_ANGLE_DEG;
-extern const double LIDAR_VIEW_ANGLE_DEG;
-extern const double LIDAR_VIEW_DISTANCE_METERS;
-extern const double LIDAR_CONFIDENCE_DISTANCE_METERS;
-extern const double LIDAR_MIN_VIEW_DISTANCE_METERS;
-extern const int MIN_LIDAR_POINTS_FOR_OCCUPANCY;
-extern const double MIN_OBJECT_HEIGHT_METERS;
-extern const double MIN_OBJECT_SEPERATION_DISTANCE;
-extern const std::vector<std::string> ROIS;
-extern const double MIN_GATE_SEPERATION;
-extern const double MAX_GATE_SEPERATION;
-extern const double MAX_GATE_ERROR_METRIC;
-extern const int MIN_HITS_FOR_VOLUME;
-extern const int OBJECT_INFLATION_PARAMETER;
+extern double MAP_SIZE_METERS;
+extern double ROI_SIZE_METERS;
+extern double VOXEL_SIZE_METERS;
+extern int MIN_HITS_FOR_OCCUPANCY;
+extern int MAX_HITS_IN_CELL;
+extern double MAXIMUM_Z_BELOW_LIDAR;
+extern double MAXIMUM_Z_ABOVE_LIDAR;
+extern double MAX_ROLL_PITCH_ANGLE_DEG;
+extern double LIDAR_VIEW_ANGLE_DEG;
+extern double LIDAR_VIEW_DISTANCE_METERS;
+extern double LIDAR_CONFIDENCE_DISTANCE_METERS;
+extern double LIDAR_MIN_VIEW_DISTANCE_METERS;
+extern int MIN_LIDAR_POINTS_FOR_OCCUPANCY;
+extern double MIN_OBJECT_HEIGHT_METERS;
+extern double MIN_OBJECT_SEPERATION_DISTANCE;
+extern std::vector<std::string> ROIS;
+extern double MIN_GATE_SEPERATION;
+extern double MAX_GATE_SEPERATION;
+extern double MAX_GATE_ERROR_METRIC;
+extern int MIN_HITS_FOR_VOLUME;
+extern int OBJECT_INFLATION_PARAMETER;
 
 #endif
