76f2502210184d50a207bb4400bfc367a3583288
Kevin Allen <kma1660@gmail.com>
2016-11-22 16:48:26 -0500

    DETECT DELIVER: lakeday bug fixes to mission

--- mission_control/navigator_missions/nav_missions/detect_deliver.py
+++ mission_control/navigator_missions/nav_missions/detect_deliver.py
@@ -24,6 +24,7 @@ class DetectDeliverMission:
     search_timeout_seconds = 300
     SHAPE_CENTER_TO_BIG_TARGET = 0.42
     SHAPE_CENTER_TO_SMALL_TARGET = -0.42
+    NUM_BALLS = 4
 
     def __init__(self, navigator):
         self.navigator = navigator
@@ -63,7 +64,7 @@ class DetectDeliverMission:
         print "Starting circle search"
         #yield self.navigator.move.look_at(navigator_tools.rosmsg_to_numpy(self.waypoint_res.objects[0].position)).go()
         pattern = self.navigator.move.d_circle_point(navigator_tools.rosmsg_to_numpy(
-            self.waypoint_res.objects[0].position), radius=self.circle_radius,  theta_offset=self.theta_offset)
+            self.waypoint_res.objects[0].position), radius=self.circle_radius, theta_offset=self.theta_offset, direction='cw')
         yield next(pattern).go()
         searcher = self.navigator.search(
             vision_proxy='get_shape', search_pattern=pattern, Shape=self.Shape, Color=self.Color)
@@ -81,14 +82,18 @@ class DetectDeliverMission:
     @txros.util.cancellableInlineCallbacks
     def align_to_target(self):
         self.markers = MarkerArray()
-        while not (yield self.is_found()):
-            fprint("Shape not found Error={}".format(self.resp.error), title="DETECT DELIVER", msg_color='red')
-        while not (yield self.get_normal()):
-            fprint("Normal found Error={}".format(self.normal_res.error), title="DETECT DELIVER", msg_color='red')
+        while True:
+            if (yield self.is_found()):
+                if (yield self.get_normal()):
+                    break
+                else:
+                    fprint("Normal found Error={}".format(self.normal_res.error), title="DETECT DELIVER", msg_color='red')
+            else:
+                fprint("Shape not found Error={}".format(self.resp.error), title="DETECT DELIVER", msg_color='red')
         yield self.get_aligned_pos()
         move = self.navigator.move.set_position(self.aligned_position).set_orientation(self.aligned_orientation).forward(self.target_offset_meters)
         move = move.left(-self.shooter_baselink_tf._p[1]).forward(-self.shooter_baselink_tf._p[0]) #Adjust for location of shooter
-        fprint("Aligning to shoot at {}".format(move), title="DETECT DELIVER", msg_color='red')
+        fprint("Aligning to shoot at {}".format(move), title="DETECT DELIVER", msg_color='green')
         yield move.go(move_type="skid")
 
 
@@ -114,16 +119,24 @@ class DetectDeliverMission:
         rect = self._bounding_rect(self.found_shape.points)
         req.tolerance = int(min(rect[0]-rect[3],rect[1]-rect[4])/2.0)
         self.normal_res = yield self.cameraLidarTransformer(req)
-        if self.normal_res.success:
-            enu_cam_tf = yield self.navigator.tf_listener.get_transform('/enu', '/'+req.header.frame_id)
-            self.shooter_baselink_tf = yield self.navigator.tf_listener.get_transform('/base_link','/shooter')
-            self.enunormal = enu_cam_tf.transform_vector(navigator_tools.rosmsg_to_numpy(self.normal_res.normal))
-            self.enupoint = enu_cam_tf.transform_point(navigator_tools.rosmsg_to_numpy(self.normal_res.closest))
-        defer.returnValue(self.normal_res.success and abs(self.normal_res.normal[2]) < .5)
+        if not self.normal_res.success:
+            defer.returnValue(False)
+        if not self.normal_is_sane(self.normal_res.normal):
+            self.normal_res.success = False
+            self.normal_res.error = "UNREASONABLE NORMAL"
+            defer.returnValue(False)
+        enu_cam_tf = yield self.navigator.tf_listener.get_transform('/enu', '/'+req.header.frame_id)
+        self.shooter_baselink_tf = yield self.navigator.tf_listener.get_transform('/base_link','/shooter')
+        self.enunormal = enu_cam_tf.transform_vector(navigator_tools.rosmsg_to_numpy(self.normal_res.normal))
+        self.enupoint = enu_cam_tf.transform_point(navigator_tools.rosmsg_to_numpy(self.normal_res.closest))
+        defer.returnValue(True)
+
+    def normal_is_sane(self, vector3):
+         return abs(navigator_tools.rosmsg_to_numpy(vector3)[1]) < 0.2
 
     @txros.util.cancellableInlineCallbacks
     def shoot_all_balls(self):
-        for i in range(4):
+        for i in range(self.NUM_BALLS):
             goal = yield self.shooterLoad.send_goal(ShooterDoAction())
             fprint("Loading Shooter {}".format(i), title="DETECT DELIVER",  msg_color='green')
             res = yield goal.get_result()
@@ -144,10 +157,11 @@ class DetectDeliverMission:
 
 @txros.util.cancellableInlineCallbacks
 def setup_mission(navigator):
-    color = yield navigator.mission_params["scan_the_code_color3"].get()
-    fprint("Setting search color to scan the code color 3 = {}".format(color), title="DETECT DELIVER",  msg_color='green')
-    fprint("Setting search shape ANY", title="DETECT DELIVER",  msg_color='green')
-    yield navigator.mission_params["detect_deliver_shape"].set("ANY")
+    color = "ANY"
+    shape = "ANY"
+    #color = yield navigator.mission_params["scan_the_code_color3"].get()
+    fprint("Setting search shape={} color={}".format(shape, color), title="DETECT DELIVER",  msg_color='green')
+    yield navigator.mission_params["detect_deliver_shape"].set(shape)
     yield navigator.mission_params["detect_deliver_color"].set(color)
 
 @txros.util.cancellableInlineCallbacks
