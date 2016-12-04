07f39b4116c72f5d83de6d6a2edbbf26ef2d0aad
David Soto <d.soto@ufl.edu>
2016-12-04 11:35:07 -0500

    PERCEPTION: make rotateKernel separate and configurable

--- perception/navigator_vision/src/navigator_vision_lib/image_filtering.cpp
+++ perception/navigator_vision/src/navigator_vision_lib/image_filtering.cpp
@@ -1,9 +1,22 @@
 #include <navigator_vision_lib/image_filtering.hpp>
 
-cv::Mat makeRotInvariant(const cv::Mat &kernel, int rotations)
+namespace nav
 {
-  // Determine affine transform to move from top left to center of output size
+
+cv::Mat rotateKernel(const cv::Mat &kernel, float theta, bool deg, bool no_expand)
+{
+  theta = deg? theta : theta * nav::PI / 180.0f;
   cv::Point2f c_org{kernel.cols * 0.5f, kernel.rows * 0.5f};  // center of original
+
+  if(no_expand) // rotates without expanding the canvas
+  {
+    cv::Mat result;
+    cv::Mat rot_mat = cv::getRotationMatrix2D(c_org, theta , 1.0f);
+    cv::warpAffine(kernel, result, rot_mat, kernel.size());
+    return result;
+  }
+
+  // Determine affine transform to move from top left to center of output size
   float hypot = std::hypot(c_org.x, c_org.y);
   cv::Point2f c_dest{hypot, hypot};
   float center_dest_coeffs[6] = {1.0f, 0.0f, c_dest.x - c_org.x, 0.0f, 1.0f, c_dest.y - c_org.y};
@@ -15,6 +28,15 @@ cv::Mat makeRotInvariant(const cv::Mat &kernel, int rotations)
   kernel.copyTo(dest_top_left);
   cv::warpAffine(dest, dest, center_dest, dest.size());  // center dest is ready for rotating
 
+  // Rotate kernel about center point
+  cv::Mat rot_mat = cv::getRotationMatrix2D(c_dest, theta , 1.0);
+  cv::warpAffine(dest, dest, rot_mat, dest.size());
+
+  return dest;
+}
+
+cv::Mat makeRotInvariant(const cv::Mat &kernel, int rotations)
+{
   // Get angles of rotation
   std::vector<float> rotation_angles;
   float delta_theta = 360.0f / rotations;
@@ -30,15 +52,11 @@ cv::Mat makeRotInvariant(const cv::Mat &kernel, int rotations)
 
   // Incrementally rotate and save versions of original kernel
   std::vector<cv::Mat> kernel_rotations;
+  cv::Mat dest = rotateKernel(kernel, 0.0f);
   kernel_rotations.push_back(dest);
-  int i = 0;
+  cv::Point2f c_dest{dest.cols * 0.5f, dest.rows * 0.5f};
   for(auto theta : rotation_angles)
-  {
-    cv::Mat rotated_kernel;
-    cv::Mat rot_mat = cv::getRotationMatrix2D(c_dest, theta, 1.0);
-    cv::warpAffine(dest, rotated_kernel, rot_mat, dest.size());
-    kernel_rotations.push_back(rotated_kernel);
-  }
+    kernel_rotations.push_back(rotateKernel(dest, theta, true, true));
 
   // Average all rotated versions
   cv::Mat sum = cv::Mat::zeros(dest.size(), CV_32S);
@@ -48,3 +66,5 @@ cv::Mat makeRotInvariant(const cv::Mat &kernel, int rotations)
   result.convertTo(result, kernel.type());
   return result;
 }
+
+}  // namespace nav
