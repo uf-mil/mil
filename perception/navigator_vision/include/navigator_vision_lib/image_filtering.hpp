430427ed9988d61a7aefc03c9f3c318f6866d4d4
David Soto <d.soto@ufl.edu>
2016-12-04 11:35:07 -0500

    PERCEPTION: make rotateKernel separate and configurable

--- perception/navigator_vision/include/navigator_vision_lib/image_filtering.hpp
+++ perception/navigator_vision/include/navigator_vision_lib/image_filtering.hpp
@@ -5,6 +5,21 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <navigator_tools.hpp>
+
+namespace nav
+{
+
+/*
+  Returns a version of the input kernel rotated about its center point.
+  kernel - original kernel
+  theta - anlgle in radians by which to rotate the kernel. Positive angles --> counterclockwise.
+  deg - OPTIONAL. Will assume that theta is given in degrees if set to true.
+  no_expand - OPTIONAL. Will leave the output size the same as the input size. Parts of the
+    original kernel may fall outside the output canvas after the rotation.
+*/
+cv::Mat rotateKernel(const cv::Mat &kernel, float theta, bool deg=false, bool no_expand=false);
+
 /*
   Creates a new kernel that is rotationally invariant by averaging rotated instances of the
   original.
@@ -14,3 +29,5 @@
     angularly.
 */
 cv::Mat makeRotInvariant(const cv::Mat &kernel, int rotations=8);
+
+} // namespace nav
