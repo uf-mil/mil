c7490f13b67e265128ed98c2c9f96685c5e5c57f
Kevin Allen <kma1660@gmail.com>
2016-12-15 03:31:11 -0500

    DETECT DELIVER: Find shapes using smooth circles and normal

--- mission_control/navigator_missions/nav_missions/detect_deliver.py
+++ mission_control/navigator_missions/nav_missions/detect_deliver.py
@@ -14,17 +14,20 @@ from image_geometry import PinholeCameraModel
 from visualization_msgs.msg import Marker,MarkerArray
 import navigator_tools
 from navigator_tools import fprint, MissingPerceptionObject
+import genpy
 
 class DetectDeliverMission:
     # Note, this will be changed when the shooter switches to actionlib
-    shoot_distance_meters = 2.7
+    shoot_distance_meters = 3.1
     theta_offset = np.pi / 2.0
     spotings_req = 1
     circle_radius = 10
+    platform_radius = 0.925
     search_timeout_seconds = 300
     SHAPE_CENTER_TO_BIG_TARGET = 0.42
     SHAPE_CENTER_TO_SMALL_TARGET = -0.42
     NUM_BALLS = 4
+    LOOK_AT_TIME = 5
 
     def __init__(self, navigator):
         self.navigator = navigator
@@ -64,29 +67,94 @@ class DetectDeliverMission:
         self.waypoint_res = res
 
     @txros.util.cancellableInlineCallbacks
-    def circle_search(self):
-        print "Starting circle search"
-        #yield self.navigator.move.look_at(navigator_tools.rosmsg_to_numpy(self.waypoint_res.objects[0].position)).go()
-        pattern = self.navigator.move.d_circle_point(navigator_tools.rosmsg_to_numpy(
-            self.waypoint_res.objects[0].position), radius=self.circle_radius, theta_offset=self.theta_offset, direction='cw')
-        yield next(pattern).go()
-        searcher = self.navigator.search(search_pattern=pattern, looker=self.search_shape)
-        yield searcher.start_search(timeout=self.search_timeout_seconds, spotings_req=self.spotings_req, move_type="skid", loop=False)
-        print "Ended Circle Search"
+    def get_any_shape(self):
+        shapes = yield self.get_shape()
+        if shapes.found:
+            for shape in shapes.shapes.list:
+                normal_res = yield self.get_normal(shape)
+                if normal_res.success:
+                    enu_cam_tf = yield self.navigator.tf_listener.get_transform('/enu', '/'+shape.header.frame_id, shape.header.stamp)
+                    self.update_shape(shape, normal_res, enu_cam_tf)
+                    defer.returnValue( ((shape.Shape, shape.Color), self.identified_shapes[(shape.Shape, shape.Color)]) )
+                else:
+                    fprint("Normal not found Error={}".format(normal_res.error), title="DETECT DELIVER", msg_color='red')
+        else:
+            fprint("shape not found Error={}".format(shapes.error), title="DETECT DELIVER", msg_color="red")
+        defer.returnValue(False)
 
-        #  fprint("Starting Circle Search", title="DETECT DELIVER",  msg_color='green')
-        #  yield self.navigator.move.look_at(navigator_tools.rosmsg_to_numpy(self.waypoint_res.objects[0].position)).yaw_left(90, unit='deg').go()
-        #  self.navigator.move.d_circle_point(navigator_tools.rosmsg_to_numpy(self.waypoint_res.objects[0].position), self.circle_radius).go()
-        #  while not (yield self.is_found()):
-            #  fprint("Shape not found Error={}".format(self.resp.error), title="DETECT DELIVER", msg_color='red')
-        #  yield self.navigator.move.stop()
-        #  fprint("Ended Circle Search", title="DETECT DELIVER",  msg_color='green')
+    @txros.util.cancellableInlineCallbacks
+    def circle_search(self):
+        platform_np = navigator_tools.rosmsg_to_numpy(self.waypoint_res.objects[0].position)
+        yield self.navigator.move.look_at(platform_np).set_position(platform_np).backward(self.circle_radius).yaw_left(90,unit='deg').go(move_type="drive")
+
+        done_circle = False
+        @txros.util.cancellableInlineCallbacks
+        def do_circle():
+            yield self.navigator.move.circle_point(platform_np).go()
+            done_circle = True
+
+        circle_defer = do_circle()
+        while not done_circle:
+            res = yield self.get_any_shape()
+            if res == False:
+                yield self.navigator.nh.sleep(0.25)
+                continue
+            fprint("Shape ({}found, using normal to look at other 3 shapes if needed".format(res[0]), title="DETECT DELIVER", msg_color="green")
+            #  circle_defer.cancel()
+            shape_color, found_shape_pose = res
+            if self.correct_shape(shape_color):
+                self.shape_pose = found_shape_pose
+                return
+            #Pick other 3 to look at
+            rot_right = np.array([[0, -1], [1, 0]])
+            (shape_point, shape_normal) = found_shape_pose
+            rotated_norm = np.append(rot_right.dot(shape_normal[:2]), 0)
+            center_point = shape_point - shape_normal * (self.platform_radius/2.0)
+            
+            point_opposite_side = center_point - shape_normal * self.circle_radius
+            move_opposite_side = self.navigator.move.set_position(point_opposite_side).look_at(center_point).yaw_left(90, unit='deg')
+
+            left_or_whatever_point = center_point + rotated_norm * self.circle_radius
+            move_left_or_whatever = self.navigator.move.set_position(left_or_whatever_point).look_at(center_point).yaw_left(90, unit='deg')
+
+            right_or_whatever_point = center_point - rotated_norm * self.circle_radius
+            move_right_or_whatever = self.navigator.move.set_position(right_or_whatever_point).look_at(center_point).yaw_left(90, unit='deg')
+
+            yield self.search_sides((move_left_or_whatever, move_opposite_side, move_right_or_whatever))
+            return
+        fprint("No shape found after complete circle",title="DETECT DELIVER", msg_color='red')
+        raise Exception("No shape found on platform")
 
     def update_shape(self, shape_res, normal_res, tf):
        self.identified_shapes[(shape_res.Shape, shape_res.Color)] = self.get_shape_pos(normal_res, tf)
 
-    def correct_shape(self, shape):
-        return (self.Color == "ANY" or self.Color == shape.Color) and (self.Shape == "ANY" or self.Shape == shape.Shape)
+    def correct_shape(self, (shape,color)):
+        return (self.Color == "ANY" or self.Color == color) and (self.Shape == "ANY" or self.Shape == shape)
+
+    @txros.util.cancellableInlineCallbacks
+    def search_side(self):
+        fprint("Searching side",title="DETECT DELIVER", msg_color='green')
+        start_time = self.navigator.nh.get_time()
+        while self.navigator.nh.get_time() - start_time < genpy.Duration(self.LOOK_AT_TIME):
+            res = yield self.get_any_shape()
+            if not res == False:
+                defer.returnValue(res)
+            yield self.navigator.nh.sleep(0.1)
+        defer.returnValue(False)
+
+    @txros.util.cancellableInlineCallbacks
+    def search_sides(self, moves):
+        for move in moves:
+            yield move.go(move_type="drive")
+            res = yield self.search_side()
+            if res == False:
+                fprint("No shape found on side",title="DETECT DELIVER", msg_color='red')
+                continue
+            shape_color, found_pose = res
+            if self.correct_shape(shape_color):
+                self.shape_pose = found_pose
+                return
+            fprint("Saw (Shape={}, Color={}) on this side".format(shape_color[0], shape_color[1]),title="DETECT DELIVER", msg_color='green')
 
     @txros.util.cancellableInlineCallbacks
     def search_shape(self):
@@ -130,7 +198,7 @@ class DetectDeliverMission:
         move = self.navigator.move.set_position(goal_point).set_orientation(goal_orientation).forward(self.target_offset_meters)
         move = move.left(-shooter_baselink_tf._p[1]).forward(-shooter_baselink_tf._p[0]) #Adjust for location of shooter
         fprint("Aligning to shoot at {}".format(move), title="DETECT DELIVER", msg_color='green')
-        yield move.go(move_type="skid")
+        yield move.go(move_type="drive")
 
     def get_shape(self):
         return self.navigator.vision_proxies["get_shape"].get_response(Shape="ANY", Color="ANY")
@@ -162,7 +230,7 @@ class DetectDeliverMission:
         defer.returnValue(normal_res)
 
     def normal_is_sane(self, vector3):
-         return abs(navigator_tools.rosmsg_to_numpy(vector3)[1]) < 0.2
+         return abs(navigator_tools.rosmsg_to_numpy(vector3)[1]) < 0.4
 
     @txros.util.cancellableInlineCallbacks
     def shoot_all_balls(self):
@@ -187,8 +255,8 @@ class DetectDeliverMission:
 
 @txros.util.cancellableInlineCallbacks
 def setup_mission(navigator):
-    color = "ANY"
-    shape = "ANY"
+    color = "RED"
+    shape = "TRIANGLE"
     #color = yield navigator.mission_params["scan_the_code_color3"].get()
     fprint("Setting search shape={} color={}".format(shape, color), title="DETECT DELIVER",  msg_color='green')
     yield navigator.mission_params["detect_deliver_shape"].set(shape)
