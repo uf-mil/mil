e4717a77fe0cebdc0df154f9435ea2212ed2e5c3
Tess <tesshbianchi@gmail.com>
2016-12-17 20:28:18 -1000

    changes to stc, and db

--- mission_systems/navigator_scan_the_code/navigator_scan_the_code/scan_the_code_lib/color_finder.py
+++ mission_systems/navigator_scan_the_code/navigator_scan_the_code/scan_the_code_lib/color_finder.py
@@ -3,6 +3,7 @@ from __future__ import division
 import cv2
 from navigator_tools import fprint
 import numpy as np
+import operator
 ___author___ = "Tess Bianchi"
 
 
@@ -15,110 +16,96 @@ class ColorFinder:
         self.black_count = 0
         self.prev_color = None
         self.curr_count = 0
-        self.K_H_LOW = 0
-        self.K_H_HIG = 5
-        self.K_V_LOW = 0
-        self.K_V_HIG = 20
+        self.offset = None
+        self.offset_sum = [0, 0, 0, 0]
+        self.num_offset = 0
 
-        self.R_H_LOW = 0
-        self.R_H_HIG = 20
-        self.R_H_LOW2 = 160
-        self.R_H_HIG2 = 180
+        self.color_map = {'r': np.radians(0), 'y': np.radians(60),
+                          'g': np.radians(120), 'b': np.radians(240)}
 
-        self.R_V_LOW = 230
-        self.R_V_HIG = 255
+    def _get_closest_color(self, hue_angle):
+        """
+        Returns a pair of the most likely color and the radian error associated with that prediction
+            `hue_angle` := The radian value associated with hue [0, 2*pi] 
+        Colors are found from `self.color_map`
+        """
+        c = np.cos(hue_angle)
+        s = np.sin(hue_angle)
+        error = np.inf
+        likely_color = 'undef'
+        for color, h_val in self.color_map.iteritems():
+            cg = np.cos(h_val)
+            sg = np.sin(h_val)
+            # We need a signed error for some math later on so no absolute value
+            this_error = np.arctan2(sg * c - cg * s, cg * c + sg * s)
+            if np.abs(this_error) < np.abs(error):
+                error = this_error
+                likely_color = color
 
-        self.B_H_LOW = 100
-        self.B_H_HIG = 140
-        self.B_V_LOW = 100
-        self.B_V_HIG = 200
+        # fprint("Likely color: {} with an hue error of {} rads.".format(likely_color, np.round(error, 3)))
+        return [likely_color, error]
 
-        self.G_H_LOW = 41
-        self.G_H_HIG = 90
-        self.G_V_LOW = 100
-        self.G_V_HIG = 200
-
-        self.Y_H_LOW = 20
-        self.Y_H_HIG = 40
-        self.Y_V_LOW = 200
-        self.Y_V_HIG = 255
-
-    def get_color(self, image, colors, debug):
+    def get_color_hsv(self, image, colors, debug):
         draw = image.copy()
         s = (colors.shape[0], 1, colors.shape[1])
         colors = np.reshape(colors, s)
         colors = colors.astype(np.float32)
         hsv = cv2.cvtColor(colors, cv2.COLOR_BGR2HSV)
-        red_vote = 0
-        black_vote = 0
-        yellow_vote = 0
-        green_vote = 0
-        blue_vote = 0
-        v_sum = 0
-        h_sum = 0
-        s_sum = 0
-        tot = 0
-        for c in hsv:
-            tot += 1
-            c = c.flatten()
-            val, sat, hue = c[0], c[1], c[2]
-            h_sum += hue
-            s_sum += sat
-            v_sum += val
-            # if hue < self.K_H_HIG and hue > self.K_H_LOW:
-            #     black_vote += 1
-            if hue < self.R_H_HIG and hue > self.R_H_LOW:
-                red_vote += 1
-            if hue < self.R_H_HIG2 and hue > self.R_H_LOW2:
-                red_vote += 1
-            # elif hue < self.B_H_HIG and hue > self.K_H_LOW and avgv < self.K_V_HIG and avgv > self.K_V_LOW:
-            #     red_vote += 1
-            if hue < self.Y_H_HIG and hue > self.Y_H_LOW:
-                yellow_vote += 1
-            if hue < self.G_H_HIG and hue > self.G_H_LOW:
-                green_vote += 1
-            if hue < self.B_H_HIG and hue > self.B_H_LOW:
-                blue_vote += 1
-            # if val < self.K_V_HIG and val > self.K_V_LOW:
-            #     black_vote += .5
-            # if val < self.B_V_HIG and val > self.B_V_LOW:
-            #     blue_vote += .5
-            # if val < self.R_V_HIG and val > self.R_V_LOW:
-            #     red_vote += .5
-            # if val < self.G_V_HIG and val > self.G_V_LOW:
-            #     green_vote += .5
-            # if val < self.Y_V_HIG and val > self.Y_V_LOW:
-            #     yellow_vote += .5
-
-        avgh, avgs, avgv = np.round(h_sum / tot), np.round(s_sum / tot), np.round(v_sum / tot)
-        m = max(red_vote, black_vote, yellow_vote, green_vote, blue_vote)
-        if np.round(s_sum / tot) == 0.0:
-            m = -1
-
-        fprint("Average H {}".format(avgh), msg_color="green")
-        fprint("Average S {}".format(avgs), msg_color="green")
-        fprint("Average V {}".format(avgv), msg_color="green")
-        color = 'n'
-        if blue_vote == m:
-            color = 'b'
-        if red_vote == m:
-            color = 'r'
-        if yellow_vote == m:
-            color = 'y'
-        if green_vote == m:
-            color = 'g'
-        if -1 == m:
+        hsv = np.mean(hsv, axis=0).flatten()
+        print hsv
+        val, hue = hsv[0], hsv[2]
+        color, error = self._get_closest_color(hue * 2)
+        if val < 10:
             color = 'k'
-        cv2.putText(draw, "{}: {}: {}: {}".format(color, avgh, avgs, avgv), (20, 20), 1, 2, (255, 0, 0))
-        # img = np.repeat(image, 20, axis=0)
-        # debug.add_image(img, "roi", topic="roi")
-        # cv2.imshow("roi", img)
-        # cv2.waitKey(0)
+
+        cv2.putText(draw, "{} | {}".format(color, error), (20, 20), 1, 2, (255, 0, 0))
+        debug.add_image(draw, "colors", topic="colors")
+        return color
+
+    def get_color(self, colors, image, debug):
+        draw = image.copy()
+        s = (colors.shape[0], 1, colors.shape[1])
+        colors = np.reshape(colors, s)
+        colors = colors.astype(np.float32)
+        scalar = np.mean(colors, axis=0).flatten()
+
+        blackness1 = scalar[0] - scalar[1]
+        blackness2 = scalar[1] - scalar[2]
+        color = "None"
+        if(abs(blackness1) < 30 and abs(blackness2) < 30 and scalar[0] < 100):
+            o = [0, scalar[0] - scalar[1], scalar[0] - scalar[2], 0]
+            self.num_offset += 1
+            self.offset_sum = np.add(o, self.offset_sum)
+            color = 'k'
+
+        if(self.offset is not None):
+            scalar = np.add(scalar, self.offset)
+
+        a = scalar[0]
+        b = scalar[1]
+        c = scalar[2]
+        if color == 'k':
+            cv2.putText(draw, "{} | {} | {} | {}".format(color, np.round(
+                a), np.round(b), np.round(c)), (20, 20), 1, 2, (255, 0, 0))
+            debug.add_image(draw, "colors", topic="colors")
+            return color
+        if(abs(a - 255) < 10 and abs(b - 255) < 10 and abs(c - 255) < 100):
+            color = 'y'
+        elif(a > b and a > c and abs(a - b) > 30):
+            color = 'b'
+        elif(b > a and b > c):
+            color = 'g'
+        elif(c > a and c > b):
+            color = 'r'
+        else:
+            color = 'g'
+
+        cv2.putText(draw, "{} | {} | {} | {}".format(color, np.round(a), np.round(b), np.round(c)), (20, 20), 1, 2, (255, 0, 0))
         debug.add_image(draw, "colors", topic="colors")
         return color
 
     def check_for_colors(self, image, colors, debug):
-        color = self.get_color(image, colors, debug)
+        color = self.get_color(colors, image, debug)
         fprint("COLOR: {}, PREV_COLOR: {}, COLORS {}".format(color, self.prev_color, self.colors), msg_color='green')
         # cv2.imshow("asfd", image)
         # cv2.waitKey(0)
@@ -131,7 +118,7 @@ class ColorFinder:
             self.curr_count += 1
 
         elif self.prev_color != color and self.prev_color != 'k':
-            if self.curr_count > 0 and (len(self.colors) == 0 or self.colors[len(self.colors) - 1] != self.prev_color):
+            if self.curr_count > 1 and (len(self.colors) == 0 or self.colors[len(self.colors) - 1] != self.prev_color):
                 self.colors.append(self.prev_color)
 
             self.curr_count = 0
