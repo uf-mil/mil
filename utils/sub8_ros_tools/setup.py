f9ff7fcf098f2eb28885ba20cef7ead6229a7cc9
mattlangford <matthew.langford95@gmail.com>
2017-01-02 19:45:21 -0500

    TOOLS: Add fprint, add more generic tool interface

--- utils/sub8_ros_tools/setup.py
+++ utils/sub8_ros_tools/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['sub8_ros_tools', 'sub8_misc_tools'],
+    packages=['sub8_ros_tools', 'sub8_misc_tools', 'sub8_tools'],
 )
 
 setup(**setup_args)
--- utils/sub8_ros_tools/sub8_ros_tools/__init__.py
+++ utils/sub8_ros_tools/sub8_ros_tools/__init__.py
@@ -6,15 +6,9 @@ import threading_helpers
 import geometry_helpers
 import func_helpers
 
-# TODO: Adjust all existing code to not require these to be top-level imports
-from init_helpers import wait_for_param, wait_for_subscriber
-from image_helpers import Image_Subscriber, Image_Publisher, make_image_msg, get_image_msg, get_parameter_range
-from msg_helpers import (
-    make_header, odom_sub,
-    rosmsg_to_numpy, pose_to_numpy, twist_to_numpy, odometry_to_numpy, posetwist_to_numpy,
-    make_wrench_stamped, make_pose_stamped,
-    numpy_to_quaternion, numpy_pair_to_pose, numpy_to_point, numpy_quat_pair_to_pose, numpy_to_twist
-)
-from threading_helpers import thread_lock
-from geometry_helpers import (make_rotation, normalize, skew_symmetric_cross, deskew, compose_transformation,
-                              project_pt_to_plane, clip_norm)
+from image_helpers import *
+from init_helper import *
+from msg_helpers import *
+from threading_helpers import *
+from geometry_helpers import *
+from func_helpers import *
