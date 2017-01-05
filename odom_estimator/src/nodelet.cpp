uf-mil/mil

commit b7987f8b46ca863a2cb3c367b1cc5896d13de091

odom_estimator: wait for dvl

odom_estimator/src/nodelet.cpp
@@ -112,7 +112,7 @@ class NodeImpl {
         dvl_filter(dvl_sub, tf_listener, "", 10),
         depth_sub(nh, "depth", 1),
         depth_filter(depth_sub, tf_listener, "", 10),
-        last_mag(boost::none), last_good_gps(boost::none), state(boost::none) {
+        last_mag(boost::none), last_good_gps(boost::none), last_good_dvl(boost::none), state(boost::none) {
       
       private_nh.getParam("have_gps", have_gps);
       private_nh.getParam("start_x_ecef", start_x_ecef);
@@ -176,7 +176,13 @@ class NodeImpl {
             return;
           }
         } else {
-          state = init_state(msg, *last_mag, Vec<3>(start_x_ecef, start_y_ecef, start_z_ecef), Vec<3>::Zero(), last_rel_pos_ecef_);
+          if(last_good_dvl && *last_good_dvl > msg.header.stamp - ros::Duration(1.5) &&
+                              *last_good_dvl < msg.header.stamp + ros::Duration(1.5)) {
+            state = init_state(msg, *last_mag, Vec<3>(start_x_ecef, start_y_ecef, start_z_ecef), Vec<3>::Zero(), last_rel_pos_ecef_);
+          } else {
+            std::cout << "dvl missing" << std::endl;
+            return;
+          }
         }
       } else {
         state = StateUpdater(msg)(*state);
@@ -423,6 +429,13 @@ class NodeImpl {
         }
       }
       
+      if(good.size() >= 3) {
+        last_good_dvl = msg.header.stamp;
+        std::cout << "got dvl" << std::endl;
+      } else {
+        std::cout << "bad dvl" << std::endl;
+      }
+      
       state = kalman_update(
         EasyDistributionFunction<State, Vec<Dynamic>, Vec<Dynamic> >(
           [&good, &local_dvl_pos, &local_dvl_orientation, this](State const &state, Vec<Dynamic> const &measurement_noise) {
@@ -511,6 +524,7 @@ class NodeImpl {
     boost::optional<Vec<3> > last_mag;
     boost::optional<ros::Time> last_good_gps;
     boost::optional<rawgps_common::Measurements> last_good_gps_msg;
+    boost::optional<ros::Time> last_good_dvl;
     boost::optional<GaussianDistribution<State> > state;
     boost::optional<Vec<3> > last_gyro;
     std::string local_frame_id;

commit 1c8fe70cde68bdf6ac9b85ce25f50eddf0e6831e

fixed issue #1, introduced in b7987f8b4

odom_estimator/src/nodelet.cpp
@@ -277,9 +277,7 @@ class NodeImpl {
         return;
       }
       Vec<3> local_dvl_pos; tf::vectorTFToEigen(transform.getOrigin(), local_dvl_pos);
-      Quaternion local_dvl_orientation; tf::quaternionTFToEigen(transform.getRotation(), local_dvl_orientation); 
-      
-      if(!state) return;
+      Quaternion local_dvl_orientation; tf::quaternionTFToEigen(transform.getRotation(), local_dvl_orientation);
       
       std::vector<uf_common::VelocityMeasurement> good;
       for(unsigned int i = 0; i < msg.velocity_measurements.size(); i++) {
@@ -295,6 +293,8 @@ class NodeImpl {
       } else {
         std::cout << "bad dvl" << std::endl;
       }
+      
+      if(!state) return;
       
       state = kalman_update(
         EasyDistributionFunction<State, Vec<Dynamic>, Vec<Dynamic> >(