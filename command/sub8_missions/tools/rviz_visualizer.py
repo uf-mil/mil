312cc00f02a185ec68935b9ef95e7795e22f80a9
Kevin Allen <kma1660@gmail.com>
2017-01-07 01:30:48 -0500

    VISUALIZATION: add battery voltage and interactive kill markers

--- command/sub8_missions/tools/rviz_visualizer.py
+++ command/sub8_missions/tools/rviz_visualizer.py
@@ -4,11 +4,13 @@ from __future__ import division
 import numpy as np
 import rospy
 import visualization_msgs.msg as visualization_msgs
+from visualization_msgs.msg import Marker, InteractiveMarkerControl, InteractiveMarker
+from interactive_markers.interactive_marker_server import *
 
 from geometry_msgs.msg import Pose, Vector3
-from std_msgs.msg import ColorRGBA
+from std_msgs.msg import ColorRGBA, Float64
 from uf_common.msg import Float64Stamped  # This needs to be deprecated
-
+from sub8_alarm import AlarmListener, AlarmBroadcaster
 import sub8_ros_tools as sub8_utils
 
 
@@ -20,6 +22,8 @@ class RvizVisualizer(object):
         rospy.init_node('revisualizer')
         self.rviz_pub = rospy.Publisher("visualization/state", visualization_msgs.Marker, queue_size=2)
         self.rviz_pub_t = rospy.Publisher("visualization/state_t", visualization_msgs.Marker, queue_size=2)
+        self.rviz_pub_utils = rospy.Publisher("visualization/bus_voltage", visualization_msgs.Marker, queue_size=2)
+        self.kill_server = InteractiveMarkerServer("interactive_kill")
 
         # text marker
         # TODO: Clean this up, there should be a way to set all of this inline
@@ -33,10 +37,89 @@ class RvizVisualizer(object):
         self.depth_marker.color = ColorRGBA(1.0, 1.0, 1.0, 1.0)
         self.depth_marker.scale.z = 0.1
 
+        # create marker for displaying current battery voltage
+        self.low_battery_threshold = 43.0
+        self.voltage_marker = visualization_msgs.Marker()
+        self.voltage_marker.header.frame_id = "base_link"
+        self.voltage_marker.lifetime = rospy.Duration(5)
+        self.voltage_marker.ns = 'sub'
+        self.voltage_marker.id = 22
+        self.voltage_marker.pose.position.x = -2.0
+        self.voltage_marker.scale.z = 0.2
+        self.voltage_marker.color.a = 1
+        self.voltage_marker.type = visualization_msgs.Marker.TEXT_VIEW_FACING
+
+
+        # create an interactive marker to display kill status and change it
+        self.need_kill_update = True
+        self.kill_marker = InteractiveMarker()
+        self.kill_marker.header.frame_id = "base_link"
+        self.kill_marker.pose.position.x = -2.3
+        self.kill_marker.name = "kill button"
+        kill_status_marker = Marker()
+        kill_status_marker.type = Marker.TEXT_VIEW_FACING
+        kill_status_marker.text = "UNKILLED"
+        kill_status_marker.id = 55
+        kill_status_marker.scale.z = 0.2
+        kill_status_marker.color.a = 1.0
+        kill_button_control = InteractiveMarkerControl()
+        kill_button_control.name = "kill button"
+        kill_button_control.interaction_mode = InteractiveMarkerControl.BUTTON
+        kill_button_control.markers.append(kill_status_marker)
+        self.kill_server.insert(self.kill_marker, self.kill_buttton_callback)
+        self.kill_marker.controls.append(kill_button_control)
+        self.killed = False
+
+        # connect kill marker to kill alarm
+        self.kill_listener = AlarmListener(alarm_name="kill", callback_funct=self.kill_alarm_callback)
+        self.kill_alarm = AlarmBroadcaster().add_alarm("kill")
+
         # distance to bottom
         self.range_sub = rospy.Subscriber("dvl/range", Float64Stamped, self.range_callback)
         # distance to surface
         self.depth_sub = rospy.Subscriber("depth", Float64Stamped, self.depth_callback)
+        # battery voltage
+        self.voltage_sub = rospy.Subscriber("/bus_voltage", Float64, self.voltage_callback)
+
+
+    def update_kill_button(self):
+        if self.killed:
+            self.kill_marker.controls[0].markers[0].text = "KILLED"
+            self.kill_marker.controls[0].markers[0].color.r = 1
+            self.kill_marker.controls[0].markers[0].color.g = 0
+        else:
+            self.kill_marker.controls[0].markers[0].text = "UNKILLED"
+            self.kill_marker.controls[0].markers[0].color.r = 0
+            self.kill_marker.controls[0].markers[0].color.g = 1
+        self.kill_server.insert(self.kill_marker)
+        self.kill_server.applyChanges()
+
+    def kill_alarm_callback(self, alarm):
+        self.need_kill_update = False
+        self.killed = not alarm.clear
+        self.update_kill_button()
+
+    def kill_buttton_callback(self, feedback):
+        if not feedback.event_type == 3:
+            return
+        if self.need_kill_update:
+            return
+        self.need_kill_update = True
+        if self.killed:
+            self.kill_alarm.clear_alarm()
+        else:
+            self.kill_alarm.raise_alarm()
+
+    def voltage_callback(self, voltage):
+        self.voltage_marker.text = str(round(voltage.data, 3)) + ' volts'
+        self.voltage_marker.header.stamp = rospy.Time()
+        if voltage.data < self.low_battery_threshold:
+            self.voltage_marker.color.r = 1
+            self.voltage_marker.color.g = 0
+        else:
+            self.voltage_marker.color.r = 0
+            self.voltage_marker.color.g = 1
+        self.rviz_pub_utils.publish(self.voltage_marker)
 
     def depth_callback(self, msg):
         '''Handle depth data sent from depth sensor'''
