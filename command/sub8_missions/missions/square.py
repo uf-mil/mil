798c77ce47a43305ff988e2edc1de885edd326d1
mattlangford <matthew.langford95@gmail.com>
2017-01-11 12:10:17 -0500

    AUTONOMOUS: Add autonomous mission

--- command/sub8_missions/missions/square.py
+++ command/sub8_missions/missions/square.py
@@ -1,12 +1,22 @@
 from txros import util
+from sub8 import pose_editor
 
+SIDE_LENGTH = 2  # meters
+SPEED_LIMIT = 1  # m/s
 
 @util.cancellableInlineCallbacks
-def run(sub_singleton):
-    print "Square"
-    square_length = 2  # meters
-    yield sub_singleton.move.forward(square_length).go()
-    yield sub_singleton.move.right(square_length).go()
-    yield sub_singleton.move.backward(square_length).go()
-    yield sub_singleton.move.left(square_length).go()
+def run(sub):
+    print "Heil!"
+    
+    center = sub.move.forward(0)
+    for i in range(4):
+        forward = sub.move.forward(SIDE_LENGTH)
+        right = forward.right(SIDE_LENGTH)
+
+        yield forward.go(speed=SPEED_LIMIT)
+        yield right.go(speed=SPEED_LIMIT)
+        yield forward.go(speed=SPEED_LIMIT)
+        yield center.go(speed=SPEED_LIMIT)
+        yield sub.move.yaw_right_deg(90).go(speed=SPEED_LIMIT)
+
     print "Done!"
