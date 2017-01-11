7d6717346042e500e0d70245b81fdb23209780ca
mattlangford <matthew.langford95@gmail.com>
2017-01-10 20:43:21 -0500

    ALARM: Add meta alarm predicate for kill

--- command/sub8_alarm/alarm_handlers/kill.py
+++ command/sub8_alarm/alarm_handlers/kill.py
@@ -10,14 +10,36 @@ class Kill(HandlerBase):
         # Keep some knowledge of which thrusters we have working
         self.kb = KillBroadcaster(id='alarm-kill', description='Kill by alarm')
         self.alarms = {}
+        self._killed = False
 
     def raised(self, alarm):
+        self._killed = True
         self.alarms[alarm.alarm_name] = True
         self.kb.send(active=True)
 
     def cleared(self, alarm):
+        self._killed = False
         self.alarms[alarm.alarm_name] = False
 
         # Make sure that ALL alarms that caused a kill have been cleared
         if not any(self.alarms.values()):
             self.kb.send(active=False)
+    
+    def meta_predicate(self, meta_alarm, sub_alarms):
+        # Stay killed until user clears
+        if self._killed:
+            return True
+
+        # Battery too low
+        if sub_alarms["bus-voltage"].severity == 5:
+            return True
+        
+        # If we lose network but don't want to go autonomous
+        if sub_alarms["network-loss"].raised and not rospy.get_param("autonomous", False):
+            return True
+        
+        # Raised if any alarms besides the two above are raised
+        return any([alarm.raised for name, alarm in sub_alarms.items() \
+                    if name not in ["bus-voltage", "network-loss"]])
+
+
--- command/sub8_alarm/alarm_handlers/network_loss.py
+++ command/sub8_alarm/alarm_handlers/network_loss.py
@@ -6,17 +6,11 @@ class NetworkLoss(HandlerBase):
     alarm_name = 'network-loss'
 
     def __init__(self):
-        self.kill = AlarmBroadcaster("network-kill", node_name="network_loss_kill")
         self.go_auto = rospy.ServiceProxy("/go_auto", Trigger)
         self.hm = HeartbeatMonitor(self.alarm_name, "/network", node_name="network_loss_kill")
          
     def raised(self, alarm):
-        if rospy.get_param("autonomous", False):
-            # What to do when cable is unplugged and autonomous is True 
-            self.go_auto()
-        else:
-            # What to do when cable is unplugged and autonomous is False 
-            self.kill.raise_alarm(severity=5)
-        
+        pass
+
     def cleared(self, alarm):
-        self.kill.clear_alarm()
+        pass
