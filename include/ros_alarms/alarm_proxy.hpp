1571cfc27c291765570095913ba1829d029f008d
David Soto <d.soto@ufl.edu>
2017-01-23 04:06:50 -0500

    ALARMSCPP: extract AlarmProxy impl to cpp file

--- include/ros_alarms/alarm_proxy.hpp
+++ include/ros_alarms/alarm_proxy.hpp
@@ -11,78 +11,30 @@ namespace ros_alarms
 
 struct AlarmProxy
 {
-  AlarmProxy() {}
+  AlarmProxy() {} // Default ctor (cleared, severity = 0, empty strings
 
-  AlarmProxy(std::string alarm_name,
+  AlarmProxy(std::string alarm_name,  // full ctor
         bool raised,
         std::string node_name,
         std::string problem_description,
         std::string json_parameters,
-        int severity)
-  {
-    std::cerr << alarm_name << (raised? "raised" : "cleared") << node_name << problem_description << json_parameters << severity << std::endl;
-    alarm_name = alarm_name;
-    raised = raised;
-    node_name = node_name;
-    problem_description = problem_description;
-    json_parameters = json_parameters;
-    severity = severity;
-    std::cerr << "***" << this->str() << std::endl;
-  }
+        int severity);
 
-  AlarmProxy(std::string alarm_name,
+  AlarmProxy(std::string alarm_name,  // ctor without node name
         bool raised,
         std::string problem_description,
         std::string json_parameters,
-        int severity)
-  {
-    *this = AlarmProxy(alarm_name, raised, ros::this_node::getName(), problem_description,
-                  json_parameters, severity);
-  }
+        int severity);
 
-  AlarmProxy(ros_alarms::Alarm msg)
-  {
-    alarm_name = msg.alarm_name;
-    raised = msg.raised;
-    node_name = msg.node_name;
-    problem_description = msg.problem_description;
-    json_parameters = msg.parameters;
-    severity = msg.severity;
-  }
+  AlarmProxy(ros_alarms::Alarm msg);  // ctor from ros msg
 
-  ros_alarms::Alarm as_msg()
-  {
-    ros_alarms::Alarm a;
-    a.alarm_name = alarm_name;
-    a.raised = raised;
-    a.node_name = node_name;
-    a.problem_description = problem_description;
-    a.parameters = json_parameters;
-    a.severity = severity;
-    return a;
-  }
+  Alarm as_msg();         // convert to ros msg
 
-  std::string str() const
-  {
-    std::stringstream repr;
-    repr << "[alarm_name: " << alarm_name << ", status: " << (raised? "raised" : "cleared");
-    if(raised)
-      repr << ", severity: " << severity;
-    repr << "]";
-    return repr.str();
-  }
+  std::string str() const;  // Returns printable representaion of AlarmProxy
 
-  bool operator ==(const AlarmProxy &other) const
-  {
-    if(alarm_name != other.alarm_name) return false;
-    if(raised != other.raised) return false;
-    if(node_name != other.node_name) return false;
-    if(problem_description != other.problem_description) return false;
-    if(json_parameters != other.json_parameters) return false;
-    if(severity != other.severity) return false;
-    return true;
-  }
+  bool operator ==(const AlarmProxy &other) const;
 
+  // Public fields
   std::string alarm_name;
   bool raised = false;
   std::string node_name;
---
+++ src/ros_alarms/alarm_proxy.cpp
@@ -0,0 +1,69 @@
+#include <ros_alarms/alarm_proxy.hpp>
+
+using namespace std;
+
+namespace ros_alarms
+{
+
+AlarmProxy::AlarmProxy(string _alarm_name, bool _raised, string _node_name,
+                       string _problem, string _json, int _severity)
+{
+  alarm_name = _alarm_name;
+  raised = _raised;
+  node_name = _node_name;
+  problem_description = _problem;
+  json_parameters = _json;
+  severity = _severity;
+}
+
+AlarmProxy::AlarmProxy(string _alarm_name, bool _raised, string _problem,
+                       string _json, int _severity)
+{
+  *this = AlarmProxy(_alarm_name, _raised, ros::this_node::getName(), _problem, _json, _severity);
+}
+
+AlarmProxy::AlarmProxy(ros_alarms::Alarm msg)
+{
+  alarm_name = msg.alarm_name;
+  raised = msg.raised;
+  node_name = msg.node_name;
+  problem_description = msg.problem_description;
+  json_parameters = msg.parameters;
+  severity = msg.severity;
+}
+
+Alarm AlarmProxy::as_msg()
+{
+  ros_alarms::Alarm a;
+  a.alarm_name = alarm_name;
+  a.raised = raised;
+  a.node_name = node_name;
+  a.problem_description = problem_description;
+  a.parameters = json_parameters;
+  a.severity = severity;
+  return a;
+}
+
+std::string AlarmProxy::str() const
+{
+  std::stringstream repr;
+  repr << "[alarm_name: " << alarm_name << ", status: " << (raised? "raised" : "cleared");
+  if(raised)
+    repr << ", severity: " << severity;
+  repr << "]";
+  return repr.str();
+}
+
+bool AlarmProxy::operator ==(const AlarmProxy &other) const
+{
+  if(alarm_name != other.alarm_name) return false;
+  if(raised != other.raised) return false;
+  if(node_name != other.node_name) return false;
+  if(problem_description != other.problem_description) return false;
+  if(json_parameters != other.json_parameters) return false;
+  if(severity != other.severity) return false;
+  return true;
+}
+
+
+}  // namespace ros_alarms
