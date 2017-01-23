36d44400cbc479ee9b5642610428e9771c9a96da
David Soto <d.soto@ufl.edu>
2017-01-23 07:51:09 -0500

    ALARMSCPP: add source docs for broadcaster, proxy

--- include/ros_alarms/alarm_proxy.hpp
+++ include/ros_alarms/alarm_proxy.hpp
@@ -11,26 +11,26 @@ namespace ros_alarms
 
 struct AlarmProxy
 {
-  AlarmProxy() {} // Default ctor (cleared, severity = 0, empty strings
+  AlarmProxy() {}                            // Default ctor (cleared, severity = 0, empty strings
 
-  AlarmProxy(std::string alarm_name,  // full ctor
+  AlarmProxy(std::string alarm_name,         // full ctor
         bool raised,
         std::string node_name,
         std::string problem_description,
         std::string json_parameters,
         int severity);
 
-  AlarmProxy(std::string alarm_name,  // ctor without node name
+  AlarmProxy(std::string alarm_name,         // ctor without node name
         bool raised,
         std::string problem_description,
         std::string json_parameters,
         int severity);
 
-  AlarmProxy(ros_alarms::Alarm msg);  // ctor from ros msg
+  AlarmProxy(ros_alarms::Alarm msg);         // ctor from ros msg
 
-  Alarm as_msg();         // convert to ros msg
+  Alarm as_msg();                            // convert to ros msg
 
-  std::string str() const;  // Returns printable representaion of AlarmProxy
+  std::string str(bool full = false) const;  // Returns printable representaion of AlarmProxy
 
   bool operator ==(const AlarmProxy &other) const;
 
--- include/ros_alarms/broadcaster.hpp
+++ include/ros_alarms/broadcaster.hpp
@@ -14,10 +14,23 @@ namespace ros_alarms
 class AlarmBroadcaster
 {
 public:
+  // Constuct a broadcaster w/ an internally controlled (default) or externally controlled
+  // (optional ptr argument) AlarmProxy
   AlarmBroadcaster(ros::NodeHandle &nh, AlarmProxy* alarm = nullptr);
+
+  // Change alarm status and publish to server
   void clear() { __alarm_ptr->raised = false; publish(); }
   void raise() { __alarm_ptr->raised = true;  publish(); }
-  void updateSeverity(uint8_t sev) { __alarm_ptr->severity = sev; publish();}
+
+  // updateSeverity always raises the alarm because it doesn't make sense for
+  // cleared alarm to have severity
+  void updateSeverity(int sev) { __alarm_ptr->severity = sev; raise();}
+
+  // Publishes current state of the AlarmProxy to the server
+  bool publish();
+
+  // Handle to update the AlarmProxy (if this is an externally managed AlPxy, then modifying
+  // could potentially have side effects for other broadcasters that may use it. Exert Caution!)
   AlarmProxy& alarm() { return *__alarm_ptr; }
 
 private:
@@ -25,7 +38,6 @@ private:
   AlarmProxy* __alarm_ptr;  // All code should refer to the proxy via ptr
   ros::ServiceClient __set_alarm;
   AlarmProxy __alarm_proxy; // This allows internal management of the alarm proxy
-  bool publish();
 };
 
 }  // namespace ros_alarms
