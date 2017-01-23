d9aff5fd0efb9c031d4df05fcfa53bc5fed0cec7
David Soto <d.soto@ufl.edu>
2017-01-22 20:14:05 -0500

    TESTS: add AlarmProxy tests

--- test/listener_test.cpp
+++ test/listener_test.cpp
@@ -14,6 +14,7 @@ using ros_alarms::HeartbeatMonitor;
 TEST(ServerCheck, setServiceCheck)
 {
   // Check for existence of alarm server
+  ros::Time::init();  // needed to use ros::Time::now() w/o creating a  NodeHandle
   ros::Duration time_out {2, 0};
   ASSERT_TRUE(ros::service::waitForService("/alarm/set", time_out)) << "/alarm/set service not available";
   
@@ -22,6 +23,7 @@ TEST(ServerCheck, setServiceCheck)
 
 TEST(ServerCheck, getServiceCheck)
 {
+  ros::Time::init();  // needed to use ros::Time::now() w/o creating a  NodeHandle
   // Check for existence of alarm server
   ros::Duration time_out {2, 0};
   ASSERT_TRUE(ros::service::waitForService("/alarm/get", time_out)) << "/alarm/get service not available";
@@ -31,7 +33,8 @@ TEST(ServerCheck, getServiceCheck)
 class AlarmTest : public ::testing::Test
 {
 public:
-  AlarmTest() : pxy(alarm_name, node_name, problem_description, json_parameters, severity)
+  AlarmTest() : pxy(alarm_name, false, node_name, problem_description, json_parameters, severity) 
+  {}
   ros::NodeHandle nh;
   string alarm_name = "test_alarm";
   string node_name = "test_alarm_client_node";
@@ -43,25 +46,28 @@ public:
 
 TEST_F(AlarmTest, alarmProxyTest)
 {
-  EXPECT_STREQ(alarm_name.c_str(), "test_alarm");
-  EXPECT_STREQ(node_name.c_str(), "test_alarm_client_node");
-  EXPECT_STREQ(problem_description.c_str(), "");
-  EXPECT_STREQ(json_parameters.c_str(), "json");
-  EXPECT_EQ(severity, 5);
-  EXPECT_EQ(pxy, AlarmProxy(pxy.as_msg()));
+  auto ctor_err {"AlarmProxy has filed different than what was wxpected based on value passed to its ctor."};
+  EXPECT_STREQ(alarm_name.c_str(), "test_alarm") << ctor_err;
+  EXPECT_STREQ(node_name.c_str(), "test_alarm_client_node") << ctor_err;
+  EXPECT_STREQ(problem_description.c_str(), "") << ctor_err;
+  EXPECT_STREQ(json_parameters.c_str(), "json") << ctor_err;
+  EXPECT_EQ(severity, 5) << ctor_err;
+  EXPECT_EQ(pxy, AlarmProxy(pxy.as_msg()))
+    << "Proxy was turned into a ros msg and back into a proxy but is different from original";
+  AlarmProxy pxy2 = pxy;  // Test copy ctor & operator ==
+  EXPECT_EQ(pxy, pxy2)
+    << "Operator == returned false when called w/ the original and a AlarmProxy copy constructed from it";
+  AlarmProxy pxy3 = pxy;
+  EXPECT_EQ(pxy2, pxy3)
+    << "Operator == returned false when called w/ 2 copies of the same AlarmProxy.";
 }
 
-TEST(AlarmTest, broascasterListenerTest)
+TEST_F(AlarmTest, broascasterListenerTest)
 {
-
-  // Alarm Proxy struct for interfacing with broadcasters and listeners
-  ros_alarms::AlarmProxy alarm_px(alarm_name, 
-  // Instantiate an alarm broadcaster
-  AlarmBroadcaster bcast{nh, alarm_px};
   return;
 }
 
-TEST(AlarmTest, heartbeatPublisher)
+TEST_F(AlarmTest, heartbeatPublisher)
 {
   return;
 }
