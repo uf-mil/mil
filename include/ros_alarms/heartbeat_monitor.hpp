e7f297e140dd8bf1612382f5a151b0e49abf7e8c
David Soto <d.soto@ufl.edu>
2017-01-30 18:43:49 -0500

    HEARTBEATCPP: restructure heartbeat recovery logic
    Also makes the monitor frequency constant (0.2s period by default) but
    configurable. Only the heartbeat listener will be responsible for
    keeping track of heartbeat recovery. Only the timer function will
    check for heartbeat loss and raise the alarm.

--- include/ros_alarms/heartbeat_monitor.hpp
+++ include/ros_alarms/heartbeat_monitor.hpp
@@ -23,9 +23,14 @@ template <typename msg_t = std_msgs::Header>
 class HeartbeatMonitor
 {
 public:
-  HeartbeatMonitor(ros::NodeHandle nh, std::string alarm_name, std::string heartbeat_topic,
-                   std::function<bool(msg_t)> predicate, ros::Duration time_to_raise= {0.1},
-                   ros::Duration time_to_clear={0.1});
+  HeartbeatMonitor(ros::NodeHandle nh,
+                   std::string alarm_name,
+                   std::string heartbeat_topic,
+                   std::function<bool(msg_t)> predicate = [](msg_t){ return true;},
+                   ros::Duration time_to_raise = ros::Duration(0.1),
+                   ros::Duration time_to_clear = ros::Duration(0.1),
+                   ros::Duration period = ros::Duration(0.02));
+                   // TODO: find out why a period < 0.02 causes callbacks not to be called
   std::string alarm_name()     const { return __alarm_proxy.alarm_name; }
   std::string heartbeat_name() const { return __heartbeat_topic; }
   bool healthy()               const { return __healthy; }
@@ -48,37 +53,40 @@ private:
   ros::Timer __status_checker;
   ros::Duration __time_to_raise;
   ros::Duration __time_to_clear;
+  ros::Duration __period;
   ros::Time __last_beat = {0, 0};
   ros::Duration __time_recovering = {0, 0};
   bool __recovering = false;
   bool __healthy = true;
   std::string __object_name;
-  void __record_heartbeat(msg_t beat_msg);  // Callback for heartbeat topic subscriber
-  void __diagnose_heartbeat(const ros::TimerEvent &te);  // Callback for __status_checker
+  void __heard_heartbeat(msg_t beat_msg);  // Callback for heartbeat topic subscriber
+  void __check_for_heartbeat_loss(const ros::TimerEvent &te);  // Callback for __status_checker
 };
 
 template <typename msg_t>
 HeartbeatMonitor<msg_t>
 ::HeartbeatMonitor(ros::NodeHandle nh, std::string alarm_name, std::string heartbeat_topic,
                    std::function<bool(msg_t)> predicate, ros::Duration time_to_raise,
-                   ros::Duration time_to_clear)
+                   ros::Duration time_to_clear, ros::Duration period)
 : __nh(nh),
   __heartbeat_topic(heartbeat_topic),
   __alarm_proxy(alarm_name, false, "", "", 5),
+  __msg_predicate(predicate),
   __alarm_broadcaster(nh, &__alarm_proxy),  // Don't use the .alarm() method on this!
   __alarm_listener(nh, __heartbeat_topic),
   __async_spinner(1, &__cb_queue),
   __last_beat(ros::Time::now()),
   __time_to_raise(time_to_raise),
-  __time_to_clear(time_to_clear)
+  __time_to_clear(time_to_clear),
+  __period(period)
 {
   // Things that depend on the privately owned CallbackQueue are initialized here
   //   because setCallbackQueue() has to be called firs
   __nh.setCallbackQueue(&__cb_queue);
   __heartbeat_listener = __nh.subscribe(__heartbeat_topic, 10,
-                                        &HeartbeatMonitor::__record_heartbeat, this);
-  __status_checker = __nh.createTimer(ros::Duration(std::min(time_to_raise, time_to_clear).toSec() / 5.0),
-                                      &HeartbeatMonitor::__diagnose_heartbeat, this);
+                                        &HeartbeatMonitor::__heard_heartbeat, this);
+  __status_checker = __nh.createTimer(__period, 
+                                      &HeartbeatMonitor::__check_for_heartbeat_loss, this);
 
   // Register cleared alarm w/ alarm server
   __alarm_broadcaster.clear();
@@ -93,19 +101,18 @@ HeartbeatMonitor<msg_t>
   std::stringstream init_msg;
   init_msg << "Node " << __alarm_proxy.node_name << " is now monitoring the following"
     " heartbeat: " << __heartbeat_topic;
-  std::cerr << init_msg.str() << std::endl;
   ROS_INFO("%s", init_msg.str().c_str());
 }
 
 template <typename msg_t>
 void HeartbeatMonitor<msg_t>
-::__record_heartbeat(msg_t beat_msg)
+::__heard_heartbeat(msg_t beat_msg)
 {
-  std::cerr << "Got Heartbeat" << std::endl;
-  // Call predicate and set last healthy beat time
   auto receipt_time = ros::Time::now();
+  auto last_beat_time  = __last_beat;
   bool valid_beat = false;
 
+  // Call predicate and set last healthy beat time
   try
   {
      valid_beat = __msg_predicate(beat_msg);
@@ -113,78 +120,72 @@ void HeartbeatMonitor<msg_t>
   catch(std::exception &e)
   {
      std::stringstream err;
-     err << __object_name << " - Predicate function threw an exeption: " << e.what();
+     err << __object_name << " - Predicate function threw an exception: " << e.what();
      ROS_WARN("%s", err.str().c_str());
   }
 
-  if(valid_beat)
+  if(!valid_beat) // Don't do stuff if beat is invalid
+    return;
+
+  __last_beat = receipt_time; // update __last_beat
+  auto time_since_prev_beat = receipt_time - last_beat_time;
+
+  // Set the last healthy beat time as a json param on the alarm proxy
+  std::stringstream json;
+  // not exactly sure if this will work with json.loads()
+  json << "{last_heathy_beat : " <<  __last_beat.toSec() << "}";
+  __alarm_proxy.json_parameters = json.str();
+
+  if(__healthy)  // We're done if the heartbeat is healthy
+    return;
+
+  // Now we will determine if we need to start recovering or increase the
+  //   recovery counter or if we are fully recovered and need to clear alarm
+
+  if(__recovering)  // Either increase recovery time or reset it
   {
-    auto time_since_prev_beat = receipt_time - __last_beat;
-    __last_beat = receipt_time;
-
-    // Set the last healthy beat time as a json param on the alarm proxy
-    std::stringstream json;
-    // not exactly sure if this will work with json.loads()
-    json << "{last_heathy_beat : " <<  __last_beat.toSec() << "}";
-    __alarm_proxy.json_parameters = json.str();
-
-    // We're done if the heartbeat is healthy
-    if(__healthy)
-      return;
-
-    // Heartbeat was not healthy
-    if(__recovering)  // Either increase recovery time or reset it
+    if(time_since_prev_beat < __time_to_raise)
     {
-      if(time_since_prev_beat < __time_to_raise)
-        __time_recovering += ros::Time::now() - __last_beat;
-      else
-        __time_recovering = ros::Duration(0);
+      __time_recovering += time_since_prev_beat;
+      if(__time_recovering > __time_to_raise)
+      {
+        // Reset health flag and exit recovery mode
+        __healthy = true;
+        __recovering = false;
+
+        // Clear heartbeat-loss alarm
+        __alarm_broadcaster.clear();
+      }
     }
-    else  // Set flag for recovery mode
+    else
     {
-      __recovering = true;
-      __time_recovering = ros::Duration(0.0);
+      __time_recovering = ros::Duration(0);
     }
+  }
+  else  // Set flag for recovery mode
+  {
+    __recovering = true;
+    __time_recovering = ros::Duration(0.0);
   }
 }
 
 template <typename msg_t>
 void HeartbeatMonitor<msg_t>
-::__diagnose_heartbeat(const ros::TimerEvent &te)
+::__check_for_heartbeat_loss(const ros::TimerEvent &te)
 {
-  std::cerr << "Checking heartbeat status, " << ros::Time::now() << std::endl;
+  if(!__healthy) // Can't lose what's already lost
+    return;
 
-  // Case 1: Heartbeat was previously healthy
-  if(__healthy)
+  ros::Duration time_since_last_beat = ros::Time::now() - __last_beat;
+
+  if(time_since_last_beat > __time_to_raise) // Raise heartbeat-loss alarm
   {
-    ros::Duration time_since_last_beat = ros::Time::now() - __last_beat;
-  
-    if(time_since_last_beat > __time_to_raise) // Raise heartbeat-loss alarm
-    {
-      __alarm_proxy.raised = true;
-      __alarm_broadcaster.raise();
-      __healthy = false;
-      std::cerr << "Raising lost heartbeat alarm" << ros::Time::now() << std::endl;
-    }
-    // Should we add a mode where we increase the severity based on the time the
-    //   heartbeat has been lost?
+    __alarm_proxy.raised = true;
+    __alarm_broadcaster.raise();
+    __healthy = false;
   }
-
-  // Case 2: Heartbeat was not previously healthy
-  else
-  {
-    if(__recovering && __time_recovering > __time_to_clear)  // Heartbeat has finished recovering
-    {
-      // Reset health flag and exit recovery mode
-      __healthy = true;
-      __recovering = false;
-
-      // Clear heartbeat-loss alarm
-      __alarm_proxy.raised = true;
-      __alarm_broadcaster.clear();
-    }
-    else {}  // Either not fully recovered or not recovering at all
-  }
+  // Should we add a mode where we increase the severity based on the time the
+  //   heartbeat has been lost?
 }
 
 template <typename msg_t>
