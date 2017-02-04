71c5c1c15f71ee004ea4dd0999cc9ba6f8a9afa2
Anthony Olive <anthony@iris-systems.net>
2017-02-04 07:02:02 -0500

    SCRIPTS: Move and update the bash aliases file for the new install process

---
+++ scripts/bash_aliases.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script will be sourced by the MIL runcom file if the install script
+# cloned this repository during a run or if the install script was run after
+# it was cloned manually. It contains general MIL commands that have been
+# aliased to make them faster or easier to execute. Becoming familiar with
+# these will likely increase productivity, so it is recommended to do so.
+
+# Source external alias modules
+source $CATKIN_DIR/src/software-common/scripts/two_line_bash.sh
+source $CATKIN_DIR/src/software-common/scripts/ros_connect.sh
+
+# Directory navigation
+alias swc="cd $CATKIN_DIR/src/software-common"
+
+# Bash sourcing
+alias srcbrc='source ~/.bashrc'
+
+# Catkin workspace management
+alias cm='catkin_make -C $CATKIN_DIR'
+
+# Alarms
+alias araise='rosrun ros_alarms raise'
+alias aclear='rosrun ros_alarms clear'
+alias areport='rosrun ros_alarms report'
+
+# Simulation
+alias killgazebo="killall -9 gazebo && killall -9 gzserver && killall -9 gzclient"
