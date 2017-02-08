1c447e97ef4560a3ab9d14c1391b7a332ff85064
Anthony Olive <anthony@iris-systems.net>
2017-02-08 01:05:27 -0500

    NETWORK: Redesign ros_connect for speed, configurability, and ease of use

--- scripts/ros_connect.sh
+++ scripts/ros_connect.sh
@@ -1,82 +1,70 @@
 #!/bin/bash
 
 # ROS Connect is a small set of functions that makes managing the currently
-# selected ROS master simple and convenient.
+# selected ROS master simple and convenient. It's default is to dynamically
+# determine which hosts are online and allow the user to select one, but a
+# hostname can be passed as an argument to bypass this process.
 
-# These are the hostnames for all devices that run a remote roscore
-SUB_HOST=mil-sub-sub8.ad.mil.ufl.edu
-WMV_HOST=mil-nav-wamv.ad.mil.ufl.edu
-PRC_HOST=mil-nav-perception.ad.mil.ufl.edu
-SHT_HOST=mil-shuttle.ad.mil.ufl.edu
+
+# These parameters define the network to search on
+SEARCH_DOMAIN="ad.mil.ufl.edu"
+SUBNET="192.168.37.0/24"
+
+# These are the hostnames for all MIL hosts that run a remote roscore
+HOSTNAMES=(	"mil-sub-sub8.$SEARCH_DOMAIN"
+		"mil-nav-wamv.$SEARCH_DOMAIN"
+		"mil-shuttle.$SEARCH_DOMAIN"
+)
+
+# These are the common names that map one-to-one to the above hostnames
+COMMNAMES=(	"SubjuGator"
+		"NaviGator"
+		"Shuttle"
+)
+
 
 check_host() {
-
-	# Attempts to ping a host to make sure it is reachable
 	HOST="$1"
 
-	HOST_PING=$(ping -w 1 -c 2 $HOST 2>&1 | grep "% packet" | cut -d" " -f 6 | tr -d "%")
+	# Attempts to ping the host to make sure it is reachable
+	HOST_PING=$(ping -c 2 $HOST 2>&1 | grep "% packet" | awk -F'[%]' '{print $1}' | awk -F'[ ]' '{print $NF}')
 	if ! [ -z "${HOST_PING}" ]; then
 
 		# Uses packet loss percentage to determine if the connection is strong
 		if [ $HOST_PING -lt 25 ]; then
 
 			# Will return true if ping was successful and packet loss was below 25%
-			return `true`
-		fi
-	fi
-	return `false`
-}
-
-check_connection() {
-	if (check_host "$SUB_HOST"); then
-		SUB_CHECK=true
-	else
-		SUB_CHECK=false
-	fi
-	if (check_host "$WMV_HOST"); then
-		WMV_CHECK=true
-	else
-		WMV_CHECK=false
-	fi
-	if (check_host "$PRC_HOST"); then
-		PRC_CHECK=true
-	else
-		PRC_CHECK=false
-	fi
-	if (check_host "$SHT_HOST"); then
-		SHT_CHECK=true
-	else
-		SHT_CHECK=false
-	fi
-
-	if ([ "$SUB_CHECK" = "false" ] && [ "$WMV_CHECK" = "false" ] &&\
-	    [ "$PRC_CHECK" = "false" ] && [ "$SHT_CHECK" = "false" ]); then
-		echo "None of the MIL roscores are available on this network"
-	else
-		if [ "$SUB_CHECK" = "true" ]; then
-			echo "SubjuGator is accessible on this network"
-		fi
-		if [ "$WMV_CHECK" = "true" ]; then
-			echo "Navigator WAMV is accessible on this network"
-		fi
-		if [ "$PRC_CHECK" = "true" ]; then
-			echo "Navigator Perception is accessible on this network"
-		fi
-		if [ "$SHT_CHECK" = "true" ]; then
-			echo "Shuttle is accessible on this network"
+			echo true
 		fi
 	fi
 }
+
+check_roscore_hosts() {
+	AVAILABLE_HOSTS=()
+
+	# Check whether or not each hostname is online
+	for ((HOST_ID=0; HOST_ID<${#HOSTNAMES[@]}; HOST_ID++)); do
+		if [ "`check_host ${HOSTNAMES[$HOST_ID]}`" = "true" ]; then
+			AVAILABLE_HOSTS+=($HOST_ID)
+		fi
+	done
+}
+
 
 set_ros_ip() {
-	LOCAL_IP="`ip route get 192.168.37.0/24 | awk '{print $NF; exit}'`"
-	LOCAL_HOSTNAME="`hostname`.ad.mil.ufl.edu"
+	LOCAL_IP="`ip route get $SUBNET | awk '{print $NF; exit}'`"
+	LOCAL_HOSTNAME="`hostname`.$SEARCH_DOMAIN"
 
-	# Sets ROS_HOSTNAME to the this machine's hostname
-	export ROS_HOSTNAME=$LOCAL_HOSTNAME
+	# Sets ROS_HOSTNAME if the hostname is resolvable on the search domain
+	if [ ! -z "`dig +short $LOCAL_HOSTNAME | awk '{ print ; exit }'`" ]; then
+		unset ROS_IP
+		export ROS_HOSTNAME=$LOCAL_HOSTNAME
 
-	# Sets ROS_IP to the IP on this machine's main NIC
-	export ROS_IP=$LOCAL_IP
+	# Sets ROS_IP to the IP address on this machine's main NIC as a fallback
+	else
+		unset ROS_HOSTNAME
+		export ROS_IP=$LOCAL_IP
+	fi
 }
 
 unset_ros_ip() {
@@ -88,86 +76,54 @@ unset_ros_ip() {
 
 set_ros_master() {
 
-	# Sets ROS_MASTER_URI to the hostname of the correct MIL roscore
-	export ROS_MASTER_URI=$REMOTE_ROSCORE_URI
-	echo "The master roscore is set to $REMOTE_ROSCORE_URI"
+	# Sets ROS_MASTER_URI to the hostname of the selected remote roscore
+	export ROS_MASTER_URI=http://$1:11311
+	echo "The master roscore is set to http://$1:11311"
 }
 
 unset_ros_master() {
 
-	# Sets ROS_MASTER_URI to point back to localhost
+	# Sets ROS_MASTER_URI to the local roscore's URI
 	export ROS_MASTER_URI=http://localhost:11311
 	echo "The master roscore is set to this machine"
 }
 
+
 ros_connect() {
-	check_connection
 
-	# If none of the MIL roscores are accessible, use localhost as the default roscore
-	if ([ "$SUB_CHECK" = "false" ] && [ "$WMV_CHECK" = "false" ] &&\
-	    [ "$PRC_CHECK" = "false" ] && [ "$SHT_CHECK" = "false" ]); then
-		ros_disconnect
+	# If a hostname was passed as an argument, connect to it
+	if [ ! -z "$1" ]; then
+		set_ros_ip
+		set_ros_master $1
 
-	# If just one MIL roscore was accessible, connect directly to that roscore
-	elif ([ "$SUB_CHECK" = "true" ] && [ "$WMV_CHECK" = "false" ] &&\
-	      [ "$PRC_CHECK" = "false" ] && [ "$SHT_CHECK" = "false" ]); then
-		REMOTE_ROSCORE_URI=http://$SUB_HOST:11311
-		set_ros_ip
-		set_ros_master
-	elif ([ "$SUB_CHECK" = "false" ] && [ "$WMV_CHECK" = "true" ] &&\
-	      [ "$PRC_CHECK" = "false" ] && [ "$SHT_CHECK" = "false" ]); then
-		REMOTE_ROSCORE_URI=http://$WMV_HOST:11311
-		set_ros_ip
-		set_ros_master
-	elif ([ "$SUB_CHECK" = "false" ] && [ "$WMV_CHECK" = "false" ] &&\
-	      [ "$PRC_CHECK" = "true" ] && [ "$SHT_CHECK" = "false" ]; then
-		REMOTE_ROSCORE_URI=http://$PRC_HOST:11311
-		set_ros_ip
-		set_ros_master
-	elif ([ "$SUB_CHECK" = "false" ] && [ "$WMV_CHECK" = "false" ] &&\
-	      [ "$PRC_CHECK" = "false" ] && [ "$SHT_CHECK" = "true" ]); then
-		REMOTE_ROSCORE_URI=http://$SHT_HOST:11311
-		set_ros_ip
-		set_ros_master
-
-	# If multiple roscores were reachable, allow the user to select one
+	# If no arguments were passed in, detect the available remote roscores
 	else
-		echo ""
-		echo "Multiple MIL roscores were detected!"
-		if ([ "$SUB_CHECK" = "true" ]); then
-			echo "	1. SubjuGator"
-		fi
-		if ([ "$WMV_CHECK" = "true" ]); then
-			echo "	2. Navigator WAMV"
-		fi
-		if ([ "$PRC_CHECK" = "true" ]); then
-			echo "	3. Navigator Perception"
-		fi
-		if ([ "$SHT_CHECK" = "true" ]); then
-			echo "	4. Shuttle"
-		fi
-		echo ""
-		echo -n "Select a roscore to connect to: "
-		read SELECTION
+		check_roscore_hosts
 
-		if [ "$SELECTION" = "1" ]; then
-			REMOTE_ROSCORE_URI=http://$SUB_HOST:11311
+		# If none of the remote roscores are accessible, use localhost as the default roscore
+		if [ ${#AVAILABLE_HOSTS[@]} -eq 0 ]; then
+			echo "None of the remote roscores are available on this network"
+			ros_disconnect
+
+		# If just one remote roscore was accessible, connect directly to that roscore
+		elif [ ${#AVAILABLE_HOSTS[@]} -eq 1 ]; then
+			echo "The only remote roscore that could be detected is running on ${COMMNAMES[$AVAILABLE_HOSTS]}"
 			set_ros_ip
-			set_ros_master
-		elif [ "$SELECTION" = "2" ]; then
-			REMOTE_ROSCORE_URI=http://$WMV_HOST:11311
-			set_ros_ip
-			set_ros_master
-		elif [ "$SELECTION" = "3" ]; then
-			REMOTE_ROSCORE_URI=http://$PRC_HOST:11311
-			set_ros_ip
-			set_ros_master
-		elif [ "$SELECTION" = "4" ]; then
-			REMOTE_ROSCORE_URI=http://$SHT_HOST:11311
-			set_ros_ip
-			set_ros_master
+			set_ros_master ${HOSTNAMES[$AVAILABLE_HOSTS]}
+
+		# If multiple remote roscores were detected, allow the user to select one
 		else
-			echo "Invalid selection value, no roscore selected"
+			echo "Multiple remote roscores were detected on this network"
+			for ((ID_INDEX=0; ID_INDEX<${#AVAILABLE_HOSTS[@]}; ID_INDEX++)); do
+				echo "	$(($ID_INDEX + 1)). ${COMMNAMES[${AVAILABLE_HOSTS[$ID_INDEX]}]}"
+			done
+			echo -n "Select a remote roscore to connect to: " && read RESPONSE
+			if [ ! "$RESPONSE" -lt 1 ] && [ ! "$RESPONSE" -gt "$ID_INDEX" ]; then
+				set_ros_ip
+				set_ros_master ${HOSTNAMES[${AVAILABLE_HOSTS[$(($RESPONSE - 1))]}]}
+			else
+				echo "Invalid selection, no roscore selected"
+			fi
 		fi
 	fi
 }
@@ -180,6 +136,6 @@ ros_disconnect() {
 }
 
 # Prints debugging output for the master roscore that is currently selected
-alias rosenv='echo "ROS_IP=$ROS_IP
+alias ros_env='echo "ROS_IP=$ROS_IP
 ROS_HOSTNAME=$ROS_HOSTNAME
 ROS_MASTER_URI=$ROS_MASTER_URI"'
