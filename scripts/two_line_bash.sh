536791f2d7feda4587bb24d7bcce9081d4383135
Anthony Olive <anthony@iris-systems.net>
2017-02-25 20:20:54 -0500

    SCRIPTS: Implement displaying sourced catkin workspace in two line bash

--- scripts/two_line_bash.sh
+++ scripts/two_line_bash.sh
@@ -8,11 +8,18 @@
 # command: touch ~/.disable_tlb
 
 
+function parse_catkin_workspace {
+	PS_WORKSPACE=""
+	local WORKSPACE_DIR="`echo $ROS_PACKAGE_PATH | cut -d ':' -f1 | sed 's@/src@@'`"
+	if [ -f $WORKSPACE_DIR/.catkin_workspace ]; then
+		PS_WORKSPACE="(catkin `echo $WORKSPACE_DIR | rev | cut -d "/" -f1 | rev`) "
+	fi
+}
+
 function parse_git_branch {
-	PS_BRANCH=''
-	PS_FILL=${PS_LINE:0:$COLUMNS}
+	PS_BRANCH=""
 	if [ -d .svn ]; then
-		PS_BRANCH="(svn r$(svn info|awk '/Revision/{print $2}'))"
+		PS_BRANCH="(svn r$(svn info|awk '/Revision/{print $2}')) "
 		return
 	elif [ -f _FOSSIL_ -o -f .fslckout ]; then
 		PS_BRANCH="(fossil $(fossil status|awk '/tags/{print $2}')) "
@@ -20,6 +27,11 @@ function parse_git_branch {
 	fi
 	ref=$(git symbolic-ref HEAD 2> /dev/null) || return
 	PS_BRANCH="(git ${ref#refs/heads/}) "
+}
+
+function parse_tlb_info {
+	parse_catkin_workspace
+	parse_git_branch
 }
 
 
@@ -30,10 +42,12 @@ if [ ! -f ~/.disable_tlb ]; then
 	BLUE="\[\033[01;34m\]"
 	YELLOW="\[\033[0;33m\]"
 
-	PS_LINE=`printf -- '- %.0s' {1..200}`
-	PROMPT_COMMAND=parse_git_branch
-	PS_INFO="$GREEN\u@\h$RESET:$BLUE\w"
+	PROMPT_COMMAND=parse_tlb_info
+	PS_LINE=`printf -- "- %.0s" {1..200}`
+	PS_FILL=${PS_LINE:0:$COLUMNS}
+	PS_INFO="$GREEN\u@\h$RESET:$BLUE\w "
+	PS_CATKIN="$YELLOW\$PS_WORKSPACE"
 	PS_GIT="$YELLOW\$PS_BRANCH"
 	PS_TIME="\[\033[\$((COLUMNS-10))G\] $RED[\t]"
-	export PS1="\${PS_FILL}\[\033[0G\]${PS_INFO} ${PS_GIT}${PS_TIME}\n${RESET}\$ "
+	export PS1="\${PS_FILL}\[\033[0G\]${PS_INFO}${PS_CATKIN}${PS_GIT}${PS_TIME}\n${RESET}\$ "
 fi
