611391c0e1bfe6b5e27f7f8b129d519b6ebedfda
Anthony Olive <anthony@iris-systems.net>
2017-03-08 20:13:01 -0500

    SCRIPTS: Change bmux so that it will work with bag

--- scripts/bmux.sh
+++ scripts/bmux.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# The bmux command is used to manage sourcing the bagging aliases for different
-# MIL vehicles. This prevents setting potentially hundreds of aliases in each
-# new shell. It also allows us to prefix the aliases with bag_* as opposed to
-# bag_{vehicle}_*. Each project with bagging aliases should contain a
-# scripts/bagging_aliases.sh file.
+# The bmux command is used to manage sourcing the bagging variables for
+# different MIL vehicles. This prevents setting potentially hundreds of
+# variables in each new shell. It also allows us to prefix the variables with
+# bag_* as opposed to bag_{vehicle}_*. Each project with bagging variables
+# should contain a scripts/bagging_variables.sh file. A bmux command to source
+# the correct set of variables for a vehicle should be added to the bash runcom
+# file of that vehicle's main user
 
 
-BAGGING_ALIAS_FILE="scripts/bagging_aliases.sh"
+BAGGING_VARIABLES_FILE="scripts/bagging_variables.sh"
 
 
-_bagging_aliases_complete() {
+_bagging_variables_file_complete() {
 	local FILE
 
-	# Check for bagging aliase files in the catkin workspace repositories
-	for FILE in $CATKIN_DIR/src/*; do
+	# Check for bagging variables files in the catkin workspace repositories
+	for FILE in $CATKIN_DIR/src/"$2"*; do
 
-		# Skip any repository that does not contain a bagging aliases file
-		[[ -f $FILE/$BAGGING_ALIAS_FILE ]] || continue
+		# Skip any repository that does not contain a bagging variables file
+		[[ -f $FILE/$BAGGING_VARIABLES_FILE ]] || continue
 
 		# Append just the name of the repository to the autocomplete list
 		COMPREPLY+=( `echo "$FILE" | rev | cut -d "/" -f1 | rev` )
@@ -26,59 +28,76 @@ _bagging_aliases_complete() {
 
 bmux() {
 	local SELECTION
-	local BAGGING_ALIASES="`alias | grep 'bag' | cut -d '=' -f1 | cut -d ' ' -f2`"
+	local BAGGING_VARIABLES="`env | grep 'bag_' | cut -d '=' -f1 | cut -d ' ' -f2`"
 
-	# Get the list of catkin workspaces containing bagging aliases files
-	_bagging_aliases_complete
+	# Get the list of catkin workspaces containing bagging variables files
+	_bagging_variables_file_complete
 
 	# Handles command line arguments
 	while [ "$#" -gt 0 ]; do
 		case $1 in
+			-c|--clear)
+				if [ ! -z "$BAGGING_VARIABLES" ]; then
+					for VARIABLE in $BAGGING_VARIABLES; do
+						unset "$VARIABLE"
+					done
+				fi
+
+				# Special variables used in the bag command
+				unset BAG_ALWAYS
+				unset BAG_HOME
+				SELECTION=false
+				shift 1
+				;;
 			-h|--help)
-				echo "Usage: bmux [OPTION] [REPOSITORY]..."
-				echo "Manager for sourcing bagging aliases for different MIL vehicles."
+				echo "Usage: bmux [OPTION]... [REPOSITORY]"
+				echo "Manager for sourcing bagging variables for different MIL vehicles."
 				echo ""
 				echo "Option		GNU long option		Meaning"
+				echo "-c		--clear			Clear current bagging variables"
 				echo "-h		--help			Display the help menu"
-				echo "-l		--list			List projects with bagging aliases"
-				echo "-s		--show			Show current bagging aliases"
-				echo "-c		--clear			Clear current bagging aliases"
+				echo "-l		--list			List projects with bagging variables"
+				echo "-s		--show			Show current bagging variables"
+				SELECTION=false
 				shift 1
 				;;
 			-l|--list)
-				echo "$COMPREPLY" | sed ':a;N;$!ba;s/\n/  /g'
+				if [ ! -z "$COMPREPLY" ]; then
+					echo "${COMPREPLY[@]}" | sed 's/ /  /g'
+				fi
+				SELECTION=false
 				shift 1
 				;;
 			-s|--show)
-				if [ ! -z "$BAGGING_ALIASES" ]; then
-					echo "$BAGGING_ALIASES" | sed ':a;N;$!ba;s/\n/  /g'
+				if [ ! -z "$BAGGING_VARIABLES" ]; then
+					echo "$BAGGING_VARIABLES" | sed ':a;N;$!ba;s/\n/  /g'
 				fi
+				SELECTION=false
 				shift 1
 				;;
-			-c|--clear)
-				if [ ! -z "$BAGGING_ALIASES" ]; then
-					for ALIAS in $BAGGING_ALIASES; do
-						unalias "$ALIAS"
-					done
-				fi
+			-*)
+				echo "Option $1 is not implemented."
+				echo "Try 'bmux --help' for more information."
+				SELECTION=false
 				shift 1
 				;;
 			*)
-				if [ ! -z "`echo $COMPREPLY | grep $1`" ]; then
-					SELECTION="$1"
-				else
-					echo "Option $1 is not implemented."
-					echo "Try 'bmux --help' for more information."
+				if [ "$SELECTION" != "false" ]; then
+					if [ -f $CATKIN_DIR/src/"$1"/$BAGGING_VARIABLES_FILE ]; then
+						SELECTION="$1"
+					else
+						echo "$CATKIN_DIR/src/$1 has no scripts/bagging_variables.sh file."
+						echo "Try 'bmux --help' for more information."
+						SELECTION=false
+					fi
 				fi
 				shift 1
 				;;
 		esac
 	done
 
-	if [ ! -z "$SELECTION" ]; then
-		if [ -f $CATKIN_DIR/src/$SELECTION/$BAGGING_ALIAS_FILE ]; then
-			source $CATKIN_DIR/src/$SELECTION/$BAGGING_ALIAS_FILE
-		fi
+	if [ ! -z "$SELECTION" ] && [ "$SELECTION" != "false" ]; then
+			source $CATKIN_DIR/src/$SELECTION/$BAGGING_VARIABLES_FILE
 	fi
 
 	unset COMPREPLY
@@ -86,4 +105,4 @@ bmux() {
 
 
 # Registers the autocompletion function to be invoked for bmux
-complete -F _bagging_aliases_complete bmux
+complete -F _bagging_variables_file_complete bmux
