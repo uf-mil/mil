46ba90e97750d08884c8d2b5cb569f1f6fdc2582
David Soto <d.soto@ufl.edu>
2017-03-19 04:51:44 -0400

    GREAT MERGE: convert sub8_sonar to mil_passive_sonar
    Added 'passive' to the name because we now have an imaging sonar.

--- drivers/mil_passive_sonar/setup.py
+++ drivers/mil_passive_sonar/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['sub8_sonar', 'multilateration'],
+    packages=['mil_passive_sonar', 'multilateration'],
 )
 
 setup(**setup_args)
