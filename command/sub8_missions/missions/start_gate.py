026b0cc720dec1561c2ff758d823153aa9ff546f
RustyBamboo <dvolya@gmail.com>
2017-04-01 13:02:03 -0400

    STARTGATE: Patched up mission.

--- command/sub8_missions/missions/start_gate.py
+++ command/sub8_missions/missions/start_gate.py
@@ -21,96 +21,98 @@ import yaml
 
 from sub8 import sub_singleton
 
-FOUND_START_GATE = False
-SPEED = 0.3
 
 fprint = sub8_tools.text_effects.FprintFactory(
-    title="START_GATE", msg_color="white").fprint
+title="START_GATE", msg_color="white").fprint
+
+class StartGateMission:
+
+    FOUND_START_GATE = False
+    SPEED = 0.3
+
+    def __init__(self, sub):
+        self.sub_singleton = sub
+
+    @txros.util.cancellableInlineCallbacks
+    def run_mission(self):
+        fprint('Starting mission')
+        fprint('Assuming camera is facing gate')
+        fprint('Leveling off', msg_color="yellow")
+        yield self.sub_singleton.move.zero_roll_and_pitch().go()
+        fprint('Going down', msg_color="yellow")
+        yield self.sub_singleton.move.down(1).go()
+
+        start_gate_enable = yield self.sub_singleton.nh.get_service_client('/vision/start_gate/enable', SetBool)
+        fprint('Turning on vision service')
+        yield start_gate_enable(SetBoolRequest(data=True))
+
+        start_gate_search = yield self.sub_singleton.nh.get_service_client('/vision/start_gate/pose', VisionRequest2D)
+        fprint("Searching for start gate pose")
+        start_gate_search_res = yield start_gate_search(VisionRequest2DRequest(target_name=''))
+
+        if not start_gate_search_res.found:
+            fprint("Waiting a few seconds and trying again:")
+            time.sleep(3)
+            start_gate_search_res = yield start_gate_search(VisionRequest2DRequest(target_name=''))
+        # yield start_gate_enable(SetBoolRequest(data=False))
+        # yield start_gate_enable(SetBoolRequest(data=True))
+        if not start_gate_search_res.found:
+            fprint("Running search pattern")
+            while not self.FOUND_START_GATE and start_gate_search_res.pose.x == 0:
+                fprint(self.FOUND_START_GATE)
+                time.sleep(1)
+                fprint("AGAIN - Searching for start gate pose")
+                self.start_gate_find()
+                yield self.search_pattern()
+        
+        fprint("Found start gate: " + str(start_gate_search_res.pose))
+        yield self.align_for_dummies( start_gate_search_res)
+        fprint("YOLO")
+
+        while(self.FOUND_START_GATE):
+            self.start_gate_find()
+            yield self.sub_singleton.move.forward(.3).zero_roll_and_pitch().go(speed=self.SPEED)
+
+        fprint("Finished")
+
+    @txros.util.cancellableInlineCallbacks
+    def start_gate_find(self):
+        start_gate_search = yield self.sub_singleton.nh.get_service_client('/vision/start_gate/pose', VisionRequest2D)
+        start_gate_search_res = yield start_gate_search(VisionRequest2DRequest(target_name=''))
+        self.FOUND_START_GATE = start_gate_search_res.found
+
+    @txros.util.cancellableInlineCallbacks
+    def search_pattern(self):
+        if not self.FOUND_START_GATE:
+            fprint("Moving up .1", msg_color="yellow")
+            yield self.sub_singleton.move.up(.1).zero_roll_and_pitch().go(speed=self.SPEED)
+            fprint("Moving down .3", msg_color="yellow")
+            yield self.sub_singleton.move.down(.3).zero_roll_and_pitch().go(speed=self.SPEED)
+        if not self.FOUND_START_GATE:
+            fprint("Moving left .3", msg_color="yellow")
+            yield self.sub_singleton.move.left(.3).zero_roll_and_pitch().go(speed=self.SPEED)
+            fprint("Moving right .3", msg_color="yellow")
+            yield self.sub_singleton.move.right(.3).zero_roll_and_pitch().go(speed=self.SPEED)
+
+
+    @txros.util.cancellableInlineCallbacks
+    def align_for_dummies(self, start_gate_search_res):
+        error_tolerance = 0.1
+        deviation = ((start_gate_search_res.pose.x - start_gate_search_res.camera_info.width/2)/start_gate_search_res.camera_info.width,(start_gate_search_res.pose.y - start_gate_search_res.camera_info.height/2)/start_gate_search_res.camera_info.height)
+           
+        fprint("Alignment -- Deviation: " + str(deviation), msg_color="cyan")
+        while(np.abs(deviation[0]) > error_tolerance):
+            fprint("Moving right " + str(.1*np.sign(deviation[0])), msg_color="yellow")
+            yield self.sub_singleton.move.right(.1*np.sign(deviation[0])).zero_roll_and_pitch().go(speed=self.SPEED)
+        while(np.abs(deviation[1]) > error_tolerance):
+            fprint("Moving up " + str(.1*np.sign(deviation[1])), msg_color="yellow")
+            yield self.sub_singleton.move.up(.1*np.sign(deviation[1])).zero_roll_and_pitch().go(speed=self.SPEED)
+        yield fprint("Finished Alignment -- Deviation: " + str(deviation), msg_color="cyan")
+
+
+
 
 @txros.util.cancellableInlineCallbacks
 def run(sub_singleton):
-    fprint('Starting mission')
-    fprint('Assuming camera is facing gate')
-    fprint('Leveling off', msg_color="yellow")
-    yield sub_singleton.move.zero_roll_and_pitch().go()
-
-    start_gate_enable = yield sub_singleton.nh.get_service_client('/vision/start_gate/enable', SetBool)
-    fprint('Turning on vision service')
-    yield start_gate_enable(SetBoolRequest(data=True))
-
-    start_gate_search = yield sub_singleton.nh.get_service_client('/vision/start_gate/pose', VisionRequest2D)
-    fprint("Searching for start gate pose")
-    start_gate_search_res = yield start_gate_search(VisionRequest2DRequest(target_name=''))
-
-    if not start_gate_search_res.found:
-        fprint("Waiting a few seconds and trying again:")
-        time.sleep(3)
-        start_gate_search_res = yield start_gate_search(VisionRequest2DRequest(target_name=''))
-    # yield start_gate_enable(SetBoolRequest(data=False))
-    # yield start_gate_enable(SetBoolRequest(data=True))
-    if not start_gate_search_res.found:
-        fprint("Running search pattern")
-        global FOUND_START_GATE
-        while not FOUND_START_GATE and start_gate_search_res.pose.x == 0:
-            fprint(FOUND_START_GATE)
-            time.sleep(1)
-            fprint("AGAIN - Searching for start gate pose")
-            start_gate_find(sub_singleton)
-            yield search_pattern(sub_singleton)
-    
-    fprint("Found start gate: " + str(start_gate_search_res.pose))
-    yield align_for_dummies(sub_singleton, start_gate_search_res)
-    fprint("YOLO")
-
-    while(FOUND_START_GATE):
-        start_gate_find(sub_singleton)
-        yield sub_singleton.move.forward(.3).zero_roll_and_pitch().go(speed=SPEED)
-
-    fprint("Finished")
-
-
-@txros.util.cancellableInlineCallbacks
-def start_gate_find(sub_singleton):
-    global FOUND_START_GATE
-    start_gate_search = yield sub_singleton.nh.get_service_client('/vision/start_gate/pose', VisionRequest2D)
-    start_gate_search_res = yield start_gate_search(VisionRequest2DRequest(target_name=''))
-    FOUND_START_GATE = start_gate_search_res.found
-
-@txros.util.cancellableInlineCallbacks
-def search_pattern(sub_singleton):
-    if not FOUND_START_GATE:
-        fprint("Moving up .3", msg_color="yellow")
-        yield sub_singleton.move.up(.3).zero_roll_and_pitch().go(speed=SPEED)
-        fprint("Moving down .3", msg_color="yellow")
-        yield sub_singleton.move.down(.3).zero_roll_and_pitch().go(speed=SPEED)
-    if not FOUND_START_GATE:
-        fprint("Moving left .3", msg_color="yellow")
-        yield sub_singleton.move.left(.3).zero_roll_and_pitch().go(speed=SPEED)
-        fprint("Moving right .3", msg_color="yellow")
-        yield sub_singleton.move.right(.3).zero_roll_and_pitch().go(speed=SPEED)
-
-    # odom_sub = nh.subscribe('/odom', Odometry)    
-
-    # odom_sub = nh.subscribe('/odom', Odometry)    
-    # fprint(odom_sub.pose)
-
-
-@txros.util.cancellableInlineCallbacks
-def align_for_dummies(sub_singleton, start_gate_search_res):
-    error_tolerance = 0.1
-    deviation = ((start_gate_search_res.pose.x - start_gate_search_res.camera_info.width/2)/start_gate_search_res.camera_info.width,(start_gate_search_res.pose.y - start_gate_search_res.camera_info.height/2)/start_gate_search_res.camera_info.height)
-       
-    fprint("Alignment -- Deviation: " + str(deviation), msg_color="cyan")
-    while(np.abs(deviation[0]) > error_tolerance):
-        fprint("Moving right " + str(.1*np.sign(deviation[0])), msg_color="yellow")
-        yield sub_singleton.move.right(.1*np.sign(deviation[0])).zero_roll_and_pitch().go(speed=SPEED)
-    while(np.abs(deviation[1]) > error_tolerance):
-        fprint("Moving up " + str(.1*np.sign(deviation[1])), msg_color="yellow")
-        yield sub_singleton.move.up(.1*np.sign(deviation[1])).zero_roll_and_pitch().go(speed=SPEED)
-    yield fprint("Finished Alignment -- Deviation: " + str(deviation), msg_color="cyan")
-    # while(True):
-
-
-
-# def mission_check():
-#     odom_sub = nh.subscribe('/odom', Odometry)
+    start_gate_mission = StartGateMission(sub_singleton)
+    yield start_gate_mission.run_mission()
