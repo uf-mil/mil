d7972e24f8c8e13766b6f666836617f827926412
David Soto <d.soto@ufl.edu>
2017-04-02 01:24:37 -0400

    VISION: integrate camera_lidar_transformer
    Node used to be in the great_merge_transformer subdirectory.
    All other nodes were considered not generic and removed from the repo.

--- perception/mil_vision/great_merge_purgatory/nodes/camera_to_lidar/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-#include "CameraLidarTransformer.hpp"
-
-int main (int argc, char** argv)
-{
-    ros::init (argc, argv, "camera_lidar_transformer");
-    CameraLidarTransformer transformer;
-    ros::spin ();
-}
