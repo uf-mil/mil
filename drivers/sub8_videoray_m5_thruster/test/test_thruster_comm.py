79eb85035b30728c871bef6206dbd975e7f88b1b
David Soto <d.soto@ufl.edu>
2017-04-17 09:43:24 -0400

    TESTS: update thruster_comm test for layout format

--- drivers/sub8_videoray_m5_thruster/test/test_thruster_comm.py
+++ drivers/sub8_videoray_m5_thruster/test/test_thruster_comm.py
@@ -2,43 +2,41 @@
 import unittest
 import numpy as np
 from sub8_thruster_comm import thruster_comm_factory, FakeThrusterPort
+import rospkg
+import rosparam
+
+rospack = rospkg.RosPack()
 
 
 class TestThrusterComm(unittest.TestCase):
     def setUp(self):
-        port = '/dev/fake_port'
-        node_id = 17
-        thruster_name = 'BRL'
-
-        self.port_info = {
-            'port': port,
-            'thrusters': {
-                'BRV': {
-                    'node_id': 16,
-                },
-                thruster_name: {
-                    'node_id': node_id,
-                }
-            }
-        }
+        sub8_thruster_mapper = rospack.get_path('sub8_thruster_mapper')
+        self.thruster_layout = rosparam.load_file(sub8_thruster_mapper
+                                                  + '/config/thruster_layout.yaml')[0][0]
 
     def test_thruster_comm_factory_fake(self):
         '''Test that the thruster factory returns a proper simulated FakeThrusterPort'''
         # This should succeed
-        thrust_comm = thruster_comm_factory(self.port_info, fake=True)
+        thrust_comm = thruster_comm_factory(self.thruster_layout['thruster_ports'][0],
+                                            self.thruster_layout['thrusters'], fake=True)
         self.assertIsNotNone(thrust_comm)
 
     def test_fake_thruster_status(self):
         '''Test that the fake thruster status published is what we expect'''
-        thrust_comm = FakeThrusterPort(self.port_info)
-        fake_status = thrust_comm.command_thruster('BRV', 0.2)
+        port_info = self.thruster_layout['thruster_ports'][0]
+        thruster_defs = self.thruster_layout['thrusters']
+        thrust_comm = FakeThrusterPort(port_info, thruster_defs)
+        fake_status = thrust_comm.command_thruster(port_info['thruster_names'][0], 0.2)
         self.assertEqual(fake_status['bus_voltage'], 48)
 
     def test_thruster_comm_factory_real_fail(self):
         '''Test that the comm factory fails to create a ThrusterPort on a port that does not exist'''
         # This should fail
+        port_info = self.thruster_layout['thruster_ports'][0]
+        thruster_defs = self.thruster_layout['thrusters']
+        port_info['port'] = 'bad_name'
         with self.assertRaises(IOError):
-            thrust_comm = thruster_comm_factory(self.port_info, fake=False)
+            thrust_comm = thruster_comm_factory(port_info, thruster_defs, fake=False)
             self.assertIsNotNone(thrust_comm)
 
 
