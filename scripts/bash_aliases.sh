c1179433c4419cfeb183703b786b03ca0c7a6f17
Kevin Allen <kma1660@gmail.com>
2017-05-14 17:38:42 -0400

    SCRIPTS: switch bag variable to image_rect_color, add fps alias

--- scripts/bash_aliases.sh
+++ scripts/bash_aliases.sh
@@ -17,3 +17,6 @@ alias sshsub="ssh sub8@${HOSTNAMES[0]} -Y"
 # Missions
 alias subm="rosrun sub8_missions run_mission"
 alias subc="rosrun sub8_missions move_command"
+
+# Cameras
+alias subfps="rostopic hz $bag_front_cams $bag_down_cam"
