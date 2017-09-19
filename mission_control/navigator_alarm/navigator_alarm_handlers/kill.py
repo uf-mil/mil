cae352c4b53fd6eca1e177aba560fae526a63ec6
Kevin Allen <kma1660@gmail.com>
2017-09-19 19:55:28 -0400

    BAG: integrate online bagger into launch and kill system
    * organize bag aliases

--- mission_control/navigator_alarm/navigator_alarm_handlers/kill.py
+++ mission_control/navigator_alarm/navigator_alarm_handlers/kill.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import rospy
 from ros_alarms import HandlerBase, Alarm
+from actionlib import SimpleActionClient, TerminalState
+from mil_msgs.msg import BagOnlineAction, BagOnlineGoal
+import os
 
 class Kill(HandlerBase):
     alarm_name = 'kill'
@@ -10,9 +13,27 @@ class Kill(HandlerBase):
         self.initial_alarm = Alarm(self.alarm_name, True,
                                    node_name='alarm_server',
                                    problem_description='Initial kill')
+        self.bag_client = SimpleActionClient("/online_bagger/bag", BagOnlineAction)
+        self.first = True
+
+    def _online_bagger_cb(self, status, result):
+        if status == 3:
+            rospy.loginfo('KILL BAG WRITTEN TO {}'.format(result.filename))
+        else:
+            rospy.logwarn('KILL BAG {}, status: {}'.format(TerminalState.to_string(status), result.status))
 
     def raised(self, alarm):
         self._killed = True
+        if self.first:
+            self.first = False
+            return
+        if 'BAG_ALWAYS' not in os.environ or 'bag_kill' not in os.environ:
+            rospy.logwarn('BAG_ALWAYS or BAG_KILL not set. Not making kill bag.')
+            return
+        goal = BagOnlineGoal(bag_name='kill.bag')
+        goal.topics = os.environ['BAG_ALWAYS'] + ' ' + os.environ['bag_kill']
+        self.bag_client.send_goal(goal, done_cb=self._online_bagger_cb)
+
 
     def cleared(self, alarm):
         self._killed = False
--- scripts/bagging_variables.sh
+++ scripts/bagging_variables.sh
@@ -7,7 +7,7 @@
 
 
 # Define topics that should be in every bag
-export BAG_ALWAYS="/odom /absodom /clock /tf /tf_static"
+export BAG_ALWAYS="/odom /absodom /clock /tf /tf_static /diagnostics_agg"
 
 # Define the directory that the bags will be stored in
 export BAG_DIR=~/bags
@@ -21,7 +21,10 @@ export bag_starboard_cam="/camera/starboard/image_raw /camera/starboard/camera_i
 export bag_down_cam="/camera/down/camera_info /camera/down/image_raw"
 export bag_velodyne="/velodyne_points"
 export bag_sick="/scan"
-export bag_motors="/BL_motor/cmd /BL_motor/feedback /BL_motor/status /BR_motor/cmd /BR_motor/feedback /BR_motor/status /FL_motor/cmd /FL_motor/feedback /FL_motor/status /FR_motor/cmd /FR_motor/feedback /FR_motor/status"
-export bag_status="/c3_trajectory_generator/waypoint /trajectory /wrench/cmd /wrench/selected /battery_monitor /adaptation /diagnostics /learn /alarm /alarm_raise"
+export bag_motors="/BL_motor/cmd /BL_motor/feedback /BL_motor/status /BR_motor/cmd /BR_motor/feedback /BR_motor/status /FL_motor/cmd /FL_motor/feedback /FL_motor/status /FR_motor/cmd /FR_motor/feedback /FR_motor/status /battery_monitor"
 export bag_hydrophones="/hydrophones/debug /hydrophones/ping /hydrophones/pose /hydrophones/processed "
-export bag_lqrrt="/ogrid_master /unclassified_markers /lqrrt/effor /lqrrt/focus /lqrrt/goal /lqrrt/impact /lqrrt/path /lqrrt/ref /lqrrt/tree /move_to/feedback /move_to/goal /move_to/result"
+export bag_object_detection="/database/objects"
+export bag_controller="/wrench/cmd /wrench/selected"
+export bag_guidance="/ogrid_master /lqrrt/ref /lqrrt/path /lqrrt/tree /lqrrt/goal /lqrrt/focus /lqrrt/effort /lqrrt/sampspace /lqrrt/effort /move_to/feedback /move_to/goal /move_to/result"
+export bag_gnc="$bag_guidance $bag_controller"
+export bag_kill="$bag_front_cam $bag_velodyne $bag_gnc $bag_starboard_cam $bag_motors"
