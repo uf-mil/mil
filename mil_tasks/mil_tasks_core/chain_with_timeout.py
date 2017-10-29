3926818dc9338151d9bfbeaf65b5eff80d2c02ca
Kevin Allen <kma1660@gmail.com>
2017-10-29 18:45:50 -0400

    TASKS: add required option to chained tasks to continue on subtask fail

--- mil_tasks/mil_tasks_core/chain_with_timeout.py
+++ mil_tasks/mil_tasks_core/chain_with_timeout.py
@@ -1,5 +1,8 @@
+from exceptions import TimeoutException, SubtaskException, ParametersException
 from twisted.internet import defer
+from twisted.python import failure
 from txros import util
+import json
 
 
 def MakeChainWithTimeout(base):
@@ -18,7 +21,7 @@ def MakeChainWithTimeout(base):
             Runs a child task, throwing an exception if more time than specified in timeout
             passes before the task finishes executing.
             '''
-            subtask = self.run_subtask(task)
+            subtask = self.run_subtask(task, parameters)
             if timeout == 0:  # Timeout of zero means no timeout
                 result = yield subtask
                 defer.returnValue(result)
@@ -30,40 +33,61 @@ def MakeChainWithTimeout(base):
                 defer.returnValue(result)
             if index == 1:
                 yield subtask.cancel()
-                raise Exception('{} timedout'.format(task))
+                raise TimeoutException(timeout)
 
-        def verify_parameters(self, parameters):
+        @classmethod
+        def decode_parameters(cls, parameters):
             '''
-            Verifies the parameters are valid for chaining tasks, raising
-            an exception otherwise
+            Goes through list of tasks to chain and fills in missing
+            attributes like timeout with defaults. If something is invalid,
+            raise an exception.
             '''
-            if type(parameters) != list:
-                raise Exception('parameters must be a list')
-            for mission in parameters:
-                if 'task' not in mission:
-                    raise Exception('invalid parameters, element missing task')
-                if not self.has_task(mission['task']):
+            parameters = json.loads(parameters)
+            if type(parameters) != dict:
+                raise ParametersException('must be a dictionary')
+            if not 'tasks' in parameters:
+                raise ParametersException('must have "tasks" list')
+            if not isinstance(parameters['tasks'], list):
+                raise ParametersException('"tasks" attribute must be a list')
+            for task in parameters['tasks']:
+                if 'task' not in task:
+                    raise Exception('invalid parameters, missing attribute "task"')
+                if not cls.has_task(task['task']):
                     raise Exception('task "{}" not available'.format(mission['task']))
+                if 'parameters' not in task:
+                    task['parameters'] = ''
+                try:
+                    task['parameters'] = cls.get_task(task['task']).decode_parameters(task['parameters'])
+                except Exception as e:
+                    raise ParametersException('Invalid parameters for {}: {}'.format(task['task'], str(e)))
+                if 'timeout' not in task:
+                    task['timeout'] = 0
+                if 'required' not in task:
+                    task['required'] = True
+            return parameters
 
         @util.cancellableInlineCallbacks
         def run(self, parameters):
             '''
             Runs a list of child tasks specified in the parameters with optional timeouts.
-
-            Currently, if any task timesout or raises an exception, the whole chain is broken and
-            is aborted.
-
-            TODO: allow child tasks to be "optional" and not abort the whole chain if they fail or timeout
             '''
-            self.verify_parameters(parameters)
-            for mission in parameters:  # Run each child task linearly
-                task = mission['task']
-                timeout = mission['timeout'] if 'timeout' in mission else 0
-                parameters = mission['parameters'] if 'parameters' in mission else ''
-                self.send_feedback('Running {}'.format(task))
-                yield self.run_subtask_with_timeout(task, timeout, parameters)
-                self.send_feedback('Done with {}'.format(task))
-                yield self.nh.sleep(1.0)
+            for task in parameters['tasks']:  # Run each child task linearly
+                def cb(final):
+                    '''
+                    Called when a subtask finishes. If it succeeded, print the result in feedback.
+                    If it failed or timedout, print the failure and stop the whole chain if that
+                    task is required.
+                    '''
+                    if isinstance(final, failure.Failure):
+                        self.send_feedback('{} FAILED: {}'.format(task['task'], final.getErrorMessage()))
+                        if task['required']:  # Fail whole chain if a required task times out or fails
+                            raise SubtaskException(task['task'], final.getErrorMessage())
+                    else:
+                        self.send_feedback('{} SUCCEEDED: {}'.format(task['task'], final))
+                        print 'NO FAIL BRO'
+                df = self.run_subtask_with_timeout(task['task'], task['timeout'], task['parameters'])
+                df.addBoth(cb)
+                yield df
             self.send_feedback('Done with all')
-            defer.returnValue('All tasks succeeded!')
+            defer.returnValue('All tasks complete or skipped.')
     return ChainWithTimeout
