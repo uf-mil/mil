64110cc8173f165ea408e624c12eccb846f2f5a7
Kevin Allen <kma1660@gmail.com>
2018-02-08 19:13:37 -0500

    ALARMS: add larger slop factor on network heartbeat

--- mission_control/navigator_alarm/navigator_alarm_handlers/network_loss.py
+++ mission_control/navigator_alarm/navigator_alarm_handlers/network_loss.py
@@ -6,7 +6,7 @@ class NetworkLoss(HandlerBase):
     alarm_name = 'network-loss'
 
     def __init__(self):
-        self.hm = HeartbeatMonitor(self.alarm_name, '/network', Header, node_name='alarm_server')
+        self.hm = HeartbeatMonitor(self.alarm_name, '/network', Header, node_name='alarm_server', prd=1.0)
 
     def raised(self, alarm):
         pass
