9b721b22a5acab725806ff7b506a702189666146
Mukhil <mukhil@outlook.com>
2018-06-04 14:03:19 -0400

    DICE: Added 1 frame per sec limitation, Added debug Image

--- perception/sub8_perception/nodes/dice_detect.py
+++ perception/sub8_perception/nodes/dice_detect.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 '''
 script to detect dice , and mark the value of the dice
-
 Subscribes to Image topic
 Publishes Image and Points
-
 '''
+
 from __future__ import print_function
 import sys
 import rospy
@@ -14,9 +13,8 @@ import cv2
 import numpy as np
 from sensor_msgs.msg import Image   # To publish Image
 from geometry_msgs.msg import Point     # To publish Points
+from std_msgs.msg import String # To publish debug statements
 from cv_bridge import CvBridge, CvBridgeError
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import pandas as pd
 from scipy.spatial import distance
 
@@ -32,35 +30,48 @@ SENTINEL_INFINITE=10000
 class DiceDetect(object):
     """docstring for DiceDetect."""
     def __init__(self):
+        '''
+        debug_image_publisher publishes the final detected image
+        debug_image_input_publisher publishes input image to the detect function
+        debug_talker publishes chatter of whether the program is running
+        '''
         self.debug_image_publisher= rospy.Publisher("dice/debugimage", Image,queue_size=1)
+        self.debug_image_input_publisher= rospy.Publisher("dice/debugimageinput", Image,queue_size=1)
         self.point_publisher= rospy.Publisher("dice/points", Point, queue_size=1)
-        self.image_subscriber= rospy.Subscriber("/camera/seecam/image_color", Image, self.callback)
+        self.debug_talker= rospy.Publisher("dice/debugtalker", String, queue_size=1)
+        self.image_subscriber= rospy.Subscriber("/camera/front/left/image_rect_color", Image, self.callback)
         self.bridge = CvBridge()
-        ##self.dice_img = dice_img
-        ###self.detect()
+
 
     def detect(self, dice_img):
 
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
         params.filterByColor=1
-        params.blobColor= 0
-        params.minDistBetweenBlobs = 1
+        params.blobColor= 0   # 0 to pick dark blobs , 255 to pick white blobs
+        params.minDistBetweenBlobs = 0
+
         # Change thresholds
         params.minThreshold = 0;
         params.maxThreshold = 255;
+        ## The idea is that, we want to check all thresholds to take care of varied lighting conditions
+
         # Filter by Area.
         params.filterByArea = True
-        params.minArea = 10
+        params.minArea = 25 # in pixels , 10 to pick up from far away , but also picks up noise
+        # 25 is an ideal amount
+        params.maxArea = 200000 # max are in pixels
+
         # Filter by Circularity
         params.filterByCircularity = True
-        params.minCircularity = 0.80
+        params.minCircularity = 0.70
         # Filter by Convexity
         params.filterByConvexity = True
-        params.minConvexity = 0.87
+        params.minConvexity = 0.8  # 1 = perfect convex hull
         # Filter by Inertia
         params.filterByInertia = True
-        params.minInertiaRatio = 0.5
+        params.minInertiaRatio = 0.4    # Defines the ellipsoid 1= detects only cirlces
+                                        # 0 = Detects even lines
 
         # Create a detector with the parameters
         ver = (cv2.__version__).split('.')
@@ -68,12 +79,6 @@ class DiceDetect(object):
             detector = cv2.SimpleBlobDetector(params)
         else :
             detector = cv2.SimpleBlobDetector_create(params)
-
-
-        ## debug to view input image into the function
-        #cv2.imshow("Orig image", im_c)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
 
         # im_c = Colored image
         im_c = dice_img
@@ -86,21 +91,12 @@ class DiceDetect(object):
 
         # For Visualization purposes
         # Draw detected blobs as red circles
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv2.drawKeypoints(
         im_c, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        ## Debug Image to display detected blobs
-        # cv2.namedWindow("Keypoints",cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow("Keypoints", WINDOW_SIZE_H,WINDOW_SIZE_W)
-        # cv2.imshow("Keypoints", im_with_keypoints)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
         ## Debug to print all blobs detected
         key_len=len(keypoints)
         #print(keypoints)
-        ##print(" ")
+
 
         ## Neighborhood search algorithm
         '''
@@ -113,46 +109,35 @@ class DiceDetect(object):
 
         for i in range(0,key_len):
             a=keypoints[i].pt
-            dist=SENTINEL_INFINITE ## temp
+            dist=SENTINEL_INFINITE
             count=0
             for j in range(i+1,key_len):
                 b=keypoints[j].pt
                 dist_itr = distance.euclidean(a,b)
-                ## Debug Statement
-                #print(dist_itr)
                 if (dist_itr<= dist and dist_itr<DIST_THRESHOLD ):
                     dist =dist_itr
-
-            ## Debug Statement
-            #print("The minimum dist is : " + str(dist) )
 
             for j in range(0,key_len):
                 b=keypoints[j].pt
                 dist_itr = distance.euclidean(a,b)
-                #print(dist_itr)
                 if (dist_itr<= SEARCH_RANGE_FOR_PIPS*dist):
                     count=count+1
             if (dist==SENTINEL_INFINITE) :
                 count=1
 
 
-            if count > 4:
+            if count > 4 :
                 ## Debug Statement
                 ## Prints all points which correspond to dice of value 4 or more
-                ##print(str(i)+" th detection is " + str(count) ) ## Debug
+                print(str(i)+" th detection is " + str(count) ) ## Debug
                 d[str(count)]= [keypoints[i].pt[0], keypoints[i].pt[1] ]
 
                 font_face = cv2.FONT_HERSHEY_SIMPLEX
                 cv2.putText(im_with_keypoints, ( str(int(keypoints[i].pt[0]))+', ' +str(int(keypoints[i].pt[1])) ),( int(keypoints[i].pt[0]), int(keypoints[i].pt[1])), font_face, 1, (255,255,255), 1, cv2.LINE_AA)
 
-        ## Debug to display image, keypoints and x, y values displayed as text
-        # cv2.namedWindow("Keypoints",cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow("Keypoints", WINDOW_SIZE_H,WINDOW_SIZE_W)
-        # cv2.imshow("Keypoints", im_with_keypoints)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        ## im_with_keypoints now displays image, keypoints and x, y values displayed as text
 
-        return d, im_with_keypoints
+        return d, im_with_keypoints, im_c
 
     def callback(self, subscriberd_data):
 
@@ -163,23 +148,29 @@ class DiceDetect(object):
             print("Error at Cv.Bridge at Subscribing")
             print(e)
 
+        # rospy.sleep is necessary to process the videofeed
+        rospy.sleep(1.) # 1 frame per
+        print("Dice Detection Running ")
+
         ## output_dict is a dictionary of points as values and the count as key
         ## output_image is an OpenCv image
-        output_dict, output_image=self.detect(cv_image)
+        output_dict, output_image, input_image=self.detect(cv_image)
 
         for key, value in output_dict.iteritems():
-            x_pos= value[0]
-            y_pos= value[1]
-            self.point_publisher.publish(Point(x_pos, y_pos, 0))
-            try:
-                self.debug_image_publisher.publish(self.bridge.cv2_to_imgmsg(output_image, "bgr8"))
-                self.point_publisher.publish(Point(x_pos, y_pos, int(key)))
-                # print("success")
-            except CvBridgeError as e:
-                print("CvBridge Error at Publishing")
-                print(e)
 
+            if (int(key) > 0) :
+                x_pos= value[0]
+                y_pos= value[1]
 
+                try:
+                    self.debug_image_publisher.publish(self.bridge.cv2_to_imgmsg(output_image, "bgr8"))
+                    self.debug_image_input_publisher.publish(self.bridge.cv2_to_imgmsg(input_image, "bgr8"))
+                    self.point_publisher.publish(Point(x_pos, y_pos, int(key)))
+                    self.debug_talker.publish(String("Dice Detection Running"))
+                    # print("success")
+                except CvBridgeError as e:
+                    print("CvBridge Error at Publishing")
+                    print(e)
 
 
 def main(args):
