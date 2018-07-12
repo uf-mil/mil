ac8a5060454a183c24565831510abc14891cf862
RustyBamboo <dvolya@gmail.com>
2018-07-12 08:50:30 -0400

    POOL: address and fix misc
    * rise coefs
    * down cam frame rate
    * depth bug in active sonar
    * increase Heartbeat monitor duration to avoid unintended autonomous misfiring

--- command/sub8_missions/missions/strip.py
+++ command/sub8_missions/missions/strip.py
@@ -1,7 +1,7 @@
 from txros import util
 from mil_misc_tools import text_effects
 
-SPEED_LIMIT = 0.1  # m/s
+SPEED_LIMIT = 0.15  # m/s
 
 fprint = text_effects.FprintFactory(title="STRIPPER", msg_color="cyan").fprint
 
@@ -16,31 +16,43 @@ def pitch(sub):
 
 @util.cancellableInlineCallbacks
 def run(sub):
-    yield sub.move.downward(0.7).go()
     fprint('Starting...')
+    yield sub.nh.sleep(5)
+    yield sub.move.forward(0).zero_roll_and_pitch().go()
+    yield sub.move.down(1).go(speed=0.1)
+    yield sub.nh.sleep(3)
     start = sub.move.forward(0).zero_roll_and_pitch()
-    fprint('Searching... pitching...')
-    yield pitch(sub)
+    # fprint('Searching... pitching...')
+    # yield pitch(sub)
     fprint('Moving to gate')
     gate = start.forward(3)
     yield gate.go(speed=SPEED_LIMIT)
-    fprint('Going past gate')
-    yield sub.move.forward(3).go(speed=SPEED_LIMIT)
-    fprint('Searching... pitching')
-    yield pitch(sub)
+    yield sub.nh.sleep(3)
+    # fprint('Searching... pitching')
+    # yield pitch(sub)
+    fprint('Going right in front of pole')
+    yield sub.move.forward(8.7).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(3)
     fprint('Going around pole')
-    yield sub.move.forward(8).go(speed=SPEED_LIMIT)
-    yield sub.move.left(1).go(speed=SPEED_LIMIT)
-    yield sub.move.forward(1.2).go(speed=SPEED_LIMIT)
-    yield sub.move.right(2).go(speed=SPEED_LIMIT)
-    yield sub.move.backward(1.2).go(speed=SPEED_LIMIT)
-    yield sub.move.left(1).go(speed=SPEED_LIMIT)
+    yield sub.move.left(1.3).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(3)
+    yield sub.move.forward(2).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(3)
+    yield sub.move.right(2.6).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(3)
+    yield sub.move.backward(2).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(3)
+    yield sub.move.left(1.3).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(5)
 
     fprint('Turning back to gate')
     yield sub.move.backward(1).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(5)
     fprint('Look at gate')
     yield sub.move.look_at(gate._pose.position).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(5)
     fprint('Going to gate')
     yield gate.yaw_left_deg(180).go(speed=SPEED_LIMIT)
+    yield sub.nh.sleep(5)
     fprint('Go past through gate')
     yield start.yaw_left_deg(180).go(speed=SPEED_LIMIT)
