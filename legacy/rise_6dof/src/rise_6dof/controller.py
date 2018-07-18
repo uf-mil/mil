d6a8cc1d82b0b5bbba97baec4c6561a1589bb24b
RustyBamboo <dvolya@gmail.com>
2018-07-18 12:20:49 -0400

    CONTROLLER: add debug

--- legacy/rise_6dof/src/rise_6dof/controller.py
+++ legacy/rise_6dof/src/rise_6dof/controller.py
@@ -7,6 +7,8 @@ from sub8.pose_editor import quat_to_rotvec
 
 numpy.set_printoptions(suppress=True, linewidth=130)
 
+DEBUG = False
+
 
 class Controller(object):
     '''
@@ -26,9 +28,11 @@ class Controller(object):
         self._rise_term_int_prev = numpy.zeros(6)
 
     def update(self, dt, desired, current):
-        print('='*130)
+        print('=' * 130)
         (p, o), (p_dot, o_dot) = current
-        (desired_p, desired_o), (desired_p_dot, desired_o_dot), (desired_p_dotdot, desired_o_dotdot) = desired
+        (desired_p, desired_o), (desired_p_dot,
+                                 desired_o_dot), (desired_p_dotdot,
+                                                  desired_o_dotdot) = desired
         world_from_body = transformations.quaternion_matrix(o)[:3, :3]
         x_dot = numpy.concatenate([
             world_from_body.dot(p_dot),
@@ -45,15 +49,16 @@ class Controller(object):
             world_from_body.dot(desired_o_dotdot),
         ])
 
-        print('{:20s} {:30}'.format('Desired Velocity:', desired_x_dot))
-
+        if DEBUG:
+            print('{:20s} {:30}'.format('Desired Velocity:', desired_x_dot))
 
         error_position_world = numpy.concatenate([
             desired_p - p,
-            quat_to_rotvec(transformations.quaternion_multiply(
-                desired_o,
-                transformations.quaternion_inverse(o),
-            )),
+            quat_to_rotvec(
+                transformations.quaternion_multiply(
+                    desired_o,
+                    transformations.quaternion_inverse(o),
+                )),
         ])
         if self.config['two_d_mode']:
             error_position_world = error_position_world * [1, 1, 0, 0, 0, 1]
@@ -66,19 +71,27 @@ class Controller(object):
 
         error_velocity_world = (desired_x_dot + body_gain(
             numpy.diag(self.config['k'])).dot(error_position_world)) - x_dot
-        print('{:20s} {:30}'.format('Error velocity:', error_velocity_world))
+
+        if DEBUG:
+            print('{:20s} {:30}'.format('Error velocity:',
+                                        error_velocity_world))
+
         if self.config['two_d_mode']:
             error_velocity_world = error_velocity_world * [1, 1, 0, 0, 0, 1]
 
-        pd_output = body_gain(numpy.diag(self.config['ks'])).dot(error_velocity_world)
-        print('{:20s} {:30}'.format('pd_output:', pd_output))
+        pd_output = body_gain(numpy.diag(
+            self.config['ks'])).dot(error_velocity_world)
+
+        if DEBUG:
+            print('{:20s} {:30}'.format('pd_output:', pd_output))
 
         output = pd_output
         if self.config['use_rise']:
             rise_term_int = body_gain(numpy.diag(self.config['ks'] * self.config['alpha'])).dot(error_velocity_world) +\
                 body_gain(numpy.diag(self.config['beta'])).dot(numpy.sign(error_velocity_world))
 
-            self._rise_term = self._rise_term + dt / 2 * (rise_term_int + self._rise_term_int_prev)
+            self._rise_term = self._rise_term + dt / 2 * (
+                rise_term_int + self._rise_term_int_prev)
             self._rise_term_int_prev = rise_term_int
 
             output = output + self._rise_term
@@ -86,11 +99,14 @@ class Controller(object):
             # zero rise term so it doesn't wind up over time
             self._rise_term = numpy.zeros(6)
             self._rise_term_int_prev = numpy.zeros(6)
-        output = output + body_gain(numpy.diag(self.config['accel_feedforward'])).dot(desired_x_dotdot)
-        output = output + body_gain(numpy.diag(self.config['vel_feedforward'])).dot(desired_x_dot)
+        output = output + body_gain(
+            numpy.diag(self.config['accel_feedforward'])).dot(desired_x_dotdot)
+        output = output + body_gain(
+            numpy.diag(self.config['vel_feedforward'])).dot(desired_x_dot)
 
         # Permitting lambda assignment b/c legacy
         wrench_from_vec = lambda output: (world_from_body.T.dot(output[0:3]), world_from_body.T.dot(output[3:6]))  # noqa
-        print('{:6} {}'.format('PD:', wrench_from_vec(pd_output)))
-        print('{:6} {}'.format('PID(R):', wrench_from_vec(output)))
+        if DEBUG:
+            print('{:6} {}'.format('PD:', wrench_from_vec(pd_output)))
+            print('{:6} {}'.format('PID(R):', wrench_from_vec(output)))
         return wrench_from_vec(pd_output), wrench_from_vec(output)
