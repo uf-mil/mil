52d3334e28e797e71e9b912e54d6c10e3f4c9d52
Lucas Bassett-Audain <LucasBA@users.noreply.github.com>
2018-07-27 19:56:41 -0400

    Switch guess points to interactive markers (#362)
    * GUESS: Switch to interactive markers from points
    
    * GUESS: Lint guess server
    
    * GUESS: format and normalize quat

--- perception/sub8_perception/nodes/guess_server.py
+++ perception/sub8_perception/nodes/guess_server.py
@@ -3,32 +3,73 @@ import sys
 import rospy
 from sub8_msgs.srv import GuessRequest, GuessRequestResponse
 from geometry_msgs.msg import PoseStamped
+from interactive_markers.interactive_marker_server import InteractiveMarker, InteractiveMarkerServer
+from visualization_msgs.msg import Marker, InteractiveMarkerControl
 
 
 class Guess:
-
     def __init__(self):
         rospy.sleep(1.0)
-        self.items = ['pinger1', 'pinger2']
-        self.locations = []
-        self.guess_service = rospy.Service('guess_location', GuessRequest, self.request_location)
-        self.clicked_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.add_guess_to_list)
+        self.items = ['pinger1', 'pinger2', 'dice']
+        self.guess_service = rospy.Service('guess_location', GuessRequest,
+                                           self.request_location)
+        self.markers_subscribers = []
+        self.markers_locations = dict.fromkeys(self.items)
+        self.markers_servers = []
+        self.markers = []
+        box_marker = Marker()
+        box_marker.type = Marker.CUBE
+        box_marker.scale.x = 0.45
+        box_marker.scale.y = 0.45
+        box_marker.scale.z = 0.45
+        box_marker.color.r = 0.0
+        box_marker.color.g = 0.5
+        box_marker.color.b = 0.5
+        box_marker.color.a = 1.0
+        box_control = InteractiveMarkerControl()
+        box_control.always_visible = True
+        box_control.markers.append(box_marker)
+        rotate_control = InteractiveMarkerControl()
+        rotate_control.name = "move_x"
+        rotate_control.orientation.w = 0.707
+        rotate_control.orientation.x = 0
+        rotate_control.orientation.y = 0.707
+        rotate_control.orientation.z = 0
+        rotate_control.interaction_mode = InteractiveMarkerControl.MOVE_PLANE
+        spacer = 0
+        for i in self.items:
+            self.markers.append(InteractiveMarker())
+            self.markers[spacer].header.frame_id = "base_link"
+            self.markers[spacer].name = i
+            self.markers[spacer].description = i
+            self.markers[spacer].controls.append(box_control)
+            self.markers[spacer].controls.append(rotate_control)
+            self.markers[spacer].pose.position.x = spacer
+            self.markers[spacer].pose.position.y = 0
+            self.markers[spacer].pose.position.z = 0
+            spacer = spacer + 1
+
+    def process_feedback(self, feedback):
+        self.markers_locations[feedback.marker_name] = PoseStamped(
+            header=feedback.header, pose=feedback.pose)
 
     def request_location(self, srv):
         req_item = srv.item
         if (req_item in self.items):
-            return GuessRequestResponse(location=self.locations[self.items.index(req_item)], found=True)
+            return GuessRequestResponse(
+                location=self.markers_locations[req_item], found=True)
         else:
             return GuessRequestResponse(found=False)
 
-    def add_guess_to_list(self, msg):
-        print('Added Item')
-        self.locations.append(msg)
-
 
 def main(args):
-    Guess()
+    server = InteractiveMarkerServer("guess_markers")
+    guess = Guess()
+    for i in range(len(guess.items)):
+        server.insert(guess.markers[i], guess.process_feedback)
+    server.applyChanges()
     rospy.spin()
+
 
 if __name__ == '__main__':
     rospy.init_node('guess')
