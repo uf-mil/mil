81748ca4034ca2bc728ad82e681582d4c5ece19f
RustyBamboo <dvolya@gmail.com>
2018-07-27 21:52:45 -0400

    START_GATE(SIMPLE): remember starting orientation

--- command/sub8_missions/missions/start_gate_simple.py
+++ command/sub8_missions/missions/start_gate_simple.py
@@ -7,6 +7,12 @@ FORWARD_METERS = 3.0
 @util.cancellableInlineCallbacks
 def run(sub):
     fprint = text_effects.FprintFactory(title='START_GATE (SIMPLE)').fprint
+    fprint('Remembering starting orientation')
+    sub_start_position, sub_start_orientation = yield sub.tx_pose()
+    fprint('Going down')
+    down = sub.move.down(1)
+    yield down.go(speed=0.1)
     fprint('Going forward')
-    yield sub.move.zero_roll_and_pitch().forward(FORWARD_METERS).go()
+    yield down.set_orientation(sub_start_orientation).zero_roll_and_pitch(
+    ).forward(FORWARD_METERS).go(speed=0.2)
     fprint('Done!')
