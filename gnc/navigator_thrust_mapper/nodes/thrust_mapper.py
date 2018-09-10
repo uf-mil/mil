bfeadd41674fd933d9f085b5b98c488a44f6cdde
Kevin Allen <kma1660@gmail.com>
2018-09-10 17:50:17 -0400

    GNC: refactor thruster_map to use URDF

--- gnc/navigator_thrust_mapper/nodes/thrust_mapper.py
+++ gnc/navigator_thrust_mapper/nodes/thrust_mapper.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import WrenchStamped
 from roboteq_msgs.msg import Command
 from ros_alarms import AlarmListener
 from navigator_thrust_mapper import ThrusterMap
+from sensor_msgs.msg import JointState
 
 
 class ThrusterMapperNode(object):
@@ -14,7 +15,10 @@ class ThrusterMapperNode(object):
     '''
     def __init__(self):
         # Used for mapping wrench to individual thrusts
-        self.thruster_map = ThrusterMap.from_ros_params()
+        urdf = rospy.get_param('robot_description', default=None)
+        if urdf is None or len(urdf) == 0:
+            raise Exception('robot description not set or empty')
+        self.thruster_map = ThrusterMap.from_urdf(urdf)
 
         # To track kill state so no thrust is sent when killed (kill board hardware also ensures this)
         self.kill = False
@@ -24,11 +28,19 @@ class ThrusterMapperNode(object):
         # Start off with no wrench
         self.wrench = np.zeros(3)
 
-        # ROS setup
-        self.BL_pub = rospy.Publisher("/BL_motor/cmd", Command, queue_size=1)
-        self.BR_pub = rospy.Publisher("/BR_motor/cmd", Command, queue_size=1)
-        self.FL_pub = rospy.Publisher("/FL_motor/cmd", Command, queue_size=1)
-        self.FR_pub = rospy.Publisher("/FR_motor/cmd", Command, queue_size=1)
+        # Publisher for each thruster
+        self.publishers = [rospy.Publisher("/{}_motor/cmd".format(name), Command, queue_size=1)
+                           for name in self.thruster_map.names]
+
+        # Joint state publisher
+        # TODO(ironmig):
+        self.joint_state_pub = rospy.Publisher('/thruster_states', JointState, queue_size=1)
+        self.joint_state_msg = JointState()
+        for name in self.thruster_map.joints:
+            self.joint_state_msg.name.append(name)
+            self.joint_state_msg.position.append(0)
+            self.joint_state_msg.effort.append(0)
+
         rospy.Subscriber("/wrench/cmd", WrenchStamped, self.wrench_cb)
 
     def kill_cb(self, alarm):
@@ -47,14 +59,18 @@ class ThrusterMapperNode(object):
         Use the mapper to find the individual thrusts needed to acheive the current body wrench.
         If killed, publish 0 to all thrusters just to be safe.
         '''
-        BL, BR, FL, FR = Command(), Command(), Command(), Command()
-        if not self.kill:  # Only get thrust of not killed, otherwise publish default 0 thrust
-            BL.setpoint, BR.setpoint, FL.setpoint, FR.setpoint = self.thruster_map.wrench_to_thrusts(self.wrench)
-        self.BL_pub.publish(BL)
-        self.BR_pub.publish(BR)
-        self.FL_pub.publish(FL)
-        self.FR_pub.publish(FR)
-
+        commands = [Command() for i in range(len(self.publishers))]
+        if not self.kill:
+            thrusts = self.thruster_map.wrench_to_thrusts(self.wrench)
+            if len(thrusts) != len(self.publishers):
+                rospy.logfatal("Number of thrusts does not equal number of publishers")
+                return
+            for i in range(len(self.publishers)):
+                commands[i].setpoint = thrusts[i]
+        for i in range(len(self.publishers)):
+            self.joint_state_msg.effort[i] = commands[i].setpoint
+            self.publishers[i].publish(commands[i])
+        self.joint_state_pub.publish(self.joint_state_msg)
 
 if __name__ == "__main__":
     rospy.init_node('thrust_mapper')
