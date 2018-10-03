e03bac11106ed9da632006d90f31755c610eaf40
grymestone <grymestone@ufl.edu>
2018-10-02 23:09:16 -0400

    TENSORFLOW: Make python libraries possible, update docker image and dockerfile.

--- perception/docker_tf/pipeline.sh
+++ perception/docker_tf/pipeline.sh
@@ -1,3 +1,4 @@
 docker run --runtime=nvidia --rm -it \
 -v $PWD/transfer_learning:/tensorflow/models/research/object_detection/transfer_learning:rw \
---privileged -p 6006:6006 grymestone:tensorpipeline01
+--privileged -p 6006:6006 mil-common/docker_tf:latest
+# --privileged -p 6006:6006 grymestone:tensorpipeline01
