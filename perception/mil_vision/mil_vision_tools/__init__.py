d683d253061713f57720bf06df7e6f60073dc0bf
Kevin Allen <kma1660@gmail.com>
2018-11-02 18:41:48 -0400

    VISION: add objects tracker

--- perception/mil_vision/mil_vision_tools/__init__.py
+++ perception/mil_vision/mil_vision_tools/__init__.py
@@ -5,3 +5,4 @@ from image_proc import ImageProc, ImageSet
 from labelbox_parser import LabelBoxParser
 from color_classifier import ContourClassifier, GaussianColorClassifier
 from vision_node import create_object_msg, VisionNode
+from objects_tracker import ObjectsTracker, CentroidObjectsTracker
