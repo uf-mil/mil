3601b28d9ef5e225c3fbf82638ef1bceebceb4e9
Boris Ermakov-Spektor <b.ermakovspektor@ufl.edu>
2018-12-09 22:00:16 -0800

    Clang format

--- utils/navigator_judgepanel/src/UI.cpp
+++ utils/navigator_judgepanel/src/UI.cpp
@@ -31,13 +31,15 @@ UI::UI()
   timer->start(1000);
 }
 
-UI::~UI() {
+UI::~UI()
+{
   delete timer;
   delete nh;
 }
 
-void UI::scanTheCodeCallback(const navigator_msgs::ScanTheCode &scan_the_code) {
-    this->color_pattern = scan_the_code.color_pattern;
+void UI::scanTheCodeCallback(const navigator_msgs::ScanTheCode &scan_the_code)
+{
+  this->color_pattern = scan_the_code.color_pattern;
 }
 
 void UI::slotTimer()
@@ -46,28 +48,32 @@ void UI::slotTimer()
   ros::spinOnce();
 
   // Scan the code GUI
-  std::array<QLabel*, 3> l = { widget.label_Color1, widget.label_Color2, widget.label_Color3 };
-  std::array<QFrame*, 3> w = { widget.color1, widget.color2, widget.color3 };
+  std::array<QLabel *, 3> l = { widget.label_Color1, widget.label_Color2, widget.label_Color3 };
+  std::array<QFrame *, 3> w = { widget.color1, widget.color2, widget.color3 };
 
   if (this->color_pattern.length() != 3)
-      return;
+    return;
 
   for (size_t ii = 0; ii <= 2; ++ii)
   {
-    switch(color_pattern.at(ii)) {
-      case 'G': l[ii]->setText(tr("GREEN"));
-                w[ii]->setStyleSheet("background-color: green");
-                break;
-      case 'R': l[ii]->setText(tr("RED"));
-                w[ii]->setStyleSheet("background-color: red");
-                break;
-      case 'B': l[ii]->setText(tr("BLUE"));
-                w[ii]->setStyleSheet("background-color: blue");
-                break;
-      default: l[ii]->setText(tr(""));
-               w[ii]->setStyleSheet("background-color: white");
-               break;
+    switch (color_pattern.at(ii))
+    {
+      case 'G':
+        l[ii]->setText(tr("GREEN"));
+        w[ii]->setStyleSheet("background-color: green");
+        break;
+      case 'R':
+        l[ii]->setText(tr("RED"));
+        w[ii]->setStyleSheet("background-color: red");
+        break;
+      case 'B':
+        l[ii]->setText(tr("BLUE"));
+        w[ii]->setStyleSheet("background-color: blue");
+        break;
+      default:
+        l[ii]->setText(tr(""));
+        w[ii]->setStyleSheet("background-color: white");
+        break;
     }
   }
-
 }
--- utils/navigator_judgepanel/src/UI.h
+++ utils/navigator_judgepanel/src/UI.h
@@ -1,8 +1,8 @@
 #ifndef _UI_H
 #define _UI_H
 
-#include <navigator_msgs/ScanTheCode.h>
 #include <nav_msgs/Odometry.h>
+#include <navigator_msgs/ScanTheCode.h>
 #include <ros/console.h>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
@@ -23,11 +23,13 @@ public:
   QTimer* timer;
 public slots:
   void slotTimer();
+
 public:
   ros::NodeHandle* nh;
   ros::Subscriber sub;
-  void scanTheCodeCallback(const navigator_msgs::ScanTheCode &scan_the_code);
+  void scanTheCodeCallback(const navigator_msgs::ScanTheCode& scan_the_code);
   std::string color_pattern = "   ";
+
 private:
   Ui::UI widget;
 };
