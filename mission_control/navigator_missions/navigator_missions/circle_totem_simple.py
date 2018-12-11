f491edd6fc59a6ec3f28f7fb03c9c52fa1cd06af
Kevin Allen <kma1660@gmail.com>
2018-12-10 16:57:38 -1000

    recommit, circle totem work

--- mission_control/navigator_missions/navigator_missions/circle_totem_simple.py
+++ mission_control/navigator_missions/navigator_missions/circle_totem_simple.py
@@ -11,29 +11,38 @@ class CircleTotemSimple(Navigator):
     Simple mission to circle totems once they have been labeled, does not
     have searching funcitonality found
     '''
-    CIRCLE_DISTANCE = 6.0  # Distance around totem to circle
+    CIRCLE_DISTANCE = 4.0  # Distance around totem to circle
     DIRECTIONS = {'RED': 'cw', 'GREEN': 'ccw', 'BLUE': 'cw', 'YELLOW': 'ccw', 'WHITE': 'ccw'}
+
+    @classmethod
+    def decode_parameters(cls, parameters):
+        return parameters.split()
 
     @cancellableInlineCallbacks
     def run(self, parameters):
-        # Get colors of intrest and directions
-        c1 = self.mission_params['scan_the_code_color1'].get()
-        c2 = self.mission_params['scan_the_code_color2'].get()
-        c3 = self.mission_params['scan_the_code_color3'].get()
-        colors = [c1, c2, c3]
+        if len(parameters) != 3:
+            colors = ["RED", "GREEN", "BLUE"]
+            self.send_feedback("No colors Specified, defaulting to {}".format(" ".join(colors)))
+        else:
+            for color in parameters:
+                if color not in self.DIRECTIONS:
+                    raise Exception("Color {} is not known.".format(color))
+            colors = parameters
 
         targets = []
         for color in colors:
-            totem = yield self.get_colored_totem(color.lower())
-            if totem is None:
+            res = yield self.get_sorted_objects("totem_" + color.lower(), n=1, throw=False)
+            if res is None:
                 self.send_feedback('Totem {} not found'.format(color))
-            else:
-                self.send_feedback("Totem {} found!".format(color))
-                targets.append((rosmsg_to_numpy(totem.pose.position), color))
+                continue
+            position = res[1][0]
+            self.send_feedback("Totem {} found!".format(color))
+            targets.append((rosmsg_to_numpy(totem.pose.position), color))
             yield self.nh.sleep(0.1)
+
         if len(targets) == 0:
-            defer.returnValue('No totems with correct color found')
-        self.send_feedback('Targets: {}'.format(targets))
+            return False
+
         yield self.nh.sleep(0.1)
         for target in targets:
             color = target[1]
@@ -42,21 +51,20 @@ class CircleTotemSimple(Navigator):
             self.send_feedback('Attempting to circle {} {}'.format(color, direction))
             self.send_feedback('Moving in front of totem')
             yield self.nh.sleep(0.1)
-            res = yield self.move.look_at(position).set_position(position).backward(self.CIRCLE_DISTANCE).go()
-            self.send_feedback('RESDFDFD {}'.format(res))
+
+            # Move close to totem
+            move = self.move.look_at(position).set_position(position).backward(self.CIRCLE_DISTANCE)
+
+            # Rotate for faster rotate
+            if  direction == 'cw':
+                move = move.yaw_left(1.57)
+            else:
+                move = move.yaw_right(1.57)
+            yield move.go()
+
             self.send_feedback('Circling!')
             yield self.nh.sleep(0.1)
-            res = yield self.move.circle_point(position, direction=direction).go()
-            self.send_feedback('Got {}'.format(res))
+            res = yield self.move.circle_point(position, direction=direction, revolutions=1.5).go()
             self.send_feedback('Done circling')
             yield self.nh.sleep(0.1)
         defer.returnValue('Success')
-
-    @cancellableInlineCallbacks
-    def get_colored_totem(self, color):
-        totems = yield self.database_query("totem_{}".format(color), raise_exception=False)
-        if not totems.found:
-            defer.returnValue(None)
-        sort = sorted(totems.objects,
-                      key=lambda totem: np.linalg.norm(self.pose[0] - rosmsg_to_numpy(totem.pose.position)))
-        defer.returnValue(sort[0])
