d953d124da339eaeff5e1c13ca00264f8395a5eb
jaxon <jaxon007@verizon.net>
2018-12-15 02:53:07 -0500

    Cleaning up night before finals

--- mission_control/navigator_missions/navigator_missions/docking.py
+++ mission_control/navigator_missions/navigator_missions/docking.py
@@ -30,7 +30,6 @@ class Docking(Navigator):
         cls.parser = parser
 
         cls.bboxsub = cls.nh.subscribe("/bbox_pub", ObjectsInImage)
-
         cls.camera_lidar_tf = cls.nh.get_service_client('/camera_to_lidar/front_right_cam', CameraToLidarTransform)
 
     @util.cancellableInlineCallbacks
@@ -38,21 +37,68 @@ class Docking(Navigator):
         # Parse Arguments
         wait_time = args.time
 
-        dock = yield self.get_sorted_objects(name='dock', n=1)
-        dock = self.dock[0][0]
-        dock_position = rosmsg_to_numpy(self.dock.pose.position)
+        # Find Dock
+        dock_position = None
+        largest_size = 0
+        boat_pos = (yield self.tx_pose)[0]
+        # Get 10 closest unclassified objects
+        unclass = yield self.get_sorted_objects(name='UNCLASSIFIED', n=10)
+        for obj in unclass[0]:
+            point = rosmsg_to_numpy(obj.pose.position)
+            scale = rosmsg_to_numpy(obj.scale)
 
+            # Filter such that we know the dock is closer than 20 meters
+            if np.linalg.norm(point - boat_pos) > 20:
+                break
+
+            size = scale[0] * scale[1]
+
+            if size > largest_size:
+                largest_size = size
+                dock_position = point
+
+        if dock_position is None:
+            self.send_feedback('Cancelling, failed to find dock position')
+            return
+
+        self.send_feedback('Found dock, looking for image')
+
+        # Find the camera input
         center_frame = yield self.get_center_frame()
-        pose_offset = yield self.get_target_pt(center_frame)
-        symbol = pose_offset[1].
-        print pose_offset
-        yield self.move.set_position(pose_offset).look_at(dock_position).go(blind=True)
+        symbol = center_frame[2].lower()
 
-        timeout = None
-        dock_timing = False
+        self.send_feedback('Identified {}'.format(symbol))
+
+        # Find the target point
+        target_pt = yield self.get_target_pt(center_frame)
+
+        self.send_feedback('Identified target')
+
+        # Identify the time to wait in the dock
+        print target_pt
+
+        if wait_time == -1:
+            if 'triangle' in symbol:
+                wait_time = 7
+            elif 'circle' in symbol:
+                wait_time = 17
+            else: # Cruciform
+                wait_time = 27
+
+        # Go to pose
+        self.send_feedback('Moving into dock')
+        yield self.move.set_position(target_pt).look_at(dock_position).go(blind=True)
+
+        # Sleep the appropriate amount of time
+        self.send_feedback('------------------------------------------------')
+        self.send_feedback('!!!!!!!!!!!!! STATION KEEPING !!!!!!!!!!!!!!!!!!')
         yield self.nh.sleep(wait_time)
+        self.send_feedback('!!!!!!!!!!!!!!! EXITING DOCK !!!!!!!!!!!!!!!!!!!')
+        self.send_feedback('------------------------------------------------')
 
-        yield self.move.backward(10).go()
+        # Back out of the dock
+        yield self.move.backward(5).go(blind=True)
+        yield self.move.backward(5).go(blind=True)
 
 
         self.send_feedback('Done with docking!')
@@ -64,12 +110,7 @@ class Docking(Navigator):
         #print msg
         c1 = rosmsg_to_numpy(msg.points[0])
         c2 = rosmsg_to_numpy(msg.points[1])
-        c1 = np.array([c1[1], c1[0], 0])
-        c2 = np.array([c2[1], c2[0], 0])
-        c1 *= 4
-        c2 *= 4
-        tmp = (((c1 + c2) / 2.0), msgf, msg.find)
-        #print tmp
+        tmp = (((c1 + c2) / 2.0), msgf, msg.name)
         defer.returnValue(tmp)
 
     @util.cancellableInlineCallbacks
@@ -78,7 +119,7 @@ class Docking(Navigator):
         msg.header.stamp = center_frame[1].header.stamp
         msg.header.frame_id = center_frame[1].header.frame_id
         msg.point = Point(x=center_frame[0][0], y=center_frame[0][1], z=0.0)
-        msg.tolerance = 100
+        msg.tolerance = 500
 
         #print 'msg'
         #print msg
@@ -87,9 +128,6 @@ class Docking(Navigator):
         pose_offset = yield self.camera_lidar_tf(msg)
 
         #print pose_offset
-
-        #pose_offset = rosmsg_to_numpy(pose_offset.closest)
-
 
         cam_to_enu = yield self.tf_listener.get_transform('enu', center_frame[1].header.frame_id)
         normal = rosmsg_to_numpy(pose_offset.normal)
@@ -100,20 +138,24 @@ class Docking(Navigator):
         found_pt = cam_to_enu.transform_point(found_pt)
         found_pt[2] = 0
 
-        #print normal
-        #print found_pt
+        print('foundpt {}'.format(found_pt))
+        print('normal  {}'.format(normal))
 
+        # Extend out by normal multiplier
         normal *= 3
-        found_pt += normal
+        found_pt_1 = found_pt + normal
+        found_pt_2 = found_pt + -1 * normal
 
+        print('foundpt1 {}'.format(found_pt_1))
+        print('foundpt2 {}'.format(found_pt_2))
 
-
-
-        #boat_pose = yield self.tx_pose
-        #boat_pose = boat_pose[0]
-
-
-        #pose_offset = np.array([125, 0, 0]) - boat_pose
-
+        # Which is closer
+        boat_pos = (yield self.tx_pose)[0]
+        if np.linalg.norm(found_pt_1 - boat_pos) > np.linalg.norm(found_pt_2 - boat_pos):
+            found_pt = found_pt_2
+            print('selected 2')
+        else:
+            found_pt = found_pt_1
+            print('selected 1')
 
         defer.returnValue(found_pt)
