14c56865eff76fe3ee5f521eabd11790056abc63
Kevin Allen <kma1660@gmail.com>
2019-01-14 15:13:50 -0500

    MISSIONS: move files for mil_missions conversion

--- command/sub8_missions/setup.py
+++ command/sub8_missions/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['sub8', 'missions'],
+    packages=['sub8_missions'],
 )
 
 setup(**setup_args)
--- command/sub8_missions/sub8_missions/__init__.py
+++ command/sub8_missions/sub8_missions/__init__.py
@@ -6,3 +6,10 @@ for module in os.listdir(os.path.dirname(__file__)):
     __import__(module[:-3], locals(), globals())
 del module
 del os
+# flake8: noqa
+import sub_singleton as sub_singleton
+import pose_editor
+
+from sub_singleton import Searcher
+from sub_singleton import SonarPointcloud
+from sub_singleton import SonarObjects
