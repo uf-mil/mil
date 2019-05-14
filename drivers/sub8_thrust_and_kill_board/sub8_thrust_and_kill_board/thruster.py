dda4f4a76956e658d50ee40553cc1f8b02fe2a15
Kevin Allen <kma1660@gmail.com>
2019-05-13 21:06:46 -0400

    THRUSTERS: map thrust to effort in thruster handle

---
+++ drivers/sub8_thrust_and_kill_board/sub8_thrust_and_kill_board/thruster.py
@@ -0,0 +1,37 @@
+from numpy.polynomial.polynomial import polyval
+from numpy import clip
+
+
+def make_thruster_dictionary(dictionary):
+    '''
+    Make a dictionary mapping thruster names to Thruster objects
+    '''
+    ret = {}
+    for thruster, content in dictionary.iteritems():
+        ret[thruster] = Thruster.from_dict(content)
+    return ret
+
+
+class Thruster(object):
+    '''
+    Models the force (thrust) to PWM (effort) of a thruster
+    '''
+    def __init__(self, forward_calibration, backward_calibration):
+        self.forward_calibration = forward_calibration
+        self.backward_calibration = backward_calibration
+
+    @classmethod
+    def from_dict(cls, data):
+        forward_calibration = data['calib']['forward']
+        backward_calibration = data['calib']['backward']
+        return cls(forward_calibration, backward_calibration)
+
+    def effort_from_thrust_unclipped(self, thrust):
+        if thrust < 0:
+            return polyval(thrust, self.backward_calibration)
+        else:
+            return polyval(thrust, self.forward_calibration)
+
+    def effort_from_thrust(self, thrust):
+        unclipped = self.effort_from_thrust_unclipped(thrust)
+        return clip(unclipped, -5., .5)
