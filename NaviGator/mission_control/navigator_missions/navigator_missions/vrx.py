842e1d8f0233086335b905f99e149fbed928e312
Kevin Allen <kma1660@gmail.com>
2019-10-20 21:27:33 -0400

    Update vrx mission for station keeping and wayfinding

--- NaviGator/mission_control/navigator_missions/navigator_missions/vrx.py
+++ NaviGator/mission_control/navigator_missions/navigator_missions/vrx.py
@@ -2,12 +2,12 @@
 from __future__ import division
 import txros
 import numpy as np
-from navigator_msgs.srv import FindPinger, FindPingerRequest, SetFrequency, SetFrequencyRequest
-from std_srvs.srv import SetBool, SetBoolRequest
-import mil_tools
-from visualization_msgs.msg import Marker, MarkerArray
-from mil_misc_tools.text_effects import fprint
+from twisted.internet import defer
+from vrx_gazebo.msg import Task
+from robot_localization.srv import FromLL, FromLLRequest
+from geographic_msgs.msg import GeoPoseStamped, GeoPath
 from navigator import Navigator
+from mil_tools import rosmsg_to_numpy
 
 ___author___ = "Kevin Allen"
 
@@ -18,13 +18,59 @@ class Vrx(Navigator):
 
     @classmethod
     def init(cls):
-        cls.taks_info_sub = cls.nh.subscribe("/vrx/task/info", MarkerArray)
+        cls.from_lla = cls.nh.get_service_client('/fromLL', FromLL)
+        cls.station_keep_goal = cls.nh.subscribe('/vrx/station_keeping/goal', GeoPoseStamped)
+        cls.task_info_sub = cls.nh.subscribe("/vrx/task/info", Task)
+        cls.wayfinding_path_sub = cls.nh.subscribe("/vrx/wayfinding/waypoints", GeoPath)
 
     #@txros.util.cancellableInlineCallbacks
     def cleanup(self):
         pass
 
     @txros.util.cancellableInlineCallbacks
+    def geo_pose_to_enu_pose(self, geo):
+        self.send_feedback('Waiting for LLA conversion')
+        enu_msg = yield self.from_lla(FromLLRequest(ll_point=geo.position))
+        position_enu = rosmsg_to_numpy(enu_msg.map_point)
+        orientation_enu = rosmsg_to_numpy(geo.orientation)
+        defer.returnValue((position_enu, orientation_enu))
+
+    @txros.util.cancellableInlineCallbacks
+    def run_station_keeping(self):
+        self.send_feedback('Waiting for station keeping goal')
+        goal_msg = yield self.station_keep_goal.get_last_message()
+        goal_pose = yield self.geo_pose_to_enu_pose(goal_msg.pose)
+        self.send_feedback('Going to {}'.format(goal_pose))
+        yield self.move.set_position(goal_pose[0]).set_orientation(goal_pose[1]).go()
+
+    @txros.util.cancellableInlineCallbacks
+    def wait_for_task_to_do(self, f):
+        while True:
+            msg = yield self.task_info_sub.get_next_message()
+            if f(msg):
+                defer.returnValue(None)
+
+    @txros.util.cancellableInlineCallbacks
+    def run_wayfinding(self):
+        self.send_feedback('Waiting for task to start')
+        yield self.wait_for_task_to_do(lambda task: task.state in ['ready', 'running'])
+        path_msg = yield self.wayfinding_path_sub.get_last_message()
+        poses = [ (yield self.geo_pose_to_enu_pose(geo_pose.pose)) for geo_pose in path_msg.poses]
+        position = self.pose[0]
+        poses = sorted(poses, key=lambda pose: np.linalg.norm(pose[0] - position))
+        self.send_feedback('Sorted poses' + str(poses))
+        yield self.wait_for_task_to_do(lambda task: task.state in ['running'])
+        for pose in poses:
+            self.send_feedback('Gong to {}'.format(pose))
+            yield self.move.set_position(pose[0]).set_orientation(pose[1]).go()
+
+
+    @txros.util.cancellableInlineCallbacks
     def run(self, parameters):
-        self.send_feedback('Running VRX')
-        yield self.move.forward(0).go()
+        msg = yield self.task_info_sub.get_next_message()
+        task_name = msg.name
+        if task_name == 'stationkeeping':
+            yield self.run_station_keeping()
+        elif task_name == 'wayfinding':
+            yield self.run_wayfinding()
+        defer.returnValue(msg)
