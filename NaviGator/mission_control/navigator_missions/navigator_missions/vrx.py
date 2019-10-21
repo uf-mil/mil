e4142852d08bf3c39e9f3ee90c5073cf14ebbe9f
Kevin Allen <kma1660@gmail.com>
2019-10-20 22:38:20 -0400

    Wait for latching msg in vrx mission

--- NaviGator/mission_control/navigator_missions/navigator_missions/vrx.py
+++ NaviGator/mission_control/navigator_missions/navigator_missions/vrx.py
@@ -36,15 +36,23 @@ class Vrx(Navigator):
         defer.returnValue((position_enu, orientation_enu))
 
     @txros.util.cancellableInlineCallbacks
+    def get_latching_msg(self, sub):
+        msg = yield sub.get_last_message()
+        if msg is None:
+            msg = yield sub.get_next_message()
+        defer.returnValue(msg)
+
+    @txros.util.cancellableInlineCallbacks
     def run_station_keeping(self):
+        yield self.wait_for_task_such_that(lambda task: task.state in ['ready', 'running'])
         self.send_feedback('Waiting for station keeping goal')
-        goal_msg = yield self.station_keep_goal.get_last_message()
+        goal_msg = yield self.get_latching_msg(self.station_keep_goal)
         goal_pose = yield self.geo_pose_to_enu_pose(goal_msg.pose)
         self.send_feedback('Going to {}'.format(goal_pose))
         yield self.move.set_position(goal_pose[0]).set_orientation(goal_pose[1]).go()
 
     @txros.util.cancellableInlineCallbacks
-    def wait_for_task_to_do(self, f):
+    def wait_for_task_such_that(self, f):
         while True:
             msg = yield self.task_info_sub.get_next_message()
             if f(msg):
@@ -53,13 +61,13 @@ class Vrx(Navigator):
     @txros.util.cancellableInlineCallbacks
     def run_wayfinding(self):
         self.send_feedback('Waiting for task to start')
-        yield self.wait_for_task_to_do(lambda task: task.state in ['ready', 'running'])
-        path_msg = yield self.wayfinding_path_sub.get_last_message()
+        yield self.wait_for_task_such_that(lambda task: task.state in ['ready', 'running'])
+        path_msg = yield self.get_latching_msg(self.wayfinding_path_sub)
         poses = [ (yield self.geo_pose_to_enu_pose(geo_pose.pose)) for geo_pose in path_msg.poses]
         position = self.pose[0]
         poses = sorted(poses, key=lambda pose: np.linalg.norm(pose[0] - position))
         self.send_feedback('Sorted poses' + str(poses))
-        yield self.wait_for_task_to_do(lambda task: task.state in ['running'])
+        yield self.wait_for_task_such_that(lambda task: task.state in ['running'])
         for pose in poses:
             self.send_feedback('Gong to {}'.format(pose))
             yield self.move.set_position(pose[0]).set_orientation(pose[1]).go()
