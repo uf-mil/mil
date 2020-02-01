ff0f364b2ee66608ae09cbd6a3b01db8118c8bad
Kevin Allen <kma1660@gmail.com>
2020-02-01 18:03:16 -0500

    DOCS: document ros_connect

--- docs/infra/network.md
+++ docs/infra/network.md
@@ -8,3 +8,15 @@ The MIL Lan uses subnet `192.168.37.0` with mask `255.255.255.0`. In other words
 ## DHCP
 The MIL network has a DHCP server which will assign a local IP to connected devices automaticaly. Some important devices, such as servers and vehicles, have
 a static IP address. Other devices are given the next unused IP from the pool within `192.168.37.150` to `192.168.37.249`.
+
+## Connect to Robot / other ROS networks
+ROS can be configured to talk only on your local machine, or allow communcation
+from your machine to another, such as one of our robots or a simulation server.
+
+Assuming you have followed the [Development guide](/docs/development/development_guide),
+you can use the function `ros_connect` to set this up.
+
+Run `ros_connect -h` for an up to date list of options.
+For example, run `ros_connect sub` to setup this terminal (and children terminals,
+such as a tmux session) to allow your ros nodes, such as rviz, to talk to
+the ros nodes on SubjuGator.
--- scripts/rc.d/network_setup.sh
+++ scripts/rc.d/network_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# A variety of bash functions / aliases to network MIL devices together,
+# through ROS or otherwise
+
+# Devices on the MIL network start with this IP
 MIL_NETWORK_PREFIX="192.168.37"
 
+# Print all devices on the specified subnet / network prefix
 list_lan_devices()
 {
   if [ $# -lt 1 ]; then
@@ -10,8 +15,13 @@ list_lan_devices()
   nmap -sP $1 -oG - | awk '/Up$/{print $2}'
 }
 
+# List all devices on the MIL network currently by scanning
 alias list_mil_devices="list_lan_devices 192.168.37.1/24"
 
+# Get you local IP on the network that you're on
+# If on the MIL network and another, chose the MIL network.
+# If not on any networks, returns 1
+# TODO: verify that "real" networks get priority over docker0 / vpn
 get_lan_ip()
 {
   IPS=( $(hostname -I) )
@@ -25,6 +35,8 @@ get_lan_ip()
     return 1
   fi
 }
+
+# Setup ros environment variables for local development with no network access
 ros_local()
 {
   export ROS_IP="127.0.0.1"
@@ -32,6 +44,8 @@ ros_local()
   export ROS_MASTER_URI="http://127.0.0.1:11311"
 }
 
+# Setup ros environment variables for local development but allowing
+# other devices on the network to talk to your roscore
 ros_lan()
 {
   if ! LAN_IP=$(get_lan_ip) ; then
@@ -44,6 +58,7 @@ ros_lan()
   export ROS_MASTER_URI="http://$LAN_IP:11311"
 }
 
+# Same as ros_lan, but enforces that it is the MIL network and errors otherwise
 ros_mil_lan()
 {
 
@@ -55,6 +70,10 @@ ros_mil_lan()
   fi
 }
 
+# Connect to the specified device on the MIL network by IP/domain
+# Errors if not on MIL network
+# TODO: add option to also specify port of remote roscore, for use
+#       when multiple roscores are running on a machine (e.g. zobelisk)
 ros_mil()
 {
   if ! ros_mil_lan; then
@@ -66,20 +85,22 @@ ros_mil()
       echo "Example: ros_mil_lan 192.168.37.60"
   fi
 
- export ROS_MASTER_URI="http://$1:11311"
+  export ROS_MASTER_URI="http://$1:11311"
 }
 
-ros_zobelisk()
-{
-  export ROS_IP="192.168.37.174"
-  export ROS_MASTER_URI="http://192.168.37.176:11311"
-}
+# Setup ros_connect alias for zobelisk
+ZOBELISK_IP="192.168.37.174"
+alias ros_zobelisk="ros_mil $ZOBELISK_IP"
 
+# Setup ros connect alias for SubjuGator
 SUBJUGATOR_IP="192.168.37.60"
 alias ros_sub="ros_mil $SUBJUGATOR_IP"
 
+# All the options you can pass to ros_connect
 ROS_CONNECT_OPTIONS="local lan sub mil -h"
 
+# A wrapper around the above functions, allowing
+# users to use a single command for ROS networking needs
 ros_connect()
 {
   if [ $# -lt 1 ] || [ $1 == "-h" ] ; then
@@ -91,9 +112,13 @@ ros_connect()
       return 1
   fi
 
+  # Simple call the function corresponding to the mode
+  # e.g "ros_connect sub" runs "ros_sub"
   COMMAND="ros_$1"
   $COMMAND
 }
+
+# Autocomplete for ROS connect, suggesting all the $ROS_CONNECT_OPTIONS
 _ros_connect_complete() {
   cur="${COMP_WORDS[COMP_CWORD]}"
   COMPREPLY=( $(compgen -W "$ROS_CONNECT_OPTIONS" -- ${cur}) )
