5573c791a0b4df2f5725fa23489e7b62d27eb550
Cole Fisher <cole.fisher@ufl.edu>
2020-07-06 20:31:11 -0400

    Updates to inclinometer, urdf, gazebo_utils

--- mil_common/simulation/mil_gazebo/include/mil_gazebo/mil_inclinometer_gazebo.hpp
+++ mil_common/simulation/mil_gazebo/include/mil_gazebo/mil_inclinometer_gazebo.hpp
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 #include <gazebo/common/common.hh>
 #include <gazebo/sensors/sensors.hh>
+#include <gazebo/sensors/GaussianNoiseModel.hh>
 #include <ignition/math/Vector3.hh>
 #include <mil_gazebo/mil_gazebo_utils.hpp>
 
@@ -22,9 +23,15 @@ private:
 
   ros::NodeHandle nh_;
   ros::Publisher pub_;
-  int queue_size;
+  std::string frame_name;
   std::string topic_name;
+  int queue_size;
 
+  double update_rate;
+  double previous_bias;
+  double velocity_random_walk;
+  double correlation_time;
+  
   gazebo::sensors::SensorPtr sensor_;
   gazebo::physics::LinkPtr parent_;
   gazebo::event::ConnectionPtr update_connection_;
@@ -33,7 +40,6 @@ private:
   ignition::math::Vector3d previous_velocity;
   ignition::math::Vector3d incremental_velocity;
 
-  std::string frame_name;
   mil_msgs::IncrementalLinearVelocityStamped msg_;
 };
 }  // namespace mil_gazebo
