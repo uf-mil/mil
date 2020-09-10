1f2ca6d9fa982102ad58ea9d847c970292c71230
Alex <alex.perez@ufl.edu>
2020-09-10 14:02:32 -0400

    IndyAv: switched p3d to sylphase
    fixed the sylphase task for indyav without copying the files from navigator
    
    I created a rostest in python to demo the sylphase change. I commented out two of the property xacros in the urdf, removing them did nothing. I changed the doc files to show the sylphase change
    
    I realied I did not commit all the changes. here are the rest
    
    final commit: commented out p3d from roslaunch and fixed rostest
    
    indyav: switched p3d to sylphase
    
    prototype: made ins publish to enu
    
    fixed method by which ins_odom is published to enu
    
    added ins to base_link connection within tf.launch, adjusted location of ins tf to be inside car, fixed docs
    
    cleaned up comments and bugs
    
    attempted fix by adding tf depend
    
    added tf import to test file
    
    made minor change to be able to push again

---
+++ IndyAV/localization/indyav_localization/test/test_sylphase.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import unittest
+import rospy
+from nav_msgs.msg import Odometry
+from time import sleep
+import rostest
+import tf
+
+class SylphaseTestCase(unittest.TestCase):
+    sylphase_pub = False
+    sylphase_hz_rate = 0
+    high_hz = False
+
+    def callback(self, data):
+        self.sylphase_hz_rate += 1
+
+    
+    def test_ins_odom_frequency(self):
+        rospy.wait_for_service("/gazebo/spawn_urdf_model")
+        rospy.Subscriber('/ins_odom', Odometry, self.callback)
+        sleep(1)      
+        
+        #tests if ins_odom is being published
+        if self.sylphase_hz_rate > 0:
+            self.sylphase_pub = True
+        self.assertTrue(self.sylphase_pub)
+        
+        #tests if ins_odom is being published at a rate above 25 hz
+        if self.sylphase_hz_rate > 25:
+            self.high_hz = True
+        self.assertTrue(self.high_hz)
+   
+    def test_base_link_enu_tf(self):
+        # wait till the robot is spawned in gazebo
+        rospy.wait_for_service("/gazebo/spawn_urdf_model")
+        tf_listener = tf.TransformListener()
+        tf_listener.waitForTransform('base_link', 'enu', rospy.Time.now(), rospy.Duration(5.0))
+
+if __name__ == '__main__':
+    rospy.init_node('test_sylphase')
+    import rostest
+    rostest.rosrun('test_package', 'test_name', SylphaseTestCase)
