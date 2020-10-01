7f1423c827a6c499d6403d7b905540ec33f7e5ac
MarshallRawson <marshallrawson@ufl.edu>
2020-10-01 11:42:16 -0400

    INDYAV: Switched to rotating tires (and changes-to-vrx-gazebo-test)

--- IndyAV/simulation/indyav_gazebo/include/indyav_gazebo/backwheel_plugin.hpp
+++ IndyAV/simulation/indyav_gazebo/include/indyav_gazebo/backwheel_plugin.hpp
@@ -1,25 +1,25 @@
 #pragma once
 
-#include <indyav_control/ThrustStamped.h>
+#include <indyav_control/RevsStamped.h>
 #include <indyav_gazebo/wheel_plugin.hpp>
 
 namespace gazebo
 {
-class BackWheelPlugin : public WheelPlugin<indyav_control::ThrustStamped>
+class BackWheelPlugin : public WheelPlugin<indyav_control::RevsStamped>
 {
 public:
   BackWheelPlugin();
   virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
   virtual void Init();
-  virtual void Callback(const indyav_control::ThrustStamped& _msg);
+  virtual void Callback(const indyav_control::RevsStamped& _msg);
   virtual void OnUpdate();
 
 protected:
-  physics::LinkPtr base_link_;
+  std::string back_axle_joint_name_;
+  physics::JointPtr back_axle_joint_;
 
-  ignition::math::Vector3<double> thrust_ = ignition::math::Vector3<double>::Zero;
-
-  double max_thrust_ = 0.0;
+  double wheel_rotational_vel_ = 0.0;
+  double max_wheel_rotational_vel_ = 0.0;
   double max_velocity_ = 0.0;
 };
 
