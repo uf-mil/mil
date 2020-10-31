3a1872ed39a9321802ba4175ff5a904637673b32
Jin-hyuk, Yu <63084448+Jinhyuk-Yu@users.noreply.github.com>
2020-10-30 22:17:09 -0400

    Add boundary conditions and re-calculate angles
    1. I change...
    1) Initial angle calculations
    2) Boundary conditions
      - Add 'Ackerman condition'
      - Add 'Maximum and minimum turning radius'
    3) Give the difference between the back wheel and the front wheel to calculate based on the real model
    
    2. What I need...
     1) the value of 'g' (in the code, I add the explanation)
     2) How to adjust if the calculated angles does not satisfy the conditions.

--- IndyAV/control/indyav_control/src/simulated_steering_driver.cpp
+++ IndyAV/control/indyav_control/src/simulated_steering_driver.cpp
@@ -1,5 +1,7 @@
 #include <std_msgs/Float64.h>
 #include <indyav_control/simulated_steering_driver.hpp>
+#include <math.h>
+#include <cmath>
 
 template <class MSG>
 SimulatedSteeringDriver<MSG>::SimulatedSteeringDriver(ros::NodeHandle* _nh, const std::string& sub_topic)
@@ -31,23 +33,37 @@ void SimulatedSteeringDriver<MSG>::Callback(const MSG& _msg)
   double cmd_angle = _msg.steering_angle;
   for (auto i = pubs_.begin(); i != pubs_.end(); ++i)
   {
-    // TODO: impliment ackerman steering angles
+        // TODO: impliment ackerman steering angles
     // Worked Mr. Daniel Williams and Mr. Jin-hyuk, Yu
 
-    // cmd_angle = theta. an input angle based on geometric bicycle model for the vehicle
-    // Steering_angle_inside = tan^(-1)((2*L*sin(theta))/(2*L*cos(theta)-W*sin(theta))) 
-    // Steering_angle_outside = tan^(-1)((2*L*sin(theta))/(2*L*cos(theta)+W*sin(theta))) 
-    // L = the wheel base = distance between the two axles
-    // T = the track width = distance between center line of each tire
-    // We don't have to find R, the radius of the turn. 
-    // GoKart mechanical properties, the wheel base and the track width, are needed.
-    
-    // Reference
-    // 1) Ackerman Steering, Robert Eisele, https://www.xarg.org/book/kinematics/ackerman-steering/
-    // 2) Ackerman Steering Geometry, https://en.wikipedia.org/wiki/Ackermann_steering_geometry
-    // 3) Page 11-12, Path Tracking for Unmanned Ground Vehicle Navigation, J. Giesbrecht, D. Mackay, J. Collier, S. Verret, DRDC Suffield, 2005
-    // 4) page 8-9, Automatic Steering Methods for Autonomous Automobile Path Tracking, Jarrod M. Snider, Robotics Institute at CMU, 2009
-    // 5) http://datagenetics.com/blog/december12016/index.html
+    // Ackermann angle calculation
+    double theta = cmd_angle; // an input angle based on geometric bicycle model for the vehicle
+    double wf, wr, L, R1, A2, R2, theta_I, theta_O;
+    wf = 94;// = the front wheel width
+    wr = 96.5;// = the rear wheel width
+    L =  104; // the length of the wheel base
+    theta_I = tan^(-1)((2*L*sin(theta))/(2*L*cos(theta)-wf*sin(theta)));// Steering_angle_inside(left)
+    theta_O = tan^(-1)((2*L*sin(theta))/(2*L*cos(theta)+wf*sin(theta));// Steering_angle_outside(right)
+    // Boundary Condition 1 : Ackermann equation satisfy check
+    R1 = (wf/2) + l/tan(theta_I);
+    A2 = 56;// the distance from the rear wheel to the center of the kart
+    R2 = sqrt(pow(A2,2)+pow(L,2)*pow((1/tan(theta)),2));
+    if (R1 == R2)
+    {
+      R = R1;
+    }
+    // Boundary Condition 2 : Space requirement for turning 
+    // Maximum and Minimum radius of vehicle turning
+    double m1, m2, RMax, RMin;
+    RMin = L/tan(theta) - wf/2 - wr/2;
+    m1 = RMin + wr;
+    m2 = L + g; // g is the length between the front wheel and bumper
+    RMax = sqrt(pow(m1,2)+pow(m2,2));
+    if (R>RMin && R<RMax)
+    {
+      theta_I = theta_L // the angle that will be put in the left side
+      theta_O = theta_R // the angle that will be put in the right side
+    }
 
     std_msgs::Float64 msg;
     msg.data = cmd_angle;
