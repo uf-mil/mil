6295e91075c1f6053d8566360fb66a75cc896791
Alex <alex.perez@ufl.edu>
2021-03-06 14:25:51 -0500

    added dynamic wrench

---
+++ SubjuGator/utils/sub8_diagnostics/src/dynamic_wrench.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <geometry_msgs/WrenchStamped.h>
+
+
+int main (int argc, char **argv){
+	ros::init(argc, argv, "dynamic_test");
+	ros::NodeHandle nh;
+	ros::Publisher pub = nh.advertise<geometry_msgs::WrenchStamped>("/wrench", 10);
+	double force = 30;
+	double secs = 0;
+	double move_time = 10;
+	ros::Rate rate(50);
+	
+	while(ros::ok()){
+		geometry_msgs::WrenchStamped msg;
+		secs = ros::Time::now().toSec();
+		
+		while (ros::Time::now().toSec() - move_time < secs){
+			msg.wrench.force.x = force;
+			pub.publish(msg);
+			rate.sleep();
+		}
+		
+		force = -force;
+		rate.sleep();
+	}
+}
