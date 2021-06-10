6558138394adac3f8cf770432dafe94e6f910b8f
Alex <alex.perez@ufl.edu>
2021-06-09 21:32:41 -0400

    changed format for badge mission

--- SubjuGator/command/sub8_missions/sub8_missions/badge_takedown.py
+++ SubjuGator/command/sub8_missions/sub8_missions/badge_takedown.py
@@ -2,7 +2,7 @@ from .sub_singleton import SubjuGator
 from txros import util
 import rospy
 import mil_ros_tools
-from sub8_msgs.msg import ThrusterCmd
+from sub8_msgs.msg import PathPoint
 from mil_misc_tools import text_effects
 
 fprint = text_effects.FprintFactory(title="BADGE_TAKEDOWN", msg_color="cyan").fprint
@@ -21,38 +21,33 @@ class BadgeTakedown(SubjuGator):
       left = forward.left(SIDE_LENGTH).zero_roll_and_pitch()
 
       move_info_sub = yield self.nh.subscribe(
-        '/move_to_badge', ThrusterCmd)
+        '/move_to_badge', PathPoint)
 
       while done == False:
         #listen to topic move where it tells us to move
         move_info = yield move_info_sub.get_next_message()
 
-        #moving to location
-        if move_info.name == "ROTATE_RIGHT":
-          yield self.move.yaw_right_deg(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "ROTATE_LEFT":
-          yield self.move.yaw_left_deg(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "FORWARD":
-          yield self.move.forward(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "LEFT":
-          yield self.move.left(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "RIGHT":
-          yield self.move.right(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "BACKWARD":
-          yield self.move.backward(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "UP":
-          yield self.move.up(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "DOWN":
-          yield self.move.down(move_info.thrust).go(speed=SPEED_LIMIT)
-        elif move_info.name == "SHOOT":
-          fprint("FIRE")
-          yield self.actuators.shoot_torpedo1()
-          yield self.actuators.shoot_torpedo2()
-          done = True
-        elif move_info.name == "ARRIVED":
+        #we do not see the badge
+        if move_info.yaw == 0.0:
+          yield self.move.yaw_right_deg(10.0).go(speed=SPEED_LIMIT)
+
+        #we have a vector
+        elif move_info.yaw == 1.0:
+           pose = move_info.position
+           vec = [pose.x, pose.y, pose.z]
+           yield self.move.relative(vec).zero_roll_and_pitch().go(speed=SPEED_LIMIT)
+
+        elif move_info.yaw == 2.0:
           fprint("We have arrived")
           done = True
-        else:
-          yield self.move.yaw_right_deg(45).go(speed=SPEED_LIMIT)
+
+        '''
+        #we have eyes on the target
+        elif move_info.yaw == 2.0
+          fprint("FIRE")
+          yield self.actuators.shoot_torpedo1()
+          yield self.actuators.sheet_torpedo2()
+          done = True
+        '''
 
       fprint("Done!")
