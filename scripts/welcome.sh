61952ed607ec54b940e6d1b2c78756253f60bfa0
cbrxyz <me@cbrxyz.com>
2021-11-16 01:58:25 -0500

    Add git info prompt

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -21,28 +21,28 @@ hash_header() { echo "########################################"; }
 clear
 cat << EOF
 
-                                                                                
-         &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#        
-       @@@@@@*,...................................................,/@@@@@@      
-     ,@@@@                                                            ,@@@@     
-     (@@@(                      /////&@* (@%////*                      &@@@*    
-     (@@@(                 #         %@* (@(         &                 &@@@*    
-     (@@@(                @@@@       %@* (@(      ,@@@@                &@@@*    
-     (@@@(               &@@@@@@%    %@* (@(    @@@@@@@#               &@@@*    
-     (@@@(              %/  @@@@@@@. %@* (@( ,@@@@@@&  #,              &@@@*    
-     (@@@(             *&     ,@@@@@@@@* (@@@@@@@@      @.             &@@@*    
-     (@@@(             @         (@@@@@* (@@@@@,         @             &@@@*    
-     (@@@(            @             @@@* (@@#            ,@            &@@@*    
-     (@@@(           &               %@* (@(              /#           &@@@*    
-     (@@@(          %/               %@* (@(               %,          &@@@*    
-     (@@@(         *@                %@* (@(                @.         &@@@*    
-     (@@@(         @                 %@* (@(                 @         &@@@*    
-     (@@@(        @                  %@* (@(                 ,@        &@@@*    
-     (@@@(  .@@@@@@@@            &@@@@@* (@@@@@#            @@@@@@@@   &@@@*    
-     (@@@#  .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   &@@@*    
-     .@@@@.                                                           (@@@@     
-       @@@@@@%////////////////////////////////////////////////////(&@@@@@@      
-         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/       
+
+         &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
+       @@@@@@*,...................................................,/@@@@@@
+     ,@@@@                                                            ,@@@@
+     (@@@(                      /////&@* (@%////*                      &@@@*
+     (@@@(                 #         %@* (@(         &                 &@@@*
+     (@@@(                @@@@       %@* (@(      ,@@@@                &@@@*
+     (@@@(               &@@@@@@%    %@* (@(    @@@@@@@#               &@@@*
+     (@@@(              %/  @@@@@@@. %@* (@( ,@@@@@@&  #,              &@@@*
+     (@@@(             *&     ,@@@@@@@@* (@@@@@@@@      @.             &@@@*
+     (@@@(             @         (@@@@@* (@@@@@,         @             &@@@*
+     (@@@(            @             @@@* (@@#            ,@            &@@@*
+     (@@@(           &               %@* (@(              /#           &@@@*
+     (@@@(          %/               %@* (@(               %,          &@@@*
+     (@@@(         *@                %@* (@(                @.         &@@@*
+     (@@@(         @                 %@* (@(                 @         &@@@*
+     (@@@(        @                  %@* (@(                 ,@        &@@@*
+     (@@@(  .@@@@@@@@            &@@@@@* (@@@@@#            @@@@@@@@   &@@@*
+     (@@@#  .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   &@@@*
+     .@@@@.                                                           (@@@@
+       @@@@@@%////////////////////////////////////////////////////(&@@@@@@
+         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/
 
 
 $(hash_header)
@@ -62,9 +62,11 @@ if [ "$Step1Confirmation" != "Y" ]
 then
     exit 0
 fi
+clear
 
 # Check user's software version
 SOFTWARE=$(uname -s)
+echo "Checking to see if ${tty_bold}Linux${tty_reset} is installed..."
 
 if [ "$SOFTWARE" != "Linux" ]
 then
@@ -85,10 +87,14 @@ EOF
     then
         exit 1
     fi
+    clear
+else
+    echo "${tty_bold}${tty_green}Great! You are using Linux."
 fi
 
 # Check user's version of Ubuntu
 UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+echo "Checking to see if ${tty_bold}Ubuntu 18.04${tty_reset} is installed..."
 
 if [ "$UBUNTU_VERSION" != "18.04" ]
 then
@@ -109,10 +115,43 @@ EOF
     then
         exit 1
     fi
+    clear
+else
+    echo "${tty_bold}${tty_green}Great! You are using Ubuntu 18.04."
 fi
 
-# Install git
+# git info prompt
 clear
+hash_header
+cat << EOF
+${tty_bold}Installing git...
+${tty_reset}
+git is a version control tool that helps developers like you to manage versions of their software. It can be accessed in the terminal by writing 'git' followed by arguments.
+
+As you grow in MIL, you can use git to add changes to the MIL codebase. Some helpful commands include:
+
+${tty_blue}git add file1.txt file2.txt ...${tty_reset}
+    Adds several files to tracking index.
+
+${tty_blue}git commit -m "This is a commit message."${tty_reset}
+    Commits several files. This marks a formal change in the repository - a change that others will be able to see.
+
+${tty_blue}git push${tty_reset}
+    This pushes the local commits to the remote repository, hosted on GitHub.
+
+These few commands can allow you to make several changes to the codebase and share them with others!
+
+${tty_bold}${tty_green}Would you like to proceed? [Y/n]${tty_reset}
+EOF
+hash_header
+read Step2Confirmation
+if [ "$Step2Confirmation" != "Y" ]
+then
+    exit 1
+fi
+clear
+
+# Install git
 echo "Checking to see if ${tty_bold}git${tty_reset} is installed..."
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
@@ -121,6 +160,7 @@ if [ $GIT_IS_AVAILABLE -ne 0 ]
 then
     echo "${tty_bold}${tty_blue}Attempting to install git... (This may require your password, which is expected.)"
     sudo apt install git
+    clear
     echo "Checking to see if ${tty_bold}git${tty_reset} is installed..."
     git --version 2>&1 >/dev/null # improvement by tripleee
     GIT_IS_AVAILABLE=$?
@@ -137,6 +177,8 @@ Git is a helpful software tool that allows you to communicate with the code repo
 Please contact another software team member for assistance.
 EOF
         hash_header
+    else
+        echo "${tty_bold}${tty_green}Great! git has been succesfully installed."
     fi
 else
     echo "${tty_bold}${tty_green}Great! git is already installed."
