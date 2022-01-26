c85056ed78fa80408d953446a4c134a9632ca8d8
uf-mil <alex.perez@ufl.edu>
2022-01-25 19:54:51 -0500

    fixed wayfinding to use TSP algorithm and wildlife task is a WIP

--- NaviGator/mission_control/navigator_missions/vrx_missions/vrx_wayfinding.py
+++ NaviGator/mission_control/navigator_missions/vrx_missions/vrx_wayfinding.py
@@ -5,8 +5,9 @@ import numpy as np
 from twisted.internet import defer
 from vrx import Vrx
 from mil_tools import rosmsg_to_numpy
+from tsp_solver.greedy import solve_tsp
 
-___author___ = "Kevin Allen"
+___author___ = "Alex Perez and Kevin Allen"
 
 
 class VrxWayfinding(Vrx):
@@ -19,10 +20,30 @@ class VrxWayfinding(Vrx):
         yield self.wait_for_task_such_that(lambda task: task.state in ['ready', 'running'])
         path_msg = yield self.get_latching_msg(self.wayfinding_path_sub)
         poses = [ (yield self.geo_pose_to_enu_pose(geo_pose.pose)) for geo_pose in path_msg.poses]
+
         position = self.pose[0]
-        poses = sorted(poses, key=lambda pose: np.linalg.norm(pose[0] - position))
-        self.send_feedback('Sorted poses' + str(poses))
+
+        #initialize distance matrix
+        poses = poses + [position]
+        array_size = len(poses)
+        start_pose_index = array_size - 1
+        dist_matrix = np.zeros( (array_size, array_size) )
+
+        #fill up distance matrix
+        for i in range(array_size):
+            for j in range(array_size):
+                dist_matrix[i][j] = np.linalg.norm(poses[i][0] - poses[j][0])
+
+        #solve tsp algorithm (ensure start point is where boat is located) & remove current position from pose list
+        path = solve_tsp(dist_matrix, endpoints=(start_pose_index,None))
+        poses = poses[:start_pose_index]
+        path = path[1:]
+
+        #self.send_feedback('Sorted poses' + str(poses))
         yield self.wait_for_task_such_that(lambda task: task.state in ['running'])
-        for pose in poses:
-            self.send_feedback('Gong to {}'.format(pose))
-            yield self.move.set_position(pose[0]).set_orientation(pose[1]).go(blind=True)
+
+        #do movements
+        for index in path:
+            self.send_feedback('Going to {}'.format(poses[index]))
+            yield self.move.set_position(poses[index][0]).set_orientation(poses[index][1]).go(blind=True)
+
