d3905fd731f2f106a383e134bb42a89eb48a0320
marenheck <maren@maren-linux.localdomain>
2022-01-31 12:40:57 -0500

    changed order of master2vrx and vrx2master scripts

--- scripts/master_to_vrx.sh
+++ scripts/master_to_vrx.sh
@@ -2,15 +2,15 @@
 #switches from the master branch to the vrx_2022 branch and compiles
 
 #There is an error in this file. Adds a second parameter to the file yaml.load (,Loader=yaml.Loader)
-#sudo sed -i 's/values_dict = yaml.load(value)/values_dict = yaml.load(value, Loader-yaml.Loader)/g' /opt/ros/melodic/lib/dynamic_reconfigure/dynparam
+#sudo sed -i 's/values_dict = yaml.load(value)/values_dict = yaml.load(value, Loader=yaml.Loader)/g' /opt/ros/melodic/lib/dynamic_reconfigure/dynparam
 
 export MIL_CONFIG_DIR=$HOME/.mil
 export MIL_WS="$(realpath $(dirname $BASH_SOURCE)/../../..)"
 MIL_REPO="$MIL_WS/src/mil"
 
-git submodule update --init --recursive
+git checkout vrx_2022
 
-catkin_make -C $MIL_WS
+git submodule update --init --recursive
 
 #removes the navigator_vrx.urdf file in navigator_gazebo
 FILE=~/catkin_ws/devel/share/navigator_gazebo/urdf/navigator_vrx.urdf
@@ -21,6 +21,6 @@ else
 	echo "no navigator_vrx.urdf to remove"
 fi
 
-git checkout vrx_2022
-
 echo "script complete"
+
+catkin_make -C $MIL_WS
--- scripts/vrx_to_master.sh
+++ scripts/vrx_to_master.sh
@@ -5,7 +5,18 @@ export MIL_CONFIG_DIR=$HOME/.mil
 export MIL_WS="$(realpath $(dirname $BASH_SOURCE)/../../..)"
 MIL_REPO="$MIL_WS/src/mil"
 
+git checkout master
+
 git submodule update --init --recursive
+
+#removes the navigator_vrx.urdf file in navigator_gazebo
+FILE=~/catkin_ws/devel/share/navigator_gazebo/urdf/navigator_vrx.urdf
+if test -f "$FILE"; then
+	rm ~/catkin_ws/devel/share/navigator_gazebo/urdf/navigator_vrx.urdf
+	echo "removed navigator_vrx.urdf"
+else
+	echo "no navigator_vrx.urdf to remove"
+fi
 
 #removes all dock_2022 files in vrx_gazebo
 if find ~/catkin_ws/src/mil/NaviGator/simulation/VRX/vrx/vrx_gazebo/models -type d -name 'dock_2022*' -exec false {} +
@@ -17,5 +28,4 @@ fi
 
 catkin_make -C $MIL_WS
 
-git checkout master
 echo "script complete"
