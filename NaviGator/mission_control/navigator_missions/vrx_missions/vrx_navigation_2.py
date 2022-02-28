f2a18f47c14608ac3b6694afb40462df93f474b1
Alex <alex.perez@ufl.edu>
2022-02-28 17:30:24 -0500

    added custom Navigation2 and Navigation3

--- NaviGator/mission_control/navigator_missions/vrx_missions/vrx_navigation_2.py
+++ NaviGator/mission_control/navigator_missions/vrx_missions/vrx_navigation_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division
+from inspect import stack
 import txros
 import numpy as np
 from twisted.internet import defer
@@ -7,6 +8,7 @@ from vrx import Vrx
 from mil_tools import rosmsg_to_numpy
 from std_srvs.srv import SetBoolRequest
 from mil_tools import quaternion_matrix
+from navigator_msgs.srv import MoveToWaypointRequest
 from mil_msgs.srv import ObjectDBQuery, ObjectDBQueryRequest, ObjectDBQueryResponse
 
 ___author___ = "Kevin Allen"
@@ -44,6 +46,26 @@ class VrxNavigation2(Vrx):
             yield self.move.look_at(after_position).set_position(after_position).go()
 
     @txros.util.cancellableInlineCallbacks
+    def go_thru_gate2(self, gate, BEFORE=5.0, AFTER=4.0):
+        center, vec = gate
+        before_position = center - (vec * BEFORE)
+        after_position = center + (vec * AFTER)
+
+        req = MoveToWaypointRequest()
+        goal_pose = before_position
+        req.target_p.position.x = before_position[0]
+        req.target_p.position.y = before_position[1]
+        req.target_p.position.z = before_position[2]
+        yield self.set_long_waypoint(req)
+
+        if AFTER > 0:
+            goal_pose = after_position
+            req.target_p.position.x = after_position[0]
+            req.target_p.position.y = after_position[1]
+            req.target_p.position.z = after_position[2]
+            yield self.set_long_waypoint(req)
+
+    @txros.util.cancellableInlineCallbacks
     def find_two_closest_buoys(self):
 
         #get total list of objects from lidar
@@ -52,16 +74,30 @@ class VrxNavigation2(Vrx):
         objects_msg = yield self.database_response(service_request)
         objects = objects_msg.objects
 
+        print(objects)
+
+        items_to_delete = []
         #delete any objects that have been passed
-        for i in range(len(objects)):
+        for i,v in enumerate(objects[:]) :
+            print(i)
+            print(len(objects))
+            print(range(len(objects)))
             if objects[i].id in self.objects_passed:
-                objects.pop(i)
+                items_to_delete.append(i)
+
+        #actually delete the objects
+        while len(items_to_delete) != 0:
+            i = items_to_delete.pop()
+            print(i)
+            objects.pop( i )
 
         #create list of buoy positions that haven't been passed
         buoy_positions = np.array([rosmsg_to_numpy(obj.pose.position) for obj in objects])
 
         #create list of distances of each buoy that hasn't been passed relative to the boat
         robot_position = (yield self.tx_pose)[0]
+        print(buoy_positions)
+        print(robot_position)
         distances = np.linalg.norm(buoy_positions - robot_position, axis=1)
 
         #sort those distances by index
@@ -76,6 +112,15 @@ class VrxNavigation2(Vrx):
         self.objects_passed.add(self.next_buoy1.id)
         self.objects_passed.add(self.next_buoy2.id)
 
+    @txros.util.cancellableInlineCallbacks
+    def go_through_next_two_buoys(self):
+
+        yield self.find_two_closest_buoys()
+
+        pos1 = rosmsg_to_numpy(self.next_buoy1.pose.position)
+        pos2 = rosmsg_to_numpy(self.next_buoy2.pose.position)
+        gate = self.get_gate(pos1, pos2, (yield self.tx_pose)[0])
+        yield self.go_thru_gate2(gate)
 
 
     @txros.util.cancellableInlineCallbacks
@@ -84,17 +129,14 @@ class VrxNavigation2(Vrx):
         self.objects_passed = set()
 
         # Wait a bit for PCDAR to get setup
-        yield self.nh.sleep(3.0)
+        yield self.nh.sleep(10.0)
         yield self.set_vrx_classifier_enabled(SetBoolRequest(data=True))
+        yield self.nh.sleep(4)
 
-        yield self.find_two_closest_buoys()
+        yield self.move.forward(5).go()
 
-        gate = self.get_gate(self.next_buoy1.pose.position, self.next_buoy2.pose.position, (yield self.tx_pose)[0])
-        yield self.go_thru_gate(gate)
-
-
-
-        yield self.move.forward(10, 'm').go()
+        for i in range(3):
+            yield self.go_through_next_two_buoys()
 
         #yield self.prepare_to_enter()
         #yield self.wait_for_task_such_that(lambda task: task.state =='running')
---
+++ NaviGator/mission_control/navigator_missions/vrx_missions/vrx_navigation_3.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+from __future__ import division
+import txros
+import numpy as np
+from twisted.internet import defer
+from vrx import Vrx
+from mil_tools import rosmsg_to_numpy
+from std_srvs.srv import SetBoolRequest
+from navigator_msgs.srv import MoveToWaypointRequest
+from mil_tools import quaternion_matrix
+import math
+import tf
+
+___author___ = "Alex Perez and Enemias Itzep"
+
+
+class VrxNavigation3(Vrx):
+    def __init__(self, *args, **kwargs):
+        super(VrxNavigation3, self).__init__(*args, **kwargs)
+
+    def point_at_goal(self, goal_pos):
+        vect = [ goal_pos[0] - self.pose[0][0], goal_pos[1] - self.pose[0][1]]
+        theta = math.atan2(vect[1], vect[0])
+        return tf.transformations.quaternion_from_euler(0,0,theta)
+
+    def closest_point_on_radius(self, start_pos, end_pos, radius):
+        #given two points, this finds the closest point to the end_pose given a radius around the end_pose
+        vector = [ end_pos[0] - start_pos[0], end_pos[1] - start_pos[1] ]
+        theta = math.atan2(vector[1], vector[0])
+        hypot = math.sqrt( (vector[0] ** 2) + (vector[1] ** 2) )
+        dist_to_point = hypot - radius
+        return [ start_pos[0] + dist_to_point * math.cos(theta), start_pos[1] + dist_to_point * math.sin(theta), 0]
+
+    def numpy_to_rosmsg(self, goal_pos, goal_ori):
+        req = MoveToWaypointRequest()
+        req.target_p.position.x = goal_pos[0]
+        req.target_p.position.y = goal_pos[1]
+        req.target_p.position.z = goal_pos[2]
+        req.target_p.orientation.x = goal_ori[0]
+        req.target_p.orientation.y = goal_ori[1]
+        req.target_p.orientation.z = goal_ori[2]
+        req.target_p.orientation.w = goal_ori[3]
+        return req
+
+    @txros.util.cancellableInlineCallbacks
+    def inspect_object(self, position):
+        # Go in front of the object, looking directly at it
+        #yield self.move.look_at(position).set_position(position).backward(6.0).go()
+
+        goal_pos = self.closest_point_on_radius(self.pose[0][0], position, 6)
+        goal_ori = self.point_at_goal(position)
+        req = self.numpy_to_rosmsg(goal_pos, goal_ori)
+        yield self.set_long_waypoint(req)
+
+        yield self.nh.sleep(5.)
+
+    def get_index_of_type(self, objects, classifications):
+        if type(classifications) == str:
+            classifications = [classifications]
+        return next(i for i, o in enumerate(objects) if o.labeled_classification in classifications and o.id not in self.objects_passed)
+
+    @staticmethod
+    def get_gate(one, two, position):
+        one = one[:2]
+        two = two[:2]
+        position = position[:2]
+        delta = (one - two)[:2]
+        rot_right= np.array([[0, -1], [1, 0]], dtype=np.float)
+        perp_vec = rot_right.dot(delta)
+        perp_vec = perp_vec / np.linalg.norm(perp_vec)
+        center = (one + two) / 2.0
+        distances = np.array([
+            np.linalg.norm((center + perp_vec) - position),
+            np.linalg.norm((center - perp_vec) - position)
+        ])
+        if np.argmin(distances) == 0:
+            perp_vec = -perp_vec
+        return np.array([center[0], center[1], 0.]), np.array([perp_vec[0], perp_vec[1], 0.])
+
+    @txros.util.cancellableInlineCallbacks
+    def go_thru_gate(self, gate, BEFORE=5.0, AFTER=4.0):
+        center, vec = gate
+        before_position = center - (vec * BEFORE)
+        after_position = center + (vec * AFTER)
+
+        req = MoveToWaypointRequest()
+        goal_pose = before_position
+        req.target_p.position.x = before_position[0]
+        req.target_p.position.y = before_position[1]
+        req.target_p.position.z = before_position[2]
+        yield self.set_long_waypoint(req)
+
+        if AFTER > 0:
+            goal_pose = after_position
+            req.target_p.position.x = after_position[0]
+            req.target_p.position.y = after_position[1]
+            req.target_p.position.z = after_position[2]
+            yield self.set_long_waypoint(req)
+
+    @txros.util.cancellableInlineCallbacks
+    def do_next_gate(self):
+        pose = yield self.tx_pose
+        p = pose[0]
+        q_mat = quaternion_matrix(pose[1])
+
+        def filter_and_sort(objects, positions):
+            positions_local = np.array([(q_mat.T.dot(position - p)) for position in positions])
+            positions_local_x = np.array(positions_local[:, 0])
+            forward_indicies = np.argwhere(positions_local_x > 1.0).flatten()
+            forward_indicies = np.array([i for i in forward_indicies if objects[i].id not in self.objects_passed])
+            distances = np.linalg.norm(positions_local[forward_indicies], axis=1)
+            indicies =  forward_indicies[np.argsort(distances).flatten()].tolist()
+            # ids = [objects[i].id for i in indicies]
+            # self.send_feedback('Im attracted to {}'.format(ids))
+            return indicies
+
+        def is_done(objects, positions):
+            try:
+                left_index = self.get_index_of_type(objects, 'mb_marker_buoy_green')
+                right_index = self.get_index_of_type(objects, 'mb_marker_buoy_red')
+            except StopIteration:
+                return None
+            end = objects[left_index].labeled_classification == 'mb_marker_buoy_black'
+            return positions[left_index], objects[left_index], positions[right_index], objects[right_index], end
+
+        left, left_obj, right, right_obj, end = yield self.explore_closest_until(is_done, filter_and_sort)
+        self.send_feedback('Going through gate of objects {} and {}'.format(left_obj.id, right_obj.id))
+        gate = self.get_gate(left, right, p)
+        yield self.go_thru_gate(gate)
+        self.objects_passed.add(left_obj.id)
+        self.objects_passed.add(right_obj.id)
+        defer.returnValue(end)
+
+    @txros.util.cancellableInlineCallbacks
+    def explore_closest_until(self, is_done, filter_and_sort):
+        '''
+        @conditon func taking in sorted objects, positions
+        @object_filter func filters and sorts
+        '''
+        move_id_tuple = None
+        service_req = None
+        dl = None
+        investigated = set()
+        while True:
+            if move_id_tuple is not None:
+                if service_req is None:
+                    service_req = self.database_query(name='all')
+                dl = defer.DeferredList([service_req, move_id_tuple[0]], fireOnOneCallback=True)
+                result, index = yield dl
+
+                # Database query sucseeded
+                if index == 0:
+                    service_req = None
+                    objects_msg = result
+                    if self.object_classified(objects_msg.objects, move_id_tuple[1]):
+                        self.send_feedback('{} identified. Canceling investigation'.format(move_id_tuple[1]))
+                        yield dl.cancel()
+                        yield move_id_tuple[0].cancel()
+                        # yield self.move.forward(0).go()
+                        move_id_tuple = None
+                # Move succeeded:
+                else:
+                    self.send_feedback('Investigated {}'.format(move_id_tuple[1]))
+                    move_id_tuple = None
+            else:
+                objects_msg = yield self.database_query(name='all')
+            objects = objects_msg.objects
+            positions = np.array([rosmsg_to_numpy(obj.pose.position) for obj in objects])
+            if len(objects) == 0:
+                indicies = []
+            else:
+                indicies = filter_and_sort(objects, positions)
+            if indicies is None or len(indicies) == 0:
+                self.send_feedback('No objects')
+                continue
+            objects = [objects[i] for i in indicies]
+            positions = positions[indicies]
+            # Exit if done
+            ret = is_done(objects, positions)
+            if ret is not None:
+                if move_id_tuple is not None:
+                    self.send_feedback('Condition met. Canceling investigation')
+                    yield dl.cancel()
+                    yield move_id_tuple[0].cancel()
+                    # yield self.move.forward(0).go()
+                    move_id_tuple = None
+                defer.returnValue(ret)
+
+            if move_id_tuple is not None:
+                continue
+
+            self.send_feedback('ALREADY INVEST {}'.format(investigated))
+
+            # Explore the next one
+            for i in xrange(len(objects)):
+                if objects[i].labeled_classification == 'UNKNOWN' and objects[i].id not in investigated:
+                    self.send_feedback('Investingating {}'.format(objects[i].id))
+                    investigated.add(objects[i].id)
+                    move = self.inspect_object(positions[i])
+                    move_id_tuple = (move, objects[i].id)
+                    break
+            if move_id_tuple is None:
+                self.send_feedback('!!!! NO MORE TO EXPLORE')
+                raise Exception('no more to explore')
+
+    def get_objects_indicies_for_start(self, objects):
+        try:
+            white_index = self.get_index_of_type(objects, 'mb_marker_buoy_white')
+            red_index = self.get_index_of_type(objects, 'mb_marker_buoy_red')
+        except StopIteration:
+            return None
+        return white_index, red_index
+
+    @staticmethod
+    def object_classified(objects, obj_id):
+        '''
+        @objects list of object messages
+        @obj_id id of object
+        @return True of object with obj_id is classified
+        '''
+        for obj in objects:
+            if obj.id == obj_id:
+                if obj.labeled_classification == 'UNKNOWN':
+                    return False
+                else:
+                    return True
+        return False
+
+    @txros.util.cancellableInlineCallbacks
+    def prepare_to_enter(self):
+        closest = []
+        robot_position = (yield self.tx_pose)[0]
+        def filter_and_sort(objects, positions):
+            distances = np.linalg.norm(positions - robot_position, axis=1)
+            argsort = np.argsort(distances)
+            return argsort
+
+        def is_done(objects, positions):
+            res = self.get_objects_indicies_for_start(objects)
+            if res is None:
+                return None
+            white_index, red_index = res
+            return objects[white_index], positions[white_index], objects[red_index], positions[red_index]
+
+        white, white_position, red, red_position = yield self.explore_closest_until(is_done, filter_and_sort)
+        self.objects_passed.add(white.id)
+        self.objects_passed.add(red.id)
+        gate = self.get_gate(white_position, red_position, robot_position)
+        self.send_feedback('Going through start gate formed by {} and {}'.format(white.id, red.id))
+        yield self.go_thru_gate(gate, AFTER=-2)
+
+    @txros.util.cancellableInlineCallbacks
+    def run(self, parameters):
+        '''
+        TODO: check for new objects in background, cancel move
+              somefucking how handle case where gates litteraly loop back and head the other direction
+        '''
+        self.objects_passed = set()
+        # Wait a bit for PCDAR to get setup
+        yield self.nh.sleep(3.0)
+        yield self.set_vrx_classifier_enabled(SetBoolRequest(data=True))
+        print("helloooooo")
+        yield self.prepare_to_enter()
+        yield self.wait_for_task_such_that(lambda task: task.state =='running')
+        yield self.move.forward(7.0).go()
+        while not (yield self.do_next_gate()):
+            pass
+        self.send_feedback('This is the last gate! Going through!')
+        yield self.move.forward(10).go()
+        yield self.set_vrx_classifier_enabled(SetBoolRequest(data=False))
+
