3857a951926dae68fdb1c7c1457788c2e4c92a6e
Alex Perez <alex.perez@ufl.edu>
2022-03-05 17:14:36 -0500

    fixed bounds

--- NaviGator/mission_control/navigator_missions/vrx_missions/vrx.py
+++ NaviGator/mission_control/navigator_missions/vrx_missions/vrx.py
@@ -109,7 +109,7 @@ class Vrx(Navigator):
         elif task_name == 'wayfinding':
             yield self.run_submission('VrxWayfinding2')
         elif task_name == 'gymkhana':
-            yield self.run_submission('VrxNavigation3')
+            yield self.run_submission('VrxNavigation')
         elif task_name == 'perception':
             yield self.run_submission('VrxPerception')
         elif task_name == 'wildlife':
