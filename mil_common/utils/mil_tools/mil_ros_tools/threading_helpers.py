21ca3ff1811d6d994519b59ad0949838d550d16b
Cameron Brown <me@cbrxyz.com>
2022-03-06 21:16:43 -0500

    Convert threading_helpers, vector_to_marker to py3, format with black, update references to py executables without .py to include .py

--- mil_common/utils/mil_tools/mil_ros_tools/threading_helpers.py
+++ mil_common/utils/mil_tools/mil_ros_tools/threading_helpers.py
@@ -1,5 +1,9 @@
-def thread_lock(lock):
-    '''Use an existing thread lock to thread-lock a function
+from threading import Lock
+from typing import Callable
+
+def thread_lock(lock: Lock):
+    """
+    Use an existing thread lock to thread-lock a function
     This prevents the function from being executed by multiple threads at once
 
     Example:
@@ -8,23 +12,26 @@ def thread_lock(lock):
 
     @thread_lock(lock)
     def my_function(a, b, c):
-        print a, b, c
-    '''
+        print(a, b, c)
+    """
 
-    def lock_thread(function_to_lock):
-        '''thread_lock(function) -> locked function
+    def lock_thread(function_to_lock: Callable):
+        """
+        thread_lock(function) -> locked function
         Thread locking decorator
             If you use this as a decorator for a function, it will apply a threading lock during
             the execution of that function, which guarantees that no ROS callbacks can change the
             state of data while it is executing. This is critical to make sure that a new message
             being sent doesn't cause a weird serial interruption
-        '''
+        """
+
         def locked_function(*args, **kwargs):
             # Get threading lock
             with lock:
                 result = function_to_lock(*args, **kwargs)
             # Return, pretending the function hasn't changed at all
             return result
+
         # Return the function with locking added
         return locked_function
 
--- mil_common/utils/mil_tools/mil_ros_tools/vector_to_marker.py
+++ mil_common/utils/mil_tools/mil_ros_tools/vector_to_marker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Vector3Stamped
 from visualization_msgs.msg import Marker
@@ -10,14 +10,15 @@ __author__ = "Kevin Allen"
 
 
 class VectorToMarker(object):
-    '''
+    """
     Node to subscribe to a Vector3Stamped topic and publish
     a rviz marker with the same content. Used to get around
     the fact that rviz cannot display Vector3Stamped messages
     @param vector_topic: topic of vecto3stamped to subscribe to
     @param marker_topic: topic of marker to publish
     @param length: length to scale vector to, if None, leave original scale
-    '''
+    """
+
     def __init__(self, vector_topic, marker_topic, length=1.0, color=[0, 0, 1, 1]):
         self.length = length
         self.pub = rospy.Publisher(marker_topic, Marker, queue_size=1)
@@ -25,7 +26,7 @@ class VectorToMarker(object):
         rospy.Subscriber(vector_topic, Vector3Stamped, self.publish)
 
     def publish(self, vec):
-        rospy.logdebug('vector received')
+        rospy.logdebug("vector received")
         marker = Marker()
         marker.header = vec.header
         marker.type = 0
@@ -38,23 +39,44 @@ class VectorToMarker(object):
         if self.length is not None:
             norm = np.linalg.norm(vec)
             if norm == 0:
-                rospy.logwarn('Zero vector recieved, skipping')
+                rospy.logwarn("Zero vector recieved, skipping")
                 return
             vec = (self.length / norm) * vec
         marker.points.append(numpy_to_point(vec))
         self.pub.publish(marker)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Publishes a rviz marker')
-    parser.add_argument('vector_topic', type=str, help='Topic of vector to subscribe to')
-    parser.add_argument('marker_topic', type=str, nargs='?', default='marker', help='Topic to publish marker to')
-    parser.add_argument('--length', '-l', type=float, default=None,
-                        help='Length of vector to output. Default: do not scale')
-    parser.add_argument('--color', type=float, nargs=4, default=[0, 0, 1, 1], metavar=('R', 'G', 'B', 'A'),
-                        help='Color of vector to publish as floats RGBA 0 to 1')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Publishes a rviz marker")
+    parser.add_argument(
+        "vector_topic", type=str, help="Topic of vector to subscribe to"
+    )
+    parser.add_argument(
+        "marker_topic",
+        type=str,
+        nargs="?",
+        default="marker",
+        help="Topic to publish marker to",
+    )
+    parser.add_argument(
+        "--length",
+        "-l",
+        type=float,
+        default=None,
+        help="Length of vector to output. Default: do not scale",
+    )
+    parser.add_argument(
+        "--color",
+        type=float,
+        nargs=4,
+        default=[0, 0, 1, 1],
+        metavar=("R", "G", "B", "A"),
+        help="Color of vector to publish as floats RGBA 0 to 1",
+    )
     args = rospy.myargv()
     args = parser.parse_args(args[1:])
-    rospy.init_node('vector_to_marker')
-    VectorToMarker(args.vector_topic, args.marker_topic, length=args.length, color=args.color)
+    rospy.init_node("vector_to_marker")
+    VectorToMarker(
+        args.vector_topic, args.marker_topic, length=args.length, color=args.color
+    )
     rospy.spin()
