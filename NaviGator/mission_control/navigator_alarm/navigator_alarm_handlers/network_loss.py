2d9ec2014e7bd2a90d691f66211b4605cfe19025
Matthew Hanson <jhanson1@ufl.edu>
2022-03-14 11:30:42 -0400

    migrated NaviGator/mission_control/navigator_alarm
    Migrated package from python2 to python3 using commands and instructions given in porting guide

--- NaviGator/mission_control/navigator_alarm/navigator_alarm_handlers/network_loss.py
+++ NaviGator/mission_control/navigator_alarm/navigator_alarm_handlers/network_loss.py
@@ -1,12 +1,14 @@
 from ros_alarms import HandlerBase, HeartbeatMonitor
 from std_msgs.msg import Header
 
-
+# Network Loss Function
 class NetworkLoss(HandlerBase):
-    alarm_name = 'network-loss'
+    alarm_name = "network-loss"
 
     def __init__(self):
-        self.hm = HeartbeatMonitor(self.alarm_name, '/network', Header, node_name='alarm_server', prd=1.0)
+        self.hm = HeartbeatMonitor(
+            self.alarm_name, "/network", Header, node_name="alarm_server", prd=1.0
+        )
 
     def raised(self, alarm):
         pass
--- NaviGator/mission_control/navigator_alarm/navigator_alarm_handlers/thruster_fault.py
+++ NaviGator/mission_control/navigator_alarm/navigator_alarm_handlers/thruster_fault.py
@@ -4,24 +4,35 @@ import rospy
 
 
 class ThrusterFault(HandlerBase):
-    alarm_name = 'thruster-fault'
+    alarm_name = "thruster-fault"
 
     def __init__(self):
         self.broadcaster = AlarmBroadcaster(self.alarm_name)
 
         # Thruster topics to listen to
-        motor_topics = ['/FL_motor', '/FR_motor', '/BL_motor', '/BR_motor']
+        motor_topics = ["/FL_motor", "/FR_motor", "/BL_motor", "/BR_motor"]
 
         # Dictionary for the faults as defined in roboteq_msgs/Status
-        self.fault_codes = {1: 'OVERHEAT', 2: 'OVERVOLTAGE', 4: 'UNDERVOLTAGE', 8: 'SHORT_CIRCUIT',
-                            16: 'EMERGENCY_STOP', 32: 'SEPEX_EXCITATION_FAULT', 64: 'MOSFET_FAILURE',
-                            128: 'STARTUP_CONFIG_FAULT'}
+        self.fault_codes = {
+            1: "OVERHEAT",
+            2: "OVERVOLTAGE",
+            4: "UNDERVOLTAGE",
+            8: "SHORT_CIRCUIT",
+            16: "EMERGENCY_STOP",
+            32: "SEPEX_EXCITATION_FAULT",
+            64: "MOSFET_FAILURE",
+            128: "STARTUP_CONFIG_FAULT",
+        }
 
         self._raised = False
         self._raised_alarms = {}
 
         # Subscribe to the status message for all thruster topics
-        [rospy.Subscriber(topic + '/status', Status, self._check_faults, topic) for topic in motor_topics]
+        ###possible change
+        [
+            rospy.Subscriber(topic + "/status", Status, self._check_faults, topic)
+            for topic in motor_topics
+        ]
 
     # Return a list that decodes the binary to strings
     def _get_fault_codes(self, fault_id):
@@ -55,10 +66,15 @@ class ThrusterFault(HandlerBase):
                 return
             self.broadcaster.raise_alarm(
                 severity=5,
-                problem_description='{} thrusters have faults'.format(
-                    len(self._raised_alarms)),
-                parameters=dict([(t, self._get_fault_codes(k))
-                                 for t, k in self._raised_alarms.iteritems()])
+                problem_description="{} thrusters have faults".format(
+                    len(self._raised_alarms)
+                ),
+                parameters=dict(
+                    [
+                        (t, self._get_fault_codes(k))
+                        for t, k in self._raised_alarms.iteritems()
+                    ]
+                ),
             )
 
     def raised(self, alarm):
