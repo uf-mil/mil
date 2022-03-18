a94f967423ca0235da9e03d449fd15243da35b90
Cameron Brown <me@cbrxyz.com>
2022-03-18 17:06:58 -0400

    New layout.html

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ intersphinx_mapping = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['docs/_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
