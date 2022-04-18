e4ea73482e0f79a52dffe7d6cba1d92baa25e595
Jordan Hanson <jhanson1@ufl.edu>
2022-04-17 20:53:31 -0400

    migrate package from python 2 to 3
    migrate package mil/mil_common/utils/mil_poi/mil_poi
    
    used the porting guide on MIL docs and used commands found there to check.

--- mil_common/utils/mil_poi/mil_poi/tx_interface.py
+++ mil_common/utils/mil_poi/mil_poi/tx_interface.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 from msg import POIArray
 from twisted.internet import defer
 import txros
@@ -6,23 +7,24 @@ from mil_ros_tools.msg_helpers import rosmsg_to_numpy
 
 
 class TxPOIClient(object):
-    '''
+    """
     TXros interface for a POI client
 
     TODO: add service interfaces for adding / moving / deleting POI
-    '''
+    """
+
     def __init__(self, nh):
-        '''
+        """
         Create a TxPOIClient with a txros nodehandle object
-        '''
+        """
         self.last_msg = None
         self.defers = {}
         self._poi_sub = nh.subscribe("/points_of_interest", POIArray, callback=self._cb)
 
     def _cb(self, msg):
-        '''
+        """
         Internal callback on new points_of_interest updates
-        '''
+        """
         self.last_msg = msg
         for poi in self.last_msg.pois:
             if poi.name not in self.defers:
@@ -34,14 +36,14 @@ class TxPOIClient(object):
 
     @txros.util.cancellableInlineCallbacks
     def get(self, name, only_fresh=False):
-        '''
+        """
         Get the position of POI in the global frame as a 3x1 numpy array.
         Note: returns a defered object which will be calledback when the POI is found, which
               may be immediately
         @param name: the name of the POI
         @param only_fresh: if the POI is already known, wait for a fresh message before returning
 
-        '''
+        """
         if self.last_msg is not None and not only_fresh:
             for poi in self.last_msg.pois:
                 if poi.name == name:
