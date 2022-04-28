a5c1c76ad040e8d7f1c42d66b065dedd06eda9f2
Cameron Brown <me@cbrxyz.com>
2022-04-28 16:48:35 -0400

    Migrate sub8_launch to py3, format with black

--- SubjuGator/command/sub8_launch/scripts/tf_republisher.py
+++ SubjuGator/command/sub8_launch/scripts/tf_republisher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import tf
@@ -7,28 +7,33 @@ import mil_misc_tools.text_effects as te
 
 
 def got_range(msg):
-    '''TODO:
-        - Make parallel to surface
-    '''
+    """TODO:
+    - Make parallel to surface
+    """
     translation = (0.0, 0.0, -msg.range)
     if rospy.Time.now() < rospy.Time(0.5):
         listener.clear()
     t = rospy.Time(0)
     try:
-        listener.waitForTransform('/base_link', '/map', t, rospy.Duration(1))
+        listener.waitForTransform("/base_link", "/map", t, rospy.Duration(1))
         trans, rot = listener.lookupTransform("/base_link", "/map", t)
         bc.sendTransform(translation, rot, rospy.Time.now(), "/ground", "/dvl")
-    except (tf.Exception, tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-        te.fprint(e, title='TF REPUB')
+    except (
+        tf.Exception,
+        tf.LookupException,
+        tf.ConnectivityException,
+        tf.ExtrapolationException,
+    ) as e:
+        te.fprint(e, title="TF REPUB")
 
 
-if __name__ == '__main__':
-    rospy.init_node('tf_republish')
+if __name__ == "__main__":
+    rospy.init_node("tf_republish")
     rospy.sleep(1)
     bc = tf.TransformBroadcaster()
     listener = tf.TransformListener()
     rospy.sleep(1)
 
-    sub = rospy.Subscriber('/dvl/range', RangeStamped, got_range)
+    sub = rospy.Subscriber("/dvl/range", RangeStamped, got_range)
 
     rospy.spin()
