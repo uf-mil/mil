dff11d77afcc885f1c56a802faad14913a4fa827
Cameron Brown <me@cbrxyz.com>
2022-06-02 17:53:36 -0400

    Format setup.py with black

--- NaviGator/utils/navigator_robotx_comms/setup.py
+++ NaviGator/utils/navigator_robotx_comms/setup.py
@@ -4,8 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['navigator_robotx_comms']
-)
+setup_args = generate_distutils_setup(packages=["navigator_robotx_comms"])
 
 setup(**setup_args)
