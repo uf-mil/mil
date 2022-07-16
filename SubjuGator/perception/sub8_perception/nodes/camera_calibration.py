8cd264adee8ce2d2c21e39e73b4ea645f3f566a2
andrew-aj <andrew.knee.j@gmail.com>
2022-07-16 18:20:50 -0400

    Camera calibration works, need to add in proper file saving

--- SubjuGator/perception/sub8_perception/nodes/camera_calibration.py
+++ SubjuGator/perception/sub8_perception/nodes/camera_calibration.py
@@ -3,8 +3,8 @@ import cv2
 import rospy
 from datetime import datetime
 import time
-from Queue import Queue
-from yaml import dump
+from collections import deque
+import yaml
 import os
 import numpy as np
 from mil_ros_tools import Image_Subscriber, Image_Publisher
@@ -17,23 +17,19 @@ class CameraCalibration:
         self.image_sub = Image_Subscriber(self.camera, self.image_cb)
         self.image_pub = Image_Publisher("/image/checkerboard")
         self.bridge = CvBridge()
-        self.curr_time = self.getMs()
+        self.count = 0
         self.checkerboard = (6,9) # measured from inner corners
         self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         self.objp = np.zeros((1, self.checkerboard[0] * self.checkerboard[1], 3), np.float32)
         self.objp[0, :, :2] = np.mgrid[0:self.checkerboard[0], 0:self.checkerboard[1]].T.reshape(-1, 2) * 25.4
-        self.objpoints = Queue()
-        self.imgpoints = Queue()
+        self.objpoints = deque()
+        self.imgpoints = deque()
         self.width = None
         self.height = None
         self.shape = None
         
-
-    def getMs(self):
-        dt_obj = datetime.strptime('20.12.2016 09:38:42,76','%d.%m.%Y %H:%M:%S,%f')
-        return int(time.mktime(dt_obj.utctimetuple()) * 1000 + dt_obj.microsecond / 1000) 
-
     def calculatePoints(self, image):
+        self.count = self.count + 1
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         ret, corners = cv2.findChessboardCorners(gray, self.checkerboard, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
@@ -43,10 +39,13 @@ class CameraCalibration:
             self.image_pub.publish(img)
             self.height, self.width = img.shape[:2]
             self.shape = gray.shape[::-1]
-            if self.getMs() - self.curr_time > 500:
-                self.imgpoints.put(corners2)
-                self.objpoints.put(self.objp)
-                self.curr_time = self.getMs()
+            if self.count >= 7: 
+                self.imgpoints.append(corners2)
+                self.objpoints.append(self.objp)
+                self.count = 0
+                print(len(self.imgpoints))
+                if len(self.imgpoints) > 50:
+                    self.calculate_matrix()
 
     def image_cb(self, image):
         self.calculatePoints(image)
@@ -59,25 +58,21 @@ class CameraCalibration:
                 break
 
     def calculate_matrix(self):
+        print("Calculating camera matrix")
         obj = list(self.objpoints)
         img = list(self.imgpoints)
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj, img, self.shape, None, None)
-        if ret == True:
-            stream = file("calibration.yaml", 'w')
-            yaml.dump(mtx, stream)
-            yaml.dump(ret, stream)
+        print("done")
+        print(mtx)
+        print(dist)
 
-    def __del__(self):
-        self.calculate_matrix()
+
 
 def main():
     rospy.init_node('camera_calibration', anonymous=False)
     CameraCalibration()
 
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Calculating camera matrix")
+    rospy.spin()
 
 if __name__ == '__main__':
     main()
