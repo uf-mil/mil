dd038e827593c779d9f5e84b1ff08230ad0bab3b
Alex Perez <alex.perez@ufl.edu>
2022-08-24 15:34:32 -0400

    Added comment in init to remember either to fix or delete a certain mission

--- NaviGator/mission_control/navigator_missions/navigator_missions/__init__.py
+++ NaviGator/mission_control/navigator_missions/navigator_missions/__init__.py
@@ -40,4 +40,5 @@ from .station_hold import StationHold
 from .stc_jaxon import ScanTheCodeJaxon
 from .teleop import Teleop
 
+# Currently breaks mission server, TODO: fix or delete
 # from .track_target import TrackTarget
