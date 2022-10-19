eb6900db5907cd8be9cbaa29aa491d274b7f1056
Zain Nasrullah <z.nasrullah@ufl.edu>
2022-10-19 15:50:38 -0400

    added mission refresh command

--- scripts/cli/mission.py
+++ scripts/cli/mission.py
@@ -1,8 +1,27 @@
+from __future__ import division, print_function
+
 import time
+
+import rospy
 
 import rich
 import rich_click as click
 from rich.console import Console
+
+from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
+from txros import NodeHandle, Service, action, rosxmlrpc, util
+
+import subprocess
+import rosgraph
+import rosgraph.masterapi
+
+import os
+import socket
+import sys
+
+
+
+#---------------
 
 
 @click.group()
@@ -22,10 +41,24 @@ def run():
 @mission.command()
 def refresh():
     """ """
-    pass
-
+    # run mission_client, to execute it's refresh command
+    # first, check if the ROS master is running- nothing will work without it
+    
+    isOnline = False
+    
+    if rosgraph.is_master_online(): # Checks the master uri and results boolean (True or False)
+        print('ROS MASTER is Online')
+        isOnline = True
+    else:
+        print('ROS MASTER is Offline')
+        
+    if isOnline == True:
+        subprocess.call("rosrun mil_missions mission_client refresh", shell=True)
+    
+    
 
 @mission.command()
 def list():
     """ """
     pass
+    
