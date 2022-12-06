0029f57ebfbaff368eab3f5fe2ec7bc1e93f7b94
Mark <m.cortez@ufl.edu>
2022-12-05 20:08:04 -0500

    The CLI script can now launch the mission_client node and execute the list command. Thus, the command 'mil mission list' should now be able to list all the available missions that can be ran.

--- scripts/cli/mission.py
+++ scripts/cli/mission.py
@@ -41,4 +41,12 @@ def list():
     """
     List all missions that can be run.
     """
-    pass
+    # checks whether the ROS master is online
+    # if online, launches the mission_client node and executes list command
+    # if offline, mission list cannot be retrieved
+    if rosgraph.is_master_online():
+        subprocess.call("rosrun mil_missions mission_client list", shell=True)
+    else:
+        rich.print(
+            "[red1 bold]ERROR: Failed to retrieve available missions. The ROS master appears to be offline."
+        )
