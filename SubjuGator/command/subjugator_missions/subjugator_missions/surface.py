b94c1807fa4b6d726eecc778e0e91e2d38cd498e
cameron brown <52760912+cbrxyz@users.noreply.github.com>
2023-03-15 10:20:41 -0400

    Remove `_PoseProxy` to just use `PoseEditor` (#1002)
    * Sub now rotates to specific orientation before moving through start gate
    
    * First trial of using PoseEditor as first-class class
    
    * Use goto() for smaller func name, update PoseEditor documentation
    
    * Replace all existing missions with new syntax
    
    * Remove PoseProxy and document more methods in SubjuGatorMission

--- SubjuGator/command/subjugator_missions/subjugator_missions/surface.py
+++ SubjuGator/command/subjugator_missions/subjugator_missions/surface.py
@@ -5,5 +5,5 @@ from .sub_singleton import SubjuGatorMission
 class Surface(SubjuGatorMission):
     async def run(self, args):
         self.send_feedback("Surfacing")
-        await self.move.depth(0.2).go()
+        await self.go(self.move().depth(0.2))
         return "Success!"
