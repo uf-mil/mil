2dd077c09f78aa6b63884d91e3fd130ab99c797f
Daniel Parra <dparra1@ufl.edu>
2023-03-27 14:49:20 -0400

    connected camera to ML

--- SubjuGator/perception/subjugator_perception/nodes/enhance_vision.py
+++ SubjuGator/perception/subjugator_perception/nodes/enhance_vision.py
@@ -32,14 +32,47 @@ from subjugator_msgs.srv import (
 class EnhanceVision:
 
     def __init__(self):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        camera = rospy.get_param("~image_topic", "/camera/front/left/image_color")
+
+        self.image_sub = Image_Subscriber(camera, self.enhance_callback)
+        self.camera_info = self.image_sub.wait_for_camera_info()
+        assert self.camera_info is not None
+        self.cam = PinholeCameraModel()
+        self.cam.fromCameraInfo(self.camera_info)
+
+
+    def enhance_callback(self, msg):
+        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device = torch.device('cpu')
 
         # Load model
         model = PhysicalNN()
         model = torch.nn.DataParallel(model).to(device)
-        checkpoint = torch.load('checkpoints/model_best_2842.pth.tar')
-            
-        print("In Progress...")
+        checkpoint = torch.load('checkpoints/model_best_2842.pth.tar', map_location=device)
+        model.load_state_dict(checkpoint['state_dict'])
+        print("=> loaded model at epoch {}".format(checkpoint['epoch']))
+        model = model.module
+        model.eval()
+
+        testtransform = transforms.Compose([
+                transforms.ToTensor(),
+            ])
+        unloader = transforms.ToPILImage()
+
+        # Create Image from array
+        img = Image.fromarray(msg.astype('uint8'), 'RGB')
+        inp = testtransform(img).unsqueeze(0)
+        inp = inp.to(device)
+        out = model(inp)
+        
+        # Place result images in directory
+        corrected = unloader(out.cpu().squeeze(0))
+        dir = '{}/results_{}'.format('.', checkpoint['epoch'])
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        corrected.save(dir+'/{}corrected.png'.format("camera_"))
+
+        
         
 if __name__== "__main__":
     rospy.init_node("enhance_vision")
