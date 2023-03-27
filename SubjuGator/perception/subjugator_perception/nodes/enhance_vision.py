34bddd46911156485d2d2510bd4aa984867d3852
Daniel Parra <dparra1@ufl.edu>
2023-03-27 18:22:35 -0400

    added topic subscriber

--- SubjuGator/perception/subjugator_perception/nodes/enhance_vision.py
+++ SubjuGator/perception/subjugator_perception/nodes/enhance_vision.py
@@ -30,8 +30,24 @@ from subjugator_msgs.srv import (
 )
 
 class EnhanceVision:
+     #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+     device = torch.device('cpu')
 
-    def __init__(self):
+    # Load model
+     model = PhysicalNN()
+     model = torch.nn.DataParallel(model).to(device)
+     checkpoint = torch.load('checkpoints/model_best_2842.pth.tar', map_location=device)
+     model.load_state_dict(checkpoint['state_dict'])
+     print("=> loaded model at epoch {}".format(checkpoint['epoch']))
+     model = model.module
+     model.eval()
+
+     testtransform = transforms.Compose([
+             transforms.ToTensor(),
+          ])
+     unloader = transforms.ToPILImage()
+
+     def __init__(self):
         camera = rospy.get_param("~image_topic", "/camera/front/left/image_color")
 
         self.image_sub = Image_Subscriber(camera, self.enhance_callback)
@@ -39,39 +55,28 @@ class EnhanceVision:
         assert self.camera_info is not None
         self.cam = PinholeCameraModel()
         self.cam.fromCameraInfo(self.camera_info)
+        self.image_pub = Image_Publisher("~enhance_cam_topic")
 
-
-    def enhance_callback(self, msg):
-        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        device = torch.device('cpu')
-
-        # Load model
-        model = PhysicalNN()
-        model = torch.nn.DataParallel(model).to(device)
-        checkpoint = torch.load('checkpoints/model_best_2842.pth.tar', map_location=device)
-        model.load_state_dict(checkpoint['state_dict'])
-        print("=> loaded model at epoch {}".format(checkpoint['epoch']))
-        model = model.module
-        model.eval()
-
-        testtransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-        unloader = transforms.ToPILImage()
-
+     def enhance_callback(self, msg):
         # Create Image from array
+        starttime = datetime.datetime.now()
         img = Image.fromarray(msg.astype('uint8'), 'RGB')
-        inp = testtransform(img).unsqueeze(0)
-        inp = inp.to(device)
-        out = model(inp)
-        
+        inp = self.testtransform(img).unsqueeze(0)
+        inp = inp.to(self.device)
+        out = self.model(inp)
+       
         # Place result images in directory
-        corrected = unloader(out.cpu().squeeze(0))
-        dir = '{}/results_{}'.format('.', checkpoint['epoch'])
+        corrected = self.unloader(out.cpu().squeeze(0))
+        print(type(corrected))
+
+        self.image_pub.publish(np.array(corrected))
+
+        dir = '{}/results_{}'.format('.', self.checkpoint['epoch'])
         if not os.path.exists(dir):
             os.makedirs(dir)
         corrected.save(dir+'/{}corrected.png'.format("camera_"))
-
+        endtime = datetime.datetime.now()
+        print(endtime-starttime)
         
         
 if __name__== "__main__":
