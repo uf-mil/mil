f0523a1a739296ad70880fd2922f019c5d071cfa
keithmk <keithmk@MIL.myguest.virtualbox.org>
2023-09-26 16:25:05 -0400

    Created Preflight node and checks if running in simulation

---
+++ mil_common/utils/mil_tools/scripts/preflight.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import rospy
+
+# Use Rich for nice printing
+if __name__ == "__main__":
+    # Initialize the Preflight Node
+    rospy.init_node("preflight")
+
+    print("Checking for Gazebo")
+    # Check if the Gazebo Node exists
+    if rospy.get_param("/is_simulation", None):
+        print("Running a Simulation")
+    else:
+        print("Running for real")
