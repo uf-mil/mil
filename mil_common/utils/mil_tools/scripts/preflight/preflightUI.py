7e68619f347d3b62267e0a2f98009d0373250020
Anthony Liao <anthonyliao@ufl.edu>
2023-10-10 15:46:12 -0400

    menu add

--- mil_common/utils/mil_tools/scripts/preflight/preflightUI.py
+++ mil_common/utils/mil_tools/scripts/preflight/preflightUI.py
@@ -5,31 +5,63 @@ inp = ""
 term = blessed.Terminal()
 pos = 0
 
+machine = ["SubjuGator", "NaviGator", "Exit"]
+options = [
+    "Hardware Checklist",
+    "Software Checklist",
+    "Rosnode Command",
+    "Back",
+    "Exit",
+]
+currmenu = machine
+
+
 while inp != "q":
-    maxPos = 3
-    height, width = term.height, term.width
-
+    maxPos = len(currmenu)
     # reprint line selected -> term.on_green"message"
-
     print(term.home + term.clear + term.blink + term.on_green("pressed ") + repr(inp))
 
-    print("testing screen\n")
-    print("checklist")
-    print("hiiardware checklist")
+    print("Preflight Checklist\n")
 
-    print(term.move_y(term.height // 2))
-    print(term.black_on_darkkhaki(term.center("press any key")))
+    for x in range(0, maxPos):
+        if x == pos:
+            print(term.blink + term.on_green(currmenu[x]))
+        else:
+            print(currmenu[x])
 
     with term.cbreak():
         inp = term.inkey()
-        if inp == "k" or inp == "KEY_UP":
+        if inp == "k" or inp.name == "KEY_UP":
             if pos == 0:
-                pos = maxPos
+                pos = maxPos - 1
             else:
                 pos -= 1
 
-        if inp == "j" or inp == "KEY_DOWN":
-            if pos == maxPos:
+        if inp == "j" or inp.name == "KEY_DOWN":
+            if pos == maxPos - 1:
                 pos = 0
             else:
                 pos += 1
+        if inp.name == "KEY_ENTER":
+            if pos == maxPos - 1:
+                inp = "q"
+                continue
+            if currmenu == machine:
+                currmenu = options
+            elif currmenu == options:
+                pass
+            else:
+                pass
+
+
+def hardware_test(terminal):
+    pass
+
+
+def software_test(terminal):
+    # this should run ros and do all the init tests
+    pass
+
+
+def run_command(terminal):
+    pass
