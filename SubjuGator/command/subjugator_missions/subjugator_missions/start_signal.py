93ae59c563af121e043746ef3703cdee24a9884a
Daniel Parra <dparra1@ufl.edu>
2024-02-20 17:17:24 -0500

    added self to buoy_positions

--- SubjuGator/command/subjugator_missions/subjugator_missions/start_signal.py
+++ SubjuGator/command/subjugator_missions/subjugator_missions/start_signal.py
@@ -3,13 +3,19 @@ from .sub_singleton import SubjuGatorMission
 
 
 class StartSignal(SubjuGatorMission):
-    buoy_positions = [[5, 10, 3], [6, 8, 5], [3, 12, 10]] # x, y, z displacements
+    buoy_positions = [[5, 10, 3], [6, 8, 5], [3, 12, 10]]  # x, y, z displacements
+
     async def run(self, args):
-        for i in len(buoy_positions):
-            self.send_feedback(f'Travelling to Buoy {i}')
-            await self.go(self.move().right(buoy_positions[i][0]).up(buoy_positions[i][1].forward(buoy_positions[i][2])))
-            self.send_feedback(f'Back to Origin')
-            await self.go(self.move().right(-buoy_positions[i][0]).up(-buoy_positions[i][1].forward(-buoy_positions[i][2])))
-
-
-
+        for i in len(self.buoy_positions):
+            self.send_feedback(f"Travelling to Buoy {i}")
+            await self.go(
+                self.move()
+                .right(self.buoy_positions[i][0])
+                .up(self.buoy_positions[i][1].forward(self.buoy_positions[i][2])),
+            )
+            self.send_feedback("Back to Origin")
+            await self.go(
+                self.move()
+                .right(-self.buoy_positions[i][0])
+                .up(-self.buoy_positions[i][1].forward(-self.buoy_positions[i][2])),
+            )
