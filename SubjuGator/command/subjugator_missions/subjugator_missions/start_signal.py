50669192e7b7085468a144cad2e58a305735e7d2
Thomas Canro <tjcanro@gmail.com>
2024-02-25 17:00:59 -0500

    fixed syntax error

--- SubjuGator/command/subjugator_missions/subjugator_missions/start_signal.py
+++ SubjuGator/command/subjugator_missions/subjugator_missions/start_signal.py
@@ -8,23 +8,20 @@ class StartSignal(SubjuGatorMission):
 
     buoy_positions = [[5, 10, 3], [6, 8, 5], [3, 12, 10]]  # x, y, z displacements
 
-""""
-    async def run(self, args):
-        for i in len(self.buoy_positions):
-            self.send_feedback(f"Travelling to Buoy {i}")
-            await self.go(
-                self.move()
-                .right(self.buoy_positions[i][0])
-                .up(self.buoy_positions[i][1].forward(self.buoy_positions[i][2])),
-            )
-            self.send_feedback("Back to Origin")
-            await self.go(
-                self.move()
-                .right(-self.buoy_positions[i][0])
-                .up(-self.buoy_positions[i][1].forward(-self.buoy_positions[i][2])),
-            )
-"""
-
+    # async def run(self, args):
+    #     for i in len(self.buoy_positions):
+    #         self.send_feedback(f"Travelling to Buoy {i}")
+    #         await self.go(
+    #             self.move()
+    #             .right(self.buoy_positions[i][0])
+    #             .up(self.buoy_positions[i][1].forward(self.buoy_positions[i][2])),
+    #         )
+    #         self.send_feedback("Back to Origin")
+    #         await self.go(
+    #             self.move()
+    #             .right(-self.buoy_positions[i][0])
+    #             .up(-self.buoy_positions[i][1].forward(-self.buoy_positions[i][2])),
+    #         )
     async def run_rotation(self, args):
         for i in len(self.args):
             pitch_angle = np.arctan(self.buoy_positions[i][2]/self.buoy_positions[i][1])
@@ -33,20 +30,17 @@ class StartSignal(SubjuGatorMission):
             await self.go(
                 # Yaw to the right if the x distance is positive and to the left if negative
                 self.move()
-                .yaw_left(yaw_angle) if self.buoy_positions[i][0] < 0 else .yaw_right(yaw_angle)
-        
-                # If the pitch angle is positive, pitch up, if negative, pitch down
-                .pitch_up(pitch_angle) if pitch_angle > 0 else .pitch_down(abs(pitch_angle))
+                .yaw_left(yaw_angle) if self.buoy_positions[i][0] < 0 else self.move().yaw_right(yaw_angle)
+                # Pitch up if angle is postive, pitch down id angle is negative
+                .pitch_up(pitch_angle) if pitch_angle > 0 else self.move().pitch_down(abs(pitch_angle))
             )
             self.send_feedback(f"Traveling forward to buoy {i}")
             await self.go(
-                self.move(
+                self.move()
                     .forward(self.buoy_positions[i][2])
-                )
             )
             self.send_feedback("Back to Origin")
             await self.go(
                 self.move()
                 .forward(-self.buoy_positions[i][2])
             )
-
