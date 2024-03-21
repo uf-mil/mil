a8bd9df2d9ded06b97f60033917fc4b31b3b8c64
Cameron Brown <me@cbrxyz.com>
2024-03-21 18:34:46 -0400

    Add magical_ros2_conversion_tool install script

---
+++ scripts/install_magical_ros2.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+## Ensure that the user is at ~/catkin_ws/src/mil
+cd ~/catkin_ws/src/mil || exit
+
+cmonly() {
+	cd ~/catkin_ws || exit
+	catkin_make --only-pkg-with-deps "$1"
+	cd - >/dev/null || exit
+}
+
+## Clone the magical_ros2 repository
+git clone https://github.com/DLu/roscompile
+cd roscompile || exit
+cmonly ros_introspection
+cmonly roscompile
+cmonly magical_ros2_conversion_tool
+cd ~/catkin_ws/src/mil || exit
+rm -rf roscompile
+
+python3 -m pip install ruamel.yaml
+
+echo -e "\n\n\n\n\n"
+echo "To call the conversion tool, run the following command:"
+echo ">>> rosrun magical_ros2_conversion_tool ros2_conversion"
+echo "This will convert the ROS1 packages in the workspace to ROS2 packages."
