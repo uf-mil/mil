5d6aff6d4cf204ccda22234686b654059b632c12
Thomas Canro <tjcanro@gmail.com>
2024-04-23 13:10:40 -0400

    down camera info for later use

--- SubjuGator/command/subjugator_missions/subjugator_missions/ocean_temperatures.py
+++ SubjuGator/command/subjugator_missions/subjugator_missions/ocean_temperatures.py
@@ -1,8 +1,26 @@
 #! usr/bin/env python3
 
+import numpy as np
+from mil_misc_tools import text_effects
+from sensor_msgs.msg import CameraInfo
+
 from .sub_singleton import SubjuGatorMission
+
+fprint = text_effects.FprintFactory(title="OCEAN TEMPERATURES", msg_color="blue").fprint
 
 
 class OceanTemperatures(SubjuGatorMission):
     async def run(self, args):
-        pass  # test 3
+        fprint("Enabling cam_ray publisher")
+
+        await self.nh.sleep(1)
+
+        fprint("Connecting camera")
+
+        cam_info_sub = self.nh.subscribe("/camera/down/camera_info", CameraInfo)
+        await cam_info_sub.setup()
+
+        fprint("Obtaining cam info message")
+        cam_info = await cam_info_sub.get_next_message()
+        cam_center = np.array([cam_info.width / 2, cam_info.height / 2])
+        fprint(f"Cam center: {cam_center}")
