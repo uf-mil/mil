25c4116d3dc8a27a433c3944a910f63ad8b93f8e
Cameron Brown <me@cbrxyz.com>
2024-06-08 17:09:07 -0400

    clang-format

--- mil_common/mil_missions/src/client.cpp
+++ mil_common/mil_missions/src/client.cpp
@@ -2,56 +2,58 @@
 
 namespace mil_missions
 {
-MissionClient::MissionClient(ros::NodeHandle &nh) : ac("/mission", true), _nh(nh) {}
+MissionClient::MissionClient(ros::NodeHandle &nh) : ac("/mission", true), _nh(nh)
+{
+}
 
 std::vector<std::string> MissionClient::available_missions()
 {
-    std::vector<std::string> missions;
-    if (!_nh.getParam("/available_missions", missions))
-    {
-        ROS_ERROR("No available missions found");
-        return {};
-    }
-    return missions;
+  std::vector<std::string> missions;
+  if (!_nh.getParam("/available_missions", missions))
+  {
+    ROS_ERROR("No available missions found");
+    return {};
+  }
+  return missions;
 }
 
 void MissionClient::run_mission(std::string mission_name, std::string parameters)
 {
-    if (!ac.waitForServer(ros::Duration(5.0)))
-    {
-        ROS_ERROR("Could not connect to mission server to run mission");
-        return;
-    }
+  if (!ac.waitForServer(ros::Duration(5.0)))
+  {
+    ROS_ERROR("Could not connect to mission server to run mission");
+    return;
+  }
 
-    mil_missions::DoMissionGoal goal;
-    goal.mission = mission_name;
-    goal.parameters = parameters;
+  mil_missions::DoMissionGoal goal;
+  goal.mission = mission_name;
+  goal.parameters = parameters;
 
-    ac.sendGoal(goal);
+  ac.sendGoal(goal);
 }
 
 void MissionClient::cancel_mission()
 {
-    ac.cancelAllGoals();
+  ac.cancelAllGoals();
 }
 
 bool MissionClient::wait_for_server(ros::Duration timeout)
 {
-    return ac.waitForServer(timeout);
+  return ac.waitForServer(timeout);
 }
 
 bool MissionClient::wait_for_result(ros::Duration timeout)
 {
-    return ac.waitForResult(timeout);
+  return ac.waitForResult(timeout);
 }
 
 DoMissionResult::ConstPtr MissionClient::get_result()
 {
-    return ac.getResult();
+  return ac.getResult();
 }
 
 actionlib::SimpleClientGoalState MissionClient::get_state()
 {
-    return ac.getState();
+  return ac.getState();
 }
 }  // namespace mil_missions
--- mil_common/mil_missions/test/test_run_mission.cpp
+++ mil_common/mil_missions/test/test_run_mission.cpp
@@ -1,17 +1,19 @@
+#include <actionlib/client/simple_action_client.h>
 #include <gtest/gtest.h>
+#include <mil_missions/DoMissionResult.h>
 #include <mil_missions/client.h>
 #include <ros/ros.h>
-#include <iostream>
 
-#include <mil_missions/DoMissionResult.h>
-#include <actionlib/client/simple_action_client.h>
+#include <iostream>
 
 using namespace mil_missions;
 
 class MissionClientTest : public ::testing::Test
 {
 protected:
-  MissionClientTest() : client(nh) {}
+  MissionClientTest() : client(nh)
+  {
+  }
 
   ros::NodeHandle nh;
   mil_missions::MissionClient client;
@@ -32,7 +34,8 @@ TEST_F(MissionClientTest, RunMission)
   EXPECT_EQ(result->result, "The darkness isn't so scary");
 }
 
-TEST_F(MissionClientTest, FailingMission) {
+TEST_F(MissionClientTest, FailingMission)
+{
   EXPECT_TRUE(client.wait_for_server());
 
   // Test running a mission
@@ -46,18 +49,19 @@ TEST_F(MissionClientTest, FailingMission) {
   EXPECT_EQ(result->result, "Exception occurred in FailingMission mission: ValueError: This is an example error!");
 }
 
-TEST_F(MissionClientTest, CancelledTest) {
-    EXPECT_TRUE(client.wait_for_server());
-    
-    // Test running a mission
-    client.run_mission("CancelledMission");
-    EXPECT_TRUE(client.wait_for_result(ros::Duration(5.0)));
-    mil_missions::DoMissionResult::ConstPtr result = client.get_result();
-    actionlib::SimpleClientGoalState state = client.get_state();
-    EXPECT_EQ(state, actionlib::SimpleClientGoalState::ABORTED);
-    EXPECT_FALSE(result->success);
-    EXPECT_TRUE(result->parameters.empty());
-    EXPECT_EQ(result->result, "mission cancelled");
+TEST_F(MissionClientTest, CancelledTest)
+{
+  EXPECT_TRUE(client.wait_for_server());
+
+  // Test running a mission
+  client.run_mission("CancelledMission");
+  EXPECT_TRUE(client.wait_for_result(ros::Duration(5.0)));
+  mil_missions::DoMissionResult::ConstPtr result = client.get_result();
+  actionlib::SimpleClientGoalState state = client.get_state();
+  EXPECT_EQ(state, actionlib::SimpleClientGoalState::ABORTED);
+  EXPECT_FALSE(result->success);
+  EXPECT_TRUE(result->parameters.empty());
+  EXPECT_EQ(result->result, "mission cancelled");
 }
 
 int main(int argc, char **argv)
