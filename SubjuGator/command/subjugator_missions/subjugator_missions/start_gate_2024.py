7fb0d4ad6d28043ce0c8a86e974b998b5c38a440
Keith Khadar <keithkhadar@gmail.com>
2024-08-01 18:10:39 -0400

    Now uses magnetic field data to align with gate

--- SubjuGator/command/subjugator_missions/subjugator_missions/start_gate_2024.py
+++ SubjuGator/command/subjugator_missions/subjugator_missions/start_gate_2024.py
@@ -2,23 +2,27 @@
 
 import math
 
+from sensor_msgs.msg import MagneticField
 from vision_stack.msg import ObjectDetections
 
 from .sub_singleton import SubjuGatorMission
 
 # Constants
-YAW_LENGTH = 15  # When searching for start gate how many degrees to turn
+PROPER_HEADING = 191.76238  # This is the compass heading that points perpendicular to the dock and towards the course
+# Compass heading for away from the dock
+ENTER_RIGHT_SIDE = True  # Which side to enter, if false enters left side
+
+
 SPEED = 0.2
-FRAME_WIDTH = 960  # Pixels
+STARTING_DOWN_DIST = 0.6
+ENDING_SIDE_DIST = 0.75  # After going through the gate how many meters to move to the side to see the path marker
 
-DIST_CONST = 700
-TRANSLATION_CONST = 1 / 700
-ANGLE_CONST = 0.942334
+FRAME_WIDTH = 960  # Pixel width of the camera image
 
-ENTER_RIGHT = True  # Enter the right side of the gate (if false enters the left side)
-DOWN_DIST = 0.7  # meters to submerge to enter the gate
-FORWARD_DIST = 2.5  # how many meters to go through the gate
-SIDE_DIST = 0.7  # how many meters to go under one of the spirals
+TRANSLATION_STEP = 0.2  # Move increment for trying to center with the red spiral
+
+DIST_CONST = 145  # This is used to estimate distance. This value is found experimentally and may need to be changed
+# If new sensors are used. decreasing this decreases the dist est
 
 DEBUG = True
 
@@ -26,17 +30,51 @@ DEBUG = True
 class StartGate2024(SubjuGatorMission):
     async def run(self, args):
         """run is a special function used as the entry point into a mission"""
-        # Call a one time setup function
+        # Setup subscribers and initialize variables
         await self.start()
 
-        # Start an update loop
-        await self.update()
+        # Align with proper heading (Look away from the wall)
+        await self.alignWithProperHeading()
+
+        # Align with a spiral on the start gate
+        await self.alignWithSide()
+
+        # Enter Gate
+        await self.enterGate()
 
         # Call a shutdown function
         await self.endMission()
 
     async def start(self):
         """This function is called once at the start of the mission. Used for initialization and setup"""
+        # Submerge the sub
+        await self.go(
+            self.move().down(STARTING_DOWN_DIST),
+            speed=SPEED,
+        )
+
+        # Get the initial heading of the sub
+        magnetic_sub = self.nh.subscribe(
+            "/imu/mag",
+            MagneticField,
+        )
+        await magnetic_sub.setup()
+
+        # Calculate heading in degrees
+        magnetic_msg = await magnetic_sub.get_next_message()
+        y = magnetic_msg.magnetic_field.y
+        x = magnetic_msg.magnetic_field.x
+
+        self.initial_heading = 0.0
+        if y > 0:
+            self.initial_heading = 90 - math.atan(x / y) * 180 / math.pi
+        if y < 0:
+            self.initial_heading = 270 - math.atan(x / y) * 180 / math.pi
+        if y == 0 and x > 0:
+            self.initial_heading = 180.0
+
+        if DEBUG:
+            print(f"Initial Heading: {self.initial_heading}.")
 
         # Subscribe to object detection message from detections
         self.detections_sub = self.nh.subscribe(
@@ -45,174 +83,102 @@ class StartGate2024(SubjuGatorMission):
         )
         await self.detections_sub.setup()
 
-        # Initialize variables
-        self.isCompleted = False
-        self.centerX = 0
-        self.centerY = 0
-        self.dist_est = 0
-        self.angle_est = 0
-        self.width_ratio = 0
+    async def alignWithProperHeading(self):
+        """This aligns the sub with the proper heading. Make sure to measure this with a compass
+        and that it points away from the wall of the pool. Enter this at the top of this file
+        """
 
-        # Submerge slightly
-        # await self.go(
-        #     self.move().down(DOWN_DIST),
-        #     speed=SPEED,
-        # )
+        # Calculate angle to rotate
+        angle = PROPER_HEADING - self.initial_heading
 
-    async def update(self):
-        """Update is continuously called"""
-        while not self.isCompleted:
-            # Get the detections from vision stack
-            detections_msg = await self.detections_sub.get_next_message()
-            detections = detections_msg.detections
+        # Rotate the sub
+        await self.go(
+            self.move().yaw_left_deg(angle),
+            speed=SPEED,
+        )
 
-            # Update start gate estimates
-            self.getInfo(detections)
+    async def getDetections(self):
+        """Gets the yolo detections and filters them to only output the red and blue spiral"""
 
-            # if not self.getInfo(detections) :
-            #     # If we did not see the start gate look for it
-            #     await self.lookForGate()
-            #     continue
+        while True:
+            # Get the spiral detections
+            detection_msg = await self.detections_sub.get_next_message()
+            detections = detection_msg.detections
 
-            # # Now that we see the gate move towards it
-            # await self.approachGate()
+            # Filter out only the spirals
+            red_spiral = blue_spiral = None
 
-            # # Then enter the gate and complete the mission
-            # await self.enterGate()
+            for d in detections:
+                if d.class_name == "Red spiral":
+                    red_spiral = d
+                elif d.class_name == "Blue spiral":
+                    blue_spiral = d
+                if red_spiral and blue_spiral:
+                    break
 
-            # # Complete mission
-            # self.isCompleted = True
+            # Pick which spiral we are going to go through
+            current_spiral = red_spiral if ENTER_RIGHT_SIDE else blue_spiral
+
+            # If we dont see the current spiral we are going to go through wait for another detection
+            if current_spiral is None:
+                continue
+
+            return current_spiral
+
+    async def alignWithSide(self):
+        """This aligns the sub with one of the sides of the start gate"""
+
+        # Initialize errors (error being how far the spiral is from the center of the screen)
+        prev_error = math.inf
+        current_error = 0
+
+        while True:
+            # Get the spiral detections
+            spiral = await self.getDetections()
+
+            # Calculate the current error
+            current_error = (FRAME_WIDTH / 2) - spiral.center_x
+
+            if DEBUG:
+                print(f"Current Error: {current_error}, Prev Error: {prev_error}")
+
+            # Check if the error went up since last time. This indicates we were aligned before so stop aligning
+            if abs(current_error) > abs(prev_error):
+                break
+
+            # Move left or right to try and align. Sign is determined from what side of the screen the current spiral is on.
+            await self.go(
+                self.move().left(
+                    TRANSLATION_STEP * (current_error / abs(current_error)),
+                ),
+                speed=SPEED,
+            )
+
+            # Before we loop again set the prev error to the current
+            prev_error = current_error
+
+    async def enterGate(self):
+        "This estimate the distance to enter the gate and enters it"
+
+        # Get the spiral detections
+        spiral = await self.getDetections()
+
+        # Estimate the distance
+        # Using width of the bounding box to estimate distance because the height of the box varies to much to be reliable
+        dist_est = DIST_CONST / spiral.width
+
+        if DEBUG:
+            print(f"Distance to move: {dist_est}")
+
+        # Move that distance
+        await self.go(self.move().forward(dist_est), speed=SPEED)
+
+        # Strafe left or right to see the path marker
+        side_dist = ENDING_SIDE_DIST * -1 if ENTER_RIGHT_SIDE else ENDING_SIDE_DIST
+
+        await self.go(self.move().right(side_dist), speed=SPEED)
 
     async def endMission(self):
         """Called on completion of the mission"""
         print("Start Gate Mission Completed")
         pass
-
-    def getInfo(self, detections):
-        """This updates the current information about the start gate
-        Returns True if new info was saved otherwise returns false
-        """
-
-        # Filter out other detections
-        spiral_detections = [
-            d for d in detections if d.class_name in ["Red spiral", "Blue spiral"]
-        ]
-
-        # Make sure we have a red and blue detection
-        if len(spiral_detections) == 2:
-            # Update center position
-            self.centerX = (
-                +spiral_detections[0].center_x + spiral_detections[1].center_x
-            ) / 2
-            self.centerY = (
-                +spiral_detections[0].center_y + spiral_detections[1].center_y
-            ) / 2
-
-            centerDistX = abs(self.centerX - spiral_detections[0].center_x)
-            # centerDistY = abs(self.centerY - spiral_detections[0].center_y)
-
-            if spiral_detections[0].class_name == "Red spiral":
-                self.width_ratio = (
-                    spiral_detections[1].width / spiral_detections[0].width
-                )
-            else:
-                self.width_ratio = (
-                    spiral_detections[0].width / spiral_detections[1].width
-                )
-
-            # Update angle estimate
-            self.angle_est = (self.width_ratio - 1) * ANGLE_CONST
-
-            # Update dist estimate
-            adjusted_centerDistX = centerDistX / math.cos(self.angle_est)
-
-            # These values were obtained experimentally, but the actual equation should be physical dist between two spirals * focal length of camera / centerDistX
-            self.dist_est = abs(DIST_CONST / adjusted_centerDistX)
-
-            if DEBUG:
-                print(f"angle: {self.angle_est}")
-                print(f"dist: {self.dist_est}")
-                print(f"width_r: {self.width_ratio}")
-                print(f"center dist x: {centerDistX}")
-                print(f"center dist FOV: {abs(self.centerX-FRAME_WIDTH/2)}")
-            return True
-
-        return False
-
-    async def lookForGate(self):
-        """Called to have the sub rotate until it sees the start gate"""
-
-        dir = 1  # Going to look right by default
-
-        # Determine which direction to look
-        if self.centerX < FRAME_WIDTH / 2:
-            # Look left
-            dir = -1
-
-        if self.centerX == 0:
-            # If we have never seen the start gate look to the right first
-            dir = 1
-
-        # Create the pose for the sub to move towards and move to it at SPEED
-        await self.go(
-            self.move().yaw_right_deg(dir * YAW_LENGTH).zero_roll_and_pitch(),
-            speed=SPEED,
-        )
-
-    async def approachGate(self):
-        """Called to have the sub move towards the start gate"""
-
-        # Strafe left or right depending on the angle of the start gate
-        # Approximate dist as parallel distance
-        parallel_dis = self.dist_est * math.sin(self.angle_est)
-
-        # TODO: Check negative angles
-
-        # Yaw left or right to align with the start gate
-        await self.go(
-            self.move().yaw_left(self.angle_est),
-            speed=SPEED,
-        )
-
-        # Align with center of gate
-        await self.go(
-            self.move().right(parallel_dis),
-            speed=SPEED,
-        )
-
-        # # Align with center of spiral
-        await self.go(
-            self.move().left((FRAME_WIDTH / 2 - self.centerX) * TRANSLATION_CONST),
-            speed=SPEED,
-        )
-
-        # Approach Gate, but dont enter (that is what the minus 2 is for)
-        dist = (
-            self.dist_est * math.cos(self.angle_est) - 2
-            if self.dist_est * math.cos(self.angle_est) > 2
-            else 0
-        )
-        await self.go(
-            self.move().forward(dist),
-            speed=SPEED,
-        )
-
-    async def enterGate(self):
-        # Choose a side
-        side = SIDE_DIST if ENTER_RIGHT else -1 * SIDE_DIST
-        await self.go(
-            self.move().right(side),
-            speed=SPEED,
-        )
-
-        # Enter the gate
-        await self.go(
-            self.move().forward(FORWARD_DIST),
-            speed=SPEED,
-        )
-
-        # Move back to the center
-        await self.go(
-            self.move().right(side * -1),
-            speed=SPEED,
-        )
