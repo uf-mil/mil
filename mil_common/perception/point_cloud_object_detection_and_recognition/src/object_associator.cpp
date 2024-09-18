7f82a75dfcd1b5834f9b792418e3e670ee0fef5a
Daniel Parra <daniel27parra@gmail.com>
2024-09-18 12:36:41 -0400

    added area for enemy detection and exclusion

--- mil_common/perception/point_cloud_object_detection_and_recognition/src/object_associator.cpp
+++ mil_common/perception/point_cloud_object_detection_and_recognition/src/object_associator.cpp
@@ -6,6 +6,9 @@
 #include <point_cloud_object_detection_and_recognition/object_associator.hpp>
 #include <unordered_set>
 
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+
 namespace pcodar
 {
 void Associator::associate(ObjectMap& prev_objects, point_cloud const& pc, clusters_t clusters, bool moving_back)
@@ -14,8 +17,8 @@ void Associator::associate(ObjectMap& prev_objects, point_cloud const& pc, clust
   std::unordered_set<uint> seen;
 
   // Establish Area of Interest (AOI) that new points are not allowed to be published while Navigator is moving backwards
-  Eigen::Vector3f min_aoi(0.0f, -2.0f, -0.5f); // Min bounds (x, y, z)
-  Eigen::Vector3f max_aoi(500.0f, 2.0f, 0.5f);   // Max bounds (x, y, z)
+  Eigen::Vector3f min_aoi(0.0f, -5.0f, -0.5f); // Min bounds (x, y, z)
+  Eigen::Vector3f max_aoi(10.0f, 5.0f, 0.5f); // Max bounds (x, y, z)
 
   // Iterate through each new cluster, finding which persistent cluster(s) it matches
   for (cluster_t const& cluster : clusters)
@@ -29,15 +32,6 @@ void Associator::associate(ObjectMap& prev_objects, point_cloud const& pc, clust
     CorrespondenceEstimation ce;
     ce.setSearchMethodTarget(cluster_search_tree, true);
     ce.setInputTarget(cluster_pc);
-
-    // Compute the centroid of the cluster
-    Eigen::Vector4f centroid;
-    pcl::compute3DCentroid(*cluster_pc, centroid);
-
-    // Check if the centroid is outside the AOI
-    bool outside_aoi = (centroid[0] < min_aoi[0] || centroid[0] > max_aoi[0] ||  // X bounds
-                        centroid[1] < min_aoi[1] || centroid[1] > max_aoi[1] ||  // Y bounds
-                        centroid[2] < min_aoi[2] || centroid[2] > max_aoi[2]);   // Z bounds
 
     using ObjectMapIt = decltype(prev_objects.objects_.begin());
     std::vector<ObjectMapIt> matches;
@@ -54,7 +48,68 @@ void Associator::associate(ObjectMap& prev_objects, point_cloud const& pc, clust
     }
 
     // If Navigator is moving back, new objects outside of the AOI are invalid
-    if (!moving_back && outside_aoi)
+    if(moving_back)
+    {
+      // Compute the centroid of the cluster
+      Eigen::Vector4f centroid;
+      pcl::compute3DCentroid(*cluster_pc, centroid);
+
+      // Transform centroid from /enu to /wamv/baselink
+      tf::TransformListener listener;
+      tf::StampedTransform transform;
+
+      try
+      {
+        // Listen for the transform from /enu to /wamv/base_link
+        listener.waitForTransform("/wamv/base_link", "/enu", ros::Time(0), ros::Duration(1.0));
+        listener.lookupTransform("/wamv/base_link", "/enu", ros::Time(0), transform);
+
+        // Apply the transformation to the centroid
+        tf::Vector3 centroid_enu(centroid[0], centroid[1], centroid[2]);
+        tf::Vector3 centroid_baselink = transform * centroid_enu;
+
+        // Update centroid with the transformed values
+        centroid[0] = centroid_baselink.x();
+        centroid[1] = centroid_baselink.y();
+        centroid[2] = centroid_baselink.z();
+
+        // Check if the centroid is outside the AOI
+        bool outside_aoi = (centroid[0] < min_aoi[0] || centroid[0] > max_aoi[0] ||  // X bounds
+                            centroid[1] < min_aoi[1] || centroid[1] > max_aoi[1] ||  // Y bounds
+                            centroid[2] < min_aoi[2] || centroid[2] > max_aoi[2]);   // Z bounds          
+
+        if(!outside_aoi) 
+        {
+          std::cout << centroid[0] << ", " << centroid[1] << ", " << centroid[2] << "\n";
+          continue;
+        }
+        else
+        {
+          if (matches.size() == 0)
+          {
+            // Add to object
+            auto id = prev_objects.add_object(cluster_pc, cluster_search_tree);
+            seen.insert(id);
+          }
+          else
+          {
+            seen.insert((*matches.at(0)).first);
+            (*matches.at(0)).second.update_points(cluster_pc, cluster_search_tree);
+            for (size_t i = 1; i < matches.size(); ++i)
+            {
+              prev_objects.erase_object(matches.at(i));
+            }
+          }
+        }
+      }
+      catch (tf::TransformException &ex)
+      {
+          ROS_ERROR("%s", ex.what());
+          ros::Duration(1.0).sleep();
+          continue;  // Skip this iteration if the transform fails
+      }
+    }
+    else
     {
       if (matches.size() == 0)
       {
@@ -72,6 +127,9 @@ void Associator::associate(ObjectMap& prev_objects, point_cloud const& pc, clust
         }
       }
     }
+    
+    
+    
     
   }
 
