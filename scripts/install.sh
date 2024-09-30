71bd99731663af924e550765c72aba276a0610e9
tjcanro <tjcanro@gmail.com>
2024-09-30 00:00:12 -0400

    added ros2 humble build to installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -221,25 +221,25 @@ sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
 
 sudo apt update && sudo apt install -y \
-  python3-flake8-docstrings \
-  python3-pip \
-  python3-pytest-cov \
-  ros-dev-tools
+	python3-flake8-docstrings \
+	python3-pip \
+	python3-pytest-cov \
+	ros-dev-tools
 
 python3 -m pip install -U \
-   flake8-blind-except \
-   flake8-builtins \
-   flake8-class-newline \
-   flake8-comprehensions \
-   flake8-deprecated \
-   flake8-import-order \
-   flake8-quotes \
-   "pytest>=5.3" \
-   pytest-repeat \
-   pytest-rerunfailures
+	flake8-blind-except \
+	flake8-builtins \
+	flake8-class-newline \
+	flake8-comprehensions \
+	flake8-deprecated \
+	flake8-import-order \
+	flake8-quotes \
+	"pytest>=5.3" \
+	pytest-repeat \
+	pytest-rerunfailures
 
 mkdir -p ~/ros2_humble/src
 cd ~/ros2_humble
@@ -256,9 +256,11 @@ sudo rosdep init
 sudo rosdep update
 rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
 
+cd ~/ros2_humble/
+colcon build --symlink-install
+
 # Restore path
 cd ~/catkin_ws/src/mil/
-
 
 cat <<EOF
 $(color "$Pur")
