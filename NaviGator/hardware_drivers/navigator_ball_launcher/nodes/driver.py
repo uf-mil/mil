f0e38076cab381e6cf70d7ae0cd9000e2ddd8603
Cameron Brown <me@cbrxyz.com>
2024-10-27 13:02:24 -0400

    classifier, ogrid fix

--- NaviGator/hardware_drivers/navigator_ball_launcher/nodes/driver.py
+++ NaviGator/hardware_drivers/navigator_ball_launcher/nodes/driver.py
@@ -4,5 +4,6 @@ from navigator_ball_launcher.driver import BallLauncherDevice
 
 if __name__ == "__main__":
     rospy.init_node("ball_launcher")
-    device = BallLauncherDevice(str(rospy.get_param("~port")))
+    port = "/dev/serial/by-id/usb-Raspberry_Pi_Pico_E6635C08CB65BC36-if00"
+    device = BallLauncherDevice(port)
     rospy.spin()
