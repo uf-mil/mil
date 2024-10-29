c490abf46ceaa7474e7da2a1a374c7c8a31bc8ec
Alex Johnson <alexjohnson@ufl.edu>
2024-10-29 00:51:46 +0000

    Created packets for boat-drone communication

---
+++ NaviGator/hardware_drivers/navigator_drone_comm/navigator_drone_comm/packets.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from electrical_protocol import Packet
+
+
+@dataclass
+class HBT(Packet, class_id=0x20, subclass_id=0x00, payload_format=""):
+    """
+    Heartbeat packet.
+    """
+
+    pass
+
+
+@dataclass
+class GPS_DRONE(Packet, class_id=0x20, subclass_id=0x01, payload_format="<fff"):
+    """
+    GPS location of drone packet.
+
+    Attributes:
+        lat (float): The latitude of the drone.
+        lon (float): The longitude of the drone.
+        alt (float): The altitude of the drone.
+    """
+
+    lat: float
+    lon: float
+    alt: float
+
+
+@dataclass
+class ESTOP(Packet, class_id=0x20, subclass_id=0x02, payload_format=""):
+    """
+    Emergency stop drone packet.
+    """
+
+    pass
+
+
+@dataclass
+class START(Packet, class_id=0x20, subclass_id=0x03, payload_format="<20s"):
+    """
+    Start drone mission packet.
+
+    Attributes:
+        name (str): The name of the mission to run on the drone.
+    """
+
+    name: str
+
+
+@dataclass
+class STOP(Packet, class_id=0x20, subclass_id=0x04, payload_format=""):
+    """
+    Stop drone and return packet.
+    """
+
+    pass
+
+
+class Color(Enum):
+    """
+    Enum to represent the color of a target.
+    """
+
+    BLUE = "b"
+    GREEN = "g"
+    RED = "r"
+
+
+@dataclass
+class TARGET(Packet, class_id=0x20, subclass_id=0x05, payload_format="<ffc"):
+    """
+    GPS of drone-identified target packet.
+
+    Attributes:
+        lat (float): The latitude of the target.
+        lon (float): The longitude of the target.
+        color (Color Enum): The color of the target.
+    """
+
+    lat: float
+    lon: float
+    color: Color
