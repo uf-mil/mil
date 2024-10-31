5430500b4bd8718949ff4889fa98d6443aa7d23d
Alex Johnson <alexjohnson@ufl.edu>
2024-10-31 03:47:48 +0000

    fixing imports

--- NaviGator/hardware_drivers/navigator_drone_comm/navigator_drone_comm/__init__.py
+++ NaviGator/hardware_drivers/navigator_drone_comm/navigator_drone_comm/__init__.py
@@ -1,6 +1,9 @@
+"""
+The :mod:`navigator_drone_comm` module is used to communicate with the drone from Navigator.
+"""
+
 from .driver import DroneCommDevice
 from .packets import (
-    Color,
     EStopPacket,
     GPSDronePacket,
     HeartbeatReceivePacket,
