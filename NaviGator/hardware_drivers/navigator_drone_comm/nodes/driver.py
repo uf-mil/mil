e1fa018067177c7e31e750022990b9d03e226d3f
Cameron Brown <me@cbrxyz.com>
2024-11-03 11:15:20 -0500

    Specify default param for radio port

--- NaviGator/hardware_drivers/navigator_drone_comm/nodes/driver.py
+++ NaviGator/hardware_drivers/navigator_drone_comm/nodes/driver.py
@@ -4,6 +4,11 @@ from navigator_drone_comm.driver import DroneCommDevice
 
 if __name__ == "__main__":
     rospy.init_node("navigator_drone_comm")
-    port = str(rospy.get_param("~port"))
+    port = str(
+        rospy.get_param(
+            "~port",
+            "/dev/serial/by-id/usb-FTDI_TTL232R-3V3_FTDCKG37-if00-port0",
+        ),
+    )
     device = DroneCommDevice(port)
     rospy.spin()
