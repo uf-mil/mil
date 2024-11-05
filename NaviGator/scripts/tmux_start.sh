ca78afe3d7c3b93c8e7056f1673ed4622f137ce0
Cameron Brown <me@cbrxyz.com>
2024-11-04 22:26:57 -0500

    NaviGator/scripts: Add script for launching tmux windows/pane (closes #1302)

--- NaviGator/scripts/tmux_start.sh
+++ NaviGator/scripts/tmux_start.sh
@@ -2,18 +2,24 @@
 if tmux has-session -t auto; then
 	tmux a -t auto
 else
+	# First window (panes)
 	tmux new-session -d -s auto
 	tmux send-keys -t auto:0.0 'roslaunch navigator_launch master.launch --screen' Enter
 	tmux split-window -h -t auto
 	tmux split-window -v -t auto
+
+	# Second window (alarms, other panes)
+	sleep 1.5
 	tmux new-window -t auto
 	tmux split-window -h -t auto:1
 	tmux split-window -v -t auto:1
 	tmux split-window -v -t auto:1.0
-	sleep 1.5
 	tmux send-keys 'amonitor kill' Enter
 	tmux split-window -h
 	tmux send-keys 'amonitor hw-kill' Enter
 	tmux select-pane -t auto:1.0
+
+	# Return to the first window
+	tmux select-window -t auto:0
 	tmux a -t auto
 fi
